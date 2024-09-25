271810189ae4341b93bdde3da731cc4b6576a6a0
Joey Cheung <cheunglokyeejoey@gmail.com>
2024-09-25 14:07:35 +1000

    Add dropout

--- recognition/ADNI_s4763354/modules.py
+++ recognition/ADNI_s4763354/modules.py
@@ -5,9 +5,7 @@ from GFNet.gfnet import GFNet
 class GFNetBinaryClassifier(nn.Module):
     def __init__(self, num_classes=2):
         super(GFNetBinaryClassifier, self).__init__()
-        self.model = GFNet(num_classes=num_classes)
-        # self.model = gfnet_tiny(pretrained=True)
-        # self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+        self.model = GFNet(num_classes=num_classes,drop_rate=0.3, dropcls=0.2)
     
     def forward(self, x):
         return self.model(x)

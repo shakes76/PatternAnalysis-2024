aa241e30c008cf3ce9f86ed4399d0ff51235f98c
MitchellJC <81349046+MitchellJC@users.noreply.github.com>
2024-09-28 14:58:11 +1000

    Basic pair dataset working

--- recognition/47034073_siamese/dataset.py
+++ recognition/47034073_siamese/dataset.py
@@ -1,11 +1,22 @@
 import pathlib
 
+import torch
+from torchvision import io
 import pandas as pd
 from torch.utils.data import Dataset
 
-class TumorDataset(Dataset):
-    def __init__(self, image_path: pathlib.Path, meta_df: pd.DataFrame) -> None:
+
+class TumorPairDataset(Dataset):
+    def __init__(self, image_path: pathlib.Path, pair_df: pd.DataFrame) -> None:
         self._image_path = image_path
-        self._metda_df = meta_df
-        
-        
+        self._pair_df = pair_df
+
+    def __len__(self) -> int:
+        return len(self._pair_df)
+
+    def __getitem__(self, index) -> tuple[torch.Tensor, torch.Tensor, int]:
+        image1_name, image2_name, target = self._pair_df.iloc[index]
+        image1 = io.read_image(self._image_path / f"{image1_name}.jpg") / 255
+        image2 = io.read_image(self._image_path / f"{image2_name}.jpg") / 255
+
+        return image1, image2, target

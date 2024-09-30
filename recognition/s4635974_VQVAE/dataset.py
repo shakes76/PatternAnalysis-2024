8605a6184cb6a91d1d8084d7413b55ca7e39137f
dcollie87 <d.collie@student.uq.edu.au>
2024-10-01 09:49:34 +1000

    added file paths

---
+++ recognition/s4635974_VQVAE/dataset.py
@@ -0,0 +1,6 @@
+
+# Directories for datasets
+train_dir = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_train'
+test_dir = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_test'
+validate_dir = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_validate'
+

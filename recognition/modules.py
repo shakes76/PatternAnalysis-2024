e82060e7474adcadfa3f3015097332d2000648c7
aditya48781262 <a.sant@student.uq.edu.au>
2024-09-30 12:02:21 +1000

    Added code for Embedding Layer
    Both in modules.py and train.py.
    
    Actual model code will be implemented soon.

--- recognition/modules.py
+++ recognition/modules.py
@@ -23,8 +23,6 @@ else:
     posterior distribution over the latent space.
     In a normal VAE, the posterior and prior assume a 
     continuous normal distribution instead of categorial (Yadav, 2019)."""
-
-
 class Encoder(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim,
                  kernel_sizes=(4, 4, 3, 1), stride=2):
@@ -32,20 +30,15 @@ class Encoder(nn.Module):
 
         k1, k2, k3, k4 = kernel_sizes
 
-        self.strided_conv1 = nn.Conv2d(
-            input_dim, hidden_dim, kernel_size=k1, stride=stride, padding=1)
-        self.strided_conv2 = nn.Conv2d(
-            hidden_dim, hidden_dim, kernel_size=k2, stride=stride, padding=1)
-
-        self.residual_conv1 = nn.Conv2d(
-            hidden_dim, hidden_dim, kernel_size=k3, padding=1)
-        self.residual_conv2 = nn.Conv2d(
-            hidden_dim, hidden_dim, kernel_size=k4, padding=0)
+        self.strided_conv1 = nn.Conv2d(input_dim, hidden_dim, kernel_size=k1, stride=stride, padding=1)
+        self.strided_conv2 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=k2, stride=stride, padding=1)
+        
+        self.residual_conv1 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=k3, padding=1)
+        self.residual_conv2 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=k4, padding=0)
 
         self.ReLU = nn.ReLU()
 
-        self.project = nn.Conv2d(
-            hidden_dim, out_channels=output_dim, kernel_size=1)
+        self.project = nn.Conv2d(hidden_dim, out_channels=output_dim, kernel_size=1)
 
         self.training = True
 
@@ -53,26 +46,23 @@ class Encoder(nn.Module):
         out = self.strided_conv1(x)
         out = self.strided_conv2(out)
 
-        # F.relu(act)
+        #F.relu(act)
         out = self.ReLU(out)
         res_out = self.residual_conv1(out)
         res_out += out
 
-        # F.relu(res_out)
+        #F.relu(res_out)
         out = self.ReLU(res_out)
         res_out = self.residual_conv2(out)
         res_out += out
 
         return res_out
 
-
 """TBD"""
-
-
 class VQEmbedLayer(nn.Module):
-    def __init__(self, embeddings, embed_dim, q_loss=0.25, decay=0.999, e=1e-5):
+    def __init__(self, embeddings, embed_dim, commitment_cost=0.25, decay=0.999, e=1e-5):
         super().__init__()
-        self.q_loss = q_loss
+        self.commitment_cost = commitment_cost
         self.decay = decay
         self.e = e
 
@@ -84,19 +74,57 @@ class VQEmbedLayer(nn.Module):
         self.register_buffer("ema_weight", self.embedding.clone())
 
     def encode(self, input):
-        return
+        M, D = self.embedding.size()
+        input_flat = input.detach().reshape(-1, D)
 
-    def retrieve(self, i):
-        return
+        distances = (-torch.cdist(input_flat, self.embedding, p=2)) ** 2
+
+        indices = torch.argmin(distances.float(), dim=-1)
+        quantised = F.embedding(indices, self.embedding)
+        quantised = quantised.view_as(input)
+        return quantised, indices.view(input.size(0), input.size(1))
+
+    def retrieve(self, random_i):
+        quantised = F.embedding(random_i, self.embedding)
+        quantised = quantised.transpose(1, 3)
+        return quantised
 
     def forward(self, input):
-        return
+        M, D = self.embedding.size()
+        input_flat = input.detach().reshape(-1, D)
+
+        distances = (-torch.cdist(input_flat, self.embedding, p=2)) ** 2
+
+        indices = torch.argmin(distances.float(), dim=-1)
+        # categorical encoding required (load_data_2D(categorical=True))
+        encodings = F.one_hot(indices, M).float()
+        quantised = F.embedding(indices, self.embedding)
+        quantised = quantised.view_as(input)
+
+        if self.training:
+            self.ema_count = self.decay * self.ema_count + (1 - self.decay) * torch.sum(encodings, dim=0)
+            n = torch.sum(self.ema_count)
+            self.ema_count = (self.ema_count + self.e) / (n + M * self.e) * n
+
+            dw = torch.matmul(encodings.t(), input_flat)
+            self.ema_weight = self.decay * self.ema_weight + (1 - self.decay) * dw
+            self.embedding = self.ema_weight / self.ema_count.unsqueeze(-1)
+
+        # Loss equations are different in blog
+        reconstr_loss = F.mse_loss(input.detach(), quantised)
+        codebook_loss = F.mse_loss(input, quantised.detach())
+        commitment_loss = self.commitment_cost * reconstr_loss
+
+        quantised = input + (quantised - input).detach()
+
+        avg = torch.mean(encodings, dim=0)
+        # ?
+        perplexity = torch.exp(-torch.sum(avg * torch.log(avg + 1e-10)))
+        return quantised, commitment_loss, codebook_loss, perplexity
 
 
 """Decodes the latent space into higher dimensionality,
    using indexed values from the dictionary of embeddings. (Yadav, 2019)"""
-
-
 class Decoder(nn.Module):
     def __init__(self, latent_dim, hidden_dim, output_dim, kernel_sizes=(1, 3, 2, 2), stride=2):
         super().__init__()
@@ -104,15 +132,11 @@ class Decoder(nn.Module):
 
         self.inner_project = nn.Conv2d(latent_dim, hidden_dim, kernel_size=1)
 
-        self.residual_conv1 = nn.Conv2d(
-            hidden_dim, hidden_dim, kernel_size=k1, stride=stride, padding=0)
-        self.residual_conv12 = nn.Conv2d(
-            hidden_dim, hidden_dim, kernel_size=k2, stride=stride, padding=1)
-
-        self.strided_conv1 = nn.Conv2d(
-            hidden_dim, hidden_dim, kernel_size=k3, stride=stride, padding=1)
-        self.strided_conv2 = nn.Conv2d(
-            hidden_dim, output_dim, kernel_size=k4, stride=stride, padding=1)
+        self.residual_conv1= nn.Conv2d(hidden_dim, hidden_dim, kernel_size=k1, stride=stride, padding=0)
+        self.residual_conv12 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=k2, stride=stride, padding=1)
+        
+        self.strided_conv1 = nn.Conv2d(hidden_dim, hidden_dim, kernel_size=k3, stride=stride, padding=1)
+        self.strided_conv2 = nn.Conv2d(hidden_dim, output_dim, kernel_size=k4, stride=stride, padding=1)
         self.ReLU = nn.ReLU()
 
     def forward(self, input):
@@ -120,19 +144,18 @@ class Decoder(nn.Module):
 
         res_out = self.residual_conv1(out)
         res_out += out
-        # F.relu(res_out)
+        #F.relu(res_out)
         out = self.ReLU(res_out)
 
         res_out = self.residual_conv1(out)
         res_out += out
-        # F.relu(res_out)
+        #F.relu(res_out)
         res_out = self.ReLU(out)
 
         res_out = self.strided_conv1(res_out)
         res_out = self.strided_conv2(res_out)
 
         return res_out
-
 
 class Model(nn.Module):
     def __init__(self, Encoder, VQEmbeddings, Decoder):
@@ -143,8 +166,8 @@ class Model(nn.Module):
 
     def forward(self, input):
         logit = self.Encoder(input)
-        # Get loss values and quantised logit
+        #Get loss values and quantised logit
         # get reconstruction from quantised logit
-
+        
         # return reconstruction and loss values
         return
--- recognition/train.py
+++ recognition/train.py
@@ -1,6 +1,7 @@
-import modules
+from modules import Encoder, Decoder, VQEmbedLayer, Model
 import dataset
 import torch
+import torch.nn as nn
 from torch.optim import Adam
 
 import numpy as np
@@ -25,7 +26,8 @@ parameters = {
     "epochs": 50, 
     "batch": 100,
     "gpu": "cuda",
-    "cpu": "cpu"
+    "cpu": "cpu",
+    "loss": nn.MSELoss()
 }
 
 device = parameters["device"] if torch.cuda.is_available() else parameters["cpu"]
@@ -34,6 +36,8 @@ print(f"Using {device}.\n")
 print(f"Train.py: Shape of first training dataset image {trainSet[0].shape}")
 print(f"Train.py: Shape of first testing dataset image {testSet[0].shape}")
 
+encoder = Encoder(input_dim=dimensions["input"], hidden_dim=dimensions["hidden"], output_dim=dimensions["latent"])
+embeddings = VQEmbedLayer(embeddings=dimensions["embeddings"], embed_dim=dimensions["latent"])
+decoder = Decoder(latent_dim=dimensions["latent"], hidden_dim=dimensions["hidden"], output_dim=dimensions["output"])
 
-
-
+vqvae = Model(Encoder=encoder, VQEmbeddings=embeddings, Decoder=decoder).to(device=device)

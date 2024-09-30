351a7abee13db8e4ebf76d0dde8a8588348f61bf
Veslemøy Rosemary <veslemoyrosemary@d-i89-197-165.student.eduroam.uq.edu.au>
2024-09-30 18:30:09 +1000

    added rangpur dataset import to dataset.py

--- recognition/StyleGAN_VBV/dataset.py
+++ recognition/StyleGAN_VBV/dataset.py
@@ -3,14 +3,16 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def get_loader(data_dir, batch_size=32):
+# Set the dataset path
+DATASET_PATH = '/home/groups/comp3710/ADNI'
+
+def get_loader(image_size):
     transform = transforms.Compose([
-        transforms.Resize((128, 128)),
+        transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
+        transforms.Normalize([0.5] * 3, [0.5] * 3),
     ])
-    
-    dataset = datasets.ImageFolder(root=data_dir, transform=transform)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    
-    return loader
+
+    dataset = datasets.ImageFolder(root=DATASET_PATH, transform=transform)
+    loader = DataLoader(dataset, batch_size=32, shuffle=True)
+    return loader, dataset

26d05b3cd1ae3126181003f64334348b43bb3f33
Chenyi Hu <chenyihu@d-i89-202-185.student.eduroam.uq.edu.au>
2024-09-30 18:25:11 +1000

    Further updated codebase on the dataset.py, function created.

--- recognition/GNN-s4764094/dataset.py
+++ recognition/GNN-s4764094/dataset.py
@@ -13,28 +13,38 @@ elif torch.cuda.is_available():
 else:
     print("CPU usage.")
 
-# Upload our dataset and transfer to tensor format
-edge_df = pd.read_csv("/Users/chenyihu/Desktop/Pycharm_Code/"
-                      "3710-PatternAnalysis-2024/facebook_large/musae_facebook_edges.csv")
+# Upload our dataset and transfer to tensor format, return those parameters
+def upload_dataset():
+    # Upload edge, target and feature
+    edge_df = pd.read_csv("/Users/chenyihu/Desktop/Pycharm_Code/"
+                          "3710-PatternAnalysis-2024/facebook_large/musae_facebook_edges.csv")
 
-target_df = pd.read_csv("/Users/chenyihu/Desktop/Pycharm_Code/"
-                        "3710-PatternAnalysis-2024/facebook_large/musae_facebook_target.csv")
+    target_df = pd.read_csv("/Users/chenyihu/Desktop/Pycharm_Code/"
+                            "3710-PatternAnalysis-2024/facebook_large/musae_facebook_target.csv")
 
-with open("/Users/chenyihu/Desktop/Pycharm_Code/3710-PatternAnalysis-2024/facebook_large/"
-          "musae_facebook_features.json", 'r') as f:
-    feature_df = json.load(f)
+    with open("/Users/chenyihu/Desktop/Pycharm_Code/3710-PatternAnalysis-2024/facebook_large/"
+              "musae_facebook_features.json", 'r') as f:
+        feature_df = json.load(f)
 
-print(feature_df)
+    # Transfer and check whether the format is correct
+    expected_length = 31
+    target_df['page_name'] = pd.Categorical(target_df['page_name']).codes
+    target_df['page_type'] = pd.Categorical(target_df['page_type']).codes
 
-# Transfer and check whether the format is correct
-target_df['page_name'] = pd.Categorical(target_df['page_name']).codes
-target_df['page_type'] = pd.Categorical(target_df['page_type']).codes
+    tensor_edges = torch.tensor([edge_df['id_1'].values, edge_df['id_2'].values], dtype=torch.long).to(device)
+    tensor_targets = torch.tensor(
+        [target_df['id'].values, target_df['facebook_id'].values, target_df['page_name'].values,
+         target_df['page_type'].values], dtype=torch.long).to(device)
+    tensor_features = torch.tensor(
+        [feature_df.get(str(node_id), [0] * expected_length)
+         if len(feature_df[str(node_id)]) == expected_length
+         else feature_df[str(node_id)] + [0] * (expected_length - len(feature_df[str(node_id)]))
+         for node_id in target_df['id'].values],
+        dtype=torch.float
+    ).to(device)
 
-tensor_edges = torch.tensor([edge_df['id_1'].values, edge_df['id_2'].values], dtype=torch.long).to(device)
-tensor_targets = torch.tensor([target_df['id'].values, target_df['facebook_id'].values, target_df['page_name'].values,
-                              target_df['page_type'].values], dtype=torch.long).to(device)
-tensor_features = torch.tensor([feature_df[str(node_id)] for node_id in target_df['id'].values],
-                               dtype=torch.float).to(device)
-
-print(tensor_edges)
-print(tensor_targets)
+    # Check the results of those tensors
+    print("Data of edges: \n", tensor_edges)
+    print("Data of targets: \n", tensor_targets)
+    print("Data of features: \n", tensor_features)
+    return tensor_edges, tensor_targets, tensor_features

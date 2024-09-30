511f9cd364d6005ef18121843318be3d50074bd8
Joey Cheung <cheunglokyeejoey@gmail.com>
2024-09-30 16:35:49 +1000

    Uses gfnet-h-b

--- recognition/ADNI_s4763354/modules.py
+++ recognition/ADNI_s4763354/modules.py
@@ -1,12 +1,17 @@
 import torch
 import torch.nn as nn
-from GFNet.gfnet import GFNet  
+from GFNet.gfnet import GFNet, GFNetPyramid
+from functools import partial
 
 class GFNetBinaryClassifier(nn.Module):
     def __init__(self, num_classes=2):
         super(GFNetBinaryClassifier, self).__init__()
-        self.model = GFNet(num_classes=num_classes,drop_rate=0.3, dropcls=0.2)
-    
+        self.model = GFNetPyramid(
+            img_size=224, num_classes=num_classes,
+            patch_size=4, embed_dim=[96, 192, 384, 768], depth=[3, 3, 27, 3],
+            mlp_ratio=[4, 4, 4, 4],
+            norm_layer=partial(nn.LayerNorm, eps=1e-6), drop_path_rate=0.4, init_values=1e-6
+        )
+
     def forward(self, x):
         return self.model(x)
-

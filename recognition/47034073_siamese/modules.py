049415bd1ae1b073dbe39d33072e2053ae080354
MitchellJC <mitchellclark447@gmail.com>
2024-10-01 14:01:57 +1000

    added distance calculation to model

--- recognition/47034073_siamese/modules.py
+++ recognition/47034073_siamese/modules.py
@@ -1,5 +1,6 @@
 from torchvision.models import efficientnet_b0
 from torch import nn
+from torch.nn.functional import pairwise_distance, binary_cross_entropy_with_logits
 from dataclasses import dataclass
 
 
@@ -18,11 +19,14 @@ class TumorTrainer:
 
     def _train_epoch(self, train_loader):
         for x1, x2, y in train_loader:
-            embeddings1 = self._model(x1)
-            embeddings2 = self._model(x2)
+            self._optimizer.zero_grad()
+            distances = self._model(x1, x2)
+            loss = binary_cross_entropy_with_logits(distances, targets)
+            loss.backward()
+            self._optimizer.step()
 
 
-class SiameseTumor(nn.Module):
+class TumorTower(nn.Module):
     def __init__(self):
         super().__init__()
         self._backbone = efficientnet_b0()
@@ -30,5 +34,12 @@ class SiameseTumor(nn.Module):
         # Makes output in feature space
         self._backbone.classifer = nn.Identity()
 
-    def forward(self, x):
-        return self._backbone(x)
+        # Weighted summation of component differences
+        self._component_adder = nn.Linear(1000, 1, bias=False)
+
+    def forward(self, x1, x2):
+        embeddings1 = self._backbone(x1)
+        embeddings2 = self._backbone(x2)
+        component_diffs = torch.abs(embeddings1 - embeddings2)
+        distances = self._component_adder(component_diffs)
+        return distances

32402af61e18e403d85d2e456a5389eba25eb951
Jamie Westerhout <SixLeopard75798@gmail.com>
2024-10-02 17:07:52 +1000

    setup up intial training loop, currently contains size miss match

--- recognition/47451933_Stable_Diffusion/train.py
+++ recognition/47451933_Stable_Diffusion/train.py
@@ -1,1 +1,51 @@
 import torch
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import random as rnd
+
+from modules import *
+from dataset import *
+
+data = Dataset()
+device = data.device
+
+encoder = Encoder()
+decoder = Decoder()
+unet = UNet()
+
+optimizer = torch.optim.Adam(list(encoder.parameters()) + 
+                             list(decoder.parameters()) + 
+                             list(unet.parameters()), lr=1e-4)
+criterion = nn.CrossEntropyLoss()
+
+loss = []
+
+def train_model(dataloader = data.train_dataloader, epochs=10):
+    encoder.train()
+    decoder.train()
+    unet.train()
+    
+    for epoch in range(epochs):
+        for i, data in enumerate(dataloader, 0):
+            images = data[0].to(device)
+            b_size = images.size(0)
+            labels = data[1].to(device)
+
+            t = rnd.randint(0,99)
+            beta = torch.linspace(0.001, 0.02, t)
+            diffused, noise = forward_diffusion(images, t, beta)
+            print(len(diffused))
+            optimizer.zero_grad()
+            outputs = encoder(diffused[rnd.randint(0,99)])
+            print(len(outputs))
+            loss = criterion(outputs, noise)
+            loss.backward()
+            optimizer.step()
+
+            plt.figure(figsize=(8,8))
+            plt.axis("off")
+            plt.title("64 Samples of Training Images")
+            plt.imshow(np.transpose(vutils.make_grid(outputs.to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+            plt.show()
+
+train_model()

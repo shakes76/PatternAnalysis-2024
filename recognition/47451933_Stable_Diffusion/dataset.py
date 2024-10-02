3fb774777e2f52f64ceb178b85627e46d6b65d40
Jamie Westerhout <SixLeopard75798@gmail.com>
2024-10-02 15:26:32 +1000

    finish of dataset by truning it into a class

--- recognition/47451933_Stable_Diffusion/dataset.py
+++ recognition/47451933_Stable_Diffusion/dataset.py
@@ -13,46 +13,80 @@ import numpy as np
 IMAGES = 0
 LABELS = 1
 #=======================================================
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-batch_size = 128
-show_images = True
-#=======================================================
 
-#paths to datasets
-adni_data_path_train = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'ADNI', 'AD_NC', 'train'))
-adni_data_path_test = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'ADNI', 'AD_NC', 'test'))
+class Dataset():
+    '''
+        class for loading data from the ADNI dataset
+    '''
+    def __init__(self, 
+                 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+                 batch_size = 128) -> None:
+        self.device = device
 
-transforms = transforms.Compose([
-    transforms.Grayscale(), #grascale images 
-    transforms.Resize(64), #the next two lines decrease the resolution to 64x64
-    transforms.CenterCrop(64),
-    transforms.ToTensor(), #turn the datat into a tensor if its not already
-    transforms.Normalize(0.5,0.5)]) #normilze the data 0.5 beacuse values between 0-1 so 0.5 is just good general value
+        #paths to datasets
+        adni_data_path_train = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'ADNI', 'AD_NC', 'train'))
+        adni_data_path_test = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'ADNI', 'AD_NC', 'test'))
 
-#create datasets from image folder
-train_dataset = torchvision.datasets.ImageFolder(root=adni_data_path_train, transform=transforms)
-test_dataset = torchvision.datasets.ImageFolder(root=adni_data_path_test, transform=transforms)
+        transforms = transforms.Compose([
+            transforms.Grayscale(), #grascale images 
+            transforms.Resize(64), #the next two lines decrease the resolution to 64x64
+            transforms.CenterCrop(64),
+            transforms.ToTensor(), #turn the datat into a tensor if its not already
+            transforms.Normalize(0.5,0.5)]) #normilze the data 0.5 beacuse values between 0-1 so 0.5 is just good general value
 
-print(train_dataset)
-print(test_dataset)
+        #create datasets from image folder
+        self.train_dataset = torchvision.datasets.ImageFolder(root=adni_data_path_train, transform=transforms)
+        self.test_dataset = torchvision.datasets.ImageFolder(root=adni_data_path_test, transform=transforms)
 
-#create data loaders
-print("Creating DataLoaders...⏳") #setup data loadeers for training to make thins easier with batching
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-print("DataLoaders Created ✅")
+        print(self.train_dataset)
+        print(self.test_dataset)
 
-#get sample of data
-images = next(iter(train_dataloader))
-print(len(images[IMAGES]), len(images[LABELS]))
-images = images[IMAGES]
+        #create data loaders
+        print("Creating DataLoaders...⏳") #setup data loadeers for training to make thins easier with batching
+        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True)
+        self.test_dataloader = torch.utils.data.DataLoader(self.test_dataset, batch_size=batch_size, shuffle=True)
+        print("DataLoaders Created ✅")
 
-#visualise the sample
-print("Showing Data Sample...⏳")
-print(images.shape)
-plt.figure(figsize=(8,8))
-plt.axis("off")
-plt.title("64 Samples of Training Images")
-plt.imshow(np.transpose(vutils.make_grid(images.to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
-if show_images: plt.show()
-print("Data Sample shown ✅")
+    def sample(self):
+        '''
+            displauy a sample from the dataset
+            using matplotlib imshow
+        '''
+        #get sample of data
+        images = next(iter(self.train_dataloader))
+        print(len(images[IMAGES]), len(images[LABELS]))
+        images = images[IMAGES]
+
+        #visualise the sample
+        print("Showing Data Sample...⏳")
+        print(images.shape)
+        plt.figure(figsize=(8,8))
+        plt.axis("off")
+        plt.title("64 Samples of Training Images")
+        plt.imshow(np.transpose(vutils.make_grid(images.to(self.device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+        plt.show()
+        print("Data Sample shown ✅")
+    
+    def get_train(self):
+        '''
+            return the dataloader for the training data
+        '''
+        return self.train_dataloader
+    
+    def get_test(self):
+        '''
+            return the test dataloader for the testing data
+        '''
+        return self.test_dataloader
+    
+    def get_train_raw(self):
+        '''
+            return the raw training data
+        '''
+        return self.train_dataset
+    
+    def get_test_raw(self):
+        '''
+            return the raw testing data
+        '''
+        return self.test_dataset

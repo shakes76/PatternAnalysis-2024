c243f2b33334bc3622505ff897d5200b85800215
Michael Tu <michaeltu0122@gmail.com>
2024-10-03 16:54:07 +1000

    edit the input features of foward function

--- recognition/gnn-node-classification-TsungTseTu/modules.py
+++ recognition/gnn-node-classification-TsungTseTu/modules.py
@@ -8,10 +8,12 @@ class GCN(torch.nn.Module):
         self.conv1 = GCNConv(input_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, output_dim)
 
-        def forward(self, data):
-            x, edge_index = data.x, data.edge_index
+        def forward(self, x, edge_index):
+            # First GCN layer+ RELU active
             x = self.conv1(x, edge_index)
             x = F.relu(x)
+
+            # Second GCN layer + log_softmax for multiclassification
             x = self.conv2(x, edge_index)
             return F.log_softmax(x, dim=1)
 

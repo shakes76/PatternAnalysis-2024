b21cb658936e639362cc3afee123716438f8152d
Fuller81 <jrf0047@gmail.com>
2024-10-03 20:46:15 +1000

    Implimented Patch Splitter Class + gitignore file
    Added "Patch Splitter" class into module.py, the class
    impliments the splitting of the images into patches
    and then an inital pass through a linear layer.
    
    gitignore file was added to block pycache

--- recognition/alzheimers_47446096/modules.py
+++ recognition/alzheimers_47446096/modules.py
@@ -1,6 +1,29 @@
-from torch.nn import Module
+import torch.nn as nn
 
-class VisionTransformer(Module):
+class PatchSplitter(nn.Module):
+    def __init__(self, patchLen: int, imgDims: tuple[int, int, int], dimOut) -> None:
+        super().__init__()
+        self.patchLen = patchLen
+        self.imgDims = imgDims
+        self.tokenOut = dimOut
+        channels, height, width = self.imgDims
+        #* Ensure that the image can be correctly split into patches
+        assert height % self.patchLen == 0
+        assert width % self.patchLen == 0
+        self.nPatches = (height / self.patchLen) * (width / self.patchLen)
+        self.partition = nn.Unfold(
+            kernel_size = self.patchLen,
+            stride = self.patchLen,
+            padding = 0
+        )
+        self.linear = nn.Linear((self.patchLen ** 2) * channels, dimOut)
+    
+    def forward(self, x):
+        x = self.partition(x).transpose(1, 0)
+        x = self.linear(x)
+        return x
+
+class VisionTransformer(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        
+

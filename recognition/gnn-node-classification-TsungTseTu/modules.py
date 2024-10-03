3d6ef7a5994dac42894025805fd0d3ccf80d6969
Michael Tu <michaeltu0122@gmail.com>
2024-10-03 17:30:28 +1000

    fix misplacement

--- recognition/gnn-node-classification-TsungTseTu/modules.py
+++ recognition/gnn-node-classification-TsungTseTu/modules.py
@@ -10,12 +10,12 @@ class GCN(torch.nn.Module):
         self.conv1 = GCNConv(input_dim, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, output_dim)
 
-        def forward(self, x, edge_index):
-            # First GCN layer+ RELU active
-            x = self.conv1(x, edge_index)
-            x = F.relu(x)
+    def forward(self, x, edge_index):
+        # First GCN layer+ RELU active
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
 
-            # Second GCN layer + log_softmax for multiclassification
-            x = self.conv2(x, edge_index)
-            return F.log_softmax(x, dim=1)
+        # Second GCN layer + log_softmax for multiclassification
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
 

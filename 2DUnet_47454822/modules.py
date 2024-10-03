dd5ef3dc4e1fa531926ccbd182c4cbc6337358ef
Jack <jack.eric.edmondson@gmail.com>
2024-10-03 13:50:47 +1000

    Fixed input shape issue

--- 2DUnet_47454822/modules.py
+++ 2DUnet_47454822/modules.py
@@ -1,13 +1,13 @@
 # Contains components of model.
 
-from keras import layers, models
+from keras import layers, models, Sequential
 from keras.src.backend import shape
 
 FULL_SIZE_IMG = 1  # set to 2 to use full size image
 INPUT_SHAPE = (32, 32, 3)
 num_classes = 4  # numb of classes in segmentation
 
-def unet_model(input_size=(128, 128, 1)):
+def unet_model(input_size=(128, 128, 1), batch_size=12):
     inputs = layers.Input(input_size)
 
     # Encoder
@@ -15,13 +15,13 @@ def unet_model(input_size=(128, 128, 1)):
     conv1 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(conv1)
     pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    # conv2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(pool1)
-    # conv2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(conv2)
-    # pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    # conv3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(pool2)
-    # conv3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(conv3)
-    # pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
 
     # conv4 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(pool3)
     # conv4 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(conv4)
@@ -29,7 +29,7 @@ def unet_model(input_size=(128, 128, 1)):
 
 
     # Bridge
-    conv5 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(pool1)
+    conv5 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(pool3)
     conv5 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
 
     # Decoder
@@ -39,21 +39,21 @@ def unet_model(input_size=(128, 128, 1)):
     # conv6 = layers.Conv2D(512, (3, 3), activation='relu', padding='same')(conv6)
 
     print("Conv5 shape: "+str(shape(conv5)))
-    # print("Conv3 shape: " + str(shape(conv3)))
-    # print("Conv2 shape: " + str(shape(conv2)))
+    print("Conv3 shape: " + str(shape(conv3)))
+    print("Conv2 shape: " + str(shape(conv2)))
     print("Conv1 shape: " + str(shape(conv1)))
 
-    # up2 = layers.Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5)  # Changed conv 6 to conv 5
-    # concat2 = layers.concatenate([up2, conv3], axis=3)
-    # conv7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(concat2)
-    # conv7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
+    up2 = layers.Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5)  # Changed conv 6 to conv 5
+    concat2 = layers.concatenate([up2, conv3], axis=3)
+    conv7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(concat2)
+    conv7 = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
 
-    # up3 = layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv7)
-    # concat3 = layers.concatenate([up3, conv2], axis=3)
-    # conv8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(concat3)
-    # conv8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(conv8)
+    up3 = layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv7)
+    concat3 = layers.concatenate([up3, conv2], axis=3)
+    conv8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(concat3)
+    conv8 = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(conv8)
 
-    up4 = layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv5) # Changed 8 to 5
+    up4 = layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv8) # Changed 8 to 5
     concat4 = layers.concatenate([up4, conv1], axis=3)
     conv9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(concat4)
     conv9 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(conv9)
@@ -67,6 +67,7 @@ def unet_model(input_size=(128, 128, 1)):
     return model
 
 
+# read: https://keras.io/examples/vision/oxford_pets_image_segmentation/
 
 #
 # import torch
--- 2DUnet_47454822/tests.py
+++ 2DUnet_47454822/tests.py
@@ -1,9 +1,13 @@
 import keras
+import numpy as np
 from keras.src.optimizers import Adam
 
 from dataset import load_dir, load_data_2D
 from tensorflow.keras.preprocessing import image
 from modules import unet_model
+
+# [BATCH_SIZE, CHANNELS, HEIGHT, WIDTH]
+# load_dir return: [BATCH_SIZE, HEIGHT, WIDTH]
 
 def invoke():
         result = load_data_2D(["/home/ja/Documents/uni/COMP3710/PatternAnalysis-2024/data/keras_slices_seg_train/seg_004_week_0_slice_0.nii.gz",
@@ -32,11 +36,26 @@ def test_train():
     X = load_dir("/home/ja/Documents/uni/COMP3710/PatternAnalysis-2024/data/keras_slices_test/")
     y = load_dir("/home/ja/Documents/uni/COMP3710/PatternAnalysis-2024/data/keras_slices_seg_test/")
 
-    model = unet_model((10, 256, 128))
+    batch_size = 32
+
+    X = np.expand_dims(X, axis=-1)  # Add a channel dimension, making the shape (batch_size, 256, 128, 1)
+    # X = np.resize(X, (batch_size, 128, 128, 1))
+
+    y = np.expand_dims(y, axis=-1)
+    # y = np.resize(y, (batch_size, 128, 128, 1))
+
+    print(f"X Shape: {np.shape(X)}")
+    print(f"y Shape: {np.shape(y)}")
+
+
+    model = unet_model((256, 128, 1), batch_size=batch_size)
     model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(x=X, y=y, batch_size=10, epochs=5, shuffle=True, verbose=2)
+    model.fit(x=X, y=y, batch_size=batch_size, epochs=1, shuffle=True, verbose=2)
 
 if __name__ == '__main__':
     # invoke_dir()
     test_train()
+
+
+# POL7010 Dynamics of Governance

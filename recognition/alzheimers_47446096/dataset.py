4416bbf1ddd4d6167a0d5b51ef2b8d99b2bede11
Fuller81 <jrf0047@gmail.com>
2024-10-04 08:57:13 +1000

    Added documentation/function headers to dataset.py
    Added some comments and function headers to getTrainLoader
    and getTestLoader to clean it up and increase usability.

--- recognition/alzheimers_47446096/dataset.py
+++ recognition/alzheimers_47446096/dataset.py
@@ -6,12 +6,32 @@ ROOT = "../ADNI/AD_NC/train"
 TESTPATH = "/test"
 TRAINPATH = "/train"
 
-def getTrainLoader(path = ROOT + TRAINPATH, batchSize = 128, shuffle = True):
+def getTrainLoader(path: str = ROOT + TRAINPATH, batchSize: int = 128, shuffle: bool = True):
+    '''
+    Get Pytorch DataLoader with ADNI training DATA
+
+    Input:
+        path: str - relative path to training data
+        batchSize: int - batch size of the DataLoader
+        suffle: bool - DataLoader shuffle option
+    Returns:
+        Pytorch DataLoader with ADNI training data loaded
+    '''
     trainData = ImageFolder(path)
     trainLoader = DataLoader(trainData, batch_size = batchSize, shuffle = shuffle)
     return trainLoader
 
 def getTestLoader(path = ROOT + TESTPATH, batchSize = 128, shuffle = True):
+    '''
+    Get Pytorch DataLoader with ADNI test DATA
+
+    Input:
+        path: str - relative path to training data
+        batchSize: int - batch size of the DataLoader
+        suffle: bool - DataLoader shuffle option
+    Returns:
+        Pytorch DataLoader with ADNI test data loaded
+    '''
     trainData = ImageFolder(path)
     trainLoader = DataLoader(trainData, batch_size = batchSize, shuffle = shuffle)
     return trainLoader

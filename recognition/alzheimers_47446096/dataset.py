5470e0de287c637b9f9699761bf06ae8bb1e4bcc
FLR81 <jrf0047@gmail.com>
2024-10-03 14:03:52 +1000

    Removed 'makeAnnotateFile' due to ability to use built in pytorch imageFolder to do the same thing.
    Implemented get test/train dataloader functions
    to get the formatted data for each section

--- recognition/alzheimers_47446096/dataset.py
+++ recognition/alzheimers_47446096/dataset.py
@@ -1,28 +1,17 @@
-import torch
-import os
-import pandas as pd
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
 
-TRAINPATH = "../ADNI/AD_NC/train"
+ROOT = "../ADNI/AD_NC/train"
+TESTPATH = "/test"
+TRAINPATH = "/train"
 
-def makeAnotateFile(path: str) -> str:
-    '''
-    Creates file with image label/class combinations
-    Input: path - path to folder that contains images seperated by class into
-        folders as seen below
-        /path
-            /class1
-                img1
-                img2
-            /class2
-            /class3
-            /etc
-    Returns: csv style string with image label combinations
-    '''
-    data = pd.DataFrame(columns = ["Label", "Class"])
-    classVal = 0
-    for classFolder in os.listdir(path):
-        for imgLabel in os.listdir(os.path.join(path, classFolder)):
-            data.loc[len(data)] = [imgLabel, classVal]
-        classVal += 1
-    return data.to_csv(index = False, header = False)
+def getTrainLoader(path = ROOT + TRAINPATH, batchSize = 128, shuffle = True):
+    trainData = ImageFolder(path)
+    trainLoader = DataLoader(trainData, batch_size = batchSize, shuffle = shuffle)
+    return trainLoader
+
+def getTestLoader(path = ROOT + TESTPATH, batchSize = 128, shuffle = True):
+    trainData = ImageFolder(path)
+    trainLoader = DataLoader(trainData, batch_size = batchSize, shuffle = shuffle)
+    return trainLoader

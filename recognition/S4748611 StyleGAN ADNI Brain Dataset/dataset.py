c78c688d37717060f345d8d8544dec8fa780cb8c
Sam Wolfenden <s.wolfenden@uqconnect.edu.au>
2024-10-03 10:31:20 +1000

    file updated to load ADNI dataset

--- recognition/S4748611 StyleGAN ADNI Brain Dataset/dataset.py
+++ recognition/S4748611 StyleGAN ADNI Brain Dataset/dataset.py
@@ -1,31 +1,42 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.datasets import mnist
+import os
 
-def load_and_preprocess_mnist():
-    # Load MNIST dataset
-    (x_train, _), (x_test, _) = mnist.load_data()
+def load_and_preprocess_adni(data_dirs, target_size=(64, 64)):
+    images = []
+    for data_dir in data_dirs:
+        for img_name in os.listdir(data_dir):
+            img_path = os.path.join(data_dir, img_name)
+            img = tf.io.read_file(img_path)
+            img = tf.image.decode_png(img, channels=1)  # Assuming PNG images, adjust if needed
+            img = tf.image.resize(img, target_size)
+            img = tf.cast(img, tf.float32)
+            images.append(img)
 
-    # Combine train and test sets
-    x_combined = np.concatenate([x_train, x_test], axis=0)
-
-    # Resize images to 64x64
-    x_resized = tf.image.resize(x_combined[..., np.newaxis], (64, 64)).numpy()
+    # Stack all images into a single tensor
+    image_tensor = tf.stack(images)
 
     # Normalize images to [-1, 1] range
-    x_normalized = (x_resized.astype(np.float32) - 127.5) / 127.5
+    normalized_images = (image_tensor - 127.5) / 127.5
 
-    return x_normalized
+    return normalized_images
 
-def create_mnist_dataset(batch_size):
-    # Load and preprocess MNIST data
-    mnist_data = load_and_preprocess_mnist()
+def create_adni_dataset(batch_size, target_size=(64, 64)):
+    data_dirs = [
+        '/home/groups/comp3710/ADNI/AD_NC/test/AD',
+        '/home/groups/comp3710/ADNI/AD_NC/test/NC',
+        '/home/groups/comp3710/ADNI/AD_NC/train/AD',
+        '/home/groups/comp3710/ADNI/AD_NC/train/NC'
+    ]
+
+    # Load and preprocess all images
+    all_images = load_and_preprocess_adni(data_dirs, target_size)
 
     # Create TensorFlow dataset
-    dataset = tf.data.Dataset.from_tensor_slices(mnist_data)
+    dataset = tf.data.Dataset.from_tensor_slices(all_images)
     
     # Shuffle and batch the dataset
-    dataset = dataset.shuffle(buffer_size=60000).batch(batch_size, drop_remainder=True)
+    dataset = dataset.shuffle(buffer_size=len(all_images)).batch(batch_size, drop_remainder=True)
 
     return dataset
 
@@ -43,13 +54,14 @@ if __name__ == "__main__":
     BATCH_SIZE = 32
     LATENT_DIM = 512
     INITIAL_SIZE = 4
+    TARGET_SIZE = (64, 64)
 
-    # Create MNIST dataset
-    mnist_dataset = create_mnist_dataset(BATCH_SIZE)
+    # Create ADNI dataset
+    adni_dataset = create_adni_dataset(BATCH_SIZE, TARGET_SIZE)
 
     # Generate random inputs
     latent_vectors, constant_inputs = generate_random_inputs(BATCH_SIZE, LATENT_DIM, INITIAL_SIZE)
 
-    print("MNIST dataset shape:", next(iter(mnist_dataset)).shape)
+    print("ADNI dataset shape:", next(iter(adni_dataset)).shape)
     print("Latent vectors shape:", latent_vectors.shape)
     print("Constant inputs shape:", constant_inputs.shape)

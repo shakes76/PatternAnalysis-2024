398bf648d3785f3da7467c7ba33d763650ce88f0
Michael Tu <michaeltu0122@gmail.com>
2024-10-03 15:11:16 +1000

    remove debug lines after successful run from dataset.py

--- recognition/gnn-node-classification-TsungTseTu/dataset.py
+++ recognition/gnn-node-classification-TsungTseTu/dataset.py
@@ -3,10 +3,6 @@ import numpy as np
 def load_facebook_data():
     dataset_path = "./data/facebook.npz"
     data = np.load(dataset_path)
-    print(data.files)
     return data
 
 
-# Run the function to inspect the dataset
-if __name__ == '__main__':
-    load_facebook_data()

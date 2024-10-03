b992a2d7194baea471b11967a6d61e73a1ed8547
MRMATTHEWLGIT <m.lockett@uq.net.au>
2024-10-03 20:45:10 +1000

    Implements the load_ADNI_dataset() function used to load the ADNI dataset into the train.py file. Furthermore, the train.py file has been set-up to load this data and plot a sample of the traning data recived.

--- recognition/StyleGAN-46988133/dataset.py
+++ recognition/StyleGAN-46988133/dataset.py
@@ -1,3 +1,37 @@
 """
 dataset.py created by Matthew Lockett 46988133
 """
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+import hyperparameters as hp
+
+# Define a transformation to be applied to the images upon being loaded in
+# REF: This transform was inspired by the PyTorch DCGAN tutoiral found at:
+# REF: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html.
+transform = transforms.Compose([
+                                transforms.Grayscale(num_output_channels=hp.NUM_CHANNELS),
+                                transforms.Resize((hp.IMAGE_SIZE, hp.IMAGE_SIZE)),
+                                transforms.CenterCrop(hp.IMAGE_SIZE),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,)), # Normalize grayscale images to between [-1, 1]
+                            ])
+
+def load_ADNI_dataset():
+    """
+    Loads the ADNI dataset into the StyleGAN model, utilising a predefined transform applied
+    to every image. The transform is specifically catered for the greyscale images of the ADNI 
+    dataset.
+
+    Return: The ADNI dataset images split into batches and transformed into a form appropriate for
+            the StyleGAN model.
+    REF: This function was based on the PyTorch DCGAN tutoiral dataset loader example found at 
+    REF: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html.
+    """    
+    # Load the ADNI dataset images located at the root directory
+    dataset = dset.ImageFolder(root=hp.ROOT, transform=transform)
+
+    # Split the images into batches for training
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=hp.BATCH_SIZE, shuffle=True)
+
+    return train_loader
--- recognition/StyleGAN-46988133/train.py
+++ recognition/StyleGAN-46988133/train.py
@@ -1,3 +1,34 @@
 """
 train.py created by Matthew Lockett 46988133
 """
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+import hyperparameters as hp
+from dataset import load_ADNI_dataset
+
+# PyTorch Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning CUDA not Found. Using CPU.")
+
+# Load the ADNI dataset images for training
+train_loader = load_ADNI_dataset()
+
+# Plot some training images
+real_batch = next(iter(train_loader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Sample Training Images for the ADNI Dataset")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.show()

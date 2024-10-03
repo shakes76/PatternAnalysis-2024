3a0b039aa2e1e0c8dcfe1cb6bca2782501bd1218
Mitchell Clark <mitchellclark447@gmail.com>
2024-10-03 15:23:46 +1000

    sped up image shrinking
    using larger batch size and num workers

--- recognition/47034073_siamese/shrink_images.py
+++ recognition/47034073_siamese/shrink_images.py
@@ -15,18 +15,20 @@ SMALL_IMAGES_DIR = DATA_DIR / "small_images"
 def main() -> None:
     SMALL_IMAGES_DIR.mkdir(exist_ok=True)
     dataset = AllTumorDataset(IMAGES_DIR)
-    loader = DataLoader(dataset, batch_size=1)
+    loader = DataLoader(dataset, batch_size=512, num_workers=6)
 
     start_time = time.time()
     n = 0
-    for image, image_name in loader:
-        n += 1
-        image = (image[0] * 255).to(torch.uint8)
-        write_jpeg(image, str(SMALL_IMAGES_DIR / f"{image_name[0]}.jpg"))
+    for images, image_names in loader:
+        for i, image in enumerate(images):
+            n += 1
+            image_name = image_names[i]
+            image = (image * 255).to(torch.uint8)
+            write_jpeg(image, str(SMALL_IMAGES_DIR / f"{image_name}.jpg"))
 
-        if time.time() - start_time > 60:
-            print(f"{n}/{len(loader.dataset)}")
-            start_time = time.time()
+            if time.time() - start_time > 30:
+                print(f"{n}/{len(loader.dataset)}")
+                start_time = time.time()
 
 
 if __name__ == "__main__":

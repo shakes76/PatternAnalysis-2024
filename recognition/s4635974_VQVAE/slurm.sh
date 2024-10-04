4420a033dde9e674ce70184d2807078708060e06
dcollie87 <dcollie87@hotmail.com>
2024-10-04 12:05:43 +1000

    edit slurm file to include output name

--- recognition/s4635974_VQVAE/slurm.sh
+++ recognition/s4635974_VQVAE/slurm.sh
@@ -4,6 +4,8 @@
 #SBATCH --time=0-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au
+#SBATCH --output=output.out
+#SBATCH --error=error.out
 
 conda activate torch
 python train.py

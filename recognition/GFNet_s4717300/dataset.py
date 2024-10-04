7cea44a2e37b6632972d0162fcbc733577ed5d50
Benjamin Jorgensen <benjamin@jorgensens.id.au>
2024-10-04 13:11:50 +1000

    feat: Added empty files for each component of the project

---
+++ recognition/GFNet_s4717300/dataset.py
@@ -0,0 +1,3 @@
+# “dataset.py" containing the data loader for loading and preprocessing your data
+
+# we need to load the nmist dataset
---
+++ recognition/GFNet_s4717300/modules.py
@@ -0,0 +1,2 @@
+## 1. “modules.py" containing the source code of the components of your model. Each component must be
+# implemented as a class or a function

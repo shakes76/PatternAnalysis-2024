3cbaecd682806b16a4f61e7db1bae8da5c22fd00
harri37 <110083492+harri37@users.noreply.github.com>
2024-10-05 18:59:22 +1000

    updated dataset to handle adni images

--- recognition/wills_harrison_s4698315_adni_transformer/dataset.py
+++ recognition/wills_harrison_s4698315_adni_transformer/dataset.py
@@ -1,34 +1,38 @@
-from pytorch import Dataset
+from torch.utils.data import Dataset
+import os 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from PIL import Image
+import numpy as np
 
 class ADNIDataset(Dataset):
-    def __init__(self, data, targets, tokenizer, max_len):
-        self.data = data
-        self.targets = targets
-        self.tokenizer = tokenizer
-        self.max_len = max_len
+    def __init__(self, ad_path, nc_path):
+        ad_files = os.listdir(ad_path)
+        nc_files = os.listdir(nc_path)
+        
+        
+        
+        ad_files_labelled = [(os.path.join(ad_path, file), 1) for file in ad_files]
+        nc_files_labelled = [(os.path.join(nc_path, file), 0) for file in nc_files]
+        
+        self.data = ad_files_labelled + nc_files_labelled
+        
+        self.augmentations = A.Compose([
+            A.GaussNoise(p=0.2),
+            A.RandomBrightnessContrast(p=0.2),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
+            A.Rotate(limit=40, p=0.5),
+            ToTensorV2()
+        ])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, item):
-        data = str(self.data[item])
-        target = self.targets[item]
-
-        encoding = self.tokenizer.encode_plus(
-            data,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            return_token_type_ids=False,
-            padding="max_length",
-            return_attention_mask=True,
-            return_tensors='pt',
-            truncation=True
-        )
-
-        return {
-            'data_text': data,
-            'input_ids': encoding['input_ids'].flatten(),
-            'attention_mask': encoding['attention_mask'].flatten(),
-            'targets': torch.tensor(target, dtype=torch.long)
-        }
+        file, label = self.data[item]
+        image = np.array(Image.open(file))
+        image = self.augmentations(image=image)['image']
+        
+        return image, label
         

b38319d62ebb3f704f8d68cb94e0c0f535e38b12
Caleb Clarke <caleb.clarke@student.uq.edu.au>
2024-10-05 13:27:27 +1000

    Restructured dataset to use ImageFolder instead of a custom class and return values from a function.

--- recognition/GFNet-47428364/dataset.py
+++ recognition/GFNet-47428364/dataset.py
@@ -1,41 +1,21 @@
-import os
-from PIL import Image
 from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
 
-class ADNIDataset(Dataset):
-    def __init__(self, AD_dir, NC_dir, transform=None):
-        self.AD_dir = AD_dir
-        self.NC_dir = NC_dir
-        self.transform = transform
-        self.images = [(os.path.join(AD_dir, file), 1) for file in os.listdir(AD_dir)]
-        self.images += [(os.path.join(NC_dir, file), 0) for file in os.listdir(NC_dir)]
+""" Returns the train and test dataloaders for the ADNI dataset """
+def get_dataloaders(batch_size=32, path="recognition/GFNet-47428364/AD_NC"):
+    # Create transformer
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+    ])
 
-    def __len__(self):
-        return len(self.images)
+    # Generate datasets
+    train_dataset = ImageFolder(root=path+"/train", transform=transform)
+    test_dataset = ImageFolder(root=path+"/test", transform=transform)
 
-    def __getitem__(self, idx):
-        path, label = self.images[idx]
-        image = Image.open(path).convert("RGB")
-    
-        if self.transform:
-            image = self.transform(image)
-        
-        return image, label
+    # Generate dataloaders
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-# Data directory
-dir = "recognition/GFNet-47428364/AD_NC"
-
-# Transform the images
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),
-])
-
-# Initialise the datasets
-train = ADNIDataset(dir + "/train/AD", dir + "/train/NC", transform=transform)
-test = ADNIDataset(dir + "/test/AD", dir + "/test/NC", transform=transform)
-
-# Creates that dataloaders
-dataloader_train = DataLoader(train, batch_size=32, shuffle=True)
-dataloader_test = DataLoader(test, batch_size=32, shuffle=True)
+    return train_dataloader, test_dataloader

11088406103581e8dae124ac18c43c33df54652b
Michael Tu <michaeltu0122@gmail.com>
2024-10-05 14:15:20 +1000

    Restructure to easier adjust layers and add batch norm layer

--- recognition/gnn-node-classification-TsungTseTu/modules.py
+++ recognition/gnn-node-classification-TsungTseTu/modules.py
@@ -5,28 +5,31 @@ from torch_geometric.nn import GCNConv
 print("GCN class loaded")
 
 class GCN(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=3,dropout=0.5):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(input_dim, hidden_dim)
-        self.conv2 = GCNConv(hidden_dim, hidden_dim)
-        self.conv3 = GCNConv(hidden_dim, hidden_dim)
-        self.conv4 = GCNConv(hidden_dim, output_dim)
-        self.dropout = torch.nn.Dropout(p=0.5) # Add dropout with 50% prob
+
+
+        # Define layers
+        self.convs = torch.nn.Modulelist()
+        sef.convs.append(GCNConv(input_dim, hidden_dim) #first
+
+        for _ in range(num_layers -2): #middle layers
+            self.convs.append(GCNConv(hidden_dim, hidden_dim))
+
+        self.convs.append(GCNConv(hidden_dim, output_dim)) #Output
+
+        # Batch Normalization layers
+        self.bns = torch.nn.ModuleList([torch.nn.BatchNorm1d(hidden_dim) for _in range(num_layers -1)])
+
+        self.dropout = torch.nn.Dropout(dropout) # Add dropout with 50% prob
 
     def forward(self, x, edge_index):
-        # First GCN layer+ RELU active
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = self.dropout(x) #apply dropout after relu
+        for i , conv in enumerate(self.convs[:-1]):
+            x = conv(x, edge_index0
+            x = self.bns[i](x) #apply batch normalization
+            x = F.relu(x) #Activation
+            x = self.dropout(x) #dropout while training
 
-        # Second GCN layer + log_softmax for multiclassification
-        x = self.conv2(x, edge_index)
-
-        # Third GCN layer
-        x = self.conv3(x, edge_index)
-        x = F.relu(x)
-
-        # Fourth GCN layer + log_softmax
-        x = self.conv4(x, edge_index)
+        x = self.convs[-1](x, edge_index) # Last GCN layer
         return F.log_softmax(x, dim=1)
 

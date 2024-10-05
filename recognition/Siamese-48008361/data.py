e8fd239149c42962bd4f1b3790712441cf3b5b20
Zain <zaino@cum.com>
2024-10-05 14:26:26 +1000

    Added doctstring for len and getitem in data.py

--- recognition/Siamese-48008361/data.py
+++ recognition/Siamese-48008361/data.py
@@ -1,3 +1,7 @@
+"""
+This module provides functionality for loading and processing the ISIC 2020 dataset
+for use in a Siamese network with triplet loss for melanoma classification.
+"""
 import os
 import random
 import pandas as pd
@@ -10,7 +14,33 @@ from sklearn.model_selection import train_test_split
 
 
 class ISIC2020Dataset(Dataset):
+    """
+    A custom Dataset class for the ISIC 2020 skin lesion dataset.
+
+    This class handles loading of images, data augmentation, and generation of triplets
+    (anchor, positive, negative) for training a Siamese network with triplet loss.
+
+    Attributes:
+        data (pd.DataFrame): The dataset containing image information and labels.
+        img_dir (str): Directory containing the images.
+        transform (callable, optional): Optional transform to be applied on a sample.
+        mode (str): 'train' for training set, 'test' for test set.
+        benign (pd.DataFrame): Subset of data containing only benign samples.
+        malignant (pd.DataFrame): Subset of data containing only malignant samples.
+    """
+
+
     def __init__(self, csv_file, img_dir, transform=None, mode='train', split_ratio=0.8):
+
+        """
+        Args:
+            csv_file (str): Path to the csv file with annotations.
+            img_dir (str): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+            mode (str): 'train' for training set, 'test' for test set.
+            split_ratio (float): Ratio of data to use for training.
+        """
+        
         self.data = pd.read_csv(csv_file)
         self.img_dir = img_dir
         self.transform = transform
@@ -21,7 +51,7 @@ class ISIC2020Dataset(Dataset):
 
         if mode == 'train':
             self.data = self.train_data
-            # Augment minority class to match majority class
+            # Augment minority class (malignant) to match majority class (benign)
             benign_count = len(self.data[self.data['target'] == 0])
             malignant_samples = self.data[self.data['target'] == 1]
             # Augment factor is the number of times we need to repeat the malignant samples
@@ -30,21 +60,33 @@ class ISIC2020Dataset(Dataset):
             self.data = pd.concat([self.data, augmented_malignant], ignore_index=True)
         else:
             self.data = self.test_data
-
+        # Create subsets for benign and malignant samples
         self.benign = self.data[self.data['target'] == 0]
         self.malignant = self.data[self.data['target'] == 1]
 
     def __len__(self):
+        """Returns the number of samples in the dataset."""
         return len(self.data)
 
     def __getitem__(self, idx):
+        
+        """
+        Generates one sample of data including a triplet (anchor, positive, negative).
+        
+        Args:
+            idx (int): Index of the sample to fetch.
+
+        Returns:
+            tuple: (anchor_img, positive_img, negative_img, anchor_label)
+        """
+
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
+        # Get the anchor image
         anchor_row = self.data.iloc[idx]
         anchor_img = self.get_image(anchor_row)
         anchor_label = anchor_row['target']
-
+        # Select positive and negative samples based on the anchor's label
         if anchor_label == 0:  # benign
             positive = self.get_image(self.benign.sample().iloc[0])
             negative = self.get_image(self.malignant.sample().iloc[0])

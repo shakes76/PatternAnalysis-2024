255b5108621b6307b44dc0ef1d9549fa1417cc87
s4698486 <s4698486@student.uq.edu.au>
2024-10-05 17:54:16 +1000

    Adds style elements like mapping network + mapping network optimisation, multiple noise channels for style mixing

--- STYLEGAN-ADNI-Generator-s4698486/train.py
+++ STYLEGAN-ADNI-Generator-s4698486/train.py
@@ -1,42 +1,72 @@
 # train.py
 import tensorflow as tf
-from modules import define_generator, define_discriminator, discriminator_loss, generator_loss
+from modules import define_discriminator, discriminator_loss, generator_loss, StyleGANGenerator, MappingNetwork#mapping_network, StyleGANGenerator#define_generator
 from dataset import load_data
 import numpy as np
 from matplotlib import pyplot as plt
 
 # Define hyperparameters
 batch_size = 10
-latent_dim = 256
-epochs = 2
+latent_dim = 512
+epochs = 25
+mixing_prob = 0.90
 
 # Initialize models
-generator = define_generator()
 discriminator = define_discriminator()
+mapping_network = MappingNetwork()  # Use the modified mapping network
+generator = StyleGANGenerator(mapping_network)
 
 # Define optimizers
-generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002)
-discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.0, beta_2=0.99, epsilon=1e-8)
+discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.0, beta_2=0.99, epsilon=1e-8) #TODO: Paper also suggests beta_1=0, beta_2=0.99, epsilon=1e-8 for all learning rates.
+
+# TODO: ADD OPTIMISER FOR MAPPING NETWORK HERE.
+mapping_network_optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001, beta_1=0.0, beta_2=0.99, epsilon=1e-8) # Papers suggest 100x lower, I will do 10x because my learning rates are already very low.
 
 # Define training step
-@tf.function
+#@tf.function
 def train_step(images):
-    noise = tf.random.normal([batch_size, latent_dim])
+    noise1 = tf.random.normal([batch_size, latent_dim])
+    noise2 = tf.random.normal([batch_size, latent_dim])
     
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        generated_images = generator(noise, training=True)
+    # Need a persistent gradient tape as we will be using it to update both the generator and the mapping network
+    # without it being persistent it will be "used" after being used for the generator.
+    with tf.GradientTape(persistent=True) as gen_tape, tf.GradientTape() as disc_tape:
+
+        # Generating two distinct subsections of style space from noise in order to 
+        # use style mixing.
+        style1 = mapping_network(noise1)
+        style2 = mapping_network(noise2)
+
+        #print("BEFORE GENERATOR WITH style1 = ", type(style1))
+        #print("BEFORE GENERATOR WITH style2 = ", type(style2))
+
+        generated_images = generator(style1, style2, mixing_prob=mixing_prob)
+        
+        #print("AFTER GENERATOR")
+
+        #print("generated images shape =", generated_images.shape)
         
         real_output = discriminator(images, training=True)
         fake_output = discriminator(generated_images, training=True)
         
-        gen_loss = generator_loss(fake_output)
-        disc_loss = discriminator_loss(real_output, fake_output)
+        
+        gen_loss = generator_loss(fake_output) #TODO: CAN POSSIBLY ADD PATH_LENGTH_REGULARISATION TO THIS
+        disc_loss = discriminator_loss(real_output, fake_output) #TODO: CAN POSSIBLY ADD R1 REGULARISATION TO THIS.
     
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
     
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+    # Update mapping network separately
+    mapping_network_variables = mapping_network.trainable_variables
+    gradients_of_mapping_network = gen_tape.gradient(gen_loss, mapping_network_variables)
+    mapping_network_optimizer.apply_gradients(zip(gradients_of_mapping_network, mapping_network_variables))
+
+    # So that it doesn't persist beyond its required usage (ie; into other epochs)
+    del gen_tape
     
     return disc_loss, gen_loss
 
@@ -48,8 +78,12 @@ def train():
     # Prepare dataset
     train_dataset = tf.data.Dataset.from_tensor_slices(images).shuffle(len(images)).batch(batch_size)
     
+    print("train dataset size =", len(list(train_dataset)))
+
     for epoch in range(epochs):
-        for image_batch in train_dataset:
+        for i, image_batch in enumerate(train_dataset):
+            #print("CALLING TRAIN STEP")
+            print("Handling batch ", i)
             d_loss, g_loss = train_step(image_batch)
         
         print(f'Epoch {epoch+1}, Discriminator Loss: {d_loss}, Generator Loss: {g_loss}')

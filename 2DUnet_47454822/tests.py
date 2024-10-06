41e0cd08fddc94439716284e75a9186d26213ea9
Jack <jack.eric.edmondson@gmail.com>
2024-10-06 14:46:06 +1000

    switched to adamW optimiser and dice loss

--- 2DUnet_47454822/tests.py
+++ 2DUnet_47454822/tests.py
@@ -1,11 +1,12 @@
 import keras
 import numpy as np
-from keras.src.optimizers import Adam
+from keras.src.optimizers import AdamW
 from keras.src.utils import to_categorical
 
 from dataset import load_dir, load_data_2D
 from tensorflow.keras.preprocessing import image
 from modules import unet_model
+from keras.src.losses import Dice
 
 # [BATCH_SIZE, CHANNELS, HEIGHT, WIDTH]
 # load_dir return: [BATCH_SIZE, HEIGHT, WIDTH]
@@ -43,7 +44,7 @@ def test_train():
     # X = np.resize(X, (batch_size, 128, 128, 1))
 
     y = np.expand_dims(y, axis=-1)
-    y = to_categorical(y, num_classes=6)
+    y = to_categorical(y, num_classes=6)  # Otherwise it has (None, 256, 128, 1) but we want (None, 256, 128, 6) where the final is the num channels/classes
     # y = np.squeeze(y, axis=-1)
     # y = np.resize(y, (batch_size, 128, 128, 1))
 
@@ -52,13 +53,16 @@ def test_train():
 
 
     model = unet_model((256, 128, 1), batch_size=batch_size)
-    model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=AdamW(), loss=Dice, metrics=['accuracy']) # used to be sparse_cat_crossent. find better loss
 
-    model.fit(x=X, y=y, batch_size=batch_size, epochs=1, shuffle=True, verbose=2)
+    model.fit(x=X, y=y, batch_size=batch_size, epochs=3, shuffle=True, verbose=2)
 
 if __name__ == '__main__':
     # invoke_dir()
     test_train()
+    print("DONE")
+
+    # minimum Dice similarity coefficient of 0.75 on the test set on the prostate label.
 
 
 # POL7010 Dynamics of Governance

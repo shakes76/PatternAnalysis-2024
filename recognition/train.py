4ebc0e0390505b999b7494417f3a7e35649678e3
aditya48781262 <a.sant@student.uq.edu.au>
2024-10-06 13:08:54 +1000

    Moved parameters and dimensions into utils.py; Saved model weights and parameters for prediction

--- recognition/train.py
+++ recognition/train.py
@@ -1,4 +1,5 @@
 from modules import Encoder, Decoder, Quantise, Model
+from utils import parameters, dimensions
 import dataset
 import torch
 import torch.nn as nn
@@ -8,33 +9,13 @@ from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 
-torch.manual_seed(111)
-
-dimensions = {
-    "size":(256, 144),
-    "input": 1, 
-    "hidden": 512, 
-    "latent": 16,
-    "embeddings": 512,
-    "output": 1,
-    "commitment_beta": 0.25
-}
-
-parameters = {
-    "lr": 2e-4, 
-    "epochs": 50, 
-    "batch": 100,
-    "gpu": "cuda",
-    "cpu": "cpu"
-}
-
-trainSet = dataset.GetTrainSet()
-trainLoader = DataLoader(trainSet, batch_size=parameters["batch"], shuffle=True)
+train_set = dataset.GetTrainSet()
+train_loader = DataLoader(train_set, batch_size=parameters["batch"], shuffle=True)
 
 device = torch.device(parameters["gpu"] if torch.cuda.is_available() else parameters["cpu"])
 print(f"VQVAE being trained on {device}.\n")
 
-print(f"Train.py: Shape of first training dataset image {trainSet[0].shape}")
+print(f"Train.py: Shape of first training dataset image {train_set[0].shape}")
 
 encoder = Encoder(input_dim=dimensions["input"], hidden_dim=dimensions["hidden"], output_dim=dimensions["latent"])
 quantise = Quantise(n_embeddings=dimensions["embeddings"], embed_dim=dimensions["latent"])
@@ -50,7 +31,7 @@ vqvae.train()
 for epoch in range(parameters["epochs"]):
     running_loss = 0.0
     # Labels not being outputted?
-    for i, image in enumerate(trainLoader):
+    for i, image in enumerate(train_loader):
         image = image.to(device)
 
         optimiser.zero_grad()
@@ -68,5 +49,5 @@ for epoch in range(parameters["epochs"]):
             print(f"Loss for {epoch + 1},{i + 1} = {loss.item()}\nreconstruction loss = {reconstr_loss.item()}, perplexity: {perplexity.item()}, commitment loss: {commitment_loss.item()}, codebook loss: {codebook_loss.item()}")
     print(f"Epoch {epoch + 1} loss: {running_loss}.")
 
-#TBD
+torch.save(vqvae.state_dict(), 'hipmri_vqvae.pth')
 
--- recognition/utils.py
+++ recognition/utils.py
@@ -5,13 +5,32 @@ import nibabel as nib
 import numpy as np
 from tqdm import tqdm
 import torch
-
+import matplotlib.pyplot as plt
+from matplotlib import transforms
 
 """
 REFERENCES (INCOMPLETE):
  Chandra, S. (2024).
 Contains function to load Nifti files as well.
 """
+
+dimensions = {
+    "size":(256, 144),
+    "input": 1, 
+    "hidden": 512, 
+    "latent": 16,
+    "embeddings": 512,
+    "output": 1,
+    "commitment_beta": 0.25
+}
+
+parameters = {
+    "lr": 2e-4, 
+    "epochs": 50, 
+    "batch": 100,
+    "gpu": "cuda",
+    "cpu": "cpu"
+}
 
 def to_channels(arr: np.ndarray , dtype =np.uint8) -> np.ndarray :
     channels = np.unique(arr)
@@ -87,11 +106,34 @@ def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float3
     else:
         return images
 
+def SaveOneImage(inputSamples, imgTitle, plotTitle):
+    plt.clf()
+    tr = transforms.Affine2D().rotate_deg(90)
 
+    inputSamples = inputSamples.cpu().detach()
 
-def SaveCheckpoint():
-    print()
-def LoadCheckpoint():
-    print()
-def Initiliase():
-    print()
+    plt.figure()
+    plt.title(f"{plotTitle}")
+    image = inputSamples[0].permute(1, 2, 0)
+    image = torch.rot90(image, k=1, dims=[0, 1])
+
+    plt.imshow(image, cmap='gray')
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.savefig(f'{imgTitle}.png'.format(0))
+        
+
+def SaveMultipleImages(inputSamples, imgTitle, plotTitle):
+    plt.clf()
+    inputSamples = inputSamples.cpu().detach()
+    plt.figure(figsize=(8,8))
+    plt.title(f"{plotTitle}")
+    for i in range(16):
+        ax = plt.subplot(4, 4, i+1) 
+        image = inputSamples[i].permute(1, 2, 0)
+        image = torch.rot90(image, k=1, dims=[0, 1])
+        plt.imshow(image, cmap='gray')
+        plt.xticks([])
+        plt.yticks([])
+    plt.savefig(f'{imgTitle}.png'.format(i))

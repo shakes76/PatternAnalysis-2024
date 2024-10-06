24563206495858b5d4b88d5eb951e8db0484414a
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-10-07 00:26:32 +1000

    feat: add basic siamese network using resnet50 as base

--- recognition/siamese-network-46978554/src/modules.py
+++ recognition/siamese-network-46978554/src/modules.py
@@ -1,1 +1,17 @@
 """Model components"""
+
+import torch.nn as nn
+from torchvision.models import resnet50
+
+
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+
+        self.resnet = resnet50()
+
+    def forward(self, x1, x2):
+        out1 = self.resnet(x1)
+        out2 = self.resnet(x2)
+
+        return out1, out2

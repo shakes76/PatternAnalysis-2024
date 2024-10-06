444549c81fa44b11f06c4dac77a4a7d110741013
beenalf <sth48624bt@outlook.com>
2024-10-06 22:31:54 +1000

    Added GFNet imported from the timm library

--- recognition/GFNet_s4784738/modules.py
+++ recognition/GFNet_s4784738/modules.py
@@ -5,4 +5,26 @@ Benjamin Thatcher
 s4784738    
 """
 
-    
+import torch
+import torch.nn as nn
+import timm  # For GFNet and pretrained models
+
+class GFNetClassifier(nn.Module):
+    def __init__(self, num_classes=2, pretrained=False):
+        super(GFNetClassifier, self).__init__()
+        
+        # Load GFNet without pretrained weights from timm
+        self.gfnet = timm.create_model('gfnet_base', pretrained=pretrained)
+        
+        # Replace the classification head to match the number of output classes
+        in_features = self.gfnet.head.fc.in_features
+        self.gfnet.head.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.gfnet(x)
+
+# Function to get the model instance
+def get_model(num_classes=2, pretrained=False):
+    model = GFNetClassifier(num_classes=num_classes, pretrained=pretrained)
+    return model
+        

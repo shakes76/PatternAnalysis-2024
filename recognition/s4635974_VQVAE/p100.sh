31557512c165a5419e4bc2e2b8fcd3fb94821191
dcollie87 <dcollie87@hotmail.com>
2024-10-08 11:22:14 +1000

    slurm

--- recognition/s4635974_VQVAE/p100.sh
+++ recognition/s4635974_VQVAE/p100.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=0-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au
-#SBATCH --output=test.out
+#SBATCH --output=predict.out
 
 conda activate torch
-python train.py
+python predict.py

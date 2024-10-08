9800626ff01a081ea298025d79ba202140c1285e
dcollie87 <dcollie87@hotmail.com>
2024-10-08 11:18:30 +1000

    p100.sh

--- recognition/s4635974_VQVAE/p100.sh
+++ recognition/s4635974_VQVAE/p100.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --partition=comp3710
-#SBATCH --account=comp3710 
+#SBATCH --partition=p100
 #SBATCH --gres=gpu:1
+#SBATCH --time=0-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au
-#SBATCH --output=train.out
+#SBATCH --output=test.out
 
 conda activate torch
-python predict.py
+python train.py

f66c7b1202e34db0c17ce69d7263c97b7882aa31
dcollie87 <dcollie87@hotmail.com>
2024-10-08 13:14:00 +1000

    .sh change

--- recognition/s4635974_VQVAE/predict.sh
+++ recognition/s4635974_VQVAE/predict.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --partition=comp3710
-#SBATCH --account=comp3710 
+#SBATCH --partition=a100-test*
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au
-#SBATCH --output=predict.out
+#SBATCH --output=predict_a100-test.out
 
 conda activate torch
 python predict.py

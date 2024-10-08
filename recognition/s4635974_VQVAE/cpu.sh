e15bf8df51b0def6ecca6e2cd8daf8cb5e1fe399
dcollie87 <dcollie87@hotmail.com>
2024-10-08 13:06:17 +1000

    cpu

--- recognition/s4635974_VQVAE/cpu.sh
+++ recognition/s4635974_VQVAE/cpu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #SBATCH --partition=cpu
-#SBATCH --account=comp3710 
-#SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au
 #SBATCH --output=predict.out

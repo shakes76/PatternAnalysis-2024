13e560a796c5d90695275f665041b00488235e59
GavinSaiun <gavin.sun56@gmail.com>
2024-10-09 09:06:46 +1000

    added GFNet block

--- recognition/GFNet_Gavin_Sun/modules.py
+++ recognition/GFNet_Gavin_Sun/modules.py
@@ -12,7 +12,7 @@ class Mlp(nn.Module):
     credit: https://www.youtube.com/watch?v=ovB0ddFtzzA&ab_channel=mildlyoverfitted
     """
 
-    def __init__(self, in_features, hidden_features, out_features, drop=0.):
+    def __init__(self, in_features, hidden_features = None, out_features = None, drop=0.):
         super().__init__()
         self.fc1 = nn.Linear(in_features, hidden_features)
         self.act = nn.GELU()
@@ -54,3 +54,16 @@ class GlobalFilter(nn.Module):
 
         return x
 
+class Block(nn.Module):
+    def __init__(self, dim, mlp_ratio=4., drop=0., norm_layer=nn.LayerNorm, h=14, w=8):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.filter = GlobalFilter(dim, h=h, w=w)
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
+
+    def forward(self, x):
+        # Skip DropPath; directly add the residual connection
+        x = x + self.mlp(self.norm2(self.filter(self.norm1(x))))
+        return x

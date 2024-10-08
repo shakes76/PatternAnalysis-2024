7407199b9c4fa7412001968e444e7ce3d2138bac
aidanlokk <aidan2327@gmail.com>
2024-10-08 15:57:45 +1000

    Coded the Decoder module/class to VQ-VAE architecture to upsample the layers and try to reconstruct an image which matches the original image dataset. I integrated ResidualStack into the Decoder for enhanced feature refinement. The input x is a compressed representation (the quantized vectors from the VQ-VAE). The conv layer increases the dimensionality (number of channels) of the input from embedding_dim to a higher num_hiddens. The upsampled image which is x_recon, is the output. This is the decoders reconstructed image and best guess at what the original image looked like

--- recognition/vqvae-s4743500/modules.py
+++ recognition/vqvae-s4743500/modules.py
@@ -87,3 +87,52 @@ class Encoder(nn.Module):
     def forward(self, x):
         h = self.conv(x)
         return self.residual_stack(h)
+    
+class Decoder(nn.Module):
+    def __init__(
+            self,
+            embedding_dim, # Will be set to 64, but try 128 if images are not clear enough
+            num_hiddens,
+            num_upsampling_layers,
+            num_residual_layers,
+            num_residual_hiddens
+    ):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels=embedding_dim,
+            out_channels=num_hiddens,
+            kernel_size=3,
+            padding=1,
+        )
+        self.residual_stack = ResidualStack(
+            num_hiddens, num_residual_layers, num_residual_hiddens
+        )
+        upconv = nn.Sequential()
+        for upsampling_layer in range(num_upsampling_layers):
+            if upsampling_layer < num_upsampling_layers - 2:
+                (in_channels, out_channels) = (num_hiddens, num_hiddens)
+            elif upsampling_layer == num_upsampling_layers - 2:
+                (in_channels, out_channels) = (num_hiddens, num_hiddens // 2)
+            else:
+                (in_channels, out_channels) = (num_hiddens // 2, 1) # 1 for grayscale images 
+
+            upconv.add_module(
+                f"up{upsampling_layer}",
+                nn.ConvTranspose2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=4,
+                    stride=2, # Stride 2 upsamples by double
+                    padding=1
+                ),
+            )
+            if upsampling_layer < num_upsampling_layers - 1:
+                upconv.add_module(f"relu{upsampling_layer}", nn.ReLU())
+
+        self.upconv = upconv
+
+    def forward(self, x):
+        h = self.conv(x)
+        h = self.residual_stack(h)
+        x_recon = self.upconv(h)
+        return x_recon

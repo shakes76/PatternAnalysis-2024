2e4e9bac117a23bb580fb7719e8a286b3710ee22
dcollie87 <dcollie87@hotmail.com>
2024-10-08 10:48:10 +1000

    updated slurm

--- recognition/s4635974_VQVAE/slurm.sh
+++ recognition/s4635974_VQVAE/slurm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=comp3710
 #SBATCH --account=comp3710 
-#SBATCH --gres=gpu:1
 #SBATCH --time=0-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au

ddfeda718e3168c7017b480e828361104683cc04
aidanlokk <aidan2327@gmail.com>
2024-10-08 13:51:34 +1000

    I created the class ResidualStack in order to help the model learn depper features from the MRI data and support deeper feature extraction in the upcoming VQ-VAE class that will be coded later. Inside the class, I added identity mapping (skip connections) to improve feature learning. ReLU activations and convolutional layers were also included to better capture MRI data features

--- recognition/vqvae-s4743500/modules.py
+++ recognition/vqvae-s4743500/modules.py
@@ -3,3 +3,36 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+
+class ResidualStack(nn.Module):
+    def __init__(self, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super().__init__()
+        # Residual blocks that add identity mapping for deeper networks. This helps
+        # the model better capture the important features of the MRI data
+        layers = []
+        for i in range(num_residual_layers):
+            layers.append(
+                nn.Sequential(
+                    nn.ReLU(),
+                    nn.Conv2d(
+                        in_channels=num_hiddens,
+                        out_channels=num_residual_hiddens,
+                        kernel_size=3,
+                        padding=1,
+                    ),
+                    nn.ReLU(),
+                    nn.Conv2d(
+                        in_channels=num_residual_hiddens,
+                        out_channels=num_hiddens,
+                        kernel_size=1,
+                    ),
+                )
+            )
+        self.layers = nn.ModuleList(layers)
+
+    def forward(self, x):
+        h = x
+        for layer in self.layers:
+            # Adds identity mapping (skip connection)
+            h = h + layer(h)
+        return torch.relu(h)

ce46dae788ca94f0f6857ab8fac39525269133cf
Al Holliday <alh1260@gmail.com>
2024-10-08 12:19:31 +1000

    Got the encoder half of the network

--- recognition/HipMRI_UNet_Al_Holliday_43950214/modules.py
+++ recognition/HipMRI_UNet_Al_Holliday_43950214/modules.py
@@ -18,7 +18,7 @@ class UNetBasicBlock(nn.Module):
     Basic building block for the U-Net
     
     Two things I could try:
-        1. Structure like Ronneberger et. al. [1], that is 3 3x3 Conv2ds w/ ReLUs followed
+        1. Structure like Ronneberger et. al. [1], that is 2 3x3 Conv2ds w/ ReLUs followed
         by a 2x2 Max Pool
         
         2. Structure like Sakboonyara and Taeprasartsit [2], which is a 3x3 Conv2d with ReLU, then
@@ -37,9 +37,9 @@ class UNetBasicBlock(nn.Module):
     """
     def __init__(self, inDim, dim, kernSz):
         super().__init__()
-        self.conv1 = nn.Conv2d(inDim, dim, kernel_size = kernSz, bias = False)
+        self.conv1 = nn.Conv2d(inDim, dim, kernel_size = kernSz)
         self.act1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(dim, dim, kernel_size = kernSz, bias = False)
+        self.conv2 = nn.Conv2d(dim, dim, kernel_size = kernSz)
         self.act2 = nn.ReLU()
     
     def forward(self, x):
@@ -52,11 +52,26 @@ class UNet(nn.Module):
     diagram from the U-Net paper).
     """
     def __init__(self):
-        # self.blk1 = UNetBasicBlock()
-        pass
+        super().__init__()
+        # encoder
+        self.blk1 = UNetBasicBlock(1, 64, 3)
+        self.blk2 = UNetBasicBlock(64, 128, 3)
+        self.blk3 = UNetBasicBlock(128, 256, 3)
+        self.blk4 = UNetBasicBlock(256, 512, 3)
+        # latent
+        self.latent = UNetBasicBlock(512, 1024, 3)
+        # decoder
     
     def forward(self, x):
-        # blk1 = self.blk1(x)
-        # maxPool1 = F.max_pool2d(blk1)
+        # just the encoder pass first
+        enc1 = self.blk1(x)
+        maxPool1 = F.max_pool2d(enc1, 2)
+        enc2 = self.blk2(maxPool1)
+        maxPool2 = F.max_pool2d(enc2, 2)
+        enc3 = self.blk3(maxPool2)
+        maxPool3 = F.max_pool2d(enc3, 2)
+        enc4 = self.blk4(maxPool3)
+        maxPool4 = F.max_pool2d(enc4, 2)
+        return maxPool4
+        #return self.latent(maxPool4)
         # skip == blk1 + blk of some other level
-        pass

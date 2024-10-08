df3121232fe4d9584270429a3123d9c81eb80d4a
aidanlokk <aidan2327@gmail.com>
2024-10-08 11:54:28 +1000

    I added more code to the ProstateMRIDataset class to improve image handling and testing. I added functionality to extract the middle slice if the MRI image is 3D. I also implemented normalization to a [0,1] range for improved training stability. I then converted the normalized images to a [0,255] range for display

--- recognition/vqvae-s4743500/dataset.py
+++ recognition/vqvae-s4743500/dataset.py
@@ -30,3 +30,36 @@ class ProstateMRIDataset(Dataset):
         # Adds a check to print a warning if no files were found
         if len(self.image_paths) == 0:
             print(f"No NIFTI files found in directory {img_dir}")
+
+    def __len__(self):
+        # Returns the number of images
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        # Loads the NIFTI image
+        img_path = self.image_paths[idx]
+        img_nifti = nib.load(img_path)
+        img_data = img_nifti.get_fdata()
+
+        # Selects middle slice if the image is 3D
+        if img_data.ndim == 3:
+            middle_slice = img_data.shape[2] // 2
+            img_data = img_data[:, :, middle_slice]
+        
+        # Normalizes the image data to a range [0, 1] to help the model train better
+        img_data_normalized = (img_data - np.min(img_data)) / (np.max(img_data) - np.min(img_data))
+        
+        # Converts range to [0, 255] for image displaying
+        img_data_255 = (img_data_normalized * 255).astype(np.uint8)
+
+        # Converts the NumPy array to a PIL Image for transformations
+        img = Image.fromarray(img_data_255)
+
+        # # Comment out above and uncomment this for testing
+        # img = Image.fromarray(np.uint8(img_data))
+
+        # Applies transformations (such as resize, crop, normalization, etc.)
+        if self.transform:
+            img = self.transform(img)  
+
+        return img

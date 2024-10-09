d75972c7ca3bde0bd13000514cf784371629d861
Al Holliday <alh1260@gmail.com>
2024-10-09 17:37:26 +1000

    Fixed up the dataloader so that it returns the segment maps too

--- recognition/HipMRI_UNet_Al_Holliday_43950214/dataset.py
+++ recognition/HipMRI_UNet_Al_Holliday_43950214/dataset.py
@@ -14,36 +14,40 @@ import torchvision.transforms as transforms
 # TODO: may be treating 'seg_*' wrong; these may be the segment map LABELS as opposed to their own
 # image set
 class HipMRI2d(torch.utils.data.Dataset):
-    def __init__(self, root, imgSet = "seg_train", transform = None, applyTrans = False):
+    def __init__(self, root, imgSet = "train", transform = None, applyTrans = False):
         self.root = root
         self.setPath = ""
+        self.segPath = ""
         self.setFile = ""
-        if imgSet == "seg_test":
-            self.setPath = "keras_slices_seg_test"
-            self.setFile = "hipmri_slices_seg_test.txt"
-        elif imgSet == "seg_train":
-            self.setPath = "keras_slices_seg_train"
-            self.setFile = "hipmri_slices_seg_train.txt"
-        elif imgSet == "seg_validate":
-            self.setPath = "keras_slices_seg_validate"
-            self.setFile = "hipmri_slices_seg_validate.txt"
-        elif imgSet == "test":
+        self.segFile = ""
+        if imgSet == "test":
             self.setPath = "keras_slices_test"
             self.setFile = "hipmri_slices_test.txt"
+            self.segPath = "keras_slices_seg_test"
+            self.segFile = "hipmri_slices_seg_test.txt"
         elif imgSet == "train":
             self.setPath = "keras_slices_train"
             self.setFile = "hipmri_slices_train.txt"
+            self.segPath = "keras_slices_seg_train"
+            self.segFile = "hipmri_slices_seg_train.txt"
         elif imgSet == "validate":
             self.setPath = "keras_slices_validate"
             self.setFile = "hipmri_slices_validate.txt"
+            self.segPath = "keras_slices_seg_validate"
+            self.segFile = "hipmri_slices_seg_validate.txt"
         else:
-            raise ValueError("imgSet must be one of 'seg_test', 'seg_train', 'seg_validate', 'test', 'train', 'validate'")
+            raise ValueError("imgSet must be one of 'test', 'train', 'validate'")
+        # note: len(pics) should equal len(segs)
         self.pics = []
+        self.segs = []
         with open(self.setFile) as f:
             for pic in f:
                 self.pics.append(pic.strip())
         # TODO: change what trans represents. Something like:
         # trans = torchvision.transforms.Compose([transforms.ToTensor(), transforms.Resize()])
+        with open(self.segFile) as g:
+            for seg in g:
+                self.segs.append(seg.strip())
         self.trans = transform
         self.applyTrans = applyTrans
     
@@ -55,18 +59,27 @@ class HipMRI2d(torch.utils.data.Dataset):
         do the stuff in load_data_2D here
         """
         imgPath = os.path.join(self.root, self.setPath, self.pics[i])
+        segPath = os.path.join(self.root, self.segPath, self.segs[i])
         niftiImg = nib.load(imgPath)
+        niftiSeg = nib.load(segPath)
         # get_fdata() returns an ndarray
         img = niftiImg.get_fdata(caching = "unchanged")
+        seg = niftiSeg.get_fdata(caching = "unchanged")
         if len(img.shape) == 3:
             img = img[: ,: ,0] # sometimes extra dims , remove
+        if len(seg.shape) == 3:
+            seg = seg[: ,: ,0]
         # turn it back into 3D because PyTorch demands a channel dimension (and it demands that it be the first dim)
         img = img[np.newaxis,:,:]
+        seg = seg[np.newaxis,:,:]
         img = img.astype(np.float32)
+        seg = seg.astype(np.float32)
         imgTensor = torch.tensor(img)
+        segTensor = torch.tensor(seg)
         if self.applyTrans:
             # TODO: look up what this Nibabel affine object is and how to use it
             # with PyTorch
             #self.trans = niftiImg.affine
             imgTensor = self.trans(imgTensor)
-        return imgTensor, 0
+            segTensor = self.trans(segTensor)
+        return imgTensor, segTensor

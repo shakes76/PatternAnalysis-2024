7987d36b9c7beafc801ff0b42a2875fc2675ee5f
ConyllUQ <conyll.forbes@student.uq.edu.au>
2024-10-09 23:52:33 +1000

    Updated modules: VQVAE module added

--- recognition/HipMRI_VQVAE_47424870/modules.py
+++ recognition/HipMRI_VQVAE_47424870/modules.py
@@ -16,9 +16,9 @@ class Encoder(nn.Module):
         x = F.relu(self.conv3(x))
         return x
 
-class VectorQuantizer(nn.Module):
+class VectorQuantiser(nn.Module):
     def __init__(self, num_embeddings, embedding_dim):
-        super(VectorQuantizer, self).__init__()
+        super(VectorQuantiser, self).__init__()
         self.embedding_dim = embedding_dim
         self.num_embeddings = num_embeddings
         self.embeddings = nn.Parameter(torch.randn(num_embeddings, embedding_dim))
@@ -40,9 +40,9 @@ class VectorQuantizer(nn.Module):
         z_q = z_q.transpose(1, 2).view(b, self.embedding_dim, h, w)
 
         # Calculate the quantization loss
-        quantization_loss = F.mse_loss(z_q.detach(), x) + F.mse_loss(z_q, x.detach())
+        quantisation_loss = F.mse_loss(z_q.detach(), x) + F.mse_loss(z_q, x.detach())
 
-        return z_q, quantization_loss
+        return z_q, quantisation_loss
     
 class Decoder(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, output_dim):
@@ -57,5 +57,18 @@ class Decoder(nn.Module):
         x = torch.sigmoid(self.conv3(x))
         return x
     
+class VQVAE(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_embeddings, embedding_dim):
+        super(VQVAE, self).__init__()
+        self.encoder = Encoder(input_dim, hidden_dim)
+        self.pre_vq_conv = nn.Conv2d(hidden_dim, embedding_dim, kernel_size=1)
+        self.vq = VectorQuantiser(num_embeddings, embedding_dim)
+        self.decoder = Decoder(embedding_dim, hidden_dim, input_dim)
 
-# Define other components: VQVAE.
+    def forward(self, x):
+        z_e = self.encoder(x)  # Encoding
+        z_e = self.pre_vq_conv(z_e)  # Pre-quantisation convolution
+        z_q, quantisation_loss = self.vq(z_e)  # Quantisation
+        x_reconstructed = self.decoder(z_q)  # Decoding
+        return x_reconstructed, quantisation_loss
+

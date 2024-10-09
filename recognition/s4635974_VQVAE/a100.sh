87e089e3211081db338e3b9422bc4956eb460695
dcollie87 <dcollie87@hotmail.com>
2024-10-10 07:33:42 +1000

    same

--- recognition/s4635974_VQVAE/a100.sh
+++ recognition/s4635974_VQVAE/a100.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=0-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s4635974@student.uq.edu.au
-#SBATCH --output=100_epochs.out
+#SBATCH --output=lr=0.003_bs=16.out.out
 
 conda activate torch
 python train.py

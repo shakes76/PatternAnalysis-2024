608459522a5c99cf0bff462cf6dc6ce0cb16fab4
Mr Carl Flottmann <s4697229@login0.compute.eait.uq.edu.au>
2024-10-09 17:53:35 +1000

    Working for creating training script. Need to transfer to computer as Ranpur is not currently working

--- recognition/46972299_Unet/dataset.py
+++ recognition/46972299_Unet/dataset.py
@@ -26,7 +26,7 @@ class ProstateDataset(Dataset):
     def __init__(self, image_dir: str, mask_dir: str, num_classes: int, num_load: int = None) -> None:
         images = [image_dir + file.name for file in Path(image_dir).glob(self.FILE_TYPE) if file.is_file()]
         masks = [mask_dir + file.name for file in Path(mask_dir).glob(self.FILE_TYPE) if file.is_file()]
-        
+
         if num_load is None:
             num_load = len(images)
 
@@ -35,19 +35,29 @@ class ProstateDataset(Dataset):
         print(f"Loading {num_load} masks from {mask_dir}")
         self.mask_3D_data = load_data_3D(masks[:num_load])
 
-        self.image_transform = tio.Compose([
+        self.transform = tio.Compose([
             tio.RescaleIntensity((0, 1)),
             tio.ZNormalization(),
-        ])
-        
-        self.mask_transform = tio.Compose([
             tio.OneHot(num_classes)
         ])
+
+        self.processed = []
 
     def __len__(self) -> int:
         return len(self.image_3D_data)
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        image = self.image_transform(torch.tensor(self.image_3D_data[idx], dtype=torch.float32))
-        mask = self.mask_transform(torch.tensor(self.mask_3D_data[idx], dtype=torch.float32))
-        return image, mask
+        image = self.image_3D_data[idx]
+        image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)
+
+        mask = self.mask_3D_data[idx]
+        mask = torch.tensor(mask, dtype=torch.float32).unsqueeze(0)
+
+        subject = tio.Subject(
+            image=tio.ScalarImage(tensor=image),
+            mask=tio.LabelMap(tensor=mask)
+        )
+        subject = self.transform(subject)
+        self.processed.append(subject)
+
+        return subject['image'].data, subject['mask'].data
--- recognition/46972299_Unet/train.py
+++ recognition/46972299_Unet/train.py
@@ -6,6 +6,7 @@ from metrics import get_loss_function
 from dataset import ProstateDataset, TOP_LEVEL_DATA_DIR, SEMANTIC_LABELS, SEMANTIC_MRS
 
 import torch
+from torch.utils.data.dataloader import DataLoader
 import torch.optim as optim
 import time
 
@@ -14,7 +15,9 @@ BATCH_SIZE = 2
 EPOCHS = 10
 NUM_CLASSES = 6 # as per powerpoint slides
 INPUT_CHANNELS = 1 # greyscale
-NUM_LOADED = 2 # set to None to load all
+NUM_LOADED = 10 # set to None to load all
+SHUFFLE = False
+WORKERS = 3
 
 # taken from the paper on the improved unet
 INITIAL_LR = 5e-4
@@ -30,7 +33,9 @@ else:
 torch.autograd.set_detect_anomaly(True) # help detect problems
 
 # setup data
-data_loader = ProstateDataset(TOP_LEVEL_DATA_DIR + SEMANTIC_MRS, TOP_LEVEL_DATA_DIR + SEMANTIC_LABELS, NUM_CLASSES, num_load=NUM_LOADED)
+dataset = ProstateDataset(TOP_LEVEL_DATA_DIR + SEMANTIC_MRS, TOP_LEVEL_DATA_DIR + SEMANTIC_LABELS,
+                          INPUT_CHANNELS, NUM_CLASSES, num_load=NUM_LOADED)
+data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=SHUFFLE, num_workers=WORKERS)
 
 # setup model
 model = Improved3DUnet(INPUT_CHANNELS, NUM_CLASSES)
@@ -39,3 +44,29 @@ optimiser = optim.Adam(model.parameters(), lr=INITIAL_LR, weight_decay=WEIGHT_DE
 scheduler = optim.lr_scheduler.LambdaLR(optimiser, lr_lambda=lambda epoch: DECAY_FACTOR ** epoch)
 # will use dice loss as per the task requirements
 criterion = get_loss_function()
+
+model = model.to(device)
+
+# ================== training procedure
+model.train()
+print(f"Training...")
+start = time.time()
+
+for epoch in range(EPOCHS):
+    for step, (image, mask) in enumerate(data_loader):
+        image = image.to(device)
+        mask = mask.to(device)
+
+        outputs = model(image)
+        loss = criterion(outputs, mask)
+        
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+    scheduler.step()
+
+end = time.time()
+elapsed = end - start
+
+print(f"Training took {str(elapsed)} secs or {str(elapsed/60)} mins in total")

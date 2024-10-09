6e9ece10c582fe4c6579ce60eaffe25d21686fef
Al Holliday <alh1260@gmail.com>
2024-10-09 15:26:06 +1000

    Getting a start on the training script

--- recognition/HipMRI_UNet_Al_Holliday_43950214/train.py
+++ recognition/HipMRI_UNet_Al_Holliday_43950214/train.py
@@ -4,5 +4,17 @@ Training script
 
 @author: al
 """
+import torch
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
 
-# TODO: put code here
+import dataset
+import modules
+
+trans = transforms.Resize((256, 256))
+# for the lab computers
+hipmri2dtrain = dataset.HipMRI2d("H:\\HipMRI", imgSet = "train", transform = trans, applyTrans = True)
+# for rangpur
+#hipmri2dtrain = dataset.HipMRI2d("/home/groups/comp3710/HipMRI_Study_open/keras_slices_data", imgSet = "train", transform = trans, applyTrans = True)
+
+net = modules.UNet()

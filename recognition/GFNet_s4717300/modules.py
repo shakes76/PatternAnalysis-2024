114f28a331580f8e14af12f28c79026334891e9b
BenjaminJorgensen <Benjamin@jorgensens.id.au>
2024-10-10 08:18:42 +1000

    feat: Implemented patch embeddings + Block structure of module

--- recognition/GFNet_s4717300/modules.py
+++ recognition/GFNet_s4717300/modules.py
@@ -2,25 +2,100 @@
 # implemented as a class or a function
 import torch
 import torch.nn as nn
+import math
 from torch.fft import rfft2, irfft2
-from timm.models.layers import to_2tuple
+from collections import OrderedDict
+from functools import partial
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
 class GFNet(nn.Module):
-    def __init__(self, dim, W, H, channels, num_classes=2) -> None:
+    
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=10, embed_dim=768, depth=12,
+                 ff_ratio=4., norm_layer=None):
         super().__init__()
-        self.dim = dim
-        self.width = W
-        self.height = H
         self.num_classes = num_classes
-        self.channels = channels
-        ## Patch embeddings 
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
 
-        self.embedded_patches = PatchEmbed(img_size=:)
+        self.patch_embed = PatchEmbed(
+                img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+        # self.pos_drop = nn.Dropout(p=drop_rate)
+
+        h = img_size // patch_size
+        w = h // 2 + 1
+
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, ff_ratio=ff_ratio,
+                drop=drop_rate, drop_path=dpr[i], norm_layer=norm_layer, h=h, w=w) #type: ignore
+            for i in range(depth)])
+        
+        self.norm = norm_layer(embed_dim)
+
+        # Classifier head
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+        trunc_normal_(self.pos_embed, std=.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore #type: ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x).mean(1)
+        return x
 
     def forward(self, x):
-        pass
+        x = self.forward_features(x)
+        x = self.final_dropout(x)
+        x = self.head(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, dim, ff_ratio=4., drop=0., drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, h=14, w=8):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.filter = GlobalFilter(dim, h=h, w=w)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * ff_ratio)
+        self.feedForward = FeedFowardNetwork(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.feedForward(self.norm2(self.filter(self.norm1(x)))))
+        return x
 
 class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
     def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
         super().__init__()
         img_size = to_2tuple(img_size)
@@ -40,43 +115,46 @@ class PatchEmbed(nn.Module):
         return x
 
 
-# NOTE: Maybe reshape the input before and after? - got it
-# CONSIDER ADDING DROPOUT
 class FeedFowardNetwork(nn.Module):
-    def __init__(self, dim, W, H, act_layer=nn.LeakyReLU, norm_layer=nn.LayerNorm):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
-        self.dim = dim
-        self.width = W
-        self.height = H
-        self.norm_layer = norm_layer
-        self.act_layer = act_layer()
-        self.fc1 = nn.Linear(dim, dim)
-        self.fc2 = nn.Linear(dim, dim)
-
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+            
     def forward(self, x):
-        x = self.norm_layer(x) # WARNING: This might be wrong - don't really know what norm_layer takes as input
         x = self.fc1(x)
         x = self.act_layer(x)
-        x = self.norm_layer(x)
+        x = self.drop(x)
         x = self.fc2(x)
-        x = self.act_layer(x)
+        x = self.drop(x)
         return x
 
-class GlobalFilterLayer(nn.Module):
-    def __init__(self, dim, h, w, norm_layer=nn.LayerNorm):
-        self.dim = dim
-        self.norm_layer = norm_layer
-        self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02)
+class GlobalFilter(nn.Module):
+    def __init__(self, dim, h=14, w=8):
         super().__init__()
+        self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02)
+        self.w = w
+        self.h = h
 
-    def forward(self, x):
-        B, H, W, C = x.shape
+    def forward(self, x, spatial_size=None):
+        B, N, C = x.shape
+        if spatial_size is None:
+            a = b = int(math.sqrt(N))
+        else:
+            a, b = spatial_size
 
-        # x = x.to(torch.float32)
+        x = x.view(B, a, b, C)
+
+        x = x.to(torch.float32)
 
         x = rfft2(x, dim=(1, 2), norm='ortho')
         weight = torch.view_as_complex(self.complex_weight)
         x = x * weight
-        x = irfft2(x, s=(H, W), dim=(1, 2), norm='ortho')
-        return x
+        x = irfft2(x, s=(a, b), dim=(1, 2), norm='ortho')
+        x = x.reshape(B, N, C)
 
+        return x

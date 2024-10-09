41e236f09ec99eb85d406f655d907017d86d2a5d
dcollie87 <dcollie87@hotmail.com>
2024-10-09 19:45:23 +1000

    added all graphs to data_viz

--- recognition/s4635974_VQVAE/data_visualisation.py
+++ recognition/s4635974_VQVAE/data_visualisation.py
@@ -28,14 +28,42 @@ epochs = range(1, epochs)
 
 plt.plot(epochs, training_output_loss, label="Training Output Loss")
 plt.plot(epochs, validation_output_loss, label="Validation Output Loss")
-plt.ylim(0, 0.01)  # Set the y-axis limits between 0 and 1
+# plt.ylim(0, 0.01)  # Set the y-axis limits between 0 and 1
 plt.xlabel('Epoch')
 plt.ylabel('Training Output Loss')
 plt.title('Training Output Loss over Epochs')
 plt.legend()
-plt.show()
 
 # Save the figure to save_dir
 save_path = os.path.join(save_dir, 'training_output_loss.png')
 plt.savefig(save_path)
 plt.close()  # Close the plot to prevent it from displaying
+
+
+# Plot Training and Validation VQ Loss
+plt.plot(epochs, training_vq_loss, label="Training VQ Loss")
+plt.plot(epochs, validation_vq_loss, label="Validation VQ Loss")
+# plt.ylim(0, max(max(training_vq_loss), max(validation_vq_loss)))  # Dynamically set the y limit
+plt.xlabel('Epoch')
+plt.ylabel('VQ Loss')
+plt.title('VQ Loss over Epochs')
+plt.legend()
+
+# Save the VQ Loss figure
+save_path_vq = os.path.join(save_dir, 'vq_loss.png')
+plt.savefig(save_path_vq)
+plt.close()  # Close the plot to prevent displaying
+
+
+# Plot SSIM over epochs
+plt.plot(epochs, ssim, label="SSIM")
+# plt.ylim(0, 1)  # SSIM values typically range between 0 and 1
+plt.xlabel('Epoch')
+plt.ylabel('SSIM')
+plt.title('SSIM over Epochs')
+plt.legend()
+
+# Save the SSIM figure
+save_path_ssim = os.path.join(save_dir, 'ssim.png')
+plt.savefig(save_path_ssim)
+plt.close()  # Close the plot to prevent displaying

9accbb0803f7be4a3e20fc0560ab2275140bf71c
Mr Arpon Sarker <s4745413@login0.compute.eait.uq.edu.au>
2024-10-09 12:46:07 +1000

    added class VectorQuantizer in modules.py

--- recognition/VQVAE_s4745413/modules.py
+++ recognition/VQVAE_s4745413/modules.py
@@ -83,3 +83,44 @@ class Decoder(nn.Module):
     def forward(self, x):
         return self.inverse_conv_stack(x)
 
+class VectorQuantizer(nn.Module):
+    """
+    bottleneck part of VQ-VAE
+    - n_emb: number of embeddings
+    - e_dim: dimension of embedding
+    - commit_cost: commitment cost
+    """
+
+    def __init__(self, n_emb, e_dim, commit_cost):
+        super(VectorQuantizer, self).__init__()
+        self.n_emb = n_emb
+        self.e_dim = e_dim
+        self.commit_cost= commit_cost 
+
+        self.embedding = nn.Embedding(self.n_emb, self.e_dim)
+        self.embedding.weight.data.uniform_(-1.0 / self.n_emb, 1.0 / self.n_emb)
+
+    def forward(self, z): # inputs the output of the encoder network into one-hot vector
+        batch = 0
+        height = 2
+        width = 3
+        channel = 1
+        z = z.permute(batch, height, width, channel).contiguous()
+        z_flattened = z.view(-1, self.e_dim)
+
+        d = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + torch.sum(self.embedding.weight**2, dim=1) - 2 * \
+        torch.matmul(z_flattened, self.embedding.weight.t())
+        min_encoding_idx= torch.argmin(d, dim=1).unsqueeze(1) # find closest encoding
+        min_encodings = torch.zeros(min_encoding_idx.shape[0], self.n_emb).to(device)
+        min_encodings.scatter_(1, min_encoding_idx, 1)
+
+        z_q = torch.matmul(min_encodings, self.embedding.weight).view(z.shape) # quantized latent vectors
+        # calculate loss
+        loss = torch.mean((z_q.detach()-z)**2) + self.commit_cost* torch.mean((z_q - z.detach()) ** 2)
+
+        z_q = z + (z_q - z).detach()
+        e_mean = torch.mean(min_encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(e_mean * torch.log(e_mean + 1e-10)))
+        z_q = z_q.permute(batch, width, channel, height).contiguous()
+        return loss, z_q, perplexity, min_encodings,min_encoding_idx 
+

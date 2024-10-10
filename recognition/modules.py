10fb337d0938be6d9c617f064e6ccab7dd116ceb
Deb <deb@debian>
2024-10-10 13:19:23 +1000

    Added encoder, bottleneck, decoder and unet functions to modules.py. The encoder computes conv's and max pooling and saves the skip connections The bottleneck computes two convolutions with 1024 features The decoder apply upconv's, adds the skip connections and does convolutions The unet function groups each of these together and makes a model

---
+++ recognition/modules.py
@@ -0,0 +1,65 @@
+import dataset
+import modules
+from dataset import testImages, trainImages, validateImages, testSegImages, trainSegImages, validateSegImages
+
+import tensorflow
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
+from tensorflow.keras.models import Model
+
+
+# List of filters to be applied in the encoding and reverse in the decoding step 
+filterList = [64,128,256,512]
+
+# Function that does the encoding of the Unet by applying convolutions and max pooling to the given tensor at each filter level
+# Parameters: inputTensor, a tf tensor that will be encoded
+# Returns: (tensor, skipConnectionList), a tuple containing the resulting encoded tensor and a list of the skip connections at each step
+def encoder(inputTensor):
+    skipConnectionList = []
+    tensor = inputTensor
+    # Applies two convolutions and a max pool for each of the filters to the given input tensor 
+    for filter in filterList: 
+        # First two convolutions applied to the tensor
+        firstConv = Conv2D(filter, kernel_size = (3,3), padding = 'same', strides = 1, activation = 'relu')(tensor)
+        secondConv = Conv2D(filter, kernel_size = (3,3), padding = 'same', strides = 1, activation = 'relu')(firstConv)
+        # Adds the skip connecton to the list 
+        skipConnectionList.append(secondConv)
+        # Computes the max pool of the second convolution
+        tensor = MaxPooling2D(pool_size = (2,2), padding = 'same')(secondConv)
+    return tensor, skipConnectionList
+
+# Function that does the decoding of the Unet by applying an up convolution followed by concatting the tensor with the skip connection and then applying convolutions.
+# Parameters: inputTensor, a tf tensor that will be decoded
+# Returns: tensor, a decoded tensor 
+def decoder(skipConnectionList, inputTensor):
+    tensor = inputTensor
+    for filter in reversed(filterList):
+        # Compute the up convolution
+        upConv = Conv2DTranspose(filter, kernel_size = (2,2), padding = 'same', activation = 'relu', strides = 2)(tensor)
+        skipConnection = skipConnectionList.pop()
+        # Concatinate the skip connection with the tensor
+        concatTensor = concatenate([upConv, skipConnection])
+        # Apply two convolutions with the given filter
+        firstConv = Conv2D(filter, kernel_size = (3,3), padding = 'same', strides = 1, activation = 'relu')(concatTensor)
+        secondConv = Conv2D(filter, kernel_size = (3,3), padding = 'same', strides = 1, activation = 'relu')(firstConv)
+        tensor = secondConv
+    # Apply the final convolution with a sigmoid activation function
+    finalConv = Conv2D(1, kernel_size=(1, 1), padding='same', strides=1, activation='sigmoid')(tensor)
+    return finalConv
+
+# Function that applies the bottleneck of the unet. 
+# Parameters: inputTensor, a tf tensor that will be decoded
+# Returns: tensor, a tensor that has gone through two convolutions 
+def bottleneck(inputTensor):
+        firstConv = Conv2D(1024, kernel_size = (3,3), padding = 'same', strides = 1, activation = 'relu')(inputTensor)
+        secondConv = Conv2D(1024, kernel_size = (3,3), padding = 'same', strides = 1, activation = 'relu')(firstConv)
+        tensor = secondConv
+        return tensor
+
+# Function that Applies the encoder, bottleneck and decoder into one unet model
+# Returns a keras model of the unet
+def unet(): 
+    inputs = Input(shape = (256,128))
+    encodedResult, skipConnectionList = encoder(inputs)
+    bottleneckResult = bottleneck(encodedResult)
+    decodedResult = decoder(skipConnectionList, bottleneckResult)
+    return Model(inputs=[inputs], outputs=[decodedResult])
--- recognition/train.py
+++
@@ -1,3 +0,0 @@
-import dataset
-import modules
-from dataset import testImages, trainImages, validateImages, testSegImages, trainSegImages, validateSegImages

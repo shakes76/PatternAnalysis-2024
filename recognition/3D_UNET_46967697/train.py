45df88ac5f564a545b29581ceee5be5c7f16b152
Damian Bellew <damian.bellew@gmail.com>
2024-10-10 16:12:09 +1000

    Add model eval (test loss)

--- recognition/3D_UNET_46967697/train.py
+++ recognition/3D_UNET_46967697/train.py
@@ -44,4 +44,34 @@ for epoch in range(EPOCHS):
 torch.save(model.state_dict(), '3d_unet_model.pth')
 
 
+# Test the model
+model.eval()
+test_loss = 0.0
+dice_score = 0.0
 
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Prediction
+        outputs = model(images)
+
+        # Compute loss
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+        
+        # Compute dice score
+        #preds = torch.sigmoid(outputs)  
+        #preds = (preds > 0.5).float()  
+
+        #intersection = (preds * labels).sum() 
+        #dice = (2. * intersection + 1e-6) / (preds.sum() + labels.sum() + 1e-6)
+        #dice_score += dice.item()
+
+# Compute the average test loss and Dice score
+avg_test_loss = test_loss / len(test_loader)
+#avg_dice_score = dice_score / len(test_loader)
+
+print(f'Average Test Loss: {avg_test_loss}')
+#print(f'Average Dice Score: {avg_dice_score}')

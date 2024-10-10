e427ce87e2430a4c7fb32ada5765700f85be3e33
yanghan8458 <ZUST-yanghan@outlook.com>
2024-10-10 13:21:31 +1000

    Forward propagation in modules.py

--- recognition/2D U-net HAN YANG 49058194/modules.py
+++ recognition/2D U-net HAN YANG 49058194/modules.py
@@ -80,6 +80,7 @@ class UNet(nn.Module):
         self.up4 = Up(128, 64)
         self.outc = OutConv(64, n_classes)
 
+    # Forward propagation
     def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)

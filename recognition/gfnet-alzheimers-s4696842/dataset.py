667d3edcb688dbf7a45364bcf730feb5d9ed14f6
Preston Harrison <preston@prestonharrison.com>
2024-10-10 13:57:10 +1000

    Only apply transforms to train data

--- recognition/gfnet-alzheimers-s4696842/dataset.py
+++ recognition/gfnet-alzheimers-s4696842/dataset.py
@@ -6,11 +6,17 @@ from torch.utils.data import DataLoader
 ADNI_IMAGE_DIMENSIONS = (256, 240)
 
 
-def get_dataloader(root_dir, batch_size, shuffle):
-    transform = transforms.Compose(
+def get_dataloader(root_dir, batch_size, shuffle, transform):
+    dataset = datasets.ImageFolder(root=root_dir, transform=transform)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    return loader
+
+
+def get_train_test_dataloaders(root_dir, train_batch_size, test_batch_size):
+    train_transform = transforms.Compose(
         [
             transforms.Grayscale(),
-           transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+            transforms.RandomAffine(degrees=10, translate=(0.1, 0.1), scale=(0.9, 1.1)),
             transforms.RandomResizedCrop(size=(256, 240), scale=(0.9, 1.1)),
             transforms.ColorJitter(brightness=0.2, contrast=0.2),
             transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2.0)),
@@ -19,16 +25,24 @@ def get_dataloader(root_dir, batch_size, shuffle):
         ]
     )
 
-    dataset = datasets.ImageFolder(root=root_dir, transform=transform)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
-    return loader
+    test_transform = transforms.Compose(
+        [
+            transforms.Grayscale(),
+            transforms.Resize(ADNI_IMAGE_DIMENSIONS),
+            transforms.ToTensor(),
+        ]
+    )
 
-
-def get_train_test_dataloaders(root_dir, train_batch_size, test_batch_size):
     train_loader = get_dataloader(
-        path.join(root_dir, "train"), train_batch_size, shuffle=True
+        path.join(root_dir, "train"),
+        train_batch_size,
+        shuffle=True,
+        transform=train_transform,
     )
     test_loader = get_dataloader(
-        path.join(root_dir, "test"), test_batch_size, shuffle=False
+        path.join(root_dir, "test"),
+        test_batch_size,
+        shuffle=False,
+        transform=test_transform,
     )
     return train_loader, test_loader

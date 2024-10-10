6ab6d732b432c5e59f84992a60dfdddaa4799633
Deb <deb@debian>
2024-10-10 16:57:44 +1000

    Adding files to rangpur to test model. using 50 epochs and a batch size of 32

--- recognition/train.py
+++ recognition/train.py
@@ -2,9 +2,23 @@ from dataset import testImages, trainImages, validateImages, testSegImages, trai
 import modules
 
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.losses import BinaryCrossentropy, Dice
+
 
 # Set up the model from the modules file
 unetModel = modules.unet()
-unetModel.compile(optimizer = Adam(), loss = BinaryCrossentropy(), metrics=['accuracy'])
-unetModel.fit(trainImages, trainSegImages, epochs=3)
+unetModel.compile(optimizer = Adam(), loss = Dice(), metrics=['accuracy', Dice])
+
+# Run the model and save 
+trainResults = unetModel.fit(trainImages, trainSegImages, validation_data = (validateImages, validateSegImages), 
+                        batch_size = 32, epochs=50)
+
+# Evaluate the model using the test datasets
+testResults = unetModel.evaluate(testImages, testSegImages)
+
+print("Test loss: ", testResults[0])
+print("Test accuracy: ", testResults[1])
+print("Test dice: ", testResults[2])
+
+
+

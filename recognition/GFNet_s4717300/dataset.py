1e838de954d8c09f85101e1cc030c6043a6709db
BenjaminJorgensen <Benjamin@jorgensens.id.au>
2024-10-11 21:27:45 +1000

    feat: Implemented checkpoint loading functions

--- recognition/GFNet_s4717300/dataset.py
+++ recognition/GFNet_s4717300/dataset.py
@@ -35,6 +35,14 @@ class GFNetDataloader():
         self._mean /= self._total_images
         self._std /= self._total_images
 
+        val_trans = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Grayscale(num_output_channels=1),
+                transforms.Resize(240),
+                transforms.Pad((0, 8), fill=0),
+                transforms.Normalize(mean=self._mean, std=self._std)
+                ])
+
         if not img_size:
             _transform = transforms.Compose([
                 transforms.ToTensor(),
@@ -46,8 +54,8 @@ class GFNetDataloader():
                 transforms.RandomRotation(30),                  # Random rotation within ±30 degrees
                 transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Randomly change brightness/contrast
                 transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),  # Random translation
-                transforms.RandomCrop(256, padding=8, padding_mode='reflect')
-                # transforms.Normalize(mean=self._mean, std=self._std),  # Use computed mean and std
+                transforms.RandomCrop(256, padding=8, padding_mode='reflect'),
+                transforms.Normalize(mean=self._mean, std=self._std),  # Use computed mean and std
                 ])
             self.img_size = 256
         else:
@@ -64,9 +72,9 @@ class GFNetDataloader():
 
 
         train_images = datasets.ImageFolder(root='./AD_NC/train', transform=_transform)
-        test_images = datasets.ImageFolder(root='./AD_NC/test', transform=_transform)
+        test_images = datasets.ImageFolder(root='./AD_NC/test', transform=val_trans)
         # train_images = datasets.ImageFolder(root='/home/groups/comp3710/ADNI/AD_NC/train', transform=_transform)
-        # test_images = datasets.ImageFolder(root='/home/groups/comp3710/ADNI/AD_NC/test', transform=_transform)
+        # test_images = datasets.ImageFolder(root='/home/groups/comp3710/ADNI/AD_NC/test', transform=val_trans)
 
         self.train_loader = DataLoader(train_images, batch_size=self.batch_size, shuffle=True)
         self.test_loader = DataLoader(test_images, batch_size=self.batch_size, shuffle=False)
--- recognition/GFNet_s4717300/train.py
+++ recognition/GFNet_s4717300/train.py
@@ -9,24 +9,39 @@ import time
 from utils import bcolors
 from torch.optim.lr_scheduler import SequentialLR, LinearLR, CosineAnnealingLR
 import csv
+import argparse
 
+parser = argparse.ArgumentParser(description='Optional argument example')
+
+# Define an optional argument, e.g., '--myarg'
+parser.add_argument('--mpath', type=str, help='Model weights to load')
+parser.add_argument('--tag', type=str, help='tag to label the models')
+
+# Parse the arguments
+args = parser.parse_args()
+
+loaded_model = args.mpath
+if not args.tag:
+    print('No tag supplied')
+    exit(3)
+tag = args.tag
 
 # Setting up Coda
 device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 print(device)
 
 # hyper-parameters
-learning_rate = 0.0001
-weight_decay = 0.05
-dropout = 0.0
-drop_path = 0.1
+learning_rate = 0.001
+weight_decay = 0.1
+dropout = 0.3
+drop_path = 0.3
 
-batches = 64
-patch_size = 16
+batches = 24
+patch_size = 8
 embed_dim = 384
-depth = 12
-ff_ratio = 4.0
-epochs = 100
+depth = 8
+ff_ratio = 2
+epochs = 30
 warmup_epochs = 5
 
 ## Load data
@@ -48,8 +63,10 @@ img_shape = (channels, image_size, image_size)
 model = GFNet(img_size=image_size, patch_size=patch_size, in_chans=channels, num_classes=num_classes, embed_dim=embed_dim, depth=depth, ff_ratio=ff_ratio, dropout=dropout, drop_path_rate=drop_path)
 model.to(device)
 model.train()
+if loaded_model:
+    model.load_state_dict(torch.load(loaded_model, weights_only=True))
 
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.1).to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 total_step = len(training)
@@ -82,7 +99,9 @@ def evaluate_model(final=False):
             total += labels.size(0)
             count += 1
             correct += (predicted == labels).sum().item()
-            avg_loss += criterion(outputs, labels)
+            avg_loss += criterion(outputs, labels) 
+            if not final and count > 10:
+                break
         val_losses.append(avg_loss / count)
 
         accuracy = (100 * correct / total)
@@ -110,32 +129,23 @@ def train_model():
             loss.backward()
             optimizer.step()
         
-            if i % 100 == 0:
+            if i % 1 == 0:
                 print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.5f}" .format(epoch+1, epochs, i+1, total_step, loss.item()))
 
 
         losses.append(loss.item())
         result, v_loss = evaluate_model() 
         if epoch % 10 == 0:
-            torch.save(model.state_dict(), 'GFNET-e{}-{}.pth'.format(epoch, round(result, 4)))
+            torch.save(model.state_dict(), 'GFNET-e{}-{}-{}.pth'.format(epoch, round(result, 4), tag))
 
-        if abs(v_loss - loss.item()) > 0.35:
-            print('=======OVERFITTED-TERMINATED======')
-            torch.save(model.state_dict(), 'GFNET-{}.pth'.format(round(result, 4)))
-            # Save losses to a CSV file
-            with open('losses.csv', 'w', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(losses)
-
-            # Save acc_hist to a CSV file
-            with open('val_loss.csv', 'w', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(val_losses)
-            exit(2)
+            if abs(v_loss - loss.item()) > 0.35:
+                print('=======OVERFITTED-TERMINATED======')
+                exit(2)
 
         scheduler.step() 
     result, _ = evaluate_model(final=True) 
-    torch.save(model.state_dict(), 'GFNET-{}.pth'.format(round(result, 4)))
+    # torch.save(model.state_dict(), 'GFNET-{}.pth'.format(round(result, 4)))
+    torch.save(model.state_dict(), 'GFNET-{}-{}.pth'.format(round(result, 4), tag))
     # Save losses to a CSV file
     with open('losses.csv', 'w', newline='') as f:
         writer = csv.writer(f)

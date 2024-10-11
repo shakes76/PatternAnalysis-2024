112354d6272774bfcc77666c70dede594425506a
kirra <kirrafletcher@gmail.com>
2024-10-11 13:46:38 +1000

    Transferred the dataloaders into functions

--- dataset.py
+++ dataset.py
@@ -18,31 +18,37 @@ import cv2
 from torch.utils.data import Dataset
 import time
 import numpy as np
-import nibabel as nib
 from tqdm import tqdm
 import utils
 import glob
 
-device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-
-train_file_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_train"
-
-train_files = sorted(glob.glob(f"{train_file_path}/**.nii.gz", recursive = True))
-
-train_dataset = utils.load_data_2D(train_files[0:64])
 
 batch_size = 10
 
-train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+def get_dataloaders(batch_size): 
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
-real_batch = next(iter(train_dataloader))
+    train_file_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_train"
 
-fig, axes = plt.subplots(1, 10, figsize=(15, 15))
-axes = axes.flatten()
-plt.title("Test1")
-for i in range(10):
-    img = real_batch[i, :, :].cpu().numpy() 
-    axes[i].imshow(img, cmap='gray' if img.shape[-1] == 1 else None)
-    axes[i].axis('off')  
+    train_files = sorted(glob.glob(f"{train_file_path}/**.nii.gz", recursive = True))
 
-plt.savefig("./Project/test1.png")
+    train_dataset = utils.load_data_2D(train_files[0:64])
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    return train_dataloader
+
+def plot_from_dataloader(batch_size: int, dataloader):
+    real_batch = next(iter(dataloader))
+
+    fig, axes = plt.subplots(1, batch_size , figsize=(15, 15))
+    axes = axes.flatten()
+    plt.title("Test1")
+    for i in range(10):
+        img = real_batch[i, :, :].cpu().numpy() 
+        axes[i].imshow(img, cmap='gray' if img.shape[-1] == 1 else None)
+        axes[i].axis('off')  
+
+    plt.savefig("./Project/test1.png")
+
+train_dataloader = get_dataloaders(batch_size)
+plot_from_dataloader(batch_size, train_dataloader)

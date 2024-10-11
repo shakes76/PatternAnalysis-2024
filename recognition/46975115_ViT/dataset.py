b230fd358113da3a8de2fd83b1211503e336b60b
Elliot Lee <113224808+kunsi81@users.noreply.github.com>
2024-10-11 18:50:18 +1000

    removed reading/using metadata file from dataset.py, cleaned up train/test transform and dataloaders for only AD_NC data. Also added normalisation and resizing

--- recognition/46975115_ViT/dataset.py
+++ recognition/46975115_ViT/dataset.py
@@ -1,57 +1,44 @@
 import torch
-from torch.utils.data import Dataset
-from PIL import Image
-import os
-import json
-from sklearn.model_selection import train_test_split
-from torchvision import transforms
+from torch.utils.data import random_split, DataLoader
+from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
 
-class ADNC_Dataset(Dataset):
-    def __init__(self, meta_data_path, base_data_dir, transform=None, split='train', val_split=0.15):
-        self.transform = transform
-        self.split = split
-        self.base_data_dir = base_data_dir
-        
-        with open(meta_data_path, 'r') as f:
-            self.meta_data = json.load(f)
-        
-        self.image_paths = []
-        self.labels = []
-        
-        all_image_paths, all_labels = self.load_data()
+def get_dataloaders(base_data_dir, batch_size=16, val_split=0.15):
+    """
+    This function loads the datasets from the given base_data_dir, splitting the train folder 
+    into training and validation sets. The directory structure is assumed to be:
+    - base_data_dir/train/AD
+    - base_data_dir/train/NC
+    - base_data_dir/test/AD
+    - base_data_dir/test/NC
+    
+    :param base_data_dir: Directory containing train and test directories with AD and NC folders
+    :param batch_size: Batch size for data loaders
+    :param val_split: Percentage of train data used for validation
+    :return: train_loader, val_loader, test_loader
+    """
+    train_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    test_transforms = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-        if split == 'train':
-            self.image_paths, _, self.labels, _ = train_test_split(all_image_paths, all_labels, test_size=val_split, stratify=all_labels)
-        elif split == 'val':
-            _, self.image_paths, _, self.labels = train_test_split(all_image_paths, all_labels, test_size=val_split, stratify=all_labels)
-        else:
-            self.image_paths = all_image_paths
-            self.labels = all_labels
+    train_data = ImageFolder(root=f"{base_data_dir}/train", transform=train_transforms)
+    
+    val_size = int(len(train_data) * val_split)
+    train_size = len(train_data) - val_size
+    train_data, val_data = random_split(train_data, [train_size, val_size])
+    
+    test_data = ImageFolder(root=f"{base_data_dir}/test", transform=test_transforms)
+    
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
-    def load_data(self):
-        all_image_paths = []
-        all_labels = []
-        
-        split_dir = os.path.join(self.base_data_dir, 'train')  
-
-        for image_id, data in self.meta_data.items():
-            if 'train' in data['masked']:
-                img_path = os.path.join(split_dir, 'AD' if data['label'] == 0 else 'NC', data['masked'])
-                all_image_paths.append(img_path)
-                all_labels.append(data['label'])
-
-        return all_image_paths, all_labels
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
-        label = self.labels[idx]
-        
-        image = Image.open(img_path).convert('RGB')
-        
-        if self.transform:
-            image = self.transform(image)
-
-        return image, torch.tensor(label, dtype=torch.long)
+    return train_loader, val_loader, test_loader

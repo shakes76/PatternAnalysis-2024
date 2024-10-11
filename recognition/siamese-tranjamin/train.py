dc630e310626e0875112ee98b45faf72b8cbf501
tranjamin <benjamin.tran@outlook.com.au>
2024-10-11 22:45:28 +1000

    increase training time to improve performance

--- recognition/siamese-tranjamin/train.py
+++ recognition/siamese-tranjamin/train.py
@@ -5,13 +5,13 @@ import keras
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_addons as tfa
+import pandas as pd
 
 from Modules import NeuralNetwork
 
-epochs = 20
 batch_size = 64
 margin = 1  # Margin for contrastive loss.
-image_shape = (28, 28)
+image_shape = (64, 64)
 
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
     "datasets/balanced", 
@@ -39,7 +39,7 @@ dataset_val = tf.keras.preprocessing.image_dataset_from_directory(
     class_names=["positive", "negative"]
     )
 
-dataset = dataset.shuffle(10000).prefetch(tf.data.AUTOTUNE)
+dataset = dataset.prefetch(tf.data.AUTOTUNE)
 dataset_val = dataset.prefetch(tf.data.AUTOTUNE)
 
 base_model = NeuralNetwork.FunctionalNetwork()
@@ -47,24 +47,25 @@ base_model.add_generic_layer(tf.keras.layers.Input(shape=(*image_shape, 3)))
 base_model.add_batch_norm()
 base_model.add_conv2D_layer((5, 5), 4, activation="tanh")
 base_model.add_pooling2D_layer("average", (2,2))
+base_model.add_conv2D_layer((5, 5), 8, activation="tanh")
+base_model.add_pooling2D_layer("average", (2, 2))
 base_model.add_conv2D_layer((5, 5), 16, activation="tanh")
+base_model.add_pooling2D_layer("average", (2, 2))
+base_model.add_conv2D_layer((5, 5), 32, activation="tanh")
 base_model.add_pooling2D_layer("average", (2, 2))
 base_model.add_flatten_layer()
 base_model.add_batch_norm()
-base_model.add_dense_layer(10, activation="tanh")
+base_model.add_dense_layer(16, activation="tanh")
 base_model.generate_functional_model()
 
 base_model.set_loss_function(tfa.losses.TripletSemiHardLoss())
-def contrastive_accuracy(y_true, y_pred):
-    return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y_true, axis=-1), tf.argmax(y_pred, axis=-1)), tf.float32))
-
-base_model.add_metric(contrastive_accuracy)
 base_model.set_optimisation("adam")
 base_model.compile_functional_model()
 base_model.summary()
 
-base_model.set_epochs(epochs)
-base_model.fit_model_batches(dataset, None, verbose=1)
+base_model.set_epochs(60)
+base_model.set_early_stopping("val_loss", patience=20, min_delta=0.01)
+base_model.fit_model_batches(dataset, dataset_val, verbose=1)
 base_model.visualise_training()
 plt.show()
 
@@ -75,7 +76,8 @@ classifier_model.add_dense_layer(32)
 classifier_model.add_dense_layer(16)
 classifier_model.add_dense_layer(1, activation="sigmoid")
 classifier_model.set_loss_function(tf.keras.losses.BinaryCrossentropy())
-classifier_model.set_epochs(70)
+classifier_model.set_epochs(300)
+classifier_model.set_early_stopping("val_loss", patience=20, min_delta=0.01)
 classifier_model.set_batch_size(128)
 classifier_model.set_optimisation("adam")
 classifier_model.add_metric(["accuracy"])
@@ -86,13 +88,12 @@ classifier_model.enable_tensorboard()
 classifier_model.enable_wandb("balanced-melanoma-")
 classifier_model.enable_model_checkpoints("./checkpoints", save_best_only=True)
 
-dataset = dataset.shuffle(10000).prefetch(tf.data.AUTOTUNE)
+dataset = dataset.prefetch(tf.data.AUTOTUNE)
 dataset_val = dataset.prefetch(tf.data.AUTOTUNE)
 
 classifier_model.compile_model()
 
-classifier_model.set_validation_split_size(0.2)
-classifier_model.fit_model_batches(dataset, None, verbose=1)
+classifier_model.fit_model_batches(dataset, dataset_val, verbose=1)
 print("--- Testing Performance ---")
 classifier_model.model.evaluate(dataset_val)
 

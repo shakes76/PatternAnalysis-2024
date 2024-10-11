9bd23288cb811db42a5056b15e71cc3bb65d72b4
Jayke <s4699163@student.uq.edu.au>
2024-10-11 15:27:38 +1000

    Added functions to read and load nifti files

---
+++ recognition/dataset.py.py
@@ -0,0 +1,75 @@
+import numpy as np
+import nibabel as nib
+from tqdm import tqdm
+import glob
+
+def to_channels(arr:np.ndarray, dtype = np.uint8) -> np.ndarray:
+    channels = np.unique(arr)
+    res = np.zeros(arr.shape + (len(channels),), dtype = dtype)
+    for c in channels:
+        c = int(c)
+        res [... , c : c +1][ arr == c ] = 1
+
+    return res
+# load medical image functions
+def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np.float32,
+    getAffines = False, early_stop = False):
+    '''
+    Load medical image data from names , cases list provided into a list for each .
+
+    This function pre - allocates 4 D arrays for conv2d to avoid excessive memory usage .
+
+    normImage : bool(normalise the image 0.0 - 1.0)
+    early_stop : Stop loading pre-maturely , leaves arrays mostly empty , for quick loading and testing scripts .
+    '''
+    affines = []
+
+    # get fixed size
+    num = len (imageNames)
+    first_case = nib.load(imageNames[0]).get_fdata(caching="unchanged")
+    if len(first_case.shape) == 3:
+        first_case = first_case [: ,: ,0] # sometimes extra dims , remove
+    if categorical:
+        first_case = to_channels (first_case, dtype = dtype)
+        rows, cols, channels = first_case.shape
+        images = np.zeros((num, rows, cols, channels), dtype = dtype)
+    else:
+        rows, cols = first_case. shape
+        images = np.zeros((num, rows, cols), dtype = dtype)
+
+    for i, inName in enumerate (tqdm(imageNames)):
+        niftiImage = nib.load(inName)
+        inImage = niftiImage.get_fdata(caching ="unchanged") # read disk only
+        affine = niftiImage.affine
+        if len (inImage.shape ) == 3:
+            inImage = inImage[: ,: ,0] # sometimes extra dims in HipMRI_study data
+            inImage = inImage.astype(dtype)
+        if normImage:
+            # ~ inImage = inImage / np . linalg . norm ( inImage )
+            # ~ inImage = 255. * inImage / inImage . max ()
+            inImage = (inImage - inImage.mean()) / inImage.std()
+        if categorical:
+            inImage = to_channels(inImage, dtype = dtype)
+            images[i ,: ,: ,:] = inImage
+        else:
+            images[i ,: ,:] = inImage
+
+        affines.append(affine)
+        if i > 20 and early_stop:
+            break
+
+    if getAffines:
+        return images, affines
+    else:
+        return images
+    
+
+def load_train(path):
+    image_list = []
+    for filename in glob.glob(path + '/*.nii.gz'): 
+        image_list.append(filename)
+    print(image_list)
+    train_set = load_data_2D(image_list, normImage=True)
+    return train_set
+print(glob.glob("C:\\Users\\jbrag\\Dev\\PatternAnalysis-2024\\recognition\\test" + '/*.nii.gz' ))
+train_Y = load_train("C:\\Users\\jbrag\\Dev\\PatternAnalysis-2024\\recognition\\test")

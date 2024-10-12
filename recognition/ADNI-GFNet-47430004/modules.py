fd75768400e4d38afaa3b7cf86052b928e8d1f49
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-10-13 09:20:29 +1000

    Add Patchembed code from original GFNet code - tune later

--- recognition/ADNI-GFNet-47430004/modules.py
+++ recognition/ADNI-GFNet-47430004/modules.py
@@ -1,7 +1,7 @@
 import torch
 import math
 import torch.nn as nn
-from timm.models.layers import DropPath
+from timm.models.layers import DropPath, to_2tuple
 
 # Got inspiration from main_gfnet.py, gfnet.py files of the following github repo:
 # https://github.com/shakes76/GFNet
@@ -70,10 +70,23 @@ class Block(nn.Module):
 
 class PatchEmbed(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
-        pass
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
 
     def forward(self, x):
-        pass
+        B, C, H, W = x.shape
+        # FIXME look at relaxing size constraints
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        return x
 
 class GFNet(nn.Module):
     # Patch embed

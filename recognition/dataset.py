0ac7432ff1529840ebe90563f6a4a53d24034470
Jayke <s4699163@student.uq.edu.au>
2024-10-12 19:02:09 +1000

    Added loader for train, test and validate for segmentation. Scales images to be same size.

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -2,14 +2,17 @@ import numpy as np
 import nibabel as nib
 from tqdm import tqdm
 import glob
+from matplotlib import pyplot
+from matplotlib import image
+import tensorflow as tf 
+import skimage.transform as sk
 
 def to_channels(arr:np.ndarray, dtype = np.uint8) -> np.ndarray:
     channels = np.unique(arr)
     res = np.zeros(arr.shape + (len(channels),), dtype = dtype)
     for c in channels:
         c = int(c)
-        res [... , c : c +1][ arr == c ] = 1
-
+        res [... , c : c +1][arr == c] = 1
     return res
 # load medical image functions
 def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np.float32,
@@ -27,10 +30,11 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
     # get fixed size
     num = len (imageNames)
     first_case = nib.load(imageNames[0]).get_fdata(caching="unchanged")
+    first_case = sk.resize(first_case, (256,144), order=1, preserve_range=True)
     if len(first_case.shape) == 3:
         first_case = first_case [: ,: ,0] # sometimes extra dims , remove
     if categorical:
-        first_case = to_channels (first_case, dtype = dtype)
+        first_case = to_channels(first_case, dtype = dtype)
         rows, cols, channels = first_case.shape
         images = np.zeros((num, rows, cols, channels), dtype = dtype)
     else:
@@ -40,6 +44,7 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
     for i, inName in enumerate (tqdm(imageNames)):
         niftiImage = nib.load(inName)
         inImage = niftiImage.get_fdata(caching ="unchanged") # read disk only
+        inImage = sk.resize(inImage, (256,144), order=1, preserve_range=True)
         affine = niftiImage.affine
         if len (inImage.shape ) == 3:
             inImage = inImage[: ,: ,0] # sometimes extra dims in HipMRI_study data
@@ -49,7 +54,9 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
             # ~ inImage = 255. * inImage / inImage . max ()
             inImage = (inImage - inImage.mean()) / inImage.std()
         if categorical:
+            print(inImage.shape)
             inImage = to_channels(inImage, dtype = dtype)
+            print(inImage.shape)
             images[i ,: ,: ,:] = inImage
         else:
             images[i ,: ,:] = inImage
@@ -64,12 +71,20 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
         return images
     
 
-def load_train(path):
+def load(path, label=False):
     image_list = []
     for filename in glob.glob(path + '/*.nii.gz'): 
         image_list.append(filename)
-    print(image_list)
-    train_set = load_data_2D(image_list, normImage=True)
+    train_set = load_data_2D(image_list, normImage=True, categorical=label)
     return train_set
-print(glob.glob("C:\\Users\\jbrag\\Dev\\PatternAnalysis-2024\\recognition\\test" + '/*.nii.gz' ))
-train_Y = load_train("C:\\Users\\jbrag\\Dev\\PatternAnalysis-2024\\recognition\\test")
+
+
+X_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_"
+train_X = load(X_path + "train")
+validate_X = load(X_path + "validate")
+test_X = load(X_path + "test")
+
+seg_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_"
+train_Y = load(seg_path + "test", label=True)
+validate_Y = load(seg_path + "validate", label=True)
+test_Y = load(seg_path + "test", label=True)

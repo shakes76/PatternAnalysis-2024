cd19a028f3720f99017f44c44c6fb7d75d91dfba
GavinSaiun <gavin.sun56@gmail.com>
2024-10-12 16:24:21 +1000

    added GFNet and Patch Embedding

--- recognition/GFNet_Gavin_Sun/modules.py
+++ recognition/GFNet_Gavin_Sun/modules.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 from functools import partial
 from collections import OrderedDict
 import math
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+import torch.fft
 
 """
 FeedForward Network
 """
 class Mlp(nn.Module):
-    """
-    credit: https://www.youtube.com/watch?v=ovB0ddFtzzA&ab_channel=mildlyoverfitted
-    """
-
-    def __init__(self, in_features, hidden_features = None, out_features = None, drop=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
         self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = nn.GELU()
+        self.act = act_layer()
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
 
@@ -55,15 +55,126 @@ class GlobalFilter(nn.Module):
         return x
 
 class Block(nn.Module):
-    def __init__(self, dim, mlp_ratio=4., drop=0., norm_layer=nn.LayerNorm, h=14, w=8):
+    def __init__(self, dim, mlp_ratio=4., drop=0., drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, h=14, w=8):
         super().__init__()
         self.norm1 = norm_layer(dim)
         self.filter = GlobalFilter(dim, h=h, w=w)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
     def forward(self, x):
-        # Skip DropPath; directly add the residual connection
-        x = x + self.mlp(self.norm2(self.filter(self.norm1(x))))
+        x = x + self.drop_path(self.mlp(self.norm2(self.filter(self.norm1(x)))))
+        return x
+    
+
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        # FIXME look at relaxing size constraints
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        return x
+
+class GFNet(nn.Module):
+    
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 mlp_ratio=4., representation_size=None, uniform_drop=False,
+                 drop_rate=0., drop_path_rate=0., norm_layer=None, 
+                 dropcls=0):
+        """
+        Args:
+            img_size (int, tuple): input image size
+            patch_size (int, tuple): patch size
+            in_chans (int): number of input channels
+            num_classes (int): number of classes for classification head
+            embed_dim (int): embedding dimension
+            depth (int): depth of transformer
+            num_heads (int): number of attention heads
+            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
+            qkv_bias (bool): enable bias for qkv if True
+            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
+            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
+            drop_rate (float): dropout rate
+            attn_drop_rate (float): attention dropout rate
+            drop_path_rate (float): stochastic depth rate
+            hybrid_backbone (nn.Module): CNN backbone to use in-place of PatchEmbed module
+            norm_layer: (nn.Module): normalization layer
+        """
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+
+        self.patch_embed = PatchEmbed(
+                img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        h = img_size // patch_size
+        w = h // 2 + 1
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)] 
+
+        
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, mlp_ratio=mlp_ratio,
+                drop=drop_rate, drop_path=dpr[i], norm_layer=norm_layer, h=h, w=w)
+            for i in range(depth)])
+        
+        self.norm = norm_layer(embed_dim)
+
+        self.pre_logits = nn.Identity()
+
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+        self.final_dropout = nn.Identity()
+
+        trunc_normal_(self.pos_embed, std=.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x).mean(1)
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.final_dropout(x)
+        x = self.head(x)
         return x

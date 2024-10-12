266fc35ffca20e886121563c8e037052d0dcd63f
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-10-13 09:23:06 +1000

    Add actual GFNet code from original GFNet code - tune later

--- recognition/ADNI-GFNet-47430004/modules.py
+++ recognition/ADNI-GFNet-47430004/modules.py
@@ -1,7 +1,9 @@
 import torch
 import math
 import torch.nn as nn
-from timm.models.layers import DropPath, to_2tuple
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+from functools import partial
+from collections import OrderedDict
 
 # Got inspiration from main_gfnet.py, gfnet.py files of the following github repo:
 # https://github.com/shakes76/GFNet
@@ -89,11 +91,103 @@ class PatchEmbed(nn.Module):
         return x
 
 class GFNet(nn.Module):
-    # Patch embed
-    pass
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 mlp_ratio=4., representation_size=None, uniform_drop=False,
+                 drop_rate=0., drop_path_rate=0., norm_layer=None, 
+                 dropcls=0):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+
+        self.patch_embed = PatchEmbed(
+                img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        h = img_size // patch_size
+        w = h // 2 + 1
+
+        if uniform_drop:
+            print('using uniform droppath with expect rate', drop_path_rate)
+            dpr = [drop_path_rate for _ in range(depth)]  # stochastic depth decay rule
+        else:
+            print('using linear droppath with expect rate', drop_path_rate * 0.5)
+            dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        # dpr = [drop_path_rate for _ in range(depth)]  # stochastic depth decay rule
+        
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, mlp_ratio=mlp_ratio,
+                drop=drop_rate, drop_path=dpr[i], norm_layer=norm_layer, h=h, w=w)
+            for i in range(depth)])
+        
+        self.norm = norm_layer(embed_dim)
+
+        # Representation layer
+        if representation_size:
+            self.num_features = representation_size
+            self.pre_logits = nn.Sequential(OrderedDict([
+                ('fc', nn.Linear(embed_dim, representation_size)),
+                ('act', nn.Tanh())
+            ]))
+        else:
+            self.pre_logits = nn.Identity()
+
+        # Classifier head
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+        if dropcls > 0:
+            print('dropout %.2f before classifier' % dropcls)
+            self.final_dropout = nn.Dropout(p=dropcls)
+        else:
+            self.final_dropout = nn.Identity()
+
+        trunc_normal_(self.pos_embed, std=.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x).mean(1)
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.final_dropout(x)
+        x = self.head(x)
+        return x
 
 def main():
-    print("Main of Modules")
+    print("Main of Modules - modules compiles/runs")
 
 if __name__ == '__main__':
     main()

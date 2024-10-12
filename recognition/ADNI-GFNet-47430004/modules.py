6401b9db7428ac6cb50807de074b8238553bde6d
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-10-13 09:18:13 +1000

    Add globalfilter code from original GFNet code - tune later

--- recognition/ADNI-GFNet-47430004/modules.py
+++ recognition/ADNI-GFNet-47430004/modules.py
@@ -1,4 +1,5 @@
 import torch
+import math
 import torch.nn as nn
 
 # Got inspiration from main_gfnet.py, gfnet.py files of the following github repo:
@@ -27,10 +28,30 @@ class mlp(nn.Module):
 
 class GlobalFilter(nn.Module):
     def __init__(self, dim, h=14, w=8):
-        pass
+        super().__init__()
+        self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02)
+        self.w = w
+        self.h = h
 
     def forward(self, x, spatial_size=None):
-        pass
+        B, N, C = x.shape
+        if spatial_size is None:
+            a = b = int(math.sqrt(N))
+        else:
+            a, b = spatial_size
+
+        x = x.view(B, a, b, C)
+
+        x = x.to(torch.float32)
+
+        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
+        weight = torch.view_as_complex(self.complex_weight)
+        x = x * weight
+        x = torch.fft.irfft2(x, s=(a, b), dim=(1, 2), norm='ortho')
+
+        x = x.reshape(B, N, C)
+
+        return x
 
 class Block(nn.Module):
     def __init__(self, dim, mlp_ratio=4., drop=0., drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, h=14, w=8):

ee61d12760156430ad0eb73b6a6d013081fd33b2
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-10-13 09:17:48 +1000

    Add mlp code from original GFNet code - tune later

--- recognition/ADNI-GFNet-47430004/modules.py
+++ recognition/ADNI-GFNet-47430004/modules.py
@@ -1,14 +1,29 @@
 import torch
 import torch.nn as nn
 
-# Got inspiration from main_gfnet.py, gfnet.py files of the github repo:
+# Got inspiration from main_gfnet.py, gfnet.py files of the following github repo:
 # https://github.com/shakes76/GFNet
 
 # Hyperparameters
 hyperparameter_1 = None
 
 class mlp(nn.Module):
-    pass
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
 
 class GlobalFilter(nn.Module):
     def __init__(self, dim, h=14, w=8):

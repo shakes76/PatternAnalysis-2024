c203cfca52d151967c72046d84ddbc09c188f0a4
Elliot Lee <113224808+kunsi81@users.noreply.github.com>
2024-10-13 03:45:31 +1000

    test acc 0.4956, revert back

--- recognition/46975115_ViT/dataset.py
+++ recognition/46975115_ViT/dataset.py
@@ -18,9 +18,7 @@ def get_dataloaders(base_data_dir, batch_size=16, val_split=0.15):
     :return: train_loader, val_loader, test_loader
     """
     train_transforms = transforms.Compose([
-        transforms.RandomResizedCrop(224),         
-        transforms.RandomHorizontalFlip(),           
-        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2), 
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])

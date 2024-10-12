eb28b7653fb3bcecc1496708d789d56eb49eeb13
firefried-fries <102931771+firefried-fries@users.noreply.github.com>
2024-10-13 02:18:47 +1000

    Defined Discriminator

--- recognition/StyleGan2_47903151/modules.py
+++ recognition/StyleGan2_47903151/modules.py
@@ -191,3 +191,69 @@ class Conv2dWeightModulate(nn.Module):
         x = F.conv2d(x, weights, padding=self.padding, groups=b)
 
         return x.reshape(-1, self.out_features, h, w)
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, log_resolution, n_features = 64, max_features = 256):
+
+        super().__init__()
+
+        features = [min(max_features, n_features * (2 ** i)) for i in range(log_resolution - 1)]
+
+        self.from_rgb = nn.Sequential(
+            EqualizedConv2d(3, n_features, 1),
+            nn.LeakyReLU(0.2, True),
+        )
+        n_blocks = len(features) - 1
+        blocks = [DiscriminatorBlock(features[i], features[i + 1]) for i in range(n_blocks)]
+        self.blocks = nn.Sequential(*blocks)
+
+        final_features = features[-1] + 1
+        self.conv = EqualizedConv2d(final_features, final_features, 3)
+        self.final = EqualizedLinear(2 * 2 * final_features, 1)
+
+    def minibatch_std(self, x):
+        batch_statistics = (
+            torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
+        )
+        return torch.cat([x, batch_statistics], dim=1)
+
+    def forward(self, x):
+
+        x = self.from_rgb(x)
+        x = self.blocks(x)
+
+        x = self.minibatch_std(x)
+        x = self.conv(x)
+        x = x.reshape(x.shape[0], -1)
+        return self.final(x)
+
+
+class DiscriminatorBlock(nn.Module):
+
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.residual = nn.Sequential(nn.AvgPool2d(kernel_size=2, stride=2), # down sampling using avg pool
+                                      EqualizedConv2d(in_features, out_features, kernel_size=1))
+
+        self.block = nn.Sequential(
+            EqualizedConv2d(in_features, in_features, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2, True),
+            EqualizedConv2d(in_features, out_features, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2, True),
+        )
+
+        self.down_sample = nn.AvgPool2d(
+            kernel_size=2, stride=2
+        )  # down sampling using avg pool
+
+        self.scale = 1 / sqrt(2)
+
+    def forward(self, x):
+        residual = self.residual(x)
+
+        x = self.block(x)
+        x = self.down_sample(x)
+
+        return (x + residual) * self.scale

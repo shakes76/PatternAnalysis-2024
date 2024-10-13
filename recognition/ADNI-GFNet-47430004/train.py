b0236fa1976e6ad1469ed622aa8ff8725cc376dc
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-10-13 10:23:31 +1000

    Original uses CrossEntropyLoss when args is none - assume this is the case, scrap DistillationLoss

--- recognition/ADNI-GFNet-47430004/train.py
+++ recognition/ADNI-GFNet-47430004/train.py
@@ -1,58 +1,13 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
+from torch.nn import functional as F
 from dataset import get_dataloaders
 import matplotlib.pyplot as plt
 
 # Got inspiration from engine.py file of the following github repo:
 # https://github.com/shakes76/GFNet
 # And my train/evaluate code from the brain GAN code.
-
-# This class has been taken from the github repo mentioned above, and
-# adjusted to fit my purposes.
-class DistillationLoss(torch.nn.Module):
-    def __init__(self, base_criterion: torch.nn.Module, teacher_model: torch.nn.Module,
-                 distillation_type: str, alpha: float, tau: float):
-        super().__init__()
-        self.base_criterion = base_criterion
-        self.teacher_model = teacher_model
-        assert distillation_type in ['none', 'soft', 'hard']
-        self.distillation_type = distillation_type
-        self.alpha = alpha
-        self.tau = tau
-
-    def forward(self, inputs, outputs, labels):
-        outputs_kd = None
-        if not isinstance(outputs, torch.Tensor):
-            # assume that the model outputs a tuple of [outputs, outputs_kd]
-            outputs, outputs_kd = outputs
-        base_loss = self.base_criterion(outputs, labels)
-        if self.distillation_type == 'none':
-            return base_loss
-
-        if outputs_kd is None:
-            raise ValueError("When knowledge distillation is enabled, the model is "
-                             "expected to return a Tuple[Tensor, Tensor] with the output of the "
-                             "class_token and the dist_token")
-        # don't backprop throught the teacher
-        with torch.no_grad():
-            teacher_outputs = self.teacher_model(inputs)
-
-        if self.distillation_type == 'soft':
-            T = self.tau
-            # taken from https://github.com/peterliht/knowledge-distillation-pytorch/blob/master/model/net.py#L100
-            # with slight modifications
-            distillation_loss = F.kl_div(
-                F.log_softmax(outputs_kd / T, dim=1),
-                F.log_softmax(teacher_outputs / T, dim=1),
-                reduction='sum',
-                log_target=True
-            ) * (T * T) / outputs_kd.numel()
-        elif self.distillation_type == 'hard':
-            distillation_loss = F.cross_entropy(outputs_kd, teacher_outputs.argmax(dim=1))
-
-        loss = base_loss * (1 - self.alpha) + distillation_loss * self.alpha
-        return loss
 
 # def train_model(model, train_loader, val_loader, num_epochs=25):
 #     criterion = nn.CrossEntropyLoss()

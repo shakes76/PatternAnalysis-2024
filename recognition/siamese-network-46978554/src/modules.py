12b58015fd634cbe8e4d1ceb48f5c83752dbcbb4
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-10-13 10:25:46 +1000

    feat: add custom MajorityClassifier

--- recognition/siamese-network-46978554/src/modules.py
+++ recognition/siamese-network-46978554/src/modules.py
@@ -43,3 +43,58 @@ class SiameseNetwork(nn.Module):
         """Forward pass through the siamese network for a pair of inputs"""
         return self.net(x1), self.net(x2)
 
+
+class MajorityClassifier:
+    """
+    A classifier that compares the similarity of unseen points to a set of reference
+    images (via pairwise distance) and picks the class that the majority of reference
+    images are similar to.
+    """
+
+    def __init__(self, margin=1.0):
+        """
+        Args:
+            margin: The margin as in contrastive loss. Used to determine whether two
+              images are considered a similar pair or not.
+        """
+
+        def threshold(x1, x2):
+            dist = F.pairwise_distance(x1, x2)
+            return (dist >= margin).float()
+
+        self.threshold = threshold
+
+    def fit(self, X, y):
+        """
+        Saves X and y as the reference embeddings and reference labels, respectively.
+        """
+        self.ref_embeddings = X
+        self.ref_targets = y
+
+    def predict_proba(self, X):
+        """
+        Returns the estimated probability of belonging to class 1 (i.e. malignant) for
+        X, based on the reference set.
+        """
+        batch_size = X.shape[0]
+
+        preds = []
+        for ref_embedding, ref_target in zip(self.ref_embeddings, self.ref_targets):
+            ref_embedding = ref_embedding.repeat(batch_size, 1)
+            ref_target = ref_target.repeat(batch_size)
+
+            pair_pred = self.threshold(ref_embedding, X)
+            pred = torch.logical_xor(pair_pred, ref_target).float()
+            preds.append(pred)
+
+        # Stack predictions so that the dimensions are [batch_size, num_ref_imgs]
+        preds = torch.stack(preds, dim=1)
+
+        return preds.mean(dim=1)
+
+    def predict(self, X):
+        """
+        Returns the estimated class for X ("hard" classification), based on the
+        reference set.
+        """
+        return (self.predict_proba(X) >= 0.5).float()

3c6477631aa32f977924e3b6df8e585a550847c1
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-10-13 10:26:33 +1000

    feat,refactor: add TSNE embeddings plot, remove old contrastive loss code

--- recognition/siamese-network-46978554/src/util.py
+++ recognition/siamese-network-46978554/src/util.py
@@ -1,40 +1,19 @@
 from pathlib import Path
 
-import torch
-import torch.nn.functional as F
-
 # Dataset directory
 # We use a downsized version of the ISIC 2020 dataset:
 # https://www.kaggle.com/datasets/nischaydnk/isic-2020-jpg-256x256-resized/data
 DATA_DIR = Path(__file__).parent.parent / "data"
 
 
-def contrastive_loss(margin):
-    """
-    REF: https://www.sciencedirect.com/topics/computer-science/contrastive-loss
-    """
-
-    def f(x1, x2, y):
-        dist = F.pairwise_distance(x1, x2)
-        dist_sq = torch.pow(dist, 2)
-
-        loss = (1 - y) * dist_sq + y * torch.pow(torch.clamp(margin - dist, min=0.0), 2)
-        loss = torch.mean(loss / 2.0, dim=0)
-
-        return loss
-
-    return f
-
-
-def contrastive_loss_threshold(margin):
-    def f(x1, x2):
-        dist = F.pairwise_distance(x1, x2)
-        return (dist >= margin).float()
-
-    return f
-
-
 def plot_pca_embeddings(embeddings, targets):
+    """
+    Plots the first two principal components of the given embeddings.
+
+    Args:
+        embeddings: Model output embeddings.
+        targets: Labels for each embedding. Used for colouring the embedded points.
+    """
     import matplotlib.pyplot as plt
     from sklearn.decomposition import PCA
 
@@ -48,6 +27,32 @@ def plot_pca_embeddings(embeddings, targets):
     plt.scatter(comp1[targets == 1], comp2[targets == 1], label="malignant")
     plt.xlabel("PC Direction 1")
     plt.ylabel("PC Direction 2")
+    plt.title("Model Embeddings (Test Set) in Principal Component Space")
+    plt.legend()
+    plt.show()
+
+
+def plot_tsne_embeddings(embeddings, targets):
+    """
+    Plots the first two TSNE components of the given embeddings.
+
+    Args:
+        embeddings: Model output embeddings.
+        targets: Labels for each embedding. Used for colouring the embedded points.
+    """
+    import matplotlib.pyplot as plt
+    from sklearn.manifold import TSNE
+
+    tsne = TSNE(n_components=2)
+    embeddings_tsne = tsne.fit_transform(embeddings)
+
+    comp1 = embeddings_tsne[:, 0]
+    comp2 = embeddings_tsne[:, 1]
+
+    plt.scatter(comp1[targets == 0], comp2[targets == 0], label="benign")
+    plt.scatter(comp1[targets == 1], comp2[targets == 1], label="malignant")
+    plt.xlabel("TSNE Direction 1")
+    plt.ylabel("TSNE Direction 2")
     plt.title("Model Embeddings (Test Set) in Principal Component Space")
     plt.legend()
     plt.show()

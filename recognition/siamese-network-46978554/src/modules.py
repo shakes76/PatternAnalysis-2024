6a1545c9bf83d359ceee9ae089fba607f4685df9
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-10-13 10:21:57 +1000

    refactor: add documentation to modules, change default forward pass to a single-input pass

--- recognition/siamese-network-46978554/src/modules.py
+++ recognition/siamese-network-46978554/src/modules.py
@@ -1,11 +1,19 @@
 """Model components"""
 
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torchvision.models import ResNet50_Weights, resnet50
 
 
 class SiameseNetwork(nn.Module):
+    """A Siamese network with a ResNet backbone."""
+
     def __init__(self, pretrained=False):
+        """
+        Args:
+            pretrained: Whether to use a ResNet with pretrained ImageNet weights.
+        """
         super(SiameseNetwork, self).__init__()
 
         # Use a ResNet backbone without the last FC layer (we'll define our own)
@@ -22,15 +30,16 @@ class SiameseNetwork(nn.Module):
             nn.Linear(in_features=256, out_features=64, bias=True)
         )
 
+        # Freeze ResNet layers if we're using pretrained weights
         if pretrained:
             for param in self.net[:-3].parameters():
                 param.requires_grad = False
 
-    def forward_one(self, x):
+    def forward(self, x):
+        """Forward pass through the siamese network"""
         return self.net(x)
 
-    def forward(self, x1, x2):
-        out1 = self.net(x1)
-        out2 = self.net(x2)
+    def forward_pair(self, x1, x2):
+        """Forward pass through the siamese network for a pair of inputs"""
+        return self.net(x1), self.net(x2)
 
-        return out1, out2

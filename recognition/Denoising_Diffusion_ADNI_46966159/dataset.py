ce7251e00a05e67f2ef5a5649844438bde969771
Luke O <luke.oakden02@outlook.com>
2024-10-14 16:08:41 +1000

    Change file structure for more accurate naming

--- recognition/Denoising_Diffusion_ADNI_46966159/dataset.py
+++ recognition/Denoising_Diffusion_ADNI_46966159/dataset.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # specify directory of data
 # dataroot = "/home/groups/comp3710/ADNI"
-dataroot = "/home/lgmoak/Nextcloud/University/Courses/COMP3710/Assessment/PatternAnalysis-2024/recognition/ADNI_46966159/ADNI"
+dataroot = "/home/lgmoak/Nextcloud/University/Courses/COMP3710/Assessment/PatternAnalysis-2024/recognition/Denoising_Diffusion_ADNI_46966159/ADNI"
 
 batch_size = 28
 image_size = 64

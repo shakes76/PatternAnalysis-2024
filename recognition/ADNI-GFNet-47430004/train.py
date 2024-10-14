6e9121da3d50e37e8b771b8464525c7c1c895f7e
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-10-14 10:55:21 +1000

    Use correct reference and use code from original

--- recognition/ADNI-GFNet-47430004/train.py
+++ recognition/ADNI-GFNet-47430004/train.py
@@ -1,87 +1,98 @@
+"""
+Got inspiration from engine.py file of the following github repo:
+https://github.com/shakes76/GFNet
+And my train/evaluate code from the brain GAN code.
+"""
+
+import math
+import sys
+from typing import Iterable, Optional
+
 import torch
-import torch.optim as optim
-import torch.nn as nn
-from torch.nn import functional as F
-from dataset import get_dataloaders
-import matplotlib.pyplot as plt
 
-# Got inspiration from engine.py file of the following github repo:
-# https://github.com/shakes76/GFNet
-# And my train/evaluate code from the brain GAN code.
+from timm.data import Mixup
+from timm.utils import accuracy, ModelEma
 
-# def train_model(model, train_loader, val_loader, num_epochs=25):
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = optim.Adam(model.parameters(), lr=0.001)
+import utils
+
+import random
+
+
+def train_one_epoch(model: torch.nn.Module, criterion,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None,
+                    set_training_mode=True):
+    model.train(set_training_mode)
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 10
+
+    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
+        samples = samples.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
+
+        if mixup_fn is not None:
+            samples, targets = mixup_fn(samples, targets)
+
+        with torch.cuda.amp.autocast():
+            outputs = model(samples)
+            loss = criterion(samples, outputs, targets)
+
+        loss_value = loss.item()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        optimizer.zero_grad()
+
+        # this attribute is added by timm on one optimizer (adahessian)
+        is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                        parameters=model.parameters(), create_graph=is_second_order)
     
-#     train_loss, val_loss, train_acc, val_acc = [], [], [], []
+        torch.cuda.synchronize()
+        if model_ema is not None:
+            model_ema.update(model)
 
-#     for epoch in range(num_epochs):
-#         model.train()
-#         running_loss = 0.0
-#         correct = 0
-#         total = 0
-        
-#         for inputs, labels in train_loader:
-#             optimizer.zero_grad()
-#             outputs = model(inputs)
-#             loss = criterion(outputs, labels)
-#             loss.backward()
-#             optimizer.step()
-            
-#             running_loss += loss.item()
-#             _, predicted = torch.max(outputs, 1)
-#             total += labels.size(0)
-#             correct += (predicted == labels).sum().item()
-        
-#         epoch_loss = running_loss / len(train_loader)
-#         epoch_acc = correct / total
-        
-#         train_loss.append(epoch_loss)
-#         train_acc.append(epoch_acc)
-        
-#         # Validation step (similar to training loop but without optimizer step)
-#         val_epoch_loss, val_epoch_acc = validate_model(model, val_loader, criterion)
-#         val_loss.append(val_epoch_loss)
-#         val_acc.append(val_epoch_acc)
-        
-#         print(f"Epoch: [{epoch+1}/{num_epochs}], Loss: {epoch_loss}, Accuracy: {epoch_acc}")
+        metric_logger.update(loss=loss_value)
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
-#     plot_metrics(train_loss, val_loss, train_acc, val_acc)
-#     torch.save(model.state_dict(), "gfnet_model.pth")
 
-# def validate_model(model, val_loader, criterion):
-#     model.eval()
-#     val_loss = 0.0
-#     correct = 0
-#     total = 0
-#     with torch.no_grad():
-#         for inputs, labels in val_loader:
-#             outputs = model(inputs)
-#             loss = criterion(outputs, labels)
-#             val_loss += loss.item()
-#             _, predicted = torch.max(outputs, 1)
-#             total += labels.size(0)
-#             correct += (predicted == labels).sum().item()
-    
-#     avg_loss = val_loss / len(val_loader)
-#     accuracy = correct / total
-#     return avg_loss, accuracy
+@torch.no_grad()
+def evaluate(data_loader, model, device):
+    criterion = torch.nn.CrossEntropyLoss()
 
-# def plot_metrics(train_loss, val_loss, train_acc, val_acc):
-#     plt.figure(figsize=(10, 4))
-    
-#     plt.subplot(1, 2, 1)
-#     plt.plot(train_loss, label='Train Loss')
-#     plt.plot(val_loss, label='Validation Loss')
-#     plt.title('Loss Over Time')
-#     plt.legend()
-    
-#     plt.subplot(1, 2, 2)
-#     plt.plot(train_acc, label='Train Accuracy')
-#     plt.plot(val_acc, label='Validation Accuracy')
-#     plt.title('Accuracy Over Time')
-#     plt.legend()
-    
-#     plt.show()
-#     plt.savefig("/home/Student/s4743000/COMP3710/PatternAnalysis-2024/recognition/ADNI-GFNet-47430004/test/train/test_train", bbox_inches='tight', pad_inches=0)
-#     plt.close()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+
+    for images, target in metric_logger.log_every(data_loader, 10, header):
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+
+        # compute output
+        with torch.cuda.amp.autocast():
+            output = model(images)
+            loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}

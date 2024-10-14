abf5ff39e8d9468cb3c0f79cf2b03cdbc93b25c2
AlexPitman04 <pitmanalex04@gmail.com>
2024-10-14 23:48:22 +1000

    [Modules] Implements UNet2D architecture.

--- recognition/UNet2D_47443349/modules.py
+++ recognition/UNet2D_47443349/modules.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class Block(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
@@ -18,36 +19,72 @@ class Block(nn.Module):
     def forward(self, x):
         return self.conv(x)
 	
+
 class Encoder(nn.Module):
     def __init__(self, in_channels, feature_sizes):
         """
         feature_sizes is a list with the number of channels/filters for each layer
         """
         super().__init__()
-        layers = [] # Each layer of the Encoder is a Block with a downsample
+        self.operations = []
+        # Each layer of the Encoder is a Block followed by a downsample
         for feature_size in feature_sizes:
-            layers.append(Block(in_channels, feature_size))
-            layers.append(nn.MaxPool2d(kernel_size=2, stride=2))  # Downsample
+            self.operations.append(Block(in_channels, feature_size))
+            self.operations.append(nn.MaxPool2d(kernel_size=2, stride=2))  # Downsample
             in_channels = feature_size # Current feature_size will be the in_channels for next layer
-        self.encoder = nn.Sequential(*layers)
+        self.encoder = nn.Sequential(*self.operations)
 
     def forward(self, x):
         return self.encoder(x)
 
+
 class Decoder(nn.Module):
     def __init__(self, feature_sizes):
         super().__init__()
-        layers = []
+        self.operations = []
         # Reverse through the feature_sizes used for Encoder
+        # Each layer of the Decoder is an upsample followed by a Block
         for i in range(len(feature_sizes) - 1, 0, -1):
-            layers.append(nn.ConvTranspose2d(feature_sizes[i], feature_sizes[i-1], kernel_size=2, stride=2)) # Upsample
-            layers.append(Block(feature_sizes[i], feature_sizes[i-1]))
-        self.decoder = nn.Sequential(*layers)
+            self.operations.append(nn.ConvTranspose2d(feature_sizes[i], feature_sizes[i-1], kernel_size=2, stride=2)) # Upsample
+            self.operations.append(Block(feature_sizes[i], feature_sizes[i-1]))
+        self.decoder = nn.Sequential(*self.operations)
     
     def forward(self, x):
         return self.decoder(x)
 
 
+class UNet2D(nn.Module):
+    def __init__(self, in_channels=3, out_channels=6, feature_sizes=[64, 128, 256], channel_dim=3):
+        super().__init__()
+        self.channel_dim = channel_dim # Dimension for skip connection concatenation
+        self.encoder = Encoder(in_channels, feature_sizes)
+        self.bottleneck = nn.Sequential(
+            Block(feature_sizes[-1], feature_sizes[-1] * 2),
+            nn.ConvTranspose2d(feature_sizes[-1] * 2, feature_sizes[-1], kernel_size=2, stride=2)
+        )
+        self.decoder = Decoder(feature_sizes)
+        self.segmentation = nn.Conv2d(feature_sizes[0], out_channels, kernel_size=1)
+        
+    def forward(self, x):
+        # Encoder
+        enc_outputs = []
+        for i, operation in enumerate(self.encoder.operations):
+            x = operation(x)
+            # Save output of a block (i%2 == 0 in operations) for skip connections
+            if i%2 == 0:
+                enc_outputs.append(x)
 
-    
-	
+        # Bottleneck
+        x = self.bottleneck(x)
+        
+        # Decoder
+        for i, operation in enumerate(self.decoder.layers):
+            x = operation(x)
+            # Concatenate with corresponding encoder output (skip connection) after upsample (i%2 == 0 in operations)
+            if i%2 == 0:
+                enc_output = enc_outputs[-(i/2)-1]
+                x = torch.cat((x, enc_output), dim=self.channel_dim)
+
+        # Output raw logits
+        return self.segmentation(x)
+

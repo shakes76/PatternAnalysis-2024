0a207d9f4b2696f196403f22a9b99c4bf84668ec
dcollie87 <dcollie87@hotmail.com>
2024-10-15 09:06:35 +1000

    same

--- recognition/s4635974_VQVAE/data_visualisation.py
+++ recognition/s4635974_VQVAE/data_visualisation.py
@@ -30,7 +30,7 @@ epochs = range(1, epochs)
 plt.figure(figsize=(12, 6))
 plt.plot(epochs, training_output_loss, label="Training Output Loss")
 plt.plot(epochs, validation_output_loss, label="Validation Output Loss")
-plt.ylim(0, 0.01)  # Set the y-axis limits between 0 and 1
+plt.ylim(0, 0.1)  # Set the y-axis limits between 0 and 1
 plt.xlabel('Epoch')
 plt.ylabel('Training Output Loss')
 plt.title('Training Output Loss over Epochs')
@@ -61,17 +61,17 @@ plt.savefig(save_path_vq)
 plt.close()  # Close the plot to prevent displaying
 
 
-# # Plot SSIM over epochs
-# plt.figure(figsize=(12, 6))
-# plt.plot(epochs, ssim, label="SSIM")
-# # plt.ylim(0, 1)  # SSIM values typically range between 0 and 1
-# plt.xlabel('Epoch')
-# plt.ylabel('SSIM')
-# plt.title('SSIM over Epochs')
-# plt.legend()
-# plt.grid(True)
+# Plot SSIM over epochs
+plt.figure(figsize=(12, 6))
+plt.plot(epochs, ssim, label="SSIM")
+# plt.ylim(0, 1)  # SSIM values typically range between 0 and 1
+plt.xlabel('Epoch')
+plt.ylabel('SSIM')
+plt.title('SSIM over Epochs')
+plt.legend()
+plt.grid(True)
 
-# # Save the SSIM figure
-# save_path_ssim = os.path.join(save_dir, 'ssim.png')
-# plt.savefig(save_path_ssim)
-# plt.close()  # Close the plot to prevent displaying
+# Save the SSIM figure
+save_path_ssim = os.path.join(save_dir, 'ssim.png')
+plt.savefig(save_path_ssim)
+plt.close()  # Close the plot to prevent displaying

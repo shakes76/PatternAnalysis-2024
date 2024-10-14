f930c06fbc71d28b1ae66fbcd2709471e99e9cca
DeadmanJ <jdeadman02@gmail.com>
2024-10-14 10:24:29 +1000

    Added importing of the model to be used

---
+++ recognition/siamese-classifier-47044232/modules.py
@@ -0,0 +1,8 @@
+"""
+File that contains the models that can be used by the classifier.
+The currently supported model is ResNet50.
+
+Made by Joshua Deadman
+"""
+
+from torchvision.models import resnet50

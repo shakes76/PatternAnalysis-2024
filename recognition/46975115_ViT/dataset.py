923f27057d61a3d0216abef86f4fc4853404b193
Elliot Lee <113224808+kunsi81@users.noreply.github.com>
2024-10-14 18:12:14 +1000

    data augmentation of training data using rotation, vertical flip and gaussian blur

--- recognition/46975115_ViT/dataset.py
+++ recognition/46975115_ViT/dataset.py
@@ -18,7 +18,12 @@ def get_dataloaders(base_data_dir, batch_size=16, val_split=0.15):
     :return: train_loader, val_loader, test_loader
     """
     train_transforms = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(30),  # Add rotation
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),
+        transforms.RandomVerticalFlip(),  # Add vertical flip
+        transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),  # Add Gaussian blur
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])

5cef5e3c3bfebc22c05eefd60d281d89a6445947
Mr Benjamin Williams <s4748456@login0.compute.eait.uq.edu.au>
2024-10-16 08:30:25 +1000

    Started the architecture, started with the convolution step between feature maps

--- recognition/3DUNET-BenWilliams-47484566/modules.py
+++ recognition/3DUNET-BenWilliams-47484566/modules.py
@@ -1,1 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class UNet3D(nn.Module):
+    #3 input and output channels
+    def __init__(self, in_channels=3, out_channels=3, base_filters=64):
+        super(UNet3D, self).__init__()
+    
+
+    def conv_step(self, in_channels, out_channels):
+        #conv + BN + relu step between feature maps
+        return nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        )

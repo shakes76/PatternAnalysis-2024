f3138fb3b9ab621cc28cc61f9aea565bac4e79fa
Al Holliday <alh1260@gmail.com>
2024-10-15 16:52:29 +1000

    Training appears to work now

--- recognition/HipMRI_UNet_Al_Holliday_43950214/train.py
+++ recognition/HipMRI_UNet_Al_Holliday_43950214/train.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
+import matplotlib.pyplot as plt
 
 import dataset
 import modules
@@ -31,7 +32,7 @@ hipmri2dtest = dataset.HipMRI2d("H:\\HipMRI", imgSet = "test", transform = trans
 #hipmri2dtest = dataset.HipMRI2d("/home/groups/comp3710/HipMRI_Study_open/keras_slices_data", imgSet = "test", transform = trans, applyTrans = True)
 trainLoader = DataLoader(hipmri2dtrain, batch_size=8, shuffle = False)
 
-net = modules.UNet(chan, outDim)
+net = modules.UNet(chan, outDim, segDim = 6)
 net = net.to(dev)
 
 lossFunc = nn.CrossEntropyLoss()
@@ -41,9 +42,14 @@ net.train()
 for epoch in range(epochs):
     for i, (img, seg) in enumerate(trainLoader):
         #print(i)
+        #plt.imshow(seg[0].squeeze().numpy())
         img = img.to(dev)
+        #seg = nn.functional.one_hot(seg.long())
+        seg = seg.squeeze().long()
         seg = seg.to(dev)
+        
         out = net(img)
+        #print(out.shape)
         # This is no longer necessary!
         #seg = TF.center_crop(seg, output_size = out.size(2))
         loss = lossFunc(out, seg)
@@ -51,9 +57,9 @@ for epoch in range(epochs):
         loss.backward()
         optm.step()
         if (i+1) % 100 == 0:
-            print ("Epoch [{}/{}]"
-                    .format(epoch+1, epochs))
-            print("loss: ", loss.item())
+            print ("Epoch [{}/{}], Loss: {:.5f}"
+                    .format(epoch+1, epochs, loss.item()))
+            #plt.imshow(out[0].cpu().detach().squeeze().numpy())
 
 print("Done")
 # save the weights

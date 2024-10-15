b32a5267242b270157d81a4d052be17c4f996802
Elliot Lee <113224808+kunsi81@users.noreply.github.com>
2024-10-15 22:19:36 +1000

    add ADNI class for labels to images, change batch size to 64 and change docstring

--- recognition/46975115_ViT/dataset.py
+++ recognition/46975115_ViT/dataset.py
@@ -1,49 +1,74 @@
+import os
+from PIL import Image
 import torch
-from torch.utils.data import random_split, DataLoader
-from torchvision import datasets, transforms
-from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
-def get_dataloaders(base_data_dir, batch_size=16, val_split=0.15):
+class ADNIDataset(Dataset):
+    def __init__(self, root_dir, split='train', transform=None):
+        self.root_dir = os.path.join(root_dir, split)
+        self.transform = transform
+        self.ad_files = sorted(os.listdir(os.path.join(self.root_dir, 'AD')))
+        self.nc_files = sorted(os.listdir(os.path.join(self.root_dir, 'NC')))
+
+        self.images = [(file, 1) for file in self.ad_files] + [(file, 0) for file in self.nc_files]
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_name, label = self.images[idx]
+        img_path = os.path.join(self.root_dir, 'AD' if label == 1 else 'NC', img_name)
+        image = Image.open(img_path).convert('L') 
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, torch.tensor(label, dtype=torch.float32)
+
+def get_dataloaders(data_dir, batch_size=64, validation_split=0.15):
     """
-    This function loads the datasets from the data directory, splitting the train folder 
-    into training and validation sets. The directory structure is as follows:
-    - base_data_dir/train/AD
-    - base_data_dir/train/NC
-    - base_data_dir/test/AD
-    - base_data_dir/test/NC
+    Creates DataLoader objects for training, validation, and testing datasets.
     
-    :param base_data_dir: Directory containing train and test directories with AD and NC folders
-    :param batch_size: Batch size for data loaders
-    :param val_split: Percentage of train data used for validation
-    :return: train_loader, val_loader, test_loader
+    Args:
+        data_dir (str or Path): The root directory of the dataset.
+        batch_size (int): The number of samples per batch.
+        validation_split (float): Proportion of training data to be used for validation.
+        
+    Returns:
+        tuple: (train_loader, val_loader, test_loader) DataLoader objects for each split.
     """
+    
+    # Define the custom transformations for training and testing
     train_transforms = transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        transforms.Resize((224, 224)),
         transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(30), 
+        transforms.RandomRotation(30),
         transforms.ColorJitter(brightness=0.3, contrast=0.4, saturation=0.3, hue=0.2),
-        transforms.RandomVerticalFlip(), 
+        transforms.RandomVerticalFlip(),
         transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize([0.5], [0.5])
     ])
     
     test_transforms = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize([0.5], [0.5])
     ])
+    # Load training and testing datasets with their respective transforms
+    train_dataset = ADNIDataset(data_dir, split='train', transform=train_transforms)
+    test_dataset = ADNIDataset(data_dir, split='test', transform=test_transforms)
 
-    train_data = ImageFolder(root=f"{base_data_dir}/train", transform=train_transforms)
-    
-    val_size = int(len(train_data) * val_split)
-    train_size = len(train_data) - val_size
-    train_data, val_data = random_split(train_data, [train_size, val_size])
-    
-    test_data = ImageFolder(root=f"{base_data_dir}/test", transform=test_transforms)
-    
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+    # Split the training data into training and validation sets
+    val_size = int(validation_split * len(train_dataset))
+    train_size = len(train_dataset) - val_size
+    train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size])
+
+    # Create DataLoader objects for each dataset
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader, test_loader
+

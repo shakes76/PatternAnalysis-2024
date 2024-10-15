5b1ab228ede70a285d1b9de9453ab6b7d7ae700c
Farhaan Rashid <farhaan.rash@gmail.com>
2024-10-15 13:20:51 +1000

    nifti data looader added to the dataset file

--- recognition/VQVAE_s4803279/dataset.py
+++ recognition/VQVAE_s4803279/dataset.py
@@ -3,3 +3,59 @@ This file contains the data loader for preprocessing the data.
 
 The data is augmented and then packed into loaders for the model.
 """
+import torch
+from torch.utils.data import Dataset, DataLoader
+import nibabel as nib
+import numpy as np
+import os
+from tqdm import tqdm
+
+
+def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:
+    channels = np.unique(arr)
+    res = np.zeros(arr.shape + (len(channels),), dtype=dtype)
+    for c in channels:
+        c = int(c)
+        res[..., c:c+1][arr == c] = 1
+    return res
+
+
+def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float32, getAffines=False, early_stop=False):
+    affines = []
+
+    num = len(imageNames)
+    first_case = nib.load(imageNames[0]).get_fdata(caching='unchanged')
+    if len(first_case.shape) == 3:
+        first_case = first_case[:,:,0]
+
+    if categorical:
+        first_case = to_channels(first_case, dtype=dtype)
+        rows, cols, channels = first_case.shape
+        images = np.zeros((num, rows, cols, channels), dtype=dtype)
+    else:
+        rows, cols = first_case.shape
+        images = np.zeros((num, rows, cols), dtype=dtype)
+
+    for i, inName in enumerate(tqdm(imageNames)):
+        niftiImage = nib.load(inName)
+        inImage = niftiImage.get_fdata(caching='unchanged')
+        affine = niftiImage.affine
+        if len(inImage.shape) == 3:
+            inImage = inImage[:,:,0]
+        inImage = inImage.astype(dtype)
+        if normImage:
+            inImage = (inImage - inImage.mean()) / inImage.std()
+        if categorical:
+            inImage = to_channels(inImage, dtype=dtype)
+            images[i,:,:,:] = inImage
+        else:
+            images[i,:,:] = inImage
+
+        affines.append(affine)
+        if i > 20 and early_stop:
+            break
+
+    if getAffines:
+        return images, affines
+    else:
+        return images

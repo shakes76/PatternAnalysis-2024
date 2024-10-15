cdd06be2a614676f772c8c1c8bf1277a9f8ef1e6
JJNTWW <jasonjung2712@gmail.com>
2024-10-15 15:34:09 +1000

    improvements to dataset and dataloader

--- recognition/StyleGAN_Jason_47492347/dataset.py
+++ recognition/StyleGAN_Jason_47492347/dataset.py
@@ -17,14 +17,21 @@ class ADNIDataset(Dataset):
             img = self.transform(img)
         return img
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Normalize((0.5,), (0.5,))
-])
 
-def get_loader(dataset, batch_size=16, shuffle=True):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+# Apply transformations depending on grayscale or RGB data
+if CHANNELS == 1:
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Grayscale(1),
+        transforms.Normalize((0.5,), (0.5,)),
+        transforms.Resize((IMG_SIZE, IMG_SIZE))
+    ])
+elif CHANNELS == 3:
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        transforms.Resize((IMG_SIZE, IMG_SIZE))
+    ])
 
 train_dataset = ADNIDataset(train_path, transform=transform)
 test_dataset = ADNIDataset(test_path, transform=transform)
@@ -35,6 +42,23 @@ val_size = len(train_dataset) - train_size
 train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
 
 # Loaders
+def get_loader(dataset):
+    return DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
 train_loader = get_loader(train_dataset)
-val_loader = get_loader(val_dataset, shuffle=False)
-test_loader = get_loader(test_dataset, shuffle=False)
+val_loader = get_loader(val_dataset)
+test_loader = get_loader(test_dataset)
+
+
+# Unit test on dataset and dataloader
+def show_samples(dataloader, n_samples=16, columns=4):
+    plt.figure(figsize=(16, 16))
+    plt.title("Training Dataloader Sample")
+    plt.axis("off")
+    for i in range(n_samples):
+        img = next(iter(dataloader))
+        plt.subplot(int(n_samples / columns), columns, i + 1)
+        plt.imshow(np.transpose(vutils.make_grid(img[0].to(DEVICE), normalize=True).cpu(), (1,2,0)))
+    plt.show()
+
+show_samples(train_loader)

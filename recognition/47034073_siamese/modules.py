5fc296897215271bd4c160ba4a741061645d2c4c
Mitchell Clark <mitchellclark447@gmail.com>
2024-10-15 18:18:34 +1000

    added documentation to modules

--- recognition/47034073_siamese/modules.py
+++ recognition/47034073_siamese/modules.py
@@ -1,9 +1,13 @@
+"""Contains PyTorch Modules"""
+
 from torchvision.models import resnet50
 import torch
 from torch import nn
 
 
 class EmbeddingNetwork(nn.Module):
+    """Embeds lesion images into 256 dimensional embedding"""
+
     def __init__(self):
         super().__init__()
         self._backbone = resnet50()
@@ -20,5 +24,12 @@ class EmbeddingNetwork(nn.Module):
             nn.Linear(512, 256),
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self._embedder(self._backbone(x))
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            images: images to compute embeddings for
+
+        Returns:
+            Nx256 tensor containing embeddings, where N is the number of images.
+        """
+        return self._embedder(self._backbone(images))

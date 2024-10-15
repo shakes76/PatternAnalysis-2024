2df96d0c4108767aa548d194a14345871edfa8f2
Mr Benjamin Williams <s4748456@login0.compute.eait.uq.edu.au>
2024-10-16 09:21:00 +1000

    First try at training loop

--- recognition/3DUNET-BenWilliams-47484566/train.py
+++ recognition/3DUNET-BenWilliams-47484566/train.py
@@ -1,1 +1,36 @@
+import torch.optim as optim
 
+import modules.py
+
+# Example setup
+model = UNet3D(in_channels=3, out_channels=3)
+
+
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+epochs = 1
+
+device='cuda'
+
+# Training loop (simplified)
+def train_model(model, dataloader, criterion, optimizer, num_epochs=25):
+    model.train()  # Set model to training mode
+    model = model.to(device)
+    for epoch in range(num_epochs):
+        loss = 0
+
+        for inputs, labels in dataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+
+            optimizer.step()
+
+            running_loss += loss.item() * inputs.size(0)
+
+        epoch_loss = running_loss / len(dataloader.dataset)
+        print(f'Epoch {epoch}/{num_epochs}')

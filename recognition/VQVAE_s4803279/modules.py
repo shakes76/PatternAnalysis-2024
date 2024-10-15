8594989dbfc9f51be7bf33a2171290094c82b70d
Farhaan Rashid <farhaan.rash@gmail.com>
2024-10-15 14:31:58 +1000

    added code book for the vq model in the module file

--- recognition/VQVAE_s4803279/modules.py
+++ recognition/VQVAE_s4803279/modules.py
@@ -3,3 +3,56 @@ This file contains the source code for the VQVAE model.
 
 Each component of the VQVAE is implemented as a class.
 """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VectorQuantizer(nn.Module):
+    """
+    Vector Quantizer module for VQVAE
+
+    Args:
+        num_embeddings: size of the codebook
+        embedding_dim: dimension of each codebook vector
+        commitment_cost: weight for commitment loss
+    """
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        super(VectorQuantizer, self).__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.commitment_cost = commitment_cost
+
+        # Create the codebook
+        self.embedding = nn.Embedding(num_embeddings, embedding_dim)
+        self.embedding.weight.data.uniform_(-1 / num_embeddings, 1 / num_embeddings)
+
+
+    def forward(self, inputs):
+        # Convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+
+        # Flatten input
+        flat_input = inputs.view(-1, self.embedding_dim)
+
+        # Calculate distances
+        distances = (torch.sum(flat_input ** 2, dim = 1, keepdim = True) + torch.sum(self.embedding.weight ** 2, dim = 1) - 2 * torch.matmul(flat_input, self.embedding.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim = 1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self.num_embeddings)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self.embedding.weight).view(input_shape)
+
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        q_latent_loss = F.mse_loss(quantized, inputs.detach())
+        loss = q_latent_loss + self.commitment_cost * e_latent_loss
+
+        quantized = inputs + (quantized - inputs).detach()  # Straight-through estimator
+
+        # Convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), encoding_indices

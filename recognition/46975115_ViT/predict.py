a4f635a81b2a6ffb2ae6aca0ab1501044bb0bb71
Elliot Lee <113224808+kunsi81@users.noreply.github.com>
2024-10-16 02:46:56 +1000

    fix normalize for 1 channel

--- recognition/46975115_ViT/predict.py
+++ recognition/46975115_ViT/predict.py
@@ -15,7 +15,7 @@ def predict(image_path, model_path='model.pth'):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize([0.5], [0.5])
     ])
     
     image = Image.open(image_path).convert('RGB')

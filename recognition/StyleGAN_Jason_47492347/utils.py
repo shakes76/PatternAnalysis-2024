e22d355c672e02a8a22f237cece3e7feccfe0d51
JJNTWW <jasonjung2712@gmail.com>
2024-10-15 15:32:04 +1000

    added some parameters and imports to utils

--- recognition/StyleGAN_Jason_47492347/utils.py
+++ recognition/StyleGAN_Jason_47492347/utils.py
@@ -1,14 +1,28 @@
 import os
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset, random_split
 from PIL import Image
+from math import log2, sqrt
 
 # set device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # relative paths to dataset
 test_path = '../ADNI_AD_NC_2D/AD_NC/test/NC'
 train_path = '../ADNI_AD_NC_2D/AD_NC/train/NC'
 
-# parameters
+# hyperparameters
+IMG_SIZE = 128
+START_SIZE = 8
+LEARNING_RATE = 1e-3
+BATCH_SIZE = 32
+LOG_RES = int(log2(IMG_SIZE))
+DIM_Z = 256
+DIM_W = 256
+CHANNELS = 1

c9a7fb2f753367d068992b66a9a2ddc68f10989a
Mr Benjamin Williams <s4748456@login0.compute.eait.uq.edu.au>
2024-10-16 08:52:12 +1000

    Initial attempt at defining all steps in forward process, to be used in forward function

--- recognition/3DUNET-BenWilliams-47484566/modules.py
+++ recognition/3DUNET-BenWilliams-47484566/modules.py
@@ -7,11 +7,32 @@ class UNet3D(nn.Module):
     #3 input and output channels
     def __init__(self, in_channels=3, out_channels=3, base_filters=64):
         super(UNet3D, self).__init__()
-    
-    
-    
+        
+        #Encoder definitions - downsampling
+        self.encoder1 = self.conv_block(in_channels, base_filters)       
+        self.encoder2 = self.conv_block(base_filters, base_filters*2)
+        self.encoder3 = self.conv_block(base_filters*2, base_filters*4)
+        self.encoder4 = self.conv_block(base_filters*4, base_filters*8)
 
+        #Decoder definitons - upsampling
+        self.decoder4 = self.conv_block(base_filters*16, base_filters*8)
+        self.decoder3 = self.conv_block(base_filters*8, base_filters*4)
+        self.decoder2 = self.conv_block(base_filters*4, base_filters*2)
+        self.decoder1 = self.conv_block(base_filters*2, base_filters)
+        
+        
+        
+        #Upconvolutions
+        self.upconv4 = self.upconv(base_filters*16, base_filters*8)
+        self.upconv3 = self.upconv(base_filters*8, base_filters*4)
+        self.upconv2 = self.upconv(base_filters*4, base_filters*2)
+        self.upconv1 = self.upconv(base_filters*2, base_filters)
 
+        #final convolution step
+        self.final_conv = nn.Conv3d(base_filters, out_channels, kernel_size=1)
+
+        #Bridge convolution between encoder and decoder
+        self.bridge = self.conv_block(base_filters*8, base_filters*16)
 
     def conv_step(self, in_channels, out_channels):
         #conv + BN + relu step between feature maps

4e93bbb637ca27b3719cfa7eb8c0e7c0cdebcb02
mrath10 <88411794+Mrath10@users.noreply.github.com>
2024-10-16 19:29:43 +1000

    imported dataframe

--- recognition/45813788_Siamese/dataset.py
+++ recognition/45813788_Siamese/dataset.py
@@ -1,2 +1,28 @@
 '''preprocess dataset'''
-print("Hello world!")
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, dataloader
+import os
+
+current_dir = os.getcwd()
+excel = os.path.join(current_dir,'recognition','45813788_Siamese','dataset', 'train-metadata.csv')
+
+df = pd.read_csv(excel)
+
+df= df.drop(columns=['Unnamed: 0'])
+df= df.drop(columns=['patient_id'])
+
+benign = df[df['target'] == 0].reset_index(drop=True)
+malignant = df[df['target'] == 1].reset_index(drop=True)
+
+
+print(malignant)
+print(benign)
+
+
+
+
+#print(malignant)
+#print(benign)
+
+# Check if the path exists

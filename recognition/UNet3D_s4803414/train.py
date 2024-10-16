d192f243b5a53421ec0d6a3e7e6a825ee40ead80
shwaa <j.beehler@uq.edu.au>
2024-10-16 22:21:18 +1000

    Added initial epochs and batch size

--- recognition/UNet3D_s4803414/train.py
+++ recognition/UNet3D_s4803414/train.py
@@ -10,3 +10,7 @@ from torchvision import transforms
 IMAGE_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_png_slices_train'
 MASK_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_png_slices_seg_train'
 MODEL_SAVE_PATH = '/home/Student/s4803414/miniconda3/model/model.pth'
+
+BATCH_SIZE = 2
+EPOCHS = 10
+LEARNING_RATE = 1e-4

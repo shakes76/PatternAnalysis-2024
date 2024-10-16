01b5c6c956b23c238f4e554f21a33d7986851da3
shwaa <j.beehler@uq.edu.au>
2024-10-17 06:01:47 +1000

    Added doubleConv block and ResNet block

--- recognition/UNet3D_s4803414/modules.py
+++ recognition/UNet3D_s4803414/modules.py
@@ -1,18 +1,57 @@
 import torch
 import torch.nn as nn
 
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResNetBlock, self).__init__()
+        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm3d(out_channels)
+
+        # Shortcut connection
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Conv3d(in_channels, out_channels, kernel_size=1, stride=stride)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += self.shortcut(x)  # Add the shortcut
+        out = self.relu(out)
+        return out
+
 class UNet3D(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UNet3D, self).__init__()
 
-        # Contracting path
+        # Contracting path using ResNetBlocks
         self.enc1 = self.contracting_block(in_channels, 64)
         self.enc2 = self.contracting_block(64, 128)
         self.enc3 = self.contracting_block(128, 256)
         self.enc4 = self.contracting_block(256, 512)
 
-        # Bottleneck
-        self.bottleneck = self.contracting_block(512, 1024)
+        # Bottleneck using ResNetBlock
+        self.bottleneck = ResNetBlock(512, 1024)
 
         # Expanding path (adjust in_channels for concatenated skip connections)
         self.dec4 = self.expanding_block(1024 + 512, 512)  # 1024 from bottleneck + 512 from enc4
@@ -25,24 +64,14 @@ class UNet3D(nn.Module):
 
     def contracting_block(self, in_channels, out_channels):
         return nn.Sequential(
-            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
+            ResNetBlock(in_channels, out_channels),
             nn.MaxPool3d(kernel_size=2, stride=2)  # Downsampling
         )
 
     def expanding_block(self, in_channels, out_channels):
         return nn.Sequential(
             nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2),  # Upsampling
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True),
+            DoubleConv(out_channels, out_channels),
         )
 
     def forward(self, x):
@@ -59,6 +88,12 @@ class UNet3D(nn.Module):
         # Bottleneck
         bottleneck = self.bottleneck(enc4)
         print("bottleneck shape:", bottleneck.shape)  # Debug print
+
+        # Combine the cropped bottleneck and enc4
+        combined_input_dec4 = torch.cat((self.center_crop(bottleneck, enc4), enc4), dim=1)  # Concatenate on channel dimension
+        print("combined_input_dec4 shape:", combined_input_dec4.shape)  # Debug print
+        dec4 = self.dec4(combined_input_dec4)  # This should work now
+        print("dec4 shape:", dec4.shape)  # Debug print
 
         # Decoding path
         dec4 = self.dec4(self.center_crop(bottleneck, enc4))  # Crop bottleneck to match enc4
@@ -81,4 +116,3 @@ class UNet3D(nn.Module):
         _, _, d, h, w = target_tensor.size()  # Get spatial dimensions of target tensor
         tensor_cropped = tensor[:, :, :d, :h, :w]  # Crop tensor to match target dimensions
         return tensor_cropped
-

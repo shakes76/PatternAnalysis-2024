9d07f82a858be9aaf2379940d7b6cdda94fdea76
BuildTools <unconfigured@null.spigotmc.org>
2024-10-16 18:41:43 +1000

    Refactors model creation and training into run_model() function to allow hyperparameter testing.
    Implements model saving

--- recognition/Siamese Network Classifier - s4696386/train.py
+++ recognition/Siamese Network Classifier - s4696386/train.py
@@ -1,8 +1,10 @@
-import torch
+import torch, numpy
 
 from modules import SiameseNetwork
 from dataset import APP_MATCHER, read_data, DEFAULT_LOCATION, PROCESSED_DATA
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+DEFAULT_SAVE_LOCATION = "E:/COMP3710 Project/PatternAnalysis-2024/"
 
 # With support from:
 # https://github.com/pytorch/examples/blob/main/siamese_network
@@ -26,12 +28,14 @@ def train(model: SiameseNetwork, device, train_loader, optimizer, epoch, log_int
         if dry_run:
             break
 
-def test(model: SiameseNetwork, device, test_loader, threshold = 0.5, verbose = False):
+def test(model: SiameseNetwork, device, test_loader, epoch: int, threshold = 0.5, verbose = False):
     model.eval()
     test_loss = 0
     correct = 0
     all_targets = []
     all_preds = []
+    save_model = False
+    benign_count = 0
 
     criterion = model.loss_criterion
 
@@ -54,21 +58,25 @@ def test(model: SiameseNetwork, device, test_loader, threshold = 0.5, verbose = 
     precision = precision_score(all_targets, all_preds)
     recall = recall_score(all_targets, all_preds)
     f1 = f1_score(all_targets, all_preds)
+    benign_count = all_preds.count(0)
 
     if verbose:
         print(f'Test set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset):.2f}%)')
-        print(f"Predictions: {all_preds}")
-        print(f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}\n')
+        # print(f"Predictions: {all_preds}")
+        print(f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1 Score: {f1:.4f}')
+        print(f"Predicted {benign_count} benigns")
+        print(f"Epoch: {epoch}\n")
+    
+    save_model = benign_count > 0
 
+    return save_model
 
-def main():
+def run_model(batch_size, epochs, learning_rate):
+    print("="*200)
+    print(f"Training with: batch_size {batch_size}, epochs {epochs}, learning_rate {learning_rate}")
     torch_seed = 10
-    batch_size = 16
     shuffle = True
     gamma = 0.7
-    epochs = 20
-    learning_rate = 0.01
-    save_model = False
     log_interval = 10
     dry_run = False
     
@@ -85,7 +93,7 @@ def main():
     test_dataset = APP_MATCHER(processed_data, train=False)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size, shuffle)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size, shuffle)
-    print("Data Loaded")
+    print("Data Loaded\n")
 
     # Create the model
     model = SiameseNetwork().to(device)
@@ -97,11 +105,28 @@ def main():
     # run epochs
     for epoch in range(1, epochs+1):
         train(model, device, train_loader, optimizer, epoch, log_interval, dry_run, verbose=False)
-        test(model, device, test_loader, verbose=True)
+        save_model = test(model, device, test_loader, epoch, verbose=True)
         scheduler.step()
 
     if save_model:
-        torch.save(model.state_dict(), "siamese_network.pt")
+        save_loc = f"{DEFAULT_SAVE_LOCATION}siamese_network_{batch_size}_{epochs}_{learning_rate}.pt"
+        torch.save((model.state_dict(), optimizer.state_dict()), save_loc)
+        print(os.getcwd())
+        print(f"Saved to {save_loc}")
+        
+
+def main():
+    batch_sizes = [12]
+    epochs = [2]
+    learning_rates = [0.001]
+    # batch_sizes = [8, 16, 32, 64]
+    # epochs = [10, 20, 30]
+    # learning_rates = [0.0001, 0.001, 0.01, 0.1, 1]
+
+    for bs in batch_sizes:
+        for e in epochs:
+            for lr in learning_rates:
+                run_model(bs, e, lr)
 
 # Only run main when running file directly (not during imports)
 if __name__ == "__main__":

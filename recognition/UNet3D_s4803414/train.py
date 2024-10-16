37cadab08e5ad2a8cb6e109c4d1f9290dc3ea73b
shwaa <j.beehler@uq.edu.au>
2024-10-16 22:21:59 +1000

    Create dataset and dataloader

--- recognition/UNet3D_s4803414/train.py
+++ recognition/UNet3D_s4803414/train.py
@@ -14,3 +14,7 @@ MODEL_SAVE_PATH = '/home/Student/s4803414/miniconda3/model/model.pth'
 BATCH_SIZE = 2
 EPOCHS = 10
 LEARNING_RATE = 1e-4
+
+# Create dataset and dataloader
+dataset = MRIDataset(image_dir=IMAGE_DIR, mask_dir=MASK_DIR, transform=transform)
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)

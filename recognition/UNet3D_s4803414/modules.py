bd387bc72a6a359c2340cd532ed3b88a8c073ac1
shwaa <j.beehler@uq.edu.au>
2024-10-17 05:27:45 +1000

    Fixed foward function

--- recognition/UNet3D_s4803414/modules.py
+++ recognition/UNet3D_s4803414/modules.py
@@ -56,9 +56,22 @@ class UNet3D(nn.Module):
         bottleneck = self.bottleneck(enc4)
 
         # Decoding path
-        dec4 = self.dec4(torch.cat((bottleneck, enc4), dim=1))  # Skip connection with enc4
-        dec3 = self.dec3(torch.cat((dec4, enc3), dim=1))        # Skip connection with enc3
-        dec2 = self.dec2(torch.cat((dec3, enc2), dim=1))        # Skip connection with enc2
-        dec1 = self.dec1(torch.cat((dec2, enc1), dim=1))        # Skip connection with enc1
+        dec4 = self.dec4(self.center_crop(bottleneck, enc4))  # Crop bottleneck to match enc4
+        dec4 = torch.cat((dec4, enc4), dim=1)  # Skip connection with enc4
+        dec3 = self.dec3(self.center_crop(dec4, enc3))  # Crop dec4 to match enc3
+        dec3 = torch.cat((dec3, enc3), dim=1)  # Skip connection with enc3
+        dec2 = self.dec2(self.center_crop(dec3, enc2))  # Crop dec3 to match enc2
+        dec2 = torch.cat((dec2, enc2), dim=1)  # Skip connection with enc2
+        dec1 = self.dec1(self.center_crop(dec2, enc1))  # Crop dec2 to match enc1
+        dec1 = torch.cat((dec1, enc1), dim=1)  # Skip connection with enc1
 
         return self.final_conv(dec1)
+
+    def center_crop(self, tensor, target_tensor):
+        """
+        Crops the input tensor to match the spatial size of the target tensor.
+        """
+        _, _, d, h, w = target_tensor.size()  # Get spatial dimensions of target tensor
+        tensor_cropped = tensor[:, :, :d, :h, :w]  # Crop tensor to match target dimensions
+        return tensor_cropped
+

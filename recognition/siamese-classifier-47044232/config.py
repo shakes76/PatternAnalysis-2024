446ffbe23e85e5048623594873896fecfb418461
DeadmanJ <jdeadman02@gmail.com>
2024-10-16 17:16:10 +1000

    Added a config file. To be used for storing configurable data.

---
+++ recognition/siamese-classifier-47044232/config.py
@@ -0,0 +1,9 @@
+"""
+The all in one place to modify hyperparameters and other key variables such as the datapath.
+
+Made by Joshua Deadman
+"""
+
+DATAPATH = "./data"
+
+# Add other variables when the train.py file is being made

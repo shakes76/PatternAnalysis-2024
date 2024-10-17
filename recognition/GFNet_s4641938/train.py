5841af2680674b19df72c4d1b4085622a5de6649
Matthew <matthew.a1.haley@gmail.com>
2024-10-17 17:54:33 +1000

    Fixed getAccuracy to correctly calculate accuracy given test ADNI data, GFNet model and device.

--- recognition/GFNet_s4641938/train.py
+++ recognition/GFNet_s4641938/train.py
@@ -71,15 +71,18 @@ def getAccuracy(test_dataloader, model, device, max_subset : int = -1):
         total_correct = 0
         total_images = 0
         for batch, (images, targets) in enumerate(test_dataloader):
-            images.to(device)
-            targets.to(device)
+            images = images.to(device)
+            targets = targets.to(device)
             outputs = model(images)
-            total_correct += outputs == targets
+            most_likely = torch.max(outputs, dim=1).indices #get class with highest score
+            correct = most_likely == targets
+            total_correct += correct.sum()
             total_images += len(images)
+
+            print(f"[{batch}/{len(test_dataloader)}] Batch accuracy: {correct.sum()/len(images)}")
             if max_subset != -1 and batch > max_subset:
                 break
-
-    return total_correct/total_images
+        return total_correct/total_images
 
 if __name__ == "__main__":
     main()

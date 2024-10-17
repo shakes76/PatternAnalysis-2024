f660110a456ceb9dcf6766975bfcd5e8a104400f
Matthew <matthew.a1.haley@gmail.com>
2024-10-17 15:34:18 +1000

    Initial code to create relevant constants and load the ADNI data.

---
+++ recognition/GFNet_s4641938/train.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from dataset import ADNI_DataLoader
+
+def main():
+    """
+    Constants
+    """
+    IMAGESIZE = 240
+    ROOTDATAPATH = "/home/groups/comp3710/ADNI/"
+    EPOCHS = 100
+    lr = 0.001
+
+    # Set device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Set seed to make replicable
+    seed = 42
+
+    # Get the data
+    print("Loading ADNI brain data")
+    full_dataHandler = ADNI_DataLoader(rootData=ROOTDATAPATH, imageSize=IMAGESIZE)
+    train_dataloader = full_dataHandler.get_dataloader(data_type="train")
+    test_dataloader = full_dataHandler.get_dataloader(data_type="test")
+
+if __name__ == "__main__":
+    main()

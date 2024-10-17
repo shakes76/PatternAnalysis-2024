fa059d1abfd18e9c0f003790d6c3d689111eacda
张相旭 <zhangxiangxu@zhangxigxudeMBP.modem>
2024-10-17 15:31:16 +1000

    change module construction and save tsne plot

--- 3710/modules.py
+++ 3710/modules.py
@@ -5,17 +5,17 @@ from torch_geometric.nn import GCNConv, BatchNorm
 class GNN(torch.nn.Module):
     def __init__(self, in_channels, out_channels, dropout_rate=0.5):
         super(GNN, self).__init__()
-        # 第一层：将输入特征维度从 128 增加到 256
-        self.conv1 = GCNConv(in_channels, 256)
-        self.batch_norm1 = BatchNorm(256)
-        # 第二层：将特征维度从 256 降到 128
-        self.conv2 = GCNConv(256, 128)
-        self.batch_norm2 = BatchNorm(128)
-        # 第三层：将特征维度从 128 降到 64
-        self.conv3 = GCNConv(128, 64)
-        self.batch_norm3 = BatchNorm(64)
-        # 输出层：将特征维度从 64 降到类别数 out_channels
-        self.conv4 = GCNConv(64, out_channels)
+        
+        self.conv1 = GCNConv(in_channels, 64)
+        self.batch_norm1 = BatchNorm(64)
+        
+        self.conv2 = GCNConv(64, 32)
+        self.batch_norm2 = BatchNorm(32)
+        
+        self.conv3 = GCNConv(32, 16)
+        self.batch_norm3 = BatchNorm(16)
+        
+        self.conv4 = GCNConv(16, out_channels)
         
         self.dropout_rate = dropout_rate
 

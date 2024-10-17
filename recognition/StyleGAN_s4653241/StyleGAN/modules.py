983cec87b3384ef2151cc21c3dc20f6ae34dc67b
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-17 15:49:33 +1000

    Added Class Structure

--- recognition/StyleGAN_s4653241/StyleGAN/modules.py
+++ recognition/StyleGAN_s4653241/StyleGAN/modules.py
@@ -7,3 +7,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import math import sqrt # yet to use
+
+
+class MappingNetwork(nn.Module):
+    def __init__(self,z_dim,w_dim):
+        super().__init__()
+
+
+class EqualizerStraights(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class EquilizerKG(nn.Module):
+    def __init__(self):
+        super().__init__()

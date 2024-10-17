bea03be4ffe17c73fe891a1d72baeb8e5b418ba6
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-17 15:41:03 +1000

    Added necessary libraries, updated slurm.sbatch,and gitignore

--- recognition/StyleGAN_s4653241/StyleGAN/modules.py
+++ recognition/StyleGAN_s4653241/StyleGAN/modules.py
@@ -1,4 +1,9 @@
-""" 
+"""
 Containing the source code of the components of your model. Each component must be
 implementated as a class or a function
 """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import math import sqrt # yet to use

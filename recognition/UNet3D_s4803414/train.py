bd577814c053a62ad5c3187bdbf24a0c666d7fbf
shwaa <j.beehler@uq.edu.au>
2024-10-17 13:24:34 +1000

    Changed epochs due to time limit

--- recognition/UNet3D_s4803414/train.py
+++ recognition/UNet3D_s4803414/train.py
@@ -14,7 +14,7 @@ MASK_DIR = '/home/groups/comp3710/HipMRI_Study_open/semantic_labels_only'
 MODEL_SAVE_PATH = '/home/Student/s4803414/miniconda3/model/model.pth'
 
 BATCH_SIZE = 2
-EPOCHS = 10
+EPOCHS = 5
 LEARNING_RATE = 1e-4
 SPLIT_RATIO = [0.8, 0.1, 0.1]  # Train, validation, and test split
 

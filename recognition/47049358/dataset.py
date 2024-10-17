faulty test score calculation fixed

recognition/47049358/dataset.py
@@ -3,13 +3,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import nibabel as nib
 from tqdm import tqdm
-import pickle
 import numpy as np
 from torch.utils.data import Dataset
-import torchvision.transforms as transforms
 import torchvision.transforms.functional as F
 import random
-import matplotlib.pyplot as plt
 import torch
 
 IMAGE_FILE_NAME = os.path.join(os.getcwd(), 'semantic_MRs_anon')
@@ -132,7 +129,7 @@ def load_data_3D(imageNames, normImage=False, categorical=False, dtype=np.float3
 
 def load_images_and_labels(image_file_name, label_file_name, early_stop = False):
     images = load_data_3D(imageNames=image_file_name, normImage = True, early_stop=early_stop)
-    labels = load_data_3D(imageNames=label_file_name, categorical=True, dtype=np.uint8, early_stop=early_stop)
+    labels = load_data_3D(imageNames=label_file_name, categorical=True, early_stop=early_stop)
     return images, labels
 
 """
@@ -172,11 +169,8 @@ X_test = [os.path.join(IMAGE_FILE_NAME, image) for image in X_test]
 y_train = [os.path.join(LABEL_FILE_NAME, label) for label in y_train]
 y_test = [os.path.join(LABEL_FILE_NAME, label) for label in y_test]
 
-X_train, y_train = load_images_and_labels(X_train, y_train, early_stop=True)
+# X_train, y_train = load_images_and_labels(X_train, y_train, early_stop=True)
 X_test, y_test = load_images_and_labels(X_test, y_test, early_stop=True)
-
-X_train = torch.Tensor(X_train)
-y_train = torch.Tensor(y_train)
 
 def random_rotation(image: torch.Tensor):
     angle = random.uniform(0, 180)
@@ -209,21 +203,25 @@ def apply_transformation(image: torch.Tensor, angle = -1, is_mask = False, hflip
         vflip = True
     return image, angle, hflip, vflip
 
-for i in range(X_train.size(0)):
+print('> Start Random Augmentation')
+
+for i in range(X_train.shape[0]):
     augment = random.randint(0, 1)
     x = X_train[i, :, :, :]
     y = y_train[i, :, :, :, :]
     if augment == 1:
-        for j in range(x.size(-1)):
+        for j in range(x.shape[-1]):
             slice = x[ : , : , j]
-            slice = slice[np.newaxis, : , :]
+            slice = torch.Tensor(slice[np.newaxis, : , :])
             transformed_img, angle, hflip, vflip = apply_transformation(slice)
-            X_train[i, : , : , j] = transformed_img
-            for k in range(y.size(-1)):
+            X_train[i, : , : , j] = transformed_img.cpu().numpy()
+            for k in range(y.shape[-1]):
                 mask = y[ : , : , j, k]
-                mask = mask[np.newaxis, :, :]
+                mask = torch.Tensor(mask[np.newaxis, :, :])
                 transformed_mask = apply_transformation(mask, angle = angle, hflip = hflip, vflip = vflip, is_mask = True)
-                y_train[i, : , : , j, k] = transformed_mask
+                y_train[i, : , : , j, k] = transformed_mask.cpu().numpy()
+
+print('> Augmentation Finished')
 
 X_train = X_train[: ,np.newaxis, :, :, :]
 X_test = X_test[:, np.newaxis, :, :, :]

recognition/47049358/predict.py
@@ -7,10 +7,11 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+from time import time
 
 
 # import from local files  
-from train import *
+from train import trained_model, loss_map, LOSS_IDX
 from dataset import X_test, y_test, Prostate3dDataset
 
 BATCH_SIZE = 2
@@ -30,38 +31,39 @@ BATCH_SIZE = 2
 def test(model, test_loader, criterion, device):
     model.eval()  # Set the model to evaluation mode
     
-    test_scores = [] # stores dice scores.
-    seg_0_scores = []
-    seg_1_scores = []
-    seg_2_scores = []
-    seg_3_scores = []
-    seg_4_scores = []
-    seg_5_scores = []
+    test_scores = np.array([]) # stores dice scores.
+    seg_0_scores = np.array([])
+    seg_1_scores = np.array([])
+    seg_2_scores = np.array([])
+    seg_3_scores = np.array([])
+    seg_4_scores = np.array([])
+    seg_5_scores = np.array([])
 
     with torch.no_grad():
         
         criterion = criterion
 
-        for inputs, masks in test_loader:
+        for i, (inputs, masks) in enumerate(test_loader):
+            print(f'Test No.{i}')
             inputs, masks = inputs.to(device), masks.to(device)
             outputs = model(inputs)
             test_loss, dice_coefs, _ = criterion(outputs, masks)
 
             for i in range(len(dice_coefs)):
                 if i == 0:
-                    seg_0_scores.append(dice_coefs[i])
+                    seg_0_scores = np.append(seg_0_scores, dice_coefs[i])
                 elif i == 1:
-                    seg_1_scores.append(dice_coefs[i])
+                    seg_1_scores = np.append(seg_1_scores, dice_coefs[i])
                 elif i == 2:
-                    seg_2_scores.append(dice_coefs[i])
+                    seg_2_scores = np.append(seg_2_scores, dice_coefs[i])
                 elif i == 3:
-                    seg_3_scores.append(dice_coefs[i])
+                    seg_3_scores = np.append(seg_3_scores, dice_coefs[i])
                 elif i == 4:
-                    seg_4_scores.append(dice_coefs[i])
+                    seg_4_scores = np.append(seg_4_scores, dice_coefs[i])
                 else:
-                    seg_5_scores.append(dice_coefs[i])
+                    seg_5_scores = np.append(seg_5_scores, dice_coefs[i])
                 
-            test_scores.append(test_loss.item())
+            test_scores = np.append(test_scores, test_loss.item())
 
     return test_scores, seg_0_scores, seg_1_scores, seg_2_scores, seg_3_scores, seg_4_scores, seg_5_scores
 
@@ -70,9 +72,8 @@ def test(model, test_loader, criterion, device):
     Takes an array of dice scores as input. 
 """
 def plot_dice(dice, criterion, segment_scores):
-    x_values = np.arange(len(dice))  # Generate x-values as indices
-    plt.figure(figsize=(8, 6))
 
+    x_values = np.arange(len(dice))  # Generate x-values as indices
     # Plot overall dice scores
     plt.plot(x_values, dice_scores, label='Overall Dice Scores')
     
@@ -80,20 +81,15 @@ def plot_dice(dice, criterion, segment_scores):
     for i, segment_score in enumerate(segment_scores):
         plt.plot(x_values, segment_score, label=f'Segment {i} Dice Scores')
 
-
     plt.xlabel("Image Index")
     plt.ylabel("Dice Coefficient")
     plt.title("Dice Coefficient across test inputs")
     plt.legend()
     plt.grid(True)
-    plt.savefig(f'dice_scores_test_{str(criterion)}_without_validation.png')
+    plt.savefig(f'dice_scores_test_{str(criterion)}.png')
     plt.close()
 
 
-"""
-    Driver method 
-
-"""
 if __name__ == "__main__":
     # connect to gpu
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -103,17 +99,19 @@ if __name__ == "__main__":
     test_set = Prostate3dDataset(X_test, y_test)
     test_loader = DataLoader(dataset = test_set, batch_size = BATCH_SIZE)
 
+    print('> Start Testing')
+
+    start = time()
+
     # perform predictions
     dice_scores, s0, s1, s2, s3, s4, s5 = test(model = trained_model, test_loader = test_loader, criterion = criterion,
                                                device = device)
     
-    dice_scores = dice_scores.cpu().numpy()
-    s0 = s1.cpu().numpy()
-    s1 = s1.cpu().numpy()
-    s2 = s2.cpu().numpy()
-    s3 = s3.cpu().numpy()
-    s4 = s4.cpu().numpy()
-    s5 = s5.cpu().numpy()
+    end = time()
+
+    elapsed_time = end - start
+    
+    print(f"> Testing completed in {elapsed_time:.2f} seconds")
 
     average_dice = np.mean(dice_scores)
     print(f"Average Dice Coefficient: {average_dice:.4f}")
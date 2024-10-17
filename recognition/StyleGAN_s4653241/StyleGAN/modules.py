dd939d0b031b4274dff5e54ff41e979ad34748e9
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-17 17:21:39 +1000

    finalize the Mapping Network

--- recognition/StyleGAN_s4653241/StyleGAN/modules.py
+++ recognition/StyleGAN_s4653241/StyleGAN/modules.py
@@ -6,18 +6,40 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-import math import sqrt # yet to use
+from math import sqrt # yet to use
 
 
 class MappingNetwork(nn.Module):
     def __init__(self,z_dim,w_dim):
         super().__init__()
 
-
+# Equalizer for the fully connected layer
+'''
+Linear layer with learning rate equalizing weights and bias
+Returns the output of the linear transformation of the tensor with bias
+'''
 class EqualizerStraights(nn.Module):
-    def __init__(self):
+    def __init__(self, in_chanel, out_chanel, bias=0):
         super().__init__()
+        self.weight = EquilizerKG([out_chanel, in_chanel])
+        self.bias = nn.Parameter(torch.ones(out_chanel) * bias)
 
+    def forward(self, x):
+        # Linear transformation
+        return F.linear(x, self.weight(), bias = self.bias)
+
+# Weight equalizer
+'''
+Maintains the weights in the network  at a similar scale during training.
+Scale the weights at each layer with a constant such that,
+ weight w' is scaled as w' = w * c where c is constant at each layer
+'''
 class EquilizerKG(nn.Module):
-    def __init__(self):
+    def __init__(self,shape):
         super().__init__()
+        # self.constanted =  1 / sqrt(torch.prod(shape[1:])) # yet to use
+        self.constanted = 1 / sqrt(np.prod(shape[1:]))
+        self.weight = nn.Parameter(torch.randn(shape))
+
+    def forward(self):
+        return self.weight * self.constanted

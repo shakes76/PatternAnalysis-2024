016ba11b4ef4aa2b1f620ebc034ee1a15c4049cd
amlannag <amlannag12@gmail.com>
2024-10-18 13:53:00 +1000

    Added the model for stable diffusion and clip tokenizer

--- recognition/Stable Diffusion - 47219647/dataset.py
+++ recognition/Stable Diffusion - 47219647/dataset.py
@@ -3,13 +3,16 @@ import torch
 import tensorflow as tf
 from torchvision import  datasets, transforms
 import torchvision
+import matplotlib.pyplot as plt
 
 def data_set_creator():
     augmentation_transforms = transforms.Compose([
+        #No vertical fliping was applied
         transforms.RandomHorizontalFlip(),
+        #To account for skewing
         transforms.RandomRotation(30),
+        #Coloring was altered to stop overfitting
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.RandomResizedCrop((256, 256)),
         transforms.ToTensor()
     ])
 
@@ -18,6 +21,23 @@ def data_set_creator():
     dataset = datasets.ImageFolder(root=data_dir, transform=augmentation_transforms)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=128, shuffle=True, num_workers=6)
     
+
+    #Shows the first few comments for quality check
+    #Uncomment if needed
+    # def show_image(image_tensor):
+    #     image = image_tensor.permute(1, 2, 0).numpy()
+    #     plt.imshow(image)
+    #     plt.axis('off')
+    #     plt.show()
+
+    # num_images_to_show = 5
+    # for i in range(num_images_to_show):
+    #     image, _ = dataset[i] 
+    #     show_image(image)
+
     return data_loader
+
+
+data_set_creator()
 
 
---
+++ recognition/Stable Diffusion - 47219647/modules.py
@@ -0,0 +1,19 @@
+from diffusers import DiffusionPipeline, UNet2DConditionModel, AutoencoderKL, DPMSolverMultistepScheduler
+from transformers import CLIPTextModel, CLIPTokenizer
+import torch
+
+
+repo_id = "stabilityai/stable-diffusion-2-base"
+
+unet_config = UNet2DConditionModel.from_pretrained(repo_id).config
+#Retrieved the Untrained Model 
+unet = UNet2DConditionModel.from_config(unet_config)
+
+vae_config = AutoencoderKL.from_pretrained(repo_id).config
+#Retrieved untrained Autoencoder
+vae = AutoencoderKL.from_config(vae_config)
+
+#Models retrieved frol CLIP
+text_encoder_config = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32").config
+#Tokenizer retrieved from CLIP
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")

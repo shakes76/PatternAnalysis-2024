Fixed weights and dice loss calculation

recognition/3DProstateSegment_s4647117/train.py
@@ -232,6 +232,10 @@ def train_loop():
         epoch_loss = running_loss / len(train_dataset)
         print(f"Training Loss: {epoch_loss:.4f}")
 
+        # Retrieve and print the current learning rate
+        current_lr = scheduler.get_last_lr()[0]
+        print(f"Learning Rate: {current_lr}")
+
         # Validation phase
         model.eval()
         val_loss = 0.0
@@ -289,7 +293,7 @@ def train_loop():
 
             # Compute per-class Dice loss and accumulate
             per_class_scores = per_class_dice_loss(outputs, batch_labels).cpu().numpy()  # Shape: (num_classes,)
-            dice_score += per_class_scores
+            dice_score += np.ones(6) - per_class_scores
 
     # Calculate average loss
     avg_test_loss = test_loss / len(test_dataset)

recognition/3DProstateSegment_s4647117/utils.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn.functional as F
 
 # Pre-computed class weights
-CLASS_WEIGHTS = torch.tensor([0.2769, 0.4681, 4.9273, 28.8276, 117.5489, 161.1025])
+# Class weights = 1 / count in that class then normalised
+# CLASS_WEIGHTS = torch.tensor([0.2769, 0.4681, 4.9273, 28.8276, 117.5489, 161.1025])
+CLASS_WEIGHTS = torch.tensor([1, 2, 3, 4, 5, 6])
 
 def per_class_dice_loss(pred, target, num_classes=6, epsilon=1e-6):
     """
aaba7a5de405e52aec8b44e50bb5282a539b1b35
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-18 19:55:01 +1000

    Finish implementing Generator

--- recognition/StyleGAN_s4653241/StyleGAN/modules.py
+++ recognition/StyleGAN_s4653241/StyleGAN/modules.py
@@ -74,46 +74,147 @@ class EquilizerKG(nn.Module):
 
 # Drip Block, basically Style Block
 class DripBlock(nn.Module):
-    def __init__(self):
+    def __init__(self,w_dim, in_chanel, out_chanel):
         super().__init__()
-    def forward(self):
-        pass
+
+        self.need_face = EqualizerStraights(w_dim, in_chanel,bias=1)
+
+        self.conv = Weight_Demod(in_chanel,out_chanel,kernel_size=3)
+
+        # Noise and bias
+        self.scale_noise  = nn.Parameter(torch.zeros(out_chanel))
+        self.bias = nn.Parameter(torch.zeros(out_chanel))
+
+        self.activation = nn.LeakyReLU(0.2)
+
+    def forward(self,x,w,noise):
+        s = self.need_face(w)
+        x = self.conv(x,s)
+        if noise is not None:
+            x = x + self.scale_noise[None,:,None,None] * noise
+        return self.activation(x + self.bias[None,:,None,None])
+
 
 class Weight_Demod(nn.Module):
-    def __init__(self):
+    def __init__(self, in_chanel, out_chanel, kernel_size, demodulate = True, eps = 1e-8):
         super().__init__()
+        self.out_chanel = out_chanel
+        self.demodulate = demodulate
+        self.padding = (kernel_size -1) // 2
 
-    def forward(self):
-        pass
+        # Weights with Equalizer learning rate
+        self.weight = EquilizerKG([out_chanel, in_chanel, kernel_size, kernel_size])
+        self.eps = eps # epsilon value for numerical stability
+
+    def forward(self, x, s):
+
+        # Get batch size, height and width
+        b, _, h, w = x.shape
+
+        # Reshape the scales
+        s = s[:, None, :, None, None]
+        weights = self.weight()[None, :, :, :, :]
+        # The result has shape [batch_size, out_features, in_features, kernel_size, kernel_size]`
+        weights = weights * s
+
+        # Weight Demodulation
+        if self.demodulate:
+            sigma_inv = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
+            weights = weights * sigma_inv
+
+        # Reshape x and weights
+        x = x.reshape(1, -1, h, w)
+        _, _, *ws = weights.shape
+        weights = weights.reshape(b * self.out_chanel, *ws)
+
+        # Group b is used to define a different kernel (weights) for each sample in the batch
+        x = F.conv2d(x, weights, padding=self.padding, groups=b)
+
+        # return x in shape of [batch_size, out_features, height, width]
+        return x.reshape(-1, self.out_chanel, h, w)
+
+
+class ToRGB(nn.Module):
+
+    def __init__(self, W_DIM, features):
+        super().__init__()
+        self.to_style = EqualizerStraights(W_DIM, features, bias=1)
+
+        self.conv = Weight_Demod(features, 3, kernel_size=1, demodulate=False)
+        self.bias = nn.Parameter(torch.zeros(3))
+        self.activation = nn.LeakyReLU(0.2, True)
+
+    def forward(self, x, w):
+
+        style = self.to_style(w)
+        x = self.conv(x, style)
+        return self.activation(x + self.bias[None, :, None, None])
 '''
 Not implemented yet
 '''
-class NoiseInjection(nn.Module):
-    def __init__(self):
+class Generator(nn.Module):
+    def __init__(self, log_reso,w_dim, n_features = 32, max_features = 256):
         super().__init__()
 
-    def forward(self, x):
-        pass
+
+
+        features = [min(max_features, n_features * (2 ** i)) for i in range(log_reso - 2, -1, -1)]
+        self.n_blocks = len(features)
+
+        # Initialize the trainable 4x4 constant tensor
+        self.initial_constant = nn.Parameter(torch.randn((1, features[0], 4, 4)))
+
+        # First style block and it's rgb output. Initialises the generator.
+        self.style_block = DripBlock(w_dim, features[0], features[0])
+        self.to_rgb = ToRGB(w_dim, features[0])
+
+        # Creates a series of Generator Blocks based on features length. 5 in this case.
+        blocks = [GeneratorBlock(w_dim, features[i - 1], features[i]) for i in range(1, self.n_blocks)]
+        self.blocks = nn.ModuleList(blocks)
+
+    def forward(self, w, input_noise):
+        batch_size = w.shape[1]
+
+        # Expand the learnt constant to match the batch size
+        x = self.initial_constant.expand(batch_size, -1, -1, -1)
+
+        # Get the first style block and the rgb img
+        x = self.style_block(x, w[0], input_noise[0][1])
+        rgb = self.to_rgb(x, w[0])
+
+        # Rest of the blocks upsample the img using interpolation set in the config file and add to the rgb from the block
+        for i in range(1, self.n_blocks):
+            x = F.interpolate(x, scale_factor=2, mode="bilinear")
+            x, rgb_new = self.blocks[i - 1](x, w[i], input_noise[i])
+            rgb = F.interpolate(rgb, scale_factor=2, mode="bilinear") + rgb_new
+
+        # tanh is used to output rgb pixel values form -1 to 1
+        return torch.tanh(rgb)
+
 
 '''
-Not implemented yet
+Testing
 '''
-class SynthesisNetwork(nn.Module):
-    def __init__(self):
+class GeneratorBlock(nn.Module):
+    def __init__(self,w_dim, in_chanel, out_chanel):
         super().__init__()
 
-    def forward(self, x):
-        pass
+        # First black changes the feature map size to the "out_chanel"
+        self.styled_block1 = DripBlock(w_dim, in_chanel, out_chanel)
+        self.styled_block2 = DripBlock(w_dim, out_chanel, out_chanel)
 
-'''
-Not implemented yet
-'''
-class Generator_Block(nn.Module):
-    def __init__(self):
-        super().__init__()
 
-    def forward(self, x):
-        pass
+
+    def forward(self, x,w,noise):
+
+        # Style blocks with Noise Tensor
+        x = self.styled_block1(x,w,noise[0])
+        x = self.styled_block2(x,w,noise[1])
+
+        # RBG img
+        rgb = self.to_rgb(x,w)
+
+        return x, rgb
 
 #-----------Discriminator----------------#
 '''
@@ -210,3 +311,47 @@ class Dis_Conv2d(nn.Module):
         return F.conv2d(x, self.weight(), bias = self.bias, padding = self.padding)
 
 #---------------------------#
+
+class PathLengthPenalty(nn.Module):
+
+    def __init__(self, beta):
+        super().__init__()
+
+        self.beta = beta
+        self.steps = nn.Parameter(torch.tensor(0.), requires_grad=False)
+
+        self.exp_sum_a = nn.Parameter(torch.tensor(0.), requires_grad=False)
+
+    def forward(self, w, x):
+
+        device = x.device
+        image_size = x.shape[2] * x.shape[3]
+        y = torch.randn(x.shape, device=device)
+
+        # Scaling
+        output = (x * y).sum() / sqrt(image_size)
+        sqrt(image_size)
+
+        # Computes gradient
+        gradients, *_ = torch.autograd.grad(outputs=output,
+                                            inputs=w,
+                                            grad_outputs=torch.ones(output.shape, device=device),
+                                            create_graph=True)
+
+        # Calculated L2-norm
+        norm = (gradients ** 2).sum(dim=2).mean(dim=1).sqrt()
+
+        # Regulatrise after first step
+        if self.steps > 0:
+            a = self.exp_sum_a / (1 - self.beta ** self.steps)
+            loss = torch.mean((norm - a) ** 2)
+        else:
+            # Return a dummpy loss tensor if computation fails
+            loss = norm.new_tensor(0)
+
+        mean = norm.mean().detach()
+        self.exp_sum_a.mul_(self.beta).add_(mean, alpha=1 - self.beta)
+        self.steps.add_(1.)
+
+        # return the penalty
+        return loss

3c12811ec8e1ac6d0f5ca478f1a2b809a00eb87a
Al Holliday <alh1260@gmail.com>
2024-10-18 16:54:28 +1000

    Changes to testThing so that it can plot predicted segment maps

--- recognition/HipMRI_UNet_Al_Holliday_43950214/testThing.py
+++ recognition/HipMRI_UNet_Al_Holliday_43950214/testThing.py
@@ -19,7 +19,7 @@ import dataset
 
 dev = torch.device('cuda')
 
-hipmri2d = dataset.HipMRI2d("C:\\Users\\al\\HipMRI", transform = transforms.Resize((256, 256)), applyTrans = True)
+hipmri2d = dataset.HipMRI2d("H:\\HipMRI", imgSet = "test", transform = transforms.Resize((256, 256)), applyTrans = True)
 hipMriLoader = DataLoader(hipmri2d, batch_size=64, shuffle=False)
 trainImg, segments = next(iter(hipMriLoader))
 firstImg = trainImg[0]
@@ -28,6 +28,7 @@ firstSeg = segments[0]
 plt.figure()
 plt.imshow(firstSeg.squeeze())
 firstImg = firstImg.to(dev)
+firstSeg = firstSeg.to(dev)
 
 chan = 1
 outDim = 64
@@ -37,6 +38,14 @@ outDim = 64
 
 #out = basicBlk(firstImg)
 
-net = modules.UNet(chan, outDim, segDim = 1)
+net = modules.UNet(chan, outDim, segDim = 6)
+net.load_state_dict(torch.load("H:\\python_work\\hipMri_unet_results\\weights_epoch_31.pth", weights_only = True))
 net = net.to(dev)
+net.eval()
 out = net(firstImg)
+
+# print a purrity picture
+out1 = torch.permute(out, (1,2,0))
+out2 = torch.argmax(out1, dim=-1)
+plt.figure()
+plt.imshow(out2.cpu().numpy())

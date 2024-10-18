eb431d957969c9851da999ed9e66d6e08c44e85c
Chen <759534580@qq.com>
2024-10-18 21:55:59 +1000

    Update train.py

--- recognition/facebook_gnn_classification/train.py
+++ recognition/facebook_gnn_classification/train.py
@@ -5,3 +5,9 @@ from torch.optim import Adam
 from torch_geometric.data import Data
 from modules import GNN
 from dataset import load_data, edges_path, features_path, labels_path
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load the dataset
+features, edge_index, labels, page_type_mapping = load_data(edges_path, features_path, labels_path)
+data = Data(x=features, edge_index=edge_index, y=labels).to(device)

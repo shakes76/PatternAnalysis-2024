ebb5779732ea81ed876b90ecd718f99c0ea6c1ac
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-19 16:04:53 +1000

    Added plot_loss

--- recognition/StyleGAN_s4653241/StyleGAN/predict.py
+++ recognition/StyleGAN_s4653241/StyleGAN/predict.py
@@ -2,3 +2,25 @@
 Shows example usage of your trained model. Print out any results and / or provide visu-
 alisations where applicable
 """
+
+# Importing the required libraries
+import matplotlib.pyplot as plt
+
+
+def plot_loss(G_loss,D_loss):
+
+    plt.figure(figsize=(10,5))
+    plt.title("Generator Loss During Training")
+    plt.plot(G_loss, label="G", color="blue")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig('gen_loss.png')
+
+    plt.figure(figsize=(10,5))
+    plt.title("Discriminator Loss During Training")
+    plt.plot(D_loss, label="D", color="red")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig('disc_loss.png')

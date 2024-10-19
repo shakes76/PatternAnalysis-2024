fc35097853dd678f0395201b6827ce2c7800fd10
bkmund <baibhav.mund@gmail.com>
2024-10-19 17:29:25 +1000

    housekeeping on files

--- recognition/ADNI-StyleGAN-48548700/train.py
+++ recognition/ADNI-StyleGAN-48548700/train.py
@@ -8,7 +8,7 @@ from torch import optim
 import matplotlib.pyplot as plt
 
 # Define constants and hyperparameters
-DATASET                 = "./OASIS"  # Path to the dataset
+DATASET                 = "./ADNI_AD_NC_2D/"  # Path to the dataset
 DEVICE                  = "cuda" if torch.cuda.is_available() else "cpu"  # Use GPU if available, otherwise use CPU
 EPOCHS                  = 301 # Number of training epochs
 LEARNING_RATE           = 1e-3  # Learning rate for optimization

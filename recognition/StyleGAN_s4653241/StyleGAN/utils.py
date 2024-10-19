63e93356a21dc40ea1cf2733b5c2af6d15a8cc80
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-19 15:00:23 +1000

    Added save_model function

--- recognition/StyleGAN_s4653241/StyleGAN/utils.py
+++ recognition/StyleGAN_s4653241/StyleGAN/utils.py
@@ -1,2 +1,19 @@
 
 '''Utils for the project'''
+
+import torch
+
+def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss,path):
+    checkpoint = {
+        'epoch': epoch,
+        'G_state_dict': G.state_dict(),
+        'D_state_dict': D.state_dict(),
+        'optimizer_G_state_dict': optimizer_G.state_dict(),
+        'optimizer_D_state_dict': optimizer_D.state_dict(),
+        'random_state': torch.get_rng_state(),
+        'cuda_random_state': torch.cuda.get_rng_state_all(),
+        'gen_loss': gen_loss,
+        'disc_loss': disc_loss,
+
+    }
+    torch.save(checkpoint, path)

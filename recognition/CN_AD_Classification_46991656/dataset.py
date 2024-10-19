45da90bf85e2cbc2b0567da9fe9599495178fe2c
Daniel Cottrell <s4699165@student.uq.edu.au>
2024-10-19 15:32:17 +1000

    Improved the structure of the BrainDataset, added functions for length and getitem.

--- recognition/CN_AD_Classification_46991656/dataset.py
+++ recognition/CN_AD_Classification_46991656/dataset.py
@@ -3,21 +3,45 @@
 import os
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from sklearn.model_selection import train_test_split
 from PIL import Image
 
 
 class BrainDataset(Dataset):
-    def __init__(self, image_paths, labels, transform=None):
-        self.image_paths = image_paths
-        self.labels = labels
+    def __init__(self, root_dir, transform=None):
+        """
+        Custom Dataset class to load brain images from a directory.
+    
+        Args:
+            root_dir (str): Path to the directory (either 'train' or 'test') containing AD and NC subfolders.
+            transform (callable, optional): Transform to apply to images.
+        """
+        self.image_paths = []
+        self.labels = []
         self.transform = transform
+
+        # Load all image paths and labels
+        for label, subfolder in enumerate(['AD', 'NC']):
+            folder_path = os.path.join(root_dir, subfolder)
+            for filename in os.listdir(folder_path):
+                if filename.endswith('.jpg') or filename.endswith('.jpeg') or filename.endswith('.png'):
+                    self.image_paths.append(os.path.join(folder_path, filename))
+                    self.labels.append(label)  # 0 for AD, 1 for NC
 
     def __len__(self):
         return len(self.image_paths)
     
-    
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        label = self.labels[idx]
 
+        # Load image
+        image = Image.open(img_path).convert('RGB')
+
+        # Apply any transformations
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
 
 
 

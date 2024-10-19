ec001978615a8d8493c2cb073d3f0879e75fe037
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-19 15:50:33 +1000

    Added checkpoint loader

--- recognition/StyleGAN_s4653241/StyleGAN/utils.py
+++ recognition/StyleGAN_s4653241/StyleGAN/utils.py
@@ -2,8 +2,9 @@
 '''Utils for the project'''
 
 import torch
+import config
 
-def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss,path):
+def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss,path = config.path + 'checkpoint.pth'):
     checkpoint = {
         'epoch': epoch,
         'G_state_dict': G.state_dict(),
@@ -11,9 +12,23 @@ def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss,path):
         'optimizer_G_state_dict': optimizer_G.state_dict(),
         'optimizer_D_state_dict': optimizer_D.state_dict(),
         'random_state': torch.get_rng_state(),
-        'cuda_random_state': torch.cuda.get_rng_state_all(),
+        'cuda_random_state': torch.cuda.get_rng_state_all(), # Save the random state of the GPU
         'gen_loss': gen_loss,
         'disc_loss': disc_loss,
 
     }
     torch.save(checkpoint, path)
+
+def load_checkpoint(path, G, D, optimizer_G, optimizer_D):
+    checkpoint = torch.load(path)
+    G.load_state_dict(checkpoint['G_state_dict'])
+    D.load_state_dict(checkpoint['D_state_dict'])
+    optimizer_G.load_state_dict(checkpoint['optimizer_G_state_dict'])
+    optimizer_D.load_state_dict(checkpoint['optimizer_D_state_dict'])
+    epoch = checkpoint['epoch']
+    gen_loss = checkpoint['gen_loss']
+    disc_loss = checkpoint['disc_loss']
+    torch.set_rng_state(checkpoint['random_state'])
+    torch.cuda.set_rng_state_all(checkpoint['cuda_random_state'])
+
+    return epoch, gen_loss, disc_loss

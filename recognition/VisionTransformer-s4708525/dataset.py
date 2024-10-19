4a276dac65877a5dfbdef0dcdfaf2401d4b45597
Mr Yuda Hendriawan Budi Handoko <s4708525@login0.compute.eait.uq.edu.au>
2024-10-19 10:52:16 +1000

    Update: count number of ADNI class per category

--- recognition/VisionTransformer-s4708525/dataset.py
+++ recognition/VisionTransformer-s4708525/dataset.py
@@ -1,6 +1,7 @@
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 import os
+from PIL import Image
 
 class ADNI_Dataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -8,8 +9,13 @@ class ADNI_Dataset(Dataset):
         self.transform = transform
         self.image_paths = []
         self.labels = []
+        self.category_counts = {'NC': 0, 'AD': 0}  
+
         for label, sub_dir in enumerate(['NC', 'AD']):
             sub_dir_path = os.path.join(root_dir, sub_dir)
+            num_images = len(os.listdir(sub_dir_path))  
+            self.category_counts[sub_dir] = num_images  
+
             for image_name in os.listdir(sub_dir_path):
                 self.image_paths.append(os.path.join(sub_dir_path, image_name))
                 self.labels.append(label)
@@ -18,9 +24,11 @@ class ADNI_Dataset(Dataset):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        # image = Image.open(self.image_paths[idx]).convert('L')  # Convert to grayscale
-        image = Image.open(self.image_paths[idx]).convert('RGB')  # 
+        image = Image.open(self.image_paths[idx]).convert('RGB')
         label = self.labels[idx]
         if self.transform:
             image = self.transform(image)
         return image, label
+
+    def get_category_counts(self):
+        return self.category_counts

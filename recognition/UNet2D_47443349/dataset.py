495e09405c5c6e35d37503b92c221e9bf8b0f270
AlexPitman04 <pitmanalex04@gmail.com>
2024-10-19 21:19:08 +1000

    [Data] Removes unnecessary one-hot encoding and fixes issue with uneven image shapes (using 0 padding).

--- recognition/UNet2D_47443349/dataset.py
+++ recognition/UNet2D_47443349/dataset.py
@@ -3,59 +3,39 @@ import nibabel as nib
 from tqdm import tqdm
 import os
 from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
 
-N_CLASSES = 6
+IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 144
 
-def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:
-    channels = np.unique(arr)
-    res = np.zeros(arr.shape + (N_CLASSES,), dtype=dtype)
-    for c in channels:
-        c = int(c)
-        res[..., c:c+1][arr == c] = 1
-    return res
-
-def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float32, getAffines=False, early_stop=False):
-    '''
+def load_data_2D(imageNames, normImage=False, dtype=np.float32, getAffines=False, early_stop=False):
+    """
     Load medical image data from names, cases list provided into a list for each.
 
     This function pre-allocates 4D arrays for conv2d to avoid excessive memory usage.
 
     normImage: bool (normalise the image 0.0 to 1.0)
     early_stop: Stop loading pre-maturely, leaves arrays mostly empty, for quick loading and testing scripts.
-    '''
-    affines = []
-
-    # Get fixed size
+    """
     num = len(imageNames)
-    first_case = nib.load(imageNames[0]).get_fdata(caching='unchanged')
-
-    if len(first_case.shape) == 3:
-        first_case = first_case[:,:,0] # Sometimes extra dims -> remove
-    
-    if categorical:
-        first_case = to_channels(first_case, dtype=dtype)
-        rows, cols, channels = first_case.shape
-        images = np.zeros((num, rows, cols, channels), dtype=dtype)
-    else:
-        rows, cols = first_case.shape
-        images = np.zeros((num, rows, cols), dtype=dtype)
+    images = np.zeros((num, IMAGE_HEIGHT, IMAGE_WIDTH), dtype=dtype)
+    affines = []
 
     for i, inName in enumerate(tqdm(imageNames)):
         niftiImage = nib.load(inName)
         inImage = niftiImage.get_fdata(caching='unchanged') # Read disk only
+
         affine = niftiImage.affine
+        affines.append(affine)
+
         if len(inImage.shape) == 3:
-            inImage = inImage[:,:,0] # Sometimes extra dims in HipMRI study data
+            inImage = inImage[:,:,0] # Sometimes extra dims in HipMRI study data -> remove
         inImage = inImage.astype(dtype)
         if normImage:
             inImage = (inImage - inImage.mean()) / inImage.std()
-        if categorical:
-            inImage = to_channels(inImage, dtype=dtype)
-            images[i,:,:,:] = inImage
-        else:
-            images[i,:,:] = inImage
-
-        affines.append(affine)
+        inImage_height, inImage_width = inImage.shape # Note: some images smaller than 256x144
+        images[i,:inImage_height,:inImage_width] = inImage
+        
         if i > 20 and early_stop:
             break
 
@@ -72,8 +52,8 @@ class ProstateDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transforms):
         self.imageNames = get_names(image_dir)
         self.maskNames = get_names(mask_dir)
-        self.images = load_data_2D(self.imageNames, normImage=True) # Images normalised
-        self.masks = load_data_2D(self.maskNames, categorical=True) # One-hot encoded
+        self.images = load_data_2D(self.imageNames, normImage=True) # Input images normalised
+        self.masks = load_data_2D(self.maskNames)
         self.transforms = transforms
 
     def __len__(self):
@@ -90,3 +70,22 @@ class ProstateDataset(Dataset):
             mask = self.transforms(mask)
 		
         return (image, mask)
+
+def test():
+    TRAIN_IMG_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_train"
+    TRAIN_MASK_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_seg_train"
+
+    imageNames = get_names(TRAIN_IMG_DIR)
+    images = load_data_2D(imageNames, normImage=False)
+    print(images.shape)
+    plt.imshow(images[11000])
+    plt.show()
+
+    maskNames = get_names(TRAIN_MASK_DIR)
+    masks = load_data_2D(maskNames, normImage=False)
+    print(masks.shape)
+    plt.imshow(masks[11000])
+    plt.show()
+
+if __name__ == "__main__":
+    test()

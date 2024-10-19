f7536270e6550c8d6c6c5df051f46b1c2eb916d8
amlannag <amlannag12@gmail.com>
2024-10-20 00:35:03 +1000

    Updated Data Set

--- recognition/Style GAN - 47219647/dataset.py
+++ recognition/Style GAN - 47219647/dataset.py
@@ -2,46 +2,38 @@ import numpy as np
 import torch
 import tensorflow as tf
 from torchvision import  datasets, transforms
-import torchvision
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
+
+
+def one_hot_encode(labels, num_classes):
+    return F.one_hot(labels, num_classes=num_classes)
 
 def data_set_creator():
     augmentation_transforms = transforms.Compose([
-        #No vertical fliping was applied
+        # No vertical flipping was applied
         transforms.RandomHorizontalFlip(),
-        #To account for skewing
+        # To account for skewing
         transforms.RandomRotation(30),
-        #Coloring was altered to stop overfitting
+        # Coloring was altered to stop overfitting
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.ToTensor()
     ])
 
-    #Change the directory to what is needed
+    # Change the directory to what is needed
     data_dir = 'recognition/Style GAN - 47219647/AD_NC/test'
     
     dataset = datasets.ImageFolder(root=data_dir, transform=augmentation_transforms)
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=128, shuffle=True, num_workers=6)
 
-    # Shows the first few comments for quality check
-    # Uncomment if needed
-    # def show_image(image_tensor,title):
-    #     image = image_tensor.permute(1, 2, 0).numpy()
-    #     plt.imshow(image)
-    #     plt.title(title)
-    #     plt.axis('off')
-    #     plt.show()
+    num_classes = len(dataset.classes)  
 
-    # num_images_to_show = 5
+    def get_one_hot_encoded_loader():
+        for batch_images, batch_labels in data_loader:
+            one_hot_labels = one_hot_encode(batch_labels, num_classes)
+            yield batch_images, one_hot_labels
 
-    # for batch_images, batch_labels in data_loader:
-    #     for i in range(num_images_to_show):
-    #         show_image(batch_images[i], f"Label: {batch_labels[i]}")
-    #     break
+    return get_one_hot_encoded_loader()
 
 
-    return data_loader
-
-
-# if __name__ == "__main__":
-#     data_loader = data_set_creator()
 

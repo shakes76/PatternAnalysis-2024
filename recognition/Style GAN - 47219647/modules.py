4c79edf21595309c7eef8474ef5b2e5ec47ef9bf
amlannag <amlannag12@gmail.com>
2024-10-19 17:27:58 +1000

    Updating the Model modules

--- recognition/Style GAN - 47219647/modules.py
+++ recognition/Style GAN - 47219647/modules.py
@@ -1,24 +1,28 @@
-from diffusers import DiffusionPipeline, UNet2DConditionModel, AutoencoderKL, DPMSolverMultistepScheduler
-from transformers import CLIPTextModel, CLIPTokenizer
-import torch
+from torchvision.transforms import ToPILImage
+from stylegan2_pytorch import StyleGAN2
 
 
-repo_id = "stabilityai/stable-diffusion-2-base"
+class Gan():
 
-unet_config = UNet2DConditionModel.from_pretrained(repo_id).config
-#Retrieved the Untrained Model 
-unet = UNet2DConditionModel.from_config(unet_config)
+    def __init__(self,image_size, latent_dim, chanels, network_capacity) -> None:
+        self.model = StyleGAN2(
+            image_size=image_size,
+            latent_dim=latent_dim,
+            network_capacity= network_capacity
+        )
 
-vae_config = AutoencoderKL.from_pretrained(repo_id).config
-#Retrieved untrained Autoencoder
-vae = AutoencoderKL.from_config(vae_config)
+        self.chanels = chanels
+    
+    def get_generator(self):
+        return self.model.G
 
-#Models retrieved frol CLIP
-text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
+    def get_discriminator(self):
+        return self.model.D
+    
+    def get_style_vector(self):
+        return self.model.SE
+    
+    def initialise_weight(self):
+        self.model._init_weights()
 
-#Tokenizer retrieved from CLIP
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-
-#Pipeline
-modelPipeline = DiffusionPipeline(unet=unet, vae=vae, text_encoder=text_encoder, tokenizer=tokenizer)
-
+    

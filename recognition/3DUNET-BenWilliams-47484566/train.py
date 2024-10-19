e9f244f260b3a137b1c6e9c6a06d486e81a95f5b
Mr Benjamin Williams <s4748456@login0.compute.eait.uq.edu.au>
2024-10-19 13:43:33 +1000

    Added dice score calculation to training loop

--- recognition/3DUNET-BenWilliams-47484566/train.py
+++ recognition/3DUNET-BenWilliams-47484566/train.py
@@ -1,28 +1,39 @@
+import torch
+from torch.utils.data import DataLoader
 import torch.optim as optim
+import modules
+from dataset import MRI3DDataset
 
-import modules.py
 
 # Example setup
-model = UNet3D(in_channels=3, out_channels=3)
+#model = UNet3D(in_channels=3, out_channels=3)
 
-criterion = crossEntropyLoss(weight=torch.tensor([1.0, 1.0, 1.0]))
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+#criterion = crossEntropyLoss(weight=torch.tensor([1.0, 1.0, 1.0]))
+#optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-epochs = 1
+#epochs = 1
 
-device='cuda'
+#device='cuda'
+
+
+
+#inputs =  load_data_3D(imageNames, normImage=False, categorical=False, dtype=np.float32, getAffines=False, orient=False, early_stop=False)
+#labels =  load_data_3D(imageNames, normImage=False, categorical=False, dtype = np.uint8, getAffines=False, orient=False, early_stop=False)
+
 
 # Training loop (simplified)
-def train_model(model, dataloader, criterion, optimizer):
-    model.train()  # Set model to training mode
+def train_model(model, criterion, optimizer):
     model = model.to(device)
-    for epoch in range(epochs):
-        loss = 0
+    model.train()
 
-        for inputs, labels in dataloader:
+    dice_scores = []
+    for epoch in range(epochs):
+        loss = 0.0
+        epoch_dice = 0.0
+
+        for inputs, labels in data:
             inputs = inputs.to(device)
             labels = labels.to(device)
-
 
             optimizer.zero_grad()
 
@@ -32,7 +43,12 @@ def train_model(model, dataloader, criterion, optimizer):
             loss.backward()
             optimizer.step()
 
-            loss += loss.item() * inputs.size(0)
+            loss += loss.item() * inputs.size(0)    
 
+        dice_score = dice_coefficient(outputs, labels)
+        epoch_dice += dice_score.item() * inputs.size(0)
+        
+        dice_scores.append(avg_dice)
         epoch_loss = running_loss / len(dataloader.dataset)
-        print(f'Epoch {epoch}/{num_epochs}, Loss: {epoch_loss:.4f}')
+        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}, Dice: {avg_dice:.4f}')
+    return dice_scores

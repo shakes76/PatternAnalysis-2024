21dc29ff369a3facce9c5c9456be5b099985c78a
Darcy Weedman <darcyweedman@gmail.com>
2024-10-19 15:47:46 +1000

    feat: __getitem__ for HipMRIDataset class

--- recognition/2DUNet_HipMRI_Segmentation_DarcyWeedman/dataset.py
+++ recognition/2DUNet_HipMRI_Segmentation_DarcyWeedman/dataset.py
@@ -23,6 +23,29 @@ class HipMRIDataset(Dataset):
         self.seg_dir = seg_dir
         self.transform = transform
         self.image_files = sorted([f for f in os.listdir(data_dir) if f.endswith('.nii.gz')])
-        
+
     def __len__(self):
         return len(self.image_files)
+    
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.data_dir, self.image_files[idx])
+        seg_path = os.path.join(self.seg_dir, self.image_files[idx])
+        
+        image = load_data_2D([img_path], normImage=True)[0]
+        mask = load_data_2D([seg_path], dtype=np.uint8)[0]
+
+        # Ensure image and mask are 2D
+        if len(image.shape) > 2:
+            image = image[:,:,0]
+        if len(mask.shape) > 2:
+            mask = mask[:,:,0]
+
+        # Add channel dimension
+        image = np.expand_dims(image, axis=0)
+        mask = np.expand_dims(mask, axis=0)
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask

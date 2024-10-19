69ead28ad4d3f77c3baf6e4668550631fca7006d
AlexPitman04 <pitmanalex04@gmail.com>
2024-10-20 00:08:30 +1000

    [Utils] Updates data paths and adds a seed setter for reproducibility.

--- recognition/UNet2D_47443349/utils.py
+++ recognition/UNet2D_47443349/utils.py
@@ -1,9 +1,26 @@
+import torch
+import numpy as np
+import random
+
+
 IMAGE_HEIGHT = 256
 IMAGE_WIDTH = 144
 
-TRAIN_IMG_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_train"
-TRAIN_MASK_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_seg_train"
-VAL_IMG_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_validate"
-VAL_MASK_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_seg_validate"
-TEST_IMG_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_test"
-TEST_MASK_DIR = "C:/Users/21pit/OneDrive/Desktop/COMP3710/Project/PatternAnalysis-2024/recognition/UNet2D_47443349/keras_slices_seg_test"
+TRAIN_IMG_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_train"
+TRAIN_MASK_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_train"
+VAL_IMG_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_validate"
+VAL_MASK_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_validate"
+TEST_IMG_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_test"
+TEST_MASK_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_test"
+
+SEED = 47443349
+def set_seed(seed=SEED):
+    """
+    For reproducibility
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)

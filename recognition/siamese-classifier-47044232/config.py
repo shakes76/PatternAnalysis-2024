88d96b5cd9984a97d9593e6d09f86d69e386b642
DeadmanJ <jdeadman02@gmail.com>
2024-10-20 00:30:20 +1000

    Defined the percentage of the malignant data to go into each dataset.

--- recognition/siamese-classifier-47044232/config.py
+++ recognition/siamese-classifier-47044232/config.py
@@ -6,4 +6,9 @@ Made by Joshua Deadman
 
 DATAPATH = "./data"
 
+# The percent of malignant data in each set (should add up to 1).
+TRAINING = 0.7
+TESTING = 0.2
+VALIDATION = 0.1
+
 # Add other variables when the train.py file is being made

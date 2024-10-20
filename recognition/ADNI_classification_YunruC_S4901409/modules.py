8e8f3e0092c0c416e5c7cfdc38a55747f2bdab97
AprillC4300 <yunru.chen3@uqconnect.edu.au>
2024-10-20 12:50:41 +1000

    modified GFNetFilter

--- recognition/ADNI_classification_YunruC_S4901409/modules.py
+++ recognition/ADNI_classification_YunruC_S4901409/modules.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.fft
 from timm.models.layers import to_2tuple
 
-class Patchembedding(nn.Module):
+class PatchEmbedding(nn.Module):
     """
     Patch the input image and flatten into a 1D sequence.
     embed_dim = embedding dimensionality, it means that each patch is represented 
@@ -41,35 +41,22 @@ domain.
 The Multi-Attention in ViT is replaced by Global Filters.
 """
 
-
-
-
-
-
-
-
-
-
-
-
-'''
 class GFNetFilter(nn.Module):
+    """
+    The Global Filter layer is copied from the https://gfnet.ivg-research.xyz/
+    with some modifications
+    """
     def __init__(self, dim , h= 14, w=8):
         super().__init__()
         self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02)
         self.w = w
         self.h = h
 
-    def forward(self, x, spatial_size = None):
-        B, N, C = x.shape
-        if spatial_size is None:
-            a = b= int(math.sqrt(N))
-        x = torch.fft.rfft2(x, dim=(-2, -1), norm='ortho')
+    def forward(self, x):
+        B, C, H, W= x.shape
+        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
         weight = torch.view_as_complex(self.complex_weight)
-        x = x * weight.view(1,C,1,1)
-        x = torch.fft.irfft2(x, s=(H, W), dim=(-2, -1), norm='ortho')
-        s = s.mean(dim=(-2, -1))
-        x = self.fc(x)
+        x = x * weight
+        x = torch.fft.irfft2(x, s=(H, W), dim=(1,2), norm='ortho')
         return x
-'''
     

07d03d524ddc8d91352a30542f543e4131717ba1
s4698486 <s4698486@student.uq.edu.au>
2024-10-20 15:44:59 +1000

    Adds constants.py, which contains constants to be shared between files - getting around circular import issues

---
+++ STYLEGAN-ADNI-Generator-s4698486/constants.py
@@ -0,0 +1,17 @@
+# Path
+DATA = "/home/groups/comp3710/ADNI/AD_NC/"
+
+# Hyper Parameters
+epochs = 50            # Number of epochs to train
+learning_rate = 0.001    # Learning rate
+channels = 1            # Number of channels (3 channels for the image if RGB)
+batch_size = 32         # Batch Size
+log_resolution = 8      # 256*256 image size as such 2^8 = 256 # use 2^7 for single gpu (faster)
+image_height = 2**log_resolution    # The height of the generated image
+image_width = 2**log_resolution     # The width of the generated image
+z_dim = 256             # Size of the z latent space [initialise to 256 for lower VRAM usage or faster training]
+w_dim = 256             # Size of the style vector latent space [initialise to 256 for lower VRAM usage or faster training]
+lambda_gp = 10          # WGAN-GP set to standard value 10
+                        
+
+save = "save"           # Rename if changing a parameter and require a new dir for saved eg

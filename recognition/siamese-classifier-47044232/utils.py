5e2b8358945560cda447b7e9255c535e82996c95
DeadmanJ <jdeadman02@gmail.com>
2024-10-20 16:33:25 +1000

    Improved outputs of split_data as it now keeps the image's label.

--- recognition/siamese-classifier-47044232/utils.py
+++ recognition/siamese-classifier-47044232/utils.py
@@ -16,31 +16,32 @@ def split_data(label_path) -> tuple[list, list, list]:
     Arguments:
         label_path (str): The absolute path to .csv file holding labels.
     Returns:
-        A tuple of lists holding the image names to go into each of the sets.
-        The order of lists is (Training, Testing, Validaiton
+        A tuple of lists holding dicts with the isic_id and target value.
+        The order of lists is (Training, Testing, Validaiton).
     """
     df = pd.read_csv(label_path)
     malignant_count = sum(df["target"] == 1)
     malignant = df[df["target"] == 1]
-    malignant = malignant.drop(["patient_id", "target"], axis="columns")
+    malignant = malignant.drop("patient_id", axis="columns")
+    # Reduce benign samples to the same size as malignant
     benign = df.drop(labels=malignant.index.to_list()).sample(n=malignant_count)
-    benign = benign.drop(["patient_id", "target"], axis="columns")
+    benign = benign.drop("patient_id", axis="columns")
 
-
+    # Exclusive sets of malignant images
     test_m = malignant.sample(n=math.floor(malignant_count * (TESTING+VALIDATION)))
     val_m = test_m.sample(n=math.floor(malignant_count * VALIDATION))
     train_m = malignant.drop(labels=test_m.index.to_list())
     test_m = test_m.drop(labels=val_m.index.to_list())
-
+    
+    # Exclusive sets of benign data
     test_b = benign.sample(n=math.floor(malignant_count * (TESTING+VALIDATION)))
     val_b = test_b.sample(n=math.floor(malignant_count * VALIDATION))
     train_b = benign.drop(labels=test_b.index.to_list())
     test_b = test_b.drop(labels=val_b.index.to_list())
 
-    train = pd.concat([train_m, train_b])["isic_id"].tolist()
-    test = pd.concat([test_m, test_b])["isic_id"].tolist()
-    val = pd.concat([val_m, val_b])["isic_id"].tolist()
+    # Sample used to randomise the order
+    train = pd.concat([train_m, train_b]).sample(frac=1)
+    test = pd.concat([test_m, test_b]).sample(frac=1)
+    val = pd.concat([val_m, val_b]).sample(frac=1)
 
-    print(len(train), len(test), len(val))
-
-    return train, test, val
+    return train.to_dict(orient="records"), test.to_dict(orient="records"), val.to_dict(orient="records")

f1da01757fdb80c7775af7b741a7e545bd92b3a3
karanVijayShankar <s4710142@student.uq.edu.au>
2024-10-21 07:06:42 +1000

    Updated training code
    Reimplemented dice coefficient calculation and evaluation during training. Included additional code to ensure TensorFlow model trains effectively

--- recognition/UNet2D_HipMRI_Project_47101425/train.py
+++ recognition/UNet2D_HipMRI_Project_47101425/train.py
@@ -4,49 +4,39 @@ import tensorflow as tf
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
     print(f"Num GPUs Available: {len(gpus)}")
 else:
     print("No GPUs available, using CPU.")
 
 
 def dice_coefficient(y_true, y_pred, epsilon=1e-6):
-    intersection = tf.reduce_sum(y_true * y_pred)
-    dice = (2. * intersection + epsilon) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + epsilon)
-    return dice
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    intersection = tf.reduce_sum(y_true * y_pred, axis=[1, 2, 3])
+    dice = (2. * intersection + epsilon) / (tf.reduce_sum(y_true, axis=[1, 2, 3]) + tf.reduce_sum(y_pred, axis=[1, 2, 3]) + epsilon)
+    return tf.reduce_mean(dice)
 
-def train_model(model, train_loader, criterion, optimizer, num_epochs=50):
-    model.compile(optimizer=optimizer, loss=criterion, metrics=[dice_coefficient])
-    print("> Training")
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coefficient(y_true, y_pred)
 
-    for epoch in range(num_epochs):
-        print(f"Epoch {epoch + 1}/{num_epochs}")
-
-        # Reset the metrics at the start of the epoch
-        running_loss = 0.0
-        running_dice = 0.0
-
-        for i, (images, labels) in enumerate(train_loader):
-            # Perform a training step
-            loss, dice_score = model.train_on_batch(images, labels)
-            running_loss += loss
-            running_dice += dice_score
-
-            if (i + 1) % 100 == 0:
-                print(f"Step [{i+1}/{len(train_loader)}], Loss: {loss:.5f}, Dice: {dice_score:.5f}")
-
-        avg_loss = running_loss / len(train_loader)
-        avg_dice = running_dice / len(train_loader)
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Average Loss: {avg_loss:.5f}, Average Dice: {avg_dice:.5f}')
+def combined_loss(y_true, y_pred):
+    bce = tf.keras.losses.binary_crossentropy(y_true, y_pred)
+    d_loss = dice_loss(y_true, y_pred)
+    return bce + d_loss
 
 input_dims = (256, 144, 1) 
 model = UNet(input_dims=input_dims)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001) 
-criterion = tf.keras.losses.BinaryCrossentropy()
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+
+model.compile(optimizer=optimizer, loss=combined_loss, metrics=[dice_coefficient])
 
 image_dir = "C:/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/HipMRI_study_keras_slices_data/keras_slices_train"
 train_dataset = MedicalImageDataset(image_dir=image_dir, normImage=True, batch_size=8, shuffle=True)
 dataset = train_dataset.get_dataset()
 
-train_model(model, dataset, criterion, optimizer, num_epochs=1)
+model.fit(dataset, epochs=1, steps_per_epoch=len(dataset))
+
 model.summary()
 model.save('unet_model', save_format='tf')

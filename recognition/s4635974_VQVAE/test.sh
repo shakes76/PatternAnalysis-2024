e47002ccc770a3191151d8271ac2c0912e737c7c
dcollie87 <dcollie87@hotmail.com>
2024-10-21 08:58:32 +1000

    testing test.sh

---
+++ recognition/s4635974_VQVAE/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --partition=a100-test
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=s4635974@student.uq.edu.au
+#SBATCH --output=test_final.out
+
+conda activate torch
+python predict.py

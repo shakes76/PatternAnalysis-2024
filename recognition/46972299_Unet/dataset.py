946a86791e348c10b3643456d2f608ef23e5ff75
TrumpetFlotty <carl_flottmann@optusnet.com.au>
2024-10-20 10:03:28 +1000

    Sort the images and masks so that they match now

--- recognition/46972299_Unet/dataset.py
+++ recognition/46972299_Unet/dataset.py
@@ -8,16 +8,11 @@ from torch.utils.data import Dataset
 from torchvision.transforms import v2 as transforms
 from pathlib import Path
 from utils import load_data_3D, cur_time
+import re
 
-# HipMRO_Study_open
+# HipMRI_Study_open
 # --> semantic_labels_only
 # --> semantic_MRs
-# --> keras_slices_data
-# ------> keras_slices_seg_test
-# ------> keras_slices_train
-# ------> keras_slices_seg_validate
-# ------> keras_slices_seg_train
-# ------> keras_slices_validate
 RANGPUR_DATA_DIR = "/home/groups/comp3710/HipMRI_Study_open/"
 SEMANTIC_LABELS = "semantic_labels_only"
 SEMANTIC_MRS = "semantic_MRs"
@@ -28,12 +23,22 @@ WINDOWS_SEP = "\\"
 
 class ProstateDataset(Dataset):
     FILE_TYPE = "*.nii.gz"
+    # file names look like <letter><three-digit-number>_Week<number>_SEMANTIc or LFOV
+    PREFIX_MATCH = r'([A-Z]+\d+_Week\d+)'
 
     def __init__(self, image_dir: str, mask_dir: str, num_classes: int, num_load: int = None, start_t: float = None) -> None:
-        images = [
-            image_dir + file.name for file in Path(image_dir).glob(self.FILE_TYPE) if file.is_file()]
-        masks = [
-            mask_dir + file.name for file in Path(mask_dir).glob(self.FILE_TYPE) if file.is_file()]
+        image_names = [file.name for file in Path(
+            image_dir).glob(self.FILE_TYPE) if file.is_file()]
+        mask_names = [file.name for file in Path(
+            mask_dir).glob(self.FILE_TYPE) if file.is_file()]
+
+        # ensure each index of the dataset matches in the images and masks
+        image_names = sorted(image_names, key=self.__match_dataset_names)
+        mask_names = sorted(mask_names, key=self.__match_dataset_names)
+
+        # add on the directory so we can load properly
+        images = [image_dir + name for name in image_names]
+        masks = [mask_dir + name for name in mask_names]
 
         if num_load is None:
             num_load = len(images)
@@ -60,3 +65,10 @@ class ProstateDataset(Dataset):
         mask = torch.tensor(mask, dtype=torch.int64).unsqueeze(0)
 
         return image, mask
+
+    def __match_dataset_names(self, filename: str) -> str:
+        match = re.search(self.PREFIX_MATCH, filename)
+        if match:
+            return match.group(1)  # the match on the prefix
+        else:
+            return None

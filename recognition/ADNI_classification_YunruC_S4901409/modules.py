7e7d03c3bba0a34d0d6cdd306ac0dcaf6c79de23
AprillC4300 <yunru.chen3@uqconnect.edu.au>
2024-10-20 13:46:58 +1000

    added GFNet class

--- recognition/ADNI_classification_YunruC_S4901409/modules.py
+++ recognition/ADNI_classification_YunruC_S4901409/modules.py
@@ -7,7 +7,7 @@ https://tintn.github.io/Implementing-Vision-Transformer-from-Scratch/
 import torch
 import torch.nn as nn
 import torch.fft
-from timm.models.layers import DropPath, to_2tuple
+from timm.models.layers import to_2tuple
 
 class PatchEmbed(nn.Module):
     """
@@ -88,7 +88,7 @@ class Block(nn.Module):
     mlp_ratio controls the expansion size of the hiden layer
     """
 
-    def __init__ (self, dim, mlp_ratio, drop = 0, h =14, w = 8):
+    def __init__ (self, dim, mlp_ratio=4, drop = 0, h =14, w = 8):
         super().__init__()
         self.norm1 = nn.LayerNorm(dim)
         self.filter = GFNetFilter(dim, h=h, w=w)
@@ -99,6 +99,64 @@ class Block(nn.Module):
     def forward(self, x):
         x = x + self.mlp(self.norm2(self.filter(self.norm1(x))))
         return x
+
+class GFNet(nn.Module):
+    """
+    GFNet model
+    """
+
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                mlp_ratio=4., drop_rate=0.):
+        super().__init__()
+        self.num_classes = num_classes
+        # num_features for consistency with other models
+        self.num_features = self.embed_dim = embed_dim
+        norm_layer = nn.LayerNorm
+
+        self.patch_embed = PatchEmbed(
+                img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        h = img_size // patch_size
+        w = h // 2 + 1
+
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, mlp_ratio=mlp_ratio,
+                drop=drop_rate, h=h, w=w)
+            for i in range(depth)])
+
+        self.norm = norm_layer(embed_dim)
+        
+        #classifier head
+        self.head = nn.Linear(self.num_features, num_classes)
+
+        #initialize the weights
+        self.apply(self._init_weights)
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x).mean(1)
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.final_dropout(x)
+        x = self.head(x)
+        return x
+
+
+
 
 
 

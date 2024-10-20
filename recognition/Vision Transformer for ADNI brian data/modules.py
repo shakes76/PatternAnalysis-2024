53817e1d165a76bf25fb4ef0dfbb6ef6e90f996c
rory2610 <rorymacleod17@gmail.com>
2024-10-20 23:43:38 +1000

    updated modules.py to have finished network classes (GFNet, MLP, PatchEmbed, Block and GlobalFilter). commented out unncessary classes for now

--- recognition/Vision Transformer for ADNI brian data/modules.py
+++ recognition/Vision Transformer for ADNI brian data/modules.py
@@ -5,17 +5,88 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.container import Sequential
+from functools import partial
+from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
 
 ## Using RaoyonGming GFNet github repo as starting point for model architecture code
 ## https://github.com/raoyongming/GFNet/blob/master/gfnet.py
+
+class GFNet(nn.Module):
+    def __init__(self, img_size, patch_size, in_channels=1, num_classes=2,
+                embed_dim=783, depth=12, ratio=3, norm_layer=None, dropout=0.3, drop_path_rate=0.1):
+        super().__init__()
+        self.num_classes=num_classes
+        self.num_features = self.embed_dim = embed_dim
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        self.patch_embed = PatchEmbed(img_size=img_size, patch_size=patch_size, 
+                                      in_chans=in_channels, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+        self.pos_drop = nn.Dropout(p=dropout)
+
+        h = img_size // patch_size
+        w = h // 2 + 1
+
+        dropPath = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]
+
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=embed_dim, mlp_ratio=ratio, norm_layer=norm_layer, h=h, w=w, drop=dropout, drop_path=dropPath[i]
+            ) for i in range(depth)
+        ])
+
+        self.norm = norm_layer(embed_dim)
+
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+        trunc_normal_(self.pos_embed, std=0.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+        
+    def get_classifier(self):
+        return self.head
+    
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x).mean(1)
+        return(x)
+    
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+
+### Feed forward section of GF network
 class MLP(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, dropout=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, dropout=0.):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         self.fnc1 = nn.Linear(in_features, hidden_features)
-        self.activation = nn.GELU
+        self.activation = act_layer
         self.fnc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(dropout)
 
@@ -64,28 +135,11 @@ class Block(nn.Module):
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, dropout=drop)
 
     def forward(self, x):
         x = x + self.drop_path(self.mlp(self.norm2(self.filter(self.norm1(x)))))
         return x
-
-    class BlockLayerScale(nn.Module):
-
-        def __init__(self, dim, mlp_ratio=4., drop=0., drop_path=0., act_layer=nn.GELU, 
-                    norm_layer=nn.LayerNorm, h=14, w=8, init_values=1e-5):
-            super().__init__()
-            self.norm1 = norm_layer(dim)
-            self.filter = GlobalFilter(dim, h=h, w=w)
-            self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-            self.norm2 = norm_layer(dim)
-            mlp_hidden_dim = int(dim * mlp_ratio)
-            self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-            self.gamma = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
-
-        def forward(self, x):
-            x = x + self.drop_path(self.gamma * self.mlp(self.norm2(self.filter(self.norm1(x)))))
-            return x
 
 class PatchEmbed(nn.Module):
     """ Image to Patch Embedding
@@ -109,21 +163,34 @@ class PatchEmbed(nn.Module):
         x = self.proj(x).flatten(2).transpose(1, 2)
         return x
 
+# class BlockLayerScale(nn.Module):
+#         def __init__(self, dim, mlp_ratio=4., drop=0., drop_path=0., act_layer=nn.GELU, 
+#                     norm_layer=nn.LayerNorm, h=14, w=8, init_values=1e-5):
+#             super().__init__()
+#             self.norm1 = norm_layer(dim)
+#             self.filter = GlobalFilter(dim, h=h, w=w)
+#             self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+#             self.norm2 = norm_layer(dim)
+#             mlp_hidden_dim = int(dim * mlp_ratio)
+#             self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+#             self.gamma = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
+#         def forward(self, x):
+#             x = x + self.drop_path(self.gamma * self.mlp(self.norm2(self.filter(self.norm1(x)))))
+#             return x
 
-class DownLayer(nn.Module):
-    """ Image to Patch Embedding
-    """
-    def __init__(self, img_size=56, dim_in=64, dim_out=128):
-        super().__init__()
-        self.img_size = img_size
-        self.dim_in = dim_in
-        self.dim_out = dim_out
-        self.proj = nn.Conv2d(dim_in, dim_out, kernel_size=2, stride=2)
-        self.num_patches = img_size * img_size // 4
-
-    def forward(self, x):
-        B, N, C = x.size()
-        x = x.view(B, self.img_size, self.img_size, C).permute(0, 3, 1, 2)
-        x = self.proj(x).permute(0, 2, 3, 1)
-        x = x.reshape(B, -1, self.dim_out)
-        return x
+# class DownLayer(nn.Module):
+#     """ Image to Patch Embedding
+#     """
+#     def __init__(self, img_size=56, dim_in=64, dim_out=128):
+#         super().__init__()
+#         self.img_size = img_size
+#         self.dim_in = dim_in
+#         self.dim_out = dim_out
+#         self.proj = nn.Conv2d(dim_in, dim_out, kernel_size=2, stride=2)
+#         self.num_patches = img_size * img_size // 4
+#     def forward(self, x):
+#         B, N, C = x.size()
+#         x = x.view(B, self.img_size, self.img_size, C).permute(0, 3, 1, 2)
+#         x = self.proj(x).permute(0, 2, 3, 1)
+#         x = x.reshape(B, -1, self.dim_out)
+#         return x

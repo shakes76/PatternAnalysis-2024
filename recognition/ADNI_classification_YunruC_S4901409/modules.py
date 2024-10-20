4b1defc7e313a7cf8c0f1abda201a8e8922079f4
AprillC4300 <yunru.chen3@uqconnect.edu.au>
2024-10-20 13:03:05 +1000

    added MLP class

--- recognition/ADNI_classification_YunruC_S4901409/modules.py
+++ recognition/ADNI_classification_YunruC_S4901409/modules.py
@@ -60,3 +60,26 @@ class GFNetFilter(nn.Module):
         x = torch.fft.irfft2(x, s=(H, W), dim=(1,2), norm='ortho')
         return x
     
+class MLP(nn.Module):
+    """
+    Multilayer perceptron
+    """
+
+    def __init__(self, in_features, hidden_features = None, out_features=None, drop=0.0):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1= nn.Linear(in_features, hidden_features)
+        self.act = nn.GELU
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self,x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+    
+
+

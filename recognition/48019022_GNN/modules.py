b7e785ea4a0b4be07873d24a0467889071108c56
anthonyngo <mail.anthonyngo@gmail.com>
2024-10-21 04:00:45 +1000

    Milestone: Implemented basic GCN module with two convolutional layers.

---
+++ recognition/48019022_GNN/modules.py
@@ -0,0 +1,25 @@
+"""
+A file containing the Graph Neural Network architectures to be used.
+These include: GCN, GAT, SGC, JK-Nets
+@author Anthony Ngo
+@date 21/10/2024
+"""
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class GCN(torch.nn.Module):
+    """
+    A simple graph convolutional network architecture
+    """
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, output_dim)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))  # Apply GCN layer and ReLU
+        x = F.dropout(x, training=self.training)  # Dropout for regularization
+        x = self.conv2(x, edge_index)  # Second GCN layer
+        return F.log_softmax(x, dim=1)  # Output class probabilities

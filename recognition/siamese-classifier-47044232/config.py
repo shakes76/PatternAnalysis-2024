3d0e2edf1c80979435ea5d867acd934e2bac8f1d
DeadmanJ <jdeadman02@gmail.com>
2024-10-20 18:35:15 +1000

    Defined the needed hyperparamters with some preliminary values

--- recognition/siamese-classifier-47044232/config.py
+++ recognition/siamese-classifier-47044232/config.py
@@ -5,10 +5,19 @@ Made by Joshua Deadman
 """
 
 DATAPATH = "./data"
+MODELPATH = "./models"
 
 # The percent of malignant data in each set (should add up to 1).
 TRAINING = 0.7
 TESTING = 0.2
 VALIDATION = 0.1
 
-# Add other variables when the train.py file is being made
+# Hyperparameters
+BATCH_SIZE = 32
+WORKERS = 4
+EPOCHS = 64
+LEARNING_RATE = 0.0002
+BETAS = (0.9,0.999)
+LOSS_MARGIN = 1
+BENIGN_LABEL = 0
+MALIGNANT_LABEL = 1

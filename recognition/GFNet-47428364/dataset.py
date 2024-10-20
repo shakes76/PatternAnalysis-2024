ceddb72eacd6982f2c8c7e9fb3a7657a0cf77c29
s47428364 <caleb.clarke@student.uq.edu.au>
2024-10-20 13:30:05 +1000

    Updated dataset to transform the datasets and updated the approporate documents. Model is still overfitting a bit, but is better.

--- recognition/GFNet-47428364/dataset.py
+++ recognition/GFNet-47428364/dataset.py
@@ -1,7 +1,27 @@
 import os
+import torch
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader, Subset, SubsetRandomSampler
+
+'''
+Gets the mean and standard deviations values from a dataloader
+'''
+def get_mean_std(dataloader):
+    mean = 0.0
+    mean_sqr = 0.0
+    n = 0
+
+    for inputs, _ in dataloader:
+        n += inputs.size(0)
+        mean += inputs.sum(dim=(0,2,3))
+        mean_sqr += (inputs ** 2).sum(dim=(0,2,3))
+
+    mean /= n * inputs.size(2) * inputs.size(3)
+    mean_sqr /= n * inputs.size(2) * inputs.size(3)
+    std = (mean_sqr - mean ** 2).sqrt()
+
+    return mean, std 
 
 '''
 Gets the list of all patient ID's from the data
@@ -13,16 +33,38 @@ def get_patient_ids(data_path):
     return patient_ids
 
 """ Returns the train and test dataloaders for the ADNI dataset """
-def get_dataloaders(batch_size=32, path="recognition/GFNet-47428364/AD_NC"):
+def get_dataloaders(batch_size=32, image_size=224, path="recognition/GFNet-47428364/AD_NC"):
     # Create transformer
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+    pre_transforms = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
     ])
 
+    pre_train_dataset = ImageFolder(root=path+"/train", transform=pre_transforms)
+    sampler = SubsetRandomSampler(torch.randperm(len(pre_train_dataset))[:1000])
+    sample_loader = DataLoader(pre_train_dataset, batch_size=batch_size, sampler=sampler)
+
+    mean, std = get_mean_std(sample_loader)
+
+    # Normalised transformations for training
+    train_transforms = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.RandomRotation(5),
+        transforms.RandomResizedCrop(image_size, scale=(0.9, 1.1)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
+    # Normalised transformations for testing
+    test_transformers = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+
     # Generate datasets
-    train_dataset = ImageFolder(root=path+"/train", transform=transform)
-    test_dataset = ImageFolder(root=path+"/test", transform=transform)
+    train_dataset = ImageFolder(root=path+"/train", transform=train_transforms)
+    test_dataset = ImageFolder(root=path+"/test", transform=test_transformers)
 
     patient_ids = get_patient_ids(path+"/train")
     train_indices = [i for i, (path_, _) in enumerate(train_dataset.samples) if path_.split('\\')[-1].split('_')[0] in patient_ids]

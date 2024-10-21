16e1e6fcd38fd4a008393c4bc55faf3fa8145a0e
DeadmanJ <jdeadman02@gmail.com>
2024-10-22 03:06:12 +1000

    Implemented the classification of the feature vector and some simple evauation. NOTE: The new code has minimal documentation and must be reviewed next.

--- recognition/siamese-classifier-47044232/train.py
+++ recognition/siamese-classifier-47044232/train.py
@@ -9,13 +9,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 import torch
-from torch.nn import TripletMarginLoss
+from torch.nn import TripletMarginLoss, CrossEntropyLoss
 from torch.optim.adam import Adam
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2
 
 import config
-from modules import SiameseNetwork
+from modules import SiameseNetwork, BinaryClassifier
 from dataset import ISICKaggleChallengeSet
 from utils import split_data
 
@@ -46,8 +46,6 @@ val_loader = DataLoader(val_set, batch_size=config.BATCH_SIZE, num_workers=confi
 model = SiameseNetwork().to(device)
 tripletloss = TripletMarginLoss(margin=config.LOSS_MARGIN)
 optimiser = Adam(model.parameters(), lr=config.LEARNING_RATE, betas=config.BETAS)
-
-# TODO generate some loss plots and use learned data to make a classifier.
 
 train_loss = []
 val_loss = []
@@ -100,11 +98,63 @@ for epoch in range(config.EPOCHS):
 stop = time.time()
 print(f"Training complete! It took {(start-stop)/60} minutes")
 
+# TODO generate some loss plots
+
+# The features can be stored while testing as training is done
+testing_features = []
+testing_labels = []
+
 # Evaluating the Siamese Network with test data
 model.eval()
 with torch.no_grad(): # Reduces memory usage
     for i, (anchor, positive, negative, label) in enumerate(test_loader):
-        loss = processes_batch(anchor, positive, negative)
+        anchor = anchor.to(device)
+        positive = positive.to(device)
+        negative = negative.to(device)
+
+        # Evaluate images
+        anchor_result, positive_result, negative_result = model(anchor, positive, negative)
+        testing_features.append(anchor_result)
+        testing_labels.append(label)
+
+        loss = tripletloss(anchor_result, positive_result, negative_result)
         test_loss.append(loss)
         if i % (len(test_loader)//2) == 0 and i != len(test_loader)-1:
             print(f"Testing: Batch: {i}, Loss: {loss.item()}")
+
+# TODO add saving of model to config.MODELPATH here
+
+""" Taking the feature vectors and using them to train a classifier. """
+# Obtain the training features
+training_features = []
+training_labels = []
+with torch.no_grad():
+    for anchor, _, _, label in train_loader:
+        features = model.forward_once(anchor.to(device))
+        training_features.append(features)
+        training_labels.append(label)
+
+model = BinaryClassifier().to(device)
+cross = CrossEntropyLoss()
+optimiser = Adam(model.parameters(), 0.001, (0.9, 0.999))
+for epoch in range(config.EPOCHS):
+    for feature, label in zip(training_features, training_labels):
+        optimiser.zero_grad()
+        out = model(feature)
+        cross(out, label.to(device)).backward()
+        optimiser.step()
+
+# Testing the classifier
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for features, labels in zip(testing_features, testing_labels):
+        out = model(features)
+        _, predicted = torch.max(out.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels.to(device)).sum().item()
+
+    print('Test Accuracy: {} %'.format(100 * correct / total))
+
+# TODO add saving of model to config.MODELPATH here

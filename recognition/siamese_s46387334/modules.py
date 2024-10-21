64d25ee9f8c3fcb005f37adafe6a3cbda522f26b
timryall <88timryall@gmail.com>
2024-10-21 10:40:51 +1000

    feat: create train.py template file

--- recognition/siamese_s46387334/modules.py
+++ recognition/siamese_s46387334/modules.py
@@ -1,4 +1,5 @@
 """
 Contains the source code for the components of the Siamese Net and Classifier.
+
 Each component is implementated as a class or a function.
 """
---
+++ recognition/siamese_s46387334/train.py
@@ -0,0 +1,6 @@
+"""
+Contains the source code for training, validating, testing and saving the model. 
+
+The model is imported from “modules.py” and the data loader is imported from “dataset.py”. 
+Plots of the losses and metrics during training will be produced.
+"""

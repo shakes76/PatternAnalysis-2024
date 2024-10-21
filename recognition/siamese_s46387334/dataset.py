3b902b388c14a537389889fa8282b8724de723cf
timryall <88timryall@gmail.com>
2024-10-21 11:53:29 +1000

    feat: add function to preform train validation test split on the data

--- recognition/siamese_s46387334/dataset.py
+++ recognition/siamese_s46387334/dataset.py
@@ -5,6 +5,8 @@ Contains the data loader for loading and preprocessing the ISIC 2020 Data
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 
 def get_isic2020_data(metadata_path, image_dir, data_subset):
@@ -28,3 +30,44 @@ def get_isic2020_data(metadata_path, image_dir, data_subset):
     # Get a list of the labels for the images we are using
     labels = [image_to_label[os.path.basename(path)] for path in image_paths]
     return np.array(image_paths), np.array(labels)
+
+def train_val_test_split(images, labels):
+    """
+    0.8 0.1 0.1 split
+
+    Stratified sampling
+    With oversampling of the training data to ensure that the
+    classes are balanced in training.
+    
+    Returns: train_images, val_images, test_images, train_labels, val_labels, test_labels
+    """
+    train_images, other_images, train_labels, other_labels = train_test_split(images, labels, test_size=0.2, stratify=labels)
+    test_images, val_images, test_labels, val_labels = train_test_split(other_images, other_labels, test_size=0.5, stratify=other_labels)
+
+    # Preform oversampling of training data
+    # Split the images into class 0 and class 1
+    class_0_images = train_images[train_labels == 0]
+    class_1_images = train_images[train_labels == 1]
+    class_0_labels = train_labels[train_labels == 0]
+    class_1_labels = train_labels[train_labels == 1]
+    
+    # Number of samples to match class 0
+    num_class_0 = len(class_0_images)
+    num_class_1 = len(class_1_images)
+    
+    # If class 1 has fewer samples, oversample it
+    if num_class_1 < num_class_0:
+        # Randomly choose from class_1_images to oversample it
+        oversample_indices = np.random.choice(np.arange(num_class_1), size=num_class_0 - num_class_1, replace=True)
+        oversampled_class_1_images = class_1_images[oversample_indices]
+        oversampled_class_1_labels = class_1_labels[oversample_indices]
+        
+        # Combine original class 1 images with the oversampled ones
+        class_1_images = np.concatenate([class_1_images, oversampled_class_1_images], axis=0)
+        class_1_labels = np.concatenate([class_1_labels, oversampled_class_1_labels], axis=0)
+    
+    # Concatenate class 0 and the new class 1 images to get the final balanced dataset
+    train_images = np.concatenate([class_0_images, class_1_images], axis=0)
+    train_labels = np.concatenate([class_0_labels, class_1_labels], axis=0)
+
+    return train_images, val_images, test_images, train_labels, val_labels, test_labels

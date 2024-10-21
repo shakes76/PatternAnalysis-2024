36d33a988d2c0f576dd300cb6e055b210a948ccd
4mark4444 <yaozheng4444@gmail.com>
2024-10-21 15:30:54 +0800

    managed to load the data and see one picture from the data base to have a better understanding of the data set.

--- main.py
+++ main.py
@@ -3,12 +3,56 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 import glob
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
 # Define the paths to your image and label directories
 image_dir = r'D:\final\Labelled_weekly_MR_images_of_the_male_pelvis-Xken7gkM-\data\HipMRI_study_complete_release_v1\semantic_MRs_anon'
 label_dir = r'D:\final\Labelled_weekly_MR_images_of_the_male_pelvis-Xken7gkM-\data\HipMRI_study_complete_release_v1\semantic_labels_anon'
 
 
+def unet_3d(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+
+    # Encoder path
+    c1 = layers.Conv3D(32, (3, 3, 3), activation='relu', padding='same')(inputs)
+    c1 = layers.Conv3D(32, (3, 3, 3), activation='relu', padding='same')(c1)
+    p1 = layers.MaxPooling3D((2, 2, 2))(c1)
+
+    c2 = layers.Conv3D(64, (3, 3, 3), activation='relu', padding='same')(p1)
+    c2 = layers.Conv3D(64, (3, 3, 3), activation='relu', padding='same')(c2)
+    p2 = layers.MaxPooling3D((2, 2, 2))(c2)
+
+    c3 = layers.Conv3D(128, (3, 3, 3), activation='relu', padding='same')(p2)
+    c3 = layers.Conv3D(128, (3, 3, 3), activation='relu', padding='same')(c3)
+    p3 = layers.MaxPooling3D((2, 2, 2))(c3)
+
+    # Bottleneck
+    c4 = layers.Conv3D(256, (3, 3, 3), activation='relu', padding='same')(p3)
+    c4 = layers.Conv3D(256, (3, 3, 3), activation='relu', padding='same')(c4)
+
+    # Decoder path
+    u5 = layers.UpSampling3D((2, 2, 2))(c4)
+    u5 = layers.concatenate([u5, c3])
+    c5 = layers.Conv3D(128, (3, 3, 3), activation='relu', padding='same')(u5)
+    c5 = layers.Conv3D(128, (3, 3, 3), activation='relu', padding='same')(c5)
+
+    u6 = layers.UpSampling3D((2, 2, 2))(c5)
+    u6 = layers.concatenate([u6, c2])
+    c6 = layers.Conv3D(64, (3, 3, 3), activation='relu', padding='same')(u6)
+    c6 = layers.Conv3D(64, (3, 3, 3), activation='relu', padding='same')(c6)
+
+    u7 = layers.UpSampling3D((2, 2, 2))(c6)
+    u7 = layers.concatenate([u7, c1])
+    c7 = layers.Conv3D(32, (3, 3, 3), activation='relu', padding='same')(u7)
+    c7 = layers.Conv3D(32, (3, 3, 3), activation='relu', padding='same')(c7)
+
+    # Output layer
+    outputs = layers.Conv3D(num_classes, (1, 1, 1), activation='softmax')(c7)
+
+    model = keras.Model(inputs=[inputs], outputs=[outputs])
+    return model
 
 
 # Get lists of image and label files

388b07a566220e5dd885d76ab7d771fdad11972f
unknown <hongyabright@gmail.com>
2024-10-21 11:17:41 +1000

    refactor(modules.py): increased init_feature size and convolution layers, improved Dice score but class 4 imbalance persists

--- recognition/UNet3D_s4764092/modules.py
+++ recognition/UNet3D_s4764092/modules.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class UNet3D(nn.Module):
-    def __init__(self, in_channels=1, out_channels=6, init_features=8):
+    def __init__(self, in_channels=1, out_channels=6, init_features=32):
         super(UNet3D, self).__init__()
 
         features = init_features
@@ -15,11 +15,15 @@ class UNet3D(nn.Module):
         self.pool3 = nn.MaxPool3d(kernel_size=2, stride=2)
         self.encoder4 = UNet3D._block(features * 4, features * 8)
         self.pool4 = nn.MaxPool3d(kernel_size=2, stride=2)
+        self.encoder5 = UNet3D._block(features * 8, features * 16)  # New layer
+        self.pool5 = nn.MaxPool3d(kernel_size=2, stride=2)
 
         # Bottleneck
-        self.bottleneck = UNet3D._block(features * 8, features * 16)
+        self.bottleneck = UNet3D._block(features * 16, features * 32)
 
         # Decoder
+        self.upconv5 = nn.ConvTranspose3d(features * 32, features * 16, kernel_size=2, stride=2)
+        self.decoder5 = UNet3D._block(features * 16 * 2, features * 16)  # New layer
         self.upconv4 = nn.ConvTranspose3d(features * 16, features * 8, kernel_size=2, stride=2)
         self.decoder4 = UNet3D._block(features * 8 * 2, features * 8)
         self.upconv3 = nn.ConvTranspose3d(features * 8, features * 4, kernel_size=2, stride=2)
@@ -39,23 +43,25 @@ class UNet3D(nn.Module):
         enc2 = self.encoder2(self.pool1(enc1))
         enc3 = self.encoder3(self.pool2(enc2))
         enc4 = self.encoder4(self.pool3(enc3))
+        enc5 = self.encoder5(self.pool4(enc4))  # New layer
 
         # Bottleneck
-        bottleneck = self.bottleneck(self.pool4(enc4))
+        bottleneck = self.bottleneck(self.pool5(enc5))
 
         # Decoder path
-        dec4 = self.upconv4(bottleneck)
+        dec5 = self.upconv5(bottleneck)  # New layer
+        dec5 = torch.cat((dec5, enc5), dim=1)
+        dec5 = self.decoder5(dec5)
+
+        dec4 = self.upconv4(dec5)
         dec4 = torch.cat((dec4, enc4), dim=1)
         dec4 = self.decoder4(dec4)
-
         dec3 = self.upconv3(dec4)
         dec3 = torch.cat((dec3, enc3), dim=1)
         dec3 = self.decoder3(dec3)
-
         dec2 = self.upconv2(dec3)
         dec2 = torch.cat((dec2, enc2), dim=1)
         dec2 = self.decoder2(dec2)
-
         dec1 = self.upconv1(dec2)
         dec1 = torch.cat((dec1, enc1), dim=1)
         dec1 = self.decoder1(dec1)

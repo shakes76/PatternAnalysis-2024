9eb341763032bd3e8b86f1444f7da250292af0bf
Mr Yuda Hendriawan Budi Handoko <s4708525@login0.compute.eait.uq.edu.au>
2024-10-21 20:02:44 +1000

    Create: create utils files to organize code

---
+++ recognition/VisionTransformer-s4708525/utils.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def show_plot_loss(train_losses, val_losses):
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_losses, label='Training Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("loss_plot_7.png", bbox_inches='tight')  # Save the plot if you want
+    plt.show()

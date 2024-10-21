c381b258b80ddd3ecbfa0f4fe22f99923a33cce5
Mr Yuda Hendriawan Budi Handoko <s4708525@login0.compute.eait.uq.edu.au>
2024-10-21 21:52:44 +1000

    Update: add train,validate,test loader with the transformation

--- recognition/VisionTransformer-s4708525/dataset.py
+++ recognition/VisionTransformer-s4708525/dataset.py
@@ -1,7 +1,13 @@
 from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 import os
 from PIL import Image
+
+train_path_local = '/Users/yudahendriawan/Course [LOCAL]/PATTERN/projects/AD_NC/train/'
+test_path_local = '/Users/yudahendriawan/Course [LOCAL]/PATTERN/projects/AD_NC/test/'
+
+train_path_server = '/home/groups/comp3710/ADNI/AD_NC/train/'
+test_path_server = '/home/groups/comp3710/ADNI/AD_NC/test/'
 
 class ADNI_Dataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -32,3 +38,32 @@ class ADNI_Dataset(Dataset):
 
     def get_category_counts(self):
         return self.category_counts
+
+transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(20),
+        transforms.CenterCrop(128),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.1155, 0.1155, 0.1155], std=[0.2224, 0.2224, 0.2224])
+    ])
+
+transform_test = transforms.Compose([
+    transforms.Resize(224),
+    transforms.CenterCrop(128),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.1155, 0.1155, 0.1155], std=[0.2224, 0.2224, 0.2224])
+])
+
+train_dataset = ADNI_Dataset(train_path_server, transform=transform)
+test_dataset = ADNI_Dataset(test_path_server, transform=transform_test)
+
+val_split = 0.2 
+
+train_size = int((1 - val_split) * len(train_dataset))
+val_size = len(train_dataset) - train_size
+train_subset, val_subset = random_split(train_dataset, [train_size, val_size])
+
+train_loader = DataLoader(train_subset, batch_size=16, shuffle=True)
+val_loader = DataLoader(val_subset, batch_size=16, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)

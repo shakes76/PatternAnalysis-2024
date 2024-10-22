05a9c111d557fea4ab9a6131c5a878e49ae42639
Mr Yuda Hendriawan Budi Handoko <s4708525@login0.compute.eait.uq.edu.au>
2024-10-22 20:54:51 +1000

    Update: make function to visualize training and validation loss and accuracy

--- recognition/VisionTransformer-s4708525/utils.py
+++ recognition/VisionTransformer-s4708525/utils.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
 def show_plot_loss(train_losses, val_losses):
+    """
+    Function to handle the visualization of the training and validation loss
+
+    Args:
+        train_losses: list of training loss
+        val_losses: list of validation loss
+    """
     plt.figure(figsize=(10, 5))
     plt.plot(train_losses, label='Training Loss')
     plt.plot(val_losses, label='Validation Loss')
@@ -9,5 +16,22 @@ def show_plot_loss(train_losses, val_losses):
     plt.ylabel('Loss')
     plt.legend()
     plt.grid(True)
-    plt.savefig("loss_plot_7.png", bbox_inches='tight')  # Save the plot if you want
-    plt.show()
+    plt.savefig("results/train_val_loss_001.png", bbox_inches='tight')
+
+def show_plot_accuracy(train_acc, val_acc):
+    """
+    Function to handle the visualization of the training and validation accuracy
+
+    Args:
+        train_acc: list of training accuracy
+        val_acc: list of validation accuracy
+    """
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_losses, label='Training Accuracy')
+    plt.plot(val_losses, label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("results/train_val_accuracy_001.png", bbox_inches='tight')

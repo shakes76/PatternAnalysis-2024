e82632d4f6004ae6722215f605ce5b5deefe951f
beenalf <sth48624bt@outlook.com>
2024-10-22 22:49:32 +1000

    Simplified training and evaluation in train.py as well as importing configuration settings from utils.py

--- recognition/GFNet_s4784738/train.py
+++ recognition/GFNet_s4784738/train.py
@@ -7,8 +7,10 @@ s4784738
 """
 
 import datetime
+import os
 from typing import Optional
 import numpy as np
+from matplotlib import pyplot as plt
 import time
 import torch
 import torch.backends.cudnn as cudnn
@@ -24,110 +26,113 @@ import torch.nn as nn
 
 from dataset import get_data_loader
 from modules import GFNet
-import utils
+from utils import get_parameters
 
-#import wandb_config
+from torchvision import transforms
+import torchvision.transforms.functional as TF
 
 def train_one_epoch(model: torch.nn.Module, criterion,
                     data_loader, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
-                    model_ema: Optional[ModelEma] = None, mixup_fn: Optional[Mixup] = None,
-                    set_training_mode=True):
-    model.train(set_training_mode)
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
-    header = 'Epoch: [{}]'.format(epoch)
-    print_freq = 100
+                    device: torch.device):
+    """
+    Trains the model for one epoch
+    """
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
 
-    for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
-        samples = samples.to(device, non_blocking=True)
-        targets = targets.to(device, non_blocking=True)
+    for inputs, labels in data_loader:
+        inputs, labels = inputs.to(device), labels.long().to(device)
 
-        if mixup_fn is not None:
-            samples, targets = mixup_fn(samples, targets)
-
-        with torch.amp.autocast('cuda'):
-            outputs = model(samples)
-            #loss = criterion(samples, outputs, targets)
-            loss = criterion(outputs, targets)
-
-
-        loss_value = loss.item()
         optimizer.zero_grad()
 
-        # this attribute is added by timm on one optimizer (adahessian)
-        is_second_order = hasattr(optimizer, 'is_second_order') and optimizer.is_second_order
-        loss_scaler(loss, optimizer, clip_grad=max_norm,
-                        parameters=model.parameters(), create_graph=is_second_order)
-    
-        torch.cuda.synchronize()
-        if model_ema is not None:
-            model_ema.update(model)
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-        metric_logger.update(loss=loss_value)
-        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        # Convert outputs to predicted class (using argmax for multi-class classification)
+        _, predicted = torch.max(outputs, 1)
+        running_loss += loss.item()
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    # After each epoch, calculate loss and accuracy
+    epoch_loss = running_loss / len(data_loader)
+    accuracy = 100 * correct / total
+
+    return epoch_loss, accuracy
 
 
 @torch.no_grad()
-def evaluate(data_loader, model, device):
-    criterion = torch.nn.CrossEntropyLoss()
-
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    header = 'Test:'
-
-    # switch to evaluation mode
+def evaluate(data_loader, model, criterion, device):
+    """
+    Performs evaluation of the model for one epoch
+    """
     model.eval()
+    test_loss = 0.0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for inputs, labels in data_loader:
+            inputs, labels = inputs.to(device), labels.long().to(device)
 
-    for images, target in metric_logger.log_every(data_loader, 10, header):
-        images = images.to(device, non_blocking=True)
-        target = target.to(device, non_blocking=True)
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
 
-        # compute output
-        with torch.amp.autocast('cuda'):
-            output = model(images)
-            loss = criterion(output, target)
+            # Convert outputs to predicted class (using argmax for multi-class classification)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-        acc1, _ = accuracy(output, target, topk=(1, 5))
+    epoch_loss = test_loss / len(data_loader)
+    accuracy = 100 * correct / total
+    return epoch_loss, accuracy
+    
 
-        batch_size = images.shape[0]
-        metric_logger.update(loss=loss.item())
-        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
-        #metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
-
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print('* Acc@1 {top1.global_avg:.3f} loss {losses.global_avg:.3f}'
-          .format(top1=metric_logger.acc1, losses=metric_logger.loss))
-
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
-
-
-def train_GFNet(dataloaders, num_epochs):
+def train_GFNet(dataloaders):
     # Model architecture and hyperparameters
-    num_classes = 2  # (AD/Normal)
-    mixup = 0.0
-    smoothing = 0.0 
+    (epochs,
+    learning_rate,
+    patch_size,
+    embed_dim,
+    num_classes,
+    depth,
+    mlp_ratio,
+    drop_rate,
+    drop_path_rate,
+    weight_decay,
+    t_max,
+    eta_min
+    ) = get_parameters()
 
     start_time = time.time()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    #wandb_config.setup_wandb()
 
-    cudnn.benchmark = True
+    #cudnn.benchmark = True
 
     # Load training and validation datasets
-    data_loader_train = dataloaders['train']
-    data_loader_val = dataloaders['val']
+    data_loader_train, data_loader_val = dataloaders['train']
+    data_loader_test = dataloaders['test']
 
     # Create model with hard-coded parameters
     print(f"Creating GFNet model with img_size: 224x224")
     model = GFNet(
-        img_size=224, patch_size=16, num_classes=num_classes, embed_dim=512, depth=19,
-        mlp_ratio=4, drop_path_rate=0.0,
+        img_size=(224, 224),
+        patch_size=patch_size,
+        num_classes=num_classes,
+        in_chans=1, # For greyscale images
+        embed_dim=embed_dim,
+        depth=depth,
+        mlp_ratio=mlp_ratio,
+        drop_rate=drop_rate,
+        drop_path_rate=drop_path_rate,
         norm_layer=partial(nn.LayerNorm, eps=1e-6)
     ).to(device)
 
@@ -136,50 +141,50 @@ def train_GFNet(dataloaders, num_epochs):
         print(f"Memory Allocated: {torch.cuda.memory_allocated(0) / 1024 ** 3:.1f} GB")
         print(f"Memory Reserved: {torch.cuda.memory_reserved(0) / 1024 ** 3:.1f} GB")
 
-    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Number of parameters: {n_parameters}")
+    #n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    #print(f"Number of parameters: {n_parameters}")
 
     # Optimizer and scheduler
-    #optimizer = create_optimizer(args, model)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-4)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     #optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
 
-    loss_scaler = NativeScaler()
+    #loss_scaler = NativeScaler()
 
-    #lr_scheduler, _ = create_scheduler(args, optimizer)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=t_max, eta_min=eta_min)
     # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
     # Loss criterion
-    if mixup > 0:
-        criterion = SoftTargetCrossEntropy()
-    elif smoothing:
-        criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
-    else:
-        criterion = torch.nn.CrossEntropyLoss()
+    #criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
+    criterion = torch.nn.CrossEntropyLoss()
 
     # Track the training and validation accuracy and loss at each epoch
     train_acc, train_loss = [], []
     val_acc, val_loss = [], []
 
-    print(f"Start training for {num_epochs} epochs")
-    for epoch in range(num_epochs):
+    print(f"Start training for {epochs} epochs")
+    for epoch in range(epochs):
         # Train model
-        train_stats = train_one_epoch(model, criterion, data_loader_train, optimizer, device, epoch, loss_scaler, max_norm=0, model_ema=None)
+        train_a, train_l = train_one_epoch(model, criterion, data_loader_train, optimizer, device)
         # Validate model
-        test_stats = evaluate(data_loader_val, model, device)
+        val_a, val_l = evaluate(data_loader_val, model, criterion, device)
 
         # Track training and validation metrics
-        train_acc.append(train_stats['acc1'])
-        train_loss.append(train_stats['loss'])
-        val_acc.append(test_stats['acc1'])
-        val_loss.append(test_stats['loss'])
+        train_acc.append(train_a)
+        train_loss.append(train_l)
+        val_acc.append(val_a)
+        val_loss.append(val_l)
 
+        #lr_scheduler.step(val_a)
         lr_scheduler.step()
-        print(f'Accuracy of training set (epoch {epoch}): {train_stats["acc1"]:.1f}%, and loss {train_stats['loss']:.1f}')
-        print(f"Accuracy on validation set (epoch {epoch}): {test_stats['acc1']:.1f}%, and loss {test_stats['loss']:.1f}") 
+        
+        print(f'Accuracy of training set (epoch {epoch+1}/{epochs}): {train_a:.1f}%, and loss {train_l:.1f}')
+        print(f'Accuracy on validation set (epoch {epoch+1}/{epochs}): {val_a:.1f}%, and loss {val_l:.1f}') 
 
-    print('Saving model:')
+    print("### Now it's time to run inference on the test dataset ###")
+    test_acc, test_loss = evaluate(data_loader_test, model, criterion, device)
+    print(f'Accuracy on test set: {test_acc:.1f}, and loss: {test_loss:.1f}\n')
+    
+    print('Saving model...')
     torch.save(model.state_dict(), 'best_model.pth')
 
     total_time = time.time() - start_time
@@ -189,11 +194,11 @@ def train_GFNet(dataloaders, num_epochs):
 if __name__ == "__main__":
     # Paths to the training and validation datasets
     dataloaders = {
-        'train': get_data_loader("/home/groups/comp3710/ADNI/AD_NC/train", 'train', batch_size = 32, shuffle = True),
-        'val': get_data_loader("/home/groups/comp3710/ADNI/AD_NC/test", 'validate', batch_size = 32, shuffle = False)
+        'train': get_data_loader("/home/groups/comp3710/ADNI/AD_NC/train", 'train', batch_size = 64, shuffle = True, split=0.2),
+        'test': get_data_loader("/home/groups/comp3710/ADNI/AD_NC/test", 'test', batch_size = 64, shuffle = False, slit=0.2)
+        #'train': get_data_loader("../AD_NC/train", 'train', batch_size = 32, shuffle = True, split=0.2),
+        #'test': get_data_loader("../AD_NC/test", 'test', batch_size = 32, shuffle = False, split=0.2)
     }
 
-    num_epochs=10
-    print('Training for {num_epochs} epochs')
-    train_GFNet(dataloaders, num_epochs=num_epochs)
+    train_GFNet(dataloaders)
     print('Finished training')

1be60dcb163423f78a5699b09b70b5ae458382eb
4mark4444 <yaozheng4444@gmail.com>
2024-10-23 02:18:05 +0800

    changed the model and tried to improve the performance.

--- recognition/3D_improved U_Net_Zheng_Yao_s4769710/modules.py
+++ recognition/3D_improved U_Net_Zheng_Yao_s4769710/modules.py
@@ -3,59 +3,107 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import tensorflow_addons as tfa
 
-# Build the 3D U-Net model
 def unet_3d(input_shape, num_classes):
     """
-    This is currently a standard implementation of the U Net.
-    REF: This code came from the tutorial Youtube video. https://youtu.be/GAYJ81M58y8?si=jQqSwcT5bDk3JE-5
+    Improved U-Net model with Instance Normalization and Leaky ReLU activation functions.
     """
     inputs = keras.Input(shape=input_shape)
 
     # Encoder
-    # Does 3D convolution than a max pooling
-    c1 = layers.Conv3D(16, 3, activation='relu', padding='same')(inputs)
-    c1 = layers.Conv3D(16, 3, activation='relu', padding='same')(c1)
+    # Block 1
+    c1 = layers.Conv3D(16, 3, padding='same', use_bias=False)(inputs)
+    c1 = tfa.layers.InstanceNormalization()(c1)
+    c1 = layers.LeakyReLU(alpha=0.01)(c1)
+    c1 = layers.Conv3D(16, 3, padding='same', use_bias=False)(c1)
+    c1 = tfa.layers.InstanceNormalization()(c1)
+    c1 = layers.LeakyReLU(alpha=0.01)(c1)
     p1 = layers.MaxPooling3D(2)(c1)
 
-    c2 = layers.Conv3D(32, 3, activation='relu', padding='same')(p1)
-    c2 = layers.Conv3D(32, 3, activation='relu', padding='same')(c2)
+    # Block 2
+    c2 = layers.Conv3D(32, 3, padding='same', use_bias=False)(p1)
+    c2 = tfa.layers.InstanceNormalization()(c2)
+    c2 = layers.LeakyReLU(alpha=0.01)(c2)
+    c2 = layers.Conv3D(32, 3, padding='same', use_bias=False)(c2)
+    c2 = tfa.layers.InstanceNormalization()(c2)
+    c2 = layers.LeakyReLU(alpha=0.01)(c2)
     p2 = layers.MaxPooling3D(2)(c2)
 
-    c3 = layers.Conv3D(64, 3, activation='relu', padding='same')(p2)
-    c3 = layers.Conv3D(64, 3, activation='relu', padding='same')(c3)
+    # Block 3
+    c3 = layers.Conv3D(64, 3, padding='same', use_bias=False)(p2)
+    c3 = tfa.layers.InstanceNormalization()(c3)
+    c3 = layers.LeakyReLU(alpha=0.01)(c3)
+    c3 = layers.Conv3D(64, 3, padding='same', use_bias=False)(c3)
+    c3 = tfa.layers.InstanceNormalization()(c3)
+    c3 = layers.LeakyReLU(alpha=0.01)(c3)
     p3 = layers.MaxPooling3D(2)(c3)
 
-    c4 = layers.Conv3D(128, 3, activation='relu', padding='same')(p3)
-    c4 = layers.Conv3D(128, 3, activation='relu', padding='same')(c4)
+    # Block 4
+    c4 = layers.Conv3D(128, 3, padding='same', use_bias=False)(p3)
+    c4 = tfa.layers.InstanceNormalization()(c4)
+    c4 = layers.LeakyReLU(alpha=0.01)(c4)
+    c4 = layers.Conv3D(128, 3, padding='same', use_bias=False)(c4)
+    c4 = tfa.layers.InstanceNormalization()(c4)
+    c4 = layers.LeakyReLU(alpha=0.01)(c4)
     p4 = layers.MaxPooling3D(2)(c4)
 
     # Bottleneck
-    bn = layers.Conv3D(256, 3, activation='relu', padding='same')(p4)
-    bn = layers.Conv3D(256, 3, activation='relu', padding='same')(bn)
+    bn = layers.Conv3D(256, 3, padding='same', use_bias=False)(p4)
+    bn = tfa.layers.InstanceNormalization()(bn)
+    bn = layers.LeakyReLU(alpha=0.01)(bn)
+    bn = layers.Conv3D(256, 3, padding='same', use_bias=False)(bn)
+    bn = tfa.layers.InstanceNormalization()(bn)
+    bn = layers.LeakyReLU(alpha=0.01)(bn)
 
     # Decoder
-    # The transpose does the up convolution.
-    u1 = layers.Conv3DTranspose(128, 2, strides=2, padding='same')(bn)
-    # Connecting to the layers from the encoder.
+    # Up Block 1
+    u1 = layers.Conv3DTranspose(128, 2, strides=2, padding='same', use_bias=False)(bn)
+    u1 = tfa.layers.InstanceNormalization()(u1)
+    u1 = layers.LeakyReLU(alpha=0.01)(u1)
     u1 = layers.concatenate([u1, c4])
-    c5 = layers.Conv3D(128, 3, activation='relu', padding='same')(u1)
-    c5 = layers.Conv3D(128, 3, activation='relu', padding='same')(c5)
+    c5 = layers.Conv3D(128, 3, padding='same', use_bias=False)(u1)
+    c5 = tfa.layers.InstanceNormalization()(c5)
+    c5 = layers.LeakyReLU(alpha=0.01)(c5)
+    c5 = layers.Conv3D(128, 3, padding='same', use_bias=False)(c5)
+    c5 = tfa.layers.InstanceNormalization()(c5)
+    c5 = layers.LeakyReLU(alpha=0.01)(c5)
 
-    u2 = layers.Conv3DTranspose(64, 2, strides=2, padding='same')(c5)
+    # Up Block 2
+    u2 = layers.Conv3DTranspose(64, 2, strides=2, padding='same', use_bias=False)(c5)
+    u2 = tfa.layers.InstanceNormalization()(u2)
+    u2 = layers.LeakyReLU(alpha=0.01)(u2)
     u2 = layers.concatenate([u2, c3])
-    c6 = layers.Conv3D(64, 3, activation='relu', padding='same')(u2)
-    c6 = layers.Conv3D(64, 3, activation='relu', padding='same')(c6)
+    c6 = layers.Conv3D(64, 3, padding='same', use_bias=False)(u2)
+    c6 = tfa.layers.InstanceNormalization()(c6)
+    c6 = layers.LeakyReLU(alpha=0.01)(c6)
+    c6 = layers.Conv3D(64, 3, padding='same', use_bias=False)(c6)
+    c6 = tfa.layers.InstanceNormalization()(c6)
+    c6 = layers.LeakyReLU(alpha=0.01)(c6)
 
-    u3 = layers.Conv3DTranspose(32, 2, strides=2, padding='same')(c6)
+    # Up Block 3
+    u3 = layers.Conv3DTranspose(32, 2, strides=2, padding='same', use_bias=False)(c6)
+    u3 = tfa.layers.InstanceNormalization()(u3)
+    u3 = layers.LeakyReLU(alpha=0.01)(u3)
     u3 = layers.concatenate([u3, c2])
-    c7 = layers.Conv3D(32, 3, activation='relu', padding='same')(u3)
-    c7 = layers.Conv3D(32, 3, activation='relu', padding='same')(c7)
+    c7 = layers.Conv3D(32, 3, padding='same', use_bias=False)(u3)
+    c7 = tfa.layers.InstanceNormalization()(c7)
+    c7 = layers.LeakyReLU(alpha=0.01)(c7)
+    c7 = layers.Conv3D(32, 3, padding='same', use_bias=False)(c7)
+    c7 = tfa.layers.InstanceNormalization()(c7)
+    c7 = layers.LeakyReLU(alpha=0.01)(c7)
 
-    u4 = layers.Conv3DTranspose(16, 2, strides=2, padding='same')(c7)
+    # Up Block 4
+    u4 = layers.Conv3DTranspose(16, 2, strides=2, padding='same', use_bias=False)(c7)
+    u4 = tfa.layers.InstanceNormalization()(u4)
+    u4 = layers.LeakyReLU(alpha=0.01)(u4)
     u4 = layers.concatenate([u4, c1])
-    c8 = layers.Conv3D(16, 3, activation='relu', padding='same')(u4)
-    c8 = layers.Conv3D(16, 3, activation='relu', padding='same')(c8)
+    c8 = layers.Conv3D(16, 3, padding='same', use_bias=False)(u4)
+    c8 = tfa.layers.InstanceNormalization()(c8)
+    c8 = layers.LeakyReLU(alpha=0.01)(c8)
+    c8 = layers.Conv3D(16, 3, padding='same', use_bias=False)(c8)
+    c8 = tfa.layers.InstanceNormalization()(c8)
+    c8 = layers.LeakyReLU(alpha=0.01)(c8)
 
     outputs = layers.Conv3D(num_classes, 1, activation='softmax')(c8)
 

1105191b2e66315df5d6770242ca2f061f2cc715
lukeybin <lukeybin@gmail.com>
2024-10-22 14:14:31 +1000

    Switched to tf tensors instead of np arrays

--- recognition/2DUnet_46992505/modules.py
+++ recognition/2DUnet_46992505/modules.py
@@ -6,10 +6,10 @@ import numpy as np
 
 # Dice coefficient function
 def dice_coefficient(y_true, y_pred, smooth=1e-6):
-    y_true_f = tf.reshape(y_true, [-1])  # Flatten the arrays
-    y_pred_f = tf.reshape(y_pred, [-1])  # Flatten the arrays
+    y_true_f = tf.cast(tf.reshape(y_true, [-1]), tf.float32)  # Flatten the arrays
+    y_pred_f = tf.cast(tf.reshape(y_pred, [-1]), tf.float32)  # Flatten the arrays
     intersection = tf.reduce_sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
+    return (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
 
 # Negative Dice coefficient (used as a loss function)
 def dice_loss(y_true, y_pred, smooth=1e-6):

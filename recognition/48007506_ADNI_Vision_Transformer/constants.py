b6e1bdc33227abc3613e560d935420f35d3210f1
MorrisWang9 <>
2024-10-22 22:45:41 +1000

    Add constants.py file that contains the constant variables that can be used when constructing the GFNet.

---
+++ recognition/48007506_ADNI_Vision_Transformer/constants.py
@@ -0,0 +1,15 @@
+"""
+constants.py
+
+Defined constants.
+
+Author: Chiao-Yu Wang (Student No. 48007506)
+"""
+IMAGE_SIZE = 128
+BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+NUM_EPOCHS = 10
+WEIGHT_DECAY = 0.0001
+TRAIN_DATA_PATH = "AD_NC/train"
+TEST_DATA_PATH = "AD_NC/test"
+MODEL_SAVE_PATH = "model/GFNet"

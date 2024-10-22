44526360e57ded58ed706365a0931b3a493d6d36
Lojlk <jkong126822@gmail.com>
2024-10-22 13:15:48 +1000

    added main training loop and saving the model

--- recognition/ViT-Task5-4697959/train.py
+++ recognition/ViT-Task5-4697959/train.py
@@ -62,3 +62,69 @@ def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=25,
     }
 
     counter = 0
+
+    # main training/validation loop
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch +1}/{num_epochs}')
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # Zero the parameter gradients
+                optimizer.zero_grad()
+
+                # Forward
+                # Track gradients only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # Backward + optimize only in train
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # Statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            # Compute epoch loss and accuracy
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            history[f'{phase}_loss'].append(epoch_loss)
+            history[f'{phase}_acc'].append(epoch_acc.item())
+
+            print(f'{phase.capitalize()} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # Deep copy the model
+            if phase == 'val':
+                if epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
+                    torch.save(best_model_wts, os.path.join(save_dir, 'best_vit_model.pth'))
+                    print("Best model updated and saved.\n")
+                    counter = 0
+                else:
+                    counter +=1
+                    print(f"No improvement for {counter} epochs.\n")
+                    if counter >= patience:
+                        print("Early stopping triggered.")
+                        model.load_state_dict(best_model_wts)
+                        return model, history
+
+    print(f'Training complete. Best val Acc: {best_acc:.4f}')

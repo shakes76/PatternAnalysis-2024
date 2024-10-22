671741a1fb10b8eae029882ce05bd7b0997a74e0
beenalf <sth48624bt@outlook.com>
2024-10-22 22:32:55 +1000

    Added support for different validation and testing splits

--- recognition/GFNet_s4784738/dataset.py
+++ recognition/GFNet_s4784738/dataset.py
@@ -7,7 +7,7 @@ s4784738
 
 import os
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from PIL import Image
 import torchvision.transforms as transforms
 
@@ -30,7 +30,7 @@ class ADNIDataset(Dataset):
             # Set the label to 1 for Alzheimer's (AD) and 0 for Normal (NC)
             label = 1 if label_name == 'AD' else 0
             
-            for image_name in os.listdir(label_dir):
+            for image_name in sorted(os.listdir(label_dir)):
                 # Ensure the images are all valid (they should all be .jpeg)
                 if image_name.endswith(('.png', '.jpg', '.jpeg')):
                     self.image_paths.append(os.path.join(label_dir, image_name))
@@ -53,7 +53,7 @@ class ADNIDataset(Dataset):
         # Open the image as grayscale
         image = Image.open(image_path).convert('L')
         label = self.labels[idx]
-        label = torch.tensor(label, dtype=torch.float32)
+        label = torch.tensor(label, dtype=torch.long)
 
         # Transform the image if transforms are being used
         if self.transform:
@@ -62,38 +62,58 @@ class ADNIDataset(Dataset):
         return image, label
 
 
-def get_data_loader(root_dir, split='train', batch_size=32, shuffle=True, num_workers=1):
+def get_data_loader(root_dir, dataset='train', batch_size=32, shuffle=True, split=0.2, num_workers=0):
     """
     Gets a dataloader with characteristics specified by the user.
 
     root_dir: The root directory in which ADNI images can be found
-    split: Specifies if the dataloader is for a training or validation/testing split
+    dataset: Specifies if the dataloader is for a training or validation/testing split
     batch_size: The batch size for images 
     shuffle: True is images should be shuffled, and False otherwise
+    split: The split between training and validation datasets
     num_workers: The number of workers
     """
     # Set up different transforms for training and testing splits
-    if split == 'train':
+    if dataset == 'train':
         transform = transforms.Compose([
             # Resize to 224x224 from the default size of 256x240 pixels
             transforms.Resize((224, 224)),
-            transforms.Grayscale(),
+            #transforms.Grayscale(),
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
-            transforms.RandomRotation(30),
+            transforms.RandomRotation(35),
             transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
             # Pre-calculated mean and standard deviation pixel values
-            transforms.Normalize([0.1155], [0.2224]),
+            transforms.Normalize(mean=[0.1155, 0.1155, 0.1155], std=[0.2224, 0.2224, 0.2224])
+            #transforms.Normalize([0.1155], [0.2224]),
         ])
     else:
         transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.Grayscale(),
+        #transforms.Grayscale(),
         transforms.ToTensor(),
-        transforms.Normalize([0.1155], [0.2224]),
+        transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+        transforms.Normalize(mean=[0.1155, 0.1155, 0.1155], std=[0.2224, 0.2224, 0.2224])
+        #transforms.Normalize([0.1155], [0.2224]),
     ])
 
-    dataset = ADNIDataset(root_dir=root_dir, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    adni_dataset = ADNIDataset(root_dir=root_dir, transform=transform)
+    #dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     
-    return dataloader
+    # Split the train dataset into training and validation sets
+    if dataset == 'train':
+        train_size = int((1 - split) * len(adni_dataset))
+        val_size = len(adni_dataset) - train_size
+        train_dataset, val_dataset = random_split(adni_dataset, [train_size, val_size])
+
+        # Return the training and validation data loaders
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,  num_workers=num_workers)
+        return train_loader, val_loader
+    else:
+        # For testing or validation dataset (i.e., after training)
+        data_loader = DataLoader(adni_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+        return data_loader
+    
+    #return dataloader

0bf4b0750ef8ec70280d65ec883b0c94efe30c62
Jehong Min <jehongmin2002@gmail.com>
2024-10-22 14:53:15 +1000

    Added docstring and weight_path flexibility

--- recognition/ISIC_47044090/modules.py
+++ recognition/ISIC_47044090/modules.py
@@ -1,11 +1,14 @@
 from ultralytics import YOLO
 import torch
 
-def yolo_model():
+def yolo_model(weights_path):
     """
     gets YOLO model and enables GPU (if available)
+
+    Parameters:
+        weights_path: path to yolov7.pt file
     """
-    model = YOLO("yolov7.pt") # gets initial weights
+    model = YOLO(weights_path) # gets initial weights
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device) # transfers device to GPU if available
     return model

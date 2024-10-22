41288e59097e5ddb18fd5f2accc067a8b2a72fcf
anthonyngo <mail.anthonyngo@gmail.com>
2024-10-22 14:42:20 +1000

    Change: Updated wandb to log hyperparameters in run name.

--- recognition/48019022_GNN/wandb_config.py
+++ recognition/48019022_GNN/wandb_config.py
@@ -3,19 +3,19 @@ Weights and Biases config file
 Structure based off Shakes' config file
 """
 import wandb
-
-import wandb
-
-def setup_wandb(architecture: str, epochs: int):
+def setup_wandb(architecture: str, epochs: int, decay, lr):
     """
     Initialises a wandb run with respective model information.
     """
+    # Log run with name that desceibes appropraite hyperparameters
     experiment_id = wandb.util.generate_id()
+    name = f"{architecture}, {epochs} epochs, {decay} decay, {lr} learning rate"
 
     # Start a run, tracking hyperparameters
     wandb.init(
         project="Facebook-Large-Page-Page-Network",
         group="GNN",
+        name=name,
         config={
             "id": experiment_id,
             "architecture": architecture,

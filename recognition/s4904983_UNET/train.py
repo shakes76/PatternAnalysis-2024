854c0ba34c4b891a858dc7ce4927ae9fb02f9168
oystkva <oystkva@stud.ntnu.no>
2024-10-22 16:12:22 +1000

    Fixed dice_coeffinicient and some changes in how data is saved

--- recognition/s4904983_UNET/train.py
+++ recognition/s4904983_UNET/train.py
@@ -16,6 +16,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def dice_coefficient(pred, target, margin=0.0):
+    """ 
+    Calculate the dice coefficient for the predicted and target segmentation masks.
+
+    Args:
+        pred (torch.Tensor): The predicted segmentation mask.
+        target (torch.Tensor): The target segmentation mask.
+
+    Returns:
+        float: The dice coefficient.
+    """
+    intersect = ((pred-target).abs() <= margin).sum().item()
+    total_sum = (pred > margin).sum().item() + (target > margin).sum().item()
+    if total_sum == 0 or intersect == 0:
+        # Laplace smoothing
+        total_sum += 1e-6
+        intersect += 1e-6
+    return (2.0 * intersect) / total_sum
+
+
 def train_unet_model():
     """ 
     Train 2D UNet model on the medical image data.
@@ -25,27 +45,27 @@ def train_unet_model():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     # Load the data
-    # train_images, train_segs = load_img_seg_pair('train')
-    # val_images, val_segs = load_img_seg_pair('validate')
-
+    print("Loading data")
     train_images, train_segs = load_img_seg_pair('train')
+    print("Training data loaded")
     val_images, val_segs = load_img_seg_pair('validate')
+    print("Validation data loaded")
 
-    train_images = train_images[:32]
-    train_segs = train_segs[:32]
-    val_images = val_images[:8]
-    val_segs = val_segs[:8]
+    train_images = train_images
+    train_segs = train_segs
+    val_images = val_images
+    val_segs = val_segs
 
     # Directory for saving the model
-    model_dir = 'C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/models/' # Local path
-    # model_dir = '/home/Student/COMP3710/project/models/' # Rangpur path
+    # model_dir = 'C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/models/' # Local path
+    model_dir = '/home/Student/COMP3710/project/models/' # Rangpur path
 
     # Hyperparameters
-    batch_size = 32
+    batch_size = 1
     learining_rate = 0.001
-    num_epochs = 100
-    output_dir = 'C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/outputs/' # Local path
-    # output_dir = '/home/Student/COMP3710/project/outputs/' # Rangpur path
+    num_epochs = 60
+    # output_dir = 'C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/outputs/' # Local path
+    output_dir = '/home/Student/COMP3710/project/outputs/' # Rangpur path
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -61,33 +81,39 @@ def train_unet_model():
 
     train_loss, val_loss = [], []
     train_dice, val_dice = [], []
+    discrete_val_loss = []
+    discrete_val_dice = []
+
+    print('\nStarting training:\n')
 
     for epoch in range(num_epochs):
         model.train()
-        running_loss = 0.0
-        running_dice = 0.0
+        running_loss, running_dice = 0.0, 0.0
 
-        for i in range(0, len(train_images), batch_size):
-            images = train_images[i:i + batch_size].to(device)
-            segs = train_segs[i:i + batch_size].to(device)
+        with tqdm(total=8, desc=f'Epoch {epoch + 1}/{num_epochs}', unit='img') as pbar:
+            for i in range(0, len(train_images), batch_size):
+                images = train_images[i:i + batch_size].to(device)
+                segs = train_segs[i:i + batch_size].to(device)
 
-            optimizer.zero_grad()
-            outputs = model(images[:,None,:,:])[:,0,:,:]
-            loss = criterion(outputs, segs)
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                outputs = model(images[:,None,:,:])[:,0,:,:]
+                loss = criterion(outputs, segs) / outputs.numel()
+                loss.backward()
+                optimizer.step()
 
-            running_loss += loss.item()
-            running_dice += dice_coefficient(outputs, segs)
+                pbar.update(images.shape[0])
+                running_loss += loss.item()
+                running_dice += dice_coefficient(outputs, segs)
 
-        train_loss.append(running_loss / len(train_images))
-        train_dice.append(running_dice / len(train_images))
+                pbar.set_postfix(**{'loss (batch)': loss.item()})
 
-        print(f'Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss[-1]:.4f}, Train Dice: {train_dice[-1]:.4f}')
+            train_loss.append(running_loss / len(train_images))
+            train_dice.append(running_dice / len(train_images))
 
-        model.eval()
-        running_loss = 0.0
-        running_dice = 0.0
+            model.eval()
+            running_loss, running_dice = 0.0, 0.0
+            disc_run_loss, disc_run_dice = 0.0, 0.0
+
 
         with torch.no_grad():
             for i in range(0, len(val_images), batch_size):
@@ -95,7 +121,7 @@ def train_unet_model():
                 segs = val_segs[i:i + batch_size].to(device)
 
                 outputs = model(images[:,None,:,:])[:,0,:,:]
-                loss = criterion(outputs, segs)
+                loss = criterion(outputs, segs) / outputs.numel()
                 
                 # if (epoch + 1 ) % 10 == 0:
                 #     for i in range(256):
@@ -114,57 +140,48 @@ def train_unet_model():
 
                 running_loss += loss.item()
                 running_dice += dice_coefficient(outputs, segs)
+                disc_run_loss += criterion(torch.round(torch.mul(outputs, 4)), segs) / outputs.numel()
+                disc_run_dice += dice_coefficient(torch.round(torch.mul(outputs, 4)), segs)
 
                 # Save output images
                 for j in range(batch_size):
-                    save_image(images[j], os.path.join(output_dir, f'image_{epoch + 1}_{i+j}.png'))
-                    save_image(segs[j], os.path.join(output_dir, f'segmentation_{epoch + 1}_{i+j}.png'))
-                    save_image(outputs[j], os.path.join(output_dir, f'output_{epoch + 1}_{i+j}.png'))
+                    save_image(
+                        torch.cat(
+                            (images[j].unsqueeze(0), 
+                            outputs[j].unsqueeze(0), 
+                            (torch.round(torch.mul(outputs[j], 4))/4).unsqueeze(0), 
+                            (segs[j]/4).unsqueeze(0)), 
+                            dim=2),
+                            os.path.join(output_dir, f'e{epoch + 1}_im{i+j}.png')
+                        )
+                    # save_image(images[j], os.path.join(output_dir, f'e{epoch + 1}_{i+j}_image.png'))
+                    # save_image((segs[j]/4), os.path.join(output_dir, f'e{epoch + 1}_{i+j}_segmentation.png'))
+                    # save_image((torch.round(torch.mul(outputs[j], 4))/4), os.path.join(output_dir, f'e{epoch + 1}_{i+j}_output.png'))
 
 
         val_loss.append(running_loss / len(val_images))
         val_dice.append(running_dice / len(val_images))
+        discrete_val_loss.append(disc_run_loss / len(val_images))
+        discrete_val_dice.append(disc_run_dice / len(val_images))
 
-        print(f'Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss[-1]:.4f}, Train Dice: {train_dice[-1]:.4f}, Val Loss: {val_loss[-1]:.4f}, Val Dice: {val_dice[-1]:.4f}')
+        print(f'Epoch {epoch + 1}/{num_epochs}, Train Loss: {train_loss[-1]:.4f}, Val Loss: {val_loss[-1]:.4f}, Discrete Val Loss: {discrete_val_loss[-1]:.4f}, Train Dice: {train_dice[-1]:.4f}, Val Dice: {val_dice[-1]:.4f}, Discrete Val Dice: {discrete_val_dice[-1]:.4f}')
     
-    # Save the model
-    torch.save(model.state_dict(), model_dir + 'unet_model.pth')
+        if (epoch + 1) % 20 == 0:
+            # Save the model
+            torch.save(model.state_dict(), model_dir + 'unet_model_ep' + str(epoch + 1) + '.pth')
 
-    # Save the loss and dice coefficient
-    np.save(output_dir + 'train_loss.npy', np.array(train_loss))
-    np.save(output_dir + 'val_loss.npy', np.array(val_loss))
+            # Save the loss and dice coefficient
+            np.save(output_dir + 'train_loss_ep' + str(epoch + 1) + '.npy', np.array(train_loss))
+            np.save(output_dir + 'val_loss_ep' + str(epoch + 1) + '.npy', np.array(val_loss))
 
-    # Plot the loss and dice coefficient
-    plt.figure()
-    plt.plot(train_loss, label='Train Loss')
-    plt.plot(val_loss, label='Val Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig(output_dir + 'loss.png')
-
-
-               
-
-
-def dice_coefficient(pred, target):
-    """ 
-    Calculate the dice coefficient for the predicted and target segmentation masks.
-
-    Args:
-        pred (torch.Tensor): The predicted segmentation mask.
-        target (torch.Tensor): The target segmentation mask.
-
-    Returns:
-        float: The dice coefficient.
-    """
-    intersect = float((pred * target).sum())
-    union = float(pred.sum() + target.sum())
-    if union == 0 or intersect == 0:
-        # Laplace smoothing
-        union += 1e-6
-        intersect += 1e-6
-    return (2.0 * intersect) / union
+            # Plot the loss and dice coefficient
+            plt.figure()
+            plt.plot(train_loss, label='Train Loss')
+            plt.plot(val_loss, label='Val Loss')
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.legend()
+            plt.savefig(output_dir + 'loss_ep' + str(epoch + 1) + '.png')
 
 
 if __name__ == '__main__':

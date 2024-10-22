94e9812431d82ca0f72628318c8564f9975dfc3c
Ojas0305 <ojmay3@gmail.com>
2024-10-22 18:07:02 +1000

    Implemented the forward method of MappingNetwork.

--- recognition/StyleGAN_47462153/modules.py
+++ recognition/StyleGAN_47462153/modules.py
@@ -9,4 +9,12 @@ class MappingNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(w_dim, w_dim)
         )
+    
+    def forward(self, z):
+        return self.mapping(z)
+
+class StyleGANGenerator(nn.Module):
+    def __init__(self, z_dim, w_dim, img_channels):
+        super().__init__()
+        self.mapping = MappingNetwork(z_dim, w_dim)
         

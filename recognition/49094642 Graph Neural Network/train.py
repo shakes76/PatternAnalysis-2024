bb5c4ae8e2653ca29ae6fff7b9a9625f3d7534a5
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 03:22:16 +1000

    Update train.py

--- recognition/49094642 Graph Neural Network/train.py
+++ recognition/49094642 Graph Neural Network/train.py
@@ -1,19 +1,18 @@
-class GNNTrainer:
+class Trainer:
     def train(model, data, optimizer):
         model.train()
-        model.optimizer()
+        optimizer.zero_grad()
         out = model(data)
         loss = F.nll_loss()
         loss.backward()
         optimizer.step()
-        return loss.item()
 
     def train(self, epochs=50):
         for epoch in range(epochs):
         loss = self.train_epoch()
         print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss:.4f}')
       
-class GNNTester:
+class Tester:
     def __init__(self, model, data, device='cuda'):
         model.test()
         self.model = model
@@ -21,11 +20,11 @@ class GNNTester:
         self.device = device
         self.model.to(device)
     
-    def test(self):
-        self.model.eval()
+    def test(model,data):
+        model.eval()
         with torch.no_grad():  
             out = self.model(self.data)  
-            pred = out.argmax(dim=1)  
+            pred = model(data).argmax(dim=1) 
             correct = (pred[self.data.test_mask] == self.data.y[self.data.test_mask]).sum() 
             acc = int(correct) / int(self.data.test_mask.sum())  
             return acc

Added data normalisation, fixed scaling of labels when resizing input using nearest neighboor interpolation

recognition/2D_UNet_46991638/dataset.py
@@ -30,7 +30,12 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
     # get fixed size
     num = len(imageNames)
     first_case = nib.load(imageNames[0]).get_fdata(caching="unchanged")
-    first_case = sk.resize(first_case, (256,256))
+    if categorical:
+        #neareat-neigboor interpolation
+        first_case = sk.resize(first_case, (256,256), order=0, anti_aliasing=False, preserve_range=True)
+    else:
+        #bi-linear
+        first_case = sk.resize(first_case, (256, 256), order=1, anti_aliasing=True, preserve_range=True)
     if len(first_case.shape) == 3:
         first_case = first_case [: ,: ,0] # sometimes extra dims , remove
     if categorical:
@@ -40,11 +45,17 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
     else:
         rows, cols = first_case.shape
         images = np.zeros((num, rows, cols), dtype = dtype)
-
-    for i, inName in enumerate (tqdm(imageNames)):
+    for i, inName in enumerate (tqdm(imageNames)):   
         niftiImage = nib.load(inName)
         inImage = niftiImage.get_fdata(caching ="unchanged") # read disk only
-        inImage = sk.resize(inImage, (256,256))
+
+        if categorical:
+            #neareat-neigboor interpolation
+            inImage = sk.resize(inImage, (256,256), order=0, anti_aliasing=False, preserve_range=True)
+        else:
+            #bi-linear
+            inImage = sk.resize(inImage, (256, 256), order=1, anti_aliasing=True, preserve_range=True)
+
         affine = niftiImage.affine
         if len (inImage.shape ) == 3:
             inImage = inImage[: ,: ,0] # sometimes extra dims in HipMRI_study data
@@ -56,13 +67,13 @@ def load_data_2D (imageNames, normImage = False, categorical = False, dtype = np
         if categorical:
             inImage = to_channels(inImage, dtype = dtype)
             images[i ,: ,: ,:] = inImage
-        else:
+        else: 
             images[i ,: ,:] = inImage
 
         affines.append(affine)
         if i > 20 and early_stop:
             break
-
+    print("H")
     if getAffines:
         return images, affines
     else:
@@ -76,13 +87,11 @@ def load(path, label=False):
     train_set = load_data_2D(image_list, normImage=False, categorical=label)
     return train_set
 
+def process_data(data_set):
+  # the method normalizes training images and adds 4th dimention 
 
-X_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_"
-train_X = load(X_path + "train")
-validate_X = load(X_path + "validate")
-test_X = load(X_path + "test")
-
-seg_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_"
-train_Y = load(seg_path + "train", label=True)
-validate_Y = load(seg_path + "validate", label=True)
-test_Y = load(seg_path + "test", label=True)
+  train_set = data_set
+  train_set = (train_set - np.mean(train_set))/ np.std(train_set)
+  train_set= (train_set- np.amin(train_set))/ np.amax(train_set- np.amin(train_set))
+  train_set = train_set[...,np.newaxis]
+  return train_set

recognition/2D_UNet_46991638/train.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 from matplotlib import pyplot
 import numpy as np
 
-l_rate = 0.0005
-epochs = 50
+l_rate = 0.005
+epochs = 30
 
 X_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_"
 train_X = data.load(X_path + "train")
@@ -14,12 +14,7 @@ test_X = data.load(X_path + "test")
 
 train_X = data.process_data(train_X)
 validate_X = data.process_data(validate_X)
-test_x = data.process_data(test_X)
-
-
-t = test_X.copy()
-t = t[...,np.newaxis]
-test_X = t
+test_X = data.process_data(test_X)
 
 seg_path = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_"
 train_Y = data.load(seg_path + "train", label=True)
@@ -27,7 +22,6 @@ train_Y = data.load(seg_path + "train", label=True)
 validate_Y = data.load(seg_path + "validate", label=True)
 
 test_Y = data.load(seg_path + "test", label=True)
-"""
 print("X shapes")
 print(train_X.shape)
 print(validate_X.shape)
@@ -35,22 +29,40 @@ print(test_X.shape)
 print("X is :")
 print(np.amin(train_X))
 print(np.amax(train_X))
+print(np.amin(test_X))
+print(np.amax(test_X))
 print(train_X[1,1:5,1:5])
 print("Y is")
 print(np.amin(train_Y))
 print(np.amax(train_Y))
-print(train_Y[1,1:5,1:5,:])
+print(np.amin(test_Y))
+print(np.amax(test_Y))
 print("Labeled shapes")
 print(train_Y.shape)
 print(validate_Y.shape)
 print(test_Y.shape)
-"""
 
+class DataGenerator(tf.keras.utils.Sequence):
+        def __init__(self, x_set, y_set, batch_size):
+            self.x, self.y = x_set, y_set
+            self.batch_size = batch_size
+
+        def __len__(self):
+            return int(np.floor(len(self.x) / self.batch_size))
+
+        def __getitem__(self, idx):
+            batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+            batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+            return batch_x, batch_y
+        
 model = modules.unet_model()
+train_gen = DataGenerator(train_X, train_Y, batch_size=32)
+val_gen = DataGenerator(validate_X, validate_Y, batch_size=32)
 
 opt= tf.keras.optimizers.Adam(learning_rate = l_rate)
 model.compile (optimizer=opt, loss= 'CategoricalCrossentropy' , metrics=[tf.keras.losses.Dice])
-history = model.fit(train_X, train_Y,  validation_data=(validate_X, validate_Y), batch_size=8,shuffle='True',epochs=epochs)
+history = model.fit(train_gen, validation_data=val_gen, epochs=epochs)
+#history = model.fit(train_X, train_Y,  validation_data=(validate_X, validate_Y), batch_size=20,shuffle='True',epochs=epochs)
 
 print("Model evaluation:")
 model.evaluate(test_X,test_Y)
@@ -64,4 +76,3 @@ pyplot.plot([1 - x for x in history.history['val_dice']], color='orange', label=
 pyplot.legend(('training','validation'))
 pyplot.savefig("Dice_Coeff")
 pyplot.clf()
-
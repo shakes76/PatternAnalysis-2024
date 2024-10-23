f123fdc68162b242c67cba035e66ef7eafe3d22f
Joseph Reid <joseph.reid@uqconnect.edu.au>
2024-10-23 10:33:22 +1000

    Added comments to modules.py

--- recognition/46982775_2DUNet/modules.py
+++ recognition/46982775_2DUNet/modules.py
@@ -2,10 +2,18 @@
 
 """
 
+# ! Reasoning for padding:
+"""
+- 256 x 128 img becomes too small in bottleneck layer when there are 5 layers
+- 256 x 128 -> 252 x 124 -> 126 x 62 -> 122 x 58 -> 61 x 29: Odd numbers create trouble in upscaling
+To allow a seamless tiling of the output segmentation map (see Figure 2), it
+is important to select the input tile size such that all 2x2 max-pooling operations
+are applied to a layer with an even x- and y-size. 
+"""
+
 import torch
 import torch.nn as nn
 import torchvision.transforms.functional as TF
-import torch.nn.functional as F
 
 class DoubleConvolution(nn.Module):
     """ Double Convolution with Batch Norm and ReLU."""
@@ -13,7 +21,7 @@ class DoubleConvolution(nn.Module):
         super().__init__()
         self.double_conv = nn.Sequential(
             # First convolution
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False), # ! Padding due to too small a size in bottleneck layer
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
             # Second convolution
@@ -77,59 +85,60 @@ class UNet(nn.Module):
         self.outconv = OutConvolution(n_features, out_channels)
 
     def forward(self, x):
-        # Input is size (1, 256, 128)
+        # Note that tensors have shape (B, C, H, W) where:
+        # B is batch size
+        # C is channels
+        # H is height
+        # W is width
+        # Input x is size (B, 1, 256, 128)
 
+        # Encoding:
         # Layer 1: Encoder
-        x = self.conv1d(x) # Size (64, 256, 128)
+        x = self.conv1d(x) # Size (B, 64, 256, 128)
         skip1 = x
-        x = self.pool(x) # Size (64, 128, 64)
-
+        x = self.pool(x) # Size (B, 64, 128, 64)
         # Layer 2: Encoder
-        x = self.conv2d(x) # Size (128, 128, 64)
+        x = self.conv2d(x) # Size (B, 128, 128, 64)
         skip2 = x
-        x = self.pool(x) # Size (128, 64, 32)
-
+        x = self.pool(x) # Size (B, 128, 64, 32)
         # Layer 3: Encoder
-        x = self.conv3d(x) # Size (256, 64, 32)
+        x = self.conv3d(x) # Size (B, 256, 64, 32)
         skip3 = x
-        x = self.pool(x) # Size (256, 32, 16)
-
+        x = self.pool(x) # Size (B, 256, 32, 16)
         # Layer 4: Encoder
-        x = self.conv4d(x) # Size (512, 32, 16)
+        x = self.conv4d(x) # Size (B, 512, 32, 16)
         skip4 = x
-        x = self.pool(x) # Size (512, 16, 8)
-
+        x = self.pool(x) # Size (B, 512, 16, 8)
         # Bottleneck layer
-        x = self.bottleneck(x) # Size (1024, 16, 8)
-        x = self.upconv1(x) # Size (512, 32, 16)
+        x = self.bottleneck(x) # Size (B, 1024, 16, 8)
+        x = self.upconv1(x) # Size (B, 512, 32, 16)
 
+        # Decoding:
         # Layer 1: Decoder
         skip4 = TF.resize(skip4, size=x.shape[2:], antialias=None)
-        x = torch.cat((skip4, x), dim=1)
-        x = self.conv1u(x)
-        x = self.upconv2(x)
-        
+        x = torch.cat((skip4, x), dim=1) # Size (B, 1024, 32, 16)
+        x = self.conv1u(x) # Size (B, 512, 32, 16)
+        x = self.upconv2(x) # Size (B, 256, 64, 32)
         # Layer 2: Decoder
         skip3 = TF.resize(skip3, size=x.shape[2:], antialias=None)
-        x = torch.cat((skip3, x), dim=1)
-        x = self.conv2u(x)
-        x = self.upconv3(x)
-
+        x = torch.cat((skip3, x), dim=1) # Size (B, 512, 64, 32)
+        x = self.conv2u(x) # Size (B, 256, 64, 32)
+        x = self.upconv3(x) # Size (B, 128, 128, 64)
         # Layer 3: Decoder
         skip2 = TF.resize(skip2, size=x.shape[2:], antialias=None)
-        x = torch.cat((skip2, x), dim=1)
-        x = self.conv3u(x)
-        x = self.upconv4(x)
-
+        x = torch.cat((skip2, x), dim=1) # Size (B, 256, 128, 64)
+        x = self.conv3u(x) # Size (B, 128, 128, 64)
+        x = self.upconv4(x) # Size (B, 64, 256, 128)
         # Layer 4: Decoder
         skip1 = TF.resize(skip1, size=x.shape[2:], antialias=None)
-        x = torch.cat((skip1, x), dim=1)
-        x = self.conv4u(x)
+        x = torch.cat((skip1, x), dim=1) # Size (B, 128, 256, 128)
+        x = self.conv4u(x) # Size (B, 64, 256, 128)
         x = self.outconv(x) # Size (6, 256, 128)
+
         return x
 
 # For testing purposes
 if __name__ == "__main__":
-    x = torch.randn(1, 1, 256, 128)
+    x = torch.randn(2, 1, 256, 128)
     model = UNet()
     model(x)

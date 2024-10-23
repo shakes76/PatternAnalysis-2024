6adc5268560b7e60674a042d999c311d6e9bf6ec
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 01:57:17 +1000

    Update predict.py

--- recognition/49094642 Graph Neural Network/predict.py
+++ recognition/49094642 Graph Neural Network/predict.py
@@ -1,10 +1,9 @@
 def visualize(model, deviece, data):
-  model.eval()
-  plot_2d_umap(features, labels=None, n_neighbors=15, min_dist=0.1, metric='euclidean'):
-  reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, n_components=2, metric=metric)
-  embedding = reducer.fit_transform(features)
-  plt.figure(figsize)=(10, 8))
-  scatter = plt.scatter(umap_embeds[:, 0], umap_embeds[:, 1], c=data.y.cpu().numpy(), cmap='Spectral', s=5)
-  plt.colorbar(scatter)
-  plt.title('UMAP')
-  plt.show()
+    model.eval()
+    with torch.no_grad():
+        embeddings = model1.conv1(data.x, data.edge_index)
+    plt.figure(figsize)=(10, 8))
+    scatter = plt.scatter(umap_embeds[:, 0], umap_embeds[:, 1], c=data.y.cpu().numpy(), cmap='Spectral', s=5)
+    plt.colorbar(scatter)
+    plt.title('UMAP')
+    plt.show()

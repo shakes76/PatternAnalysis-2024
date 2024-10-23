5a13c9ac046f30f9dd09afd39f723a04b4616397
KaiIsCodin <k.moh@student.uq.edu.au>
2024-10-24 00:13:59 +0800

    turn images and labels into tensor in train.py

--- recognition/HipMRI_Study/train.py
+++ recognition/HipMRI_Study/train.py
@@ -1,7 +1,7 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from modules import UNet
+from modules import UNet, dice_coefficient
 from dataset import load_data_2D
 import matplotlib.pyplot as plt
 import os
@@ -16,48 +16,45 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Initialize model
 model = UNet(in_channels=1, out_channels=1).to(device)
-
-# Loss and optimizer
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 train_images_filenames = [('keras_slices_seg_train/' + f) for f in os.listdir('keras_slices_seg_train') if f.endswith('.nii.gz')]
 train_labels_filenames = [('keras_slices_train/' + f) for f in os.listdir('keras_slices_train') if f.endswith('.nii.gz')]
-val_images_filenames = [('keras_slices_seg_validate/' + f) for f in os.listdir('keras_slices_seg_validate') if f.endswith('.nii.gz')]
-val_labels_filenames = [('keras_slices_validate/' + f) for f in os.listdir('keras_slices_validate') if f.endswith('.nii.gz')]
-train_images = load_data_2D(train_images_filenames)
-train_labels = load_data_2D(train_labels_filenames)
-val_images = load_data_2D(val_images_filenames)
-val_labels = load_data_2D(val_labels_filenames)
+# val_images_filenames = [('keras_slices_seg_validate/' + f) for f in os.listdir('keras_slices_seg_validate') if f.endswith('.nii.gz')]
+# val_labels_filenames = [('keras_slices_validate/' + f) for f in os.listdir('keras_slices_validate') if f.endswith('.nii.gz')]
+train_images = load_data_2D(train_images_filenames, normImage=True)
+train_labels = load_data_2D(train_labels_filenames, normImage=True)
+# val_images = load_data_2D(val_images_filenames)
+# val_labels = load_data_2D(val_labels_filenames)
 
 # Training loop
 def train_model():
-    train_losses, val_losses = [], []
+    train_losses, val_losses, dice_scores = [], [], []
 
     for epoch in range(epochs):
         model.train()
         epoch_train_loss = 0
 
-        for i in range(len(train_images)):
-            image = train_images[i].to(device)
-            label = train_labels[i].to(device)
+        images = torch.from_numpy(train_images).float().to(device)
+        labels = torch.from_numpy(train_labels).float().to(device)
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
 
-            # Forward pass
-            outputs = model(image)
-            loss = criterion(outputs, label)
+        # Backward pass and optimization
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            epoch_train_loss += loss.item()
+        epoch_train_loss += loss.item()
 
         train_losses.append(epoch_train_loss / len(train_images))
 
         # Validation
         model.eval()
         epoch_val_loss = 0
+        """
         with torch.no_grad():
             for i in range(len(val_images)):
                 image = val_images[i].to(device)
@@ -65,8 +62,9 @@ def train_model():
                 outputs = model(image)
                 loss = criterion(outputs, label)
                 epoch_val_loss += loss.item()
+        """
 
-        val_losses.append(epoch_val_loss / len(val_images))
+        # val_losses.append(epoch_val_loss / len(val_images))
 
         print(f'Epoch [{epoch + 1}/{epochs}], Train Loss: {train_losses[-1]:.4f}, Val Loss: {val_losses[-1]:.4f}')
 

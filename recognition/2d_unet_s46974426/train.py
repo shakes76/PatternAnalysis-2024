d523966fb5c3dc0f28921d34e984619b92029089
Robert Slomka <s4697442@uq.edu.au>
2024-10-23 12:33:36 +1000

    topic-recognition - first cut of training functionality added

--- recognition/2d_unet_s46974426/train.py
+++ recognition/2d_unet_s46974426/train.py
@@ -1,20 +1,50 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from modules import UNet
+from utils import load_data_2D
+from modules import dice_loss
 import os
-import numpy as np
-from dataset import load_data_2D
 
-# folder that contains slices
-image_folder = r'C:/Users/rober/Desktop/COMP3710/keras_slices_seg_train'
-image_filenames = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith('.nii.gz')]
+# paths to the train, test, validation data
+train_dir = r'C:/Users/rober/Desktop/COMP3710/keras_slices_seg_train'
+val_dir = r'C:/Users/rober/Desktop/COMP3710/keras_slices_seg_validate'
 
-# test loading the images
-images = load_data_2D(image_filenames, normImage=True, categorical=False, dtype=np.float32, early_stop=False)
-images, affines = load_data_2D(image_filenames, normImage=True, getAffines=True)
-print(f'Loaded {len(affines)} affines')
+# hyperparameters
+batch_size = 8
+epochs = 50
+learning_rate = 1e-4 # 0.0001
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 
-import matplotlib.pyplot as plt
+# load training data
+train_images = load_data_2D([os.path.join(train_dir, f) for f in os.listdir(train_dir)])
+train_loader = DataLoader(train_images, batch_size=batch_size, shuffle=True)
 
-# display one imaage to confirm loading was correct
-plt.imshow(images[0, :, :], cmap='gray')
-plt.title('First Image Slice')
-plt.axis('off')
-plt.show()
+# initialize model
+model = UNet().to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+criterion = nn.BCELoss()  # Binary Cross-Entropy loss for segmentation
+
+# training loop
+for epoch in range(epochs):
+    model.train()
+    running_loss = 0.0
+    for i, data in enumerate(train_loader):
+        inputs, labels = data['image'].to(device), data['label'].to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        
+        # Calculate both BCE and Dice loss
+        bce_loss = criterion(outputs, labels)
+        dice = dice_loss(outputs, labels)
+        loss = bce_loss + dice
+        
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    
+    print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader)}")

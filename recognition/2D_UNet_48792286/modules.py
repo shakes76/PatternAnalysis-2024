b5691a29514ffb57e983b37e721b98df40134aa1
Sylvie1216 <xinyue.yuan2@student.uq.edu.au>
2024-10-24 03:53:19 +1000

    initial draft

--- recognition/2D_UNet_48792286/modules.py
+++ recognition/2D_UNet_48792286/modules.py
@@ -1,1 +1,64 @@
+import torch
+import torch.nn as nn
 
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+        self.encoder1 = self.conv_block(1, 32)
+        self.encoder2 = self.conv_block(32, 64)
+        self.encoder3 = self.conv_block(64, 128)
+        self.encoder4 = self.conv_block(128, 256)
+        self.bottleneck = self.conv_block(256, 512)
+
+        self.decoder4 = self.upconv_block(512, 256)
+        self.decoder3 = self.upconv_block(256, 128)
+        self.decoder2 = self.upconv_block(128, 64)
+        self.decoder1 = self.upconv_block(64, 32)
+
+        self.final_conv = nn.Conv2d(32, 1, kernel_size=1)
+
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def upconv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(nn.MaxPool2d(2)(enc1))
+        enc3 = self.encoder3(nn.MaxPool2d(2)(enc2))
+        enc4 = self.encoder4(nn.MaxPool2d(2)(enc3))
+
+        bottleneck = self.bottleneck(nn.MaxPool2d(2)(enc4))
+
+        dec4 = self.decoder4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = self.decoder4(dec4)
+
+        dec3 = self.decoder3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+
+        dec2 = self.decoder2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+
+        dec1 = self.decoder1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+
+        return self.final_conv(dec1)
+
+def dice_coefficient(y_true, y_pred, smooth=1e-6):
+    y_true_flat = y_true.view(-1)
+    y_pred_flat = y_pred.view(-1)
+    intersection = (y_true_flat * y_pred_flat).sum()
+    return (2. * intersection + smooth) / (y_true_flat.sum() + y_pred_flat.sum() + smooth)

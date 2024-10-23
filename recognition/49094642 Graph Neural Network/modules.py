78ab558f7c0c3c990e50c798643363d989f8f06d
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-23 22:09:06 +1000

    Update modules.py

---
+++ recognition/49094642 Graph Neural Network/modules.py
@@ -0,0 +1,12 @@
+class GNN(torch.nn.Module):
+  def __init__(self, input_dim, hidden_dim, output_dim):
+    super(GNN, self).__init__()
+    self.conv1 = GCNConv(input_dim, hidden_dim)
+    self.conv2 = GCNConv(hidden_dim, output_dim)
+
+  def forward(self, data):
+    x, edge_index = data.x, data.edge_index 
+    x = self.conv1(x, edge_index)
+    x = F.relu(x)
+    x = self.conv2(x, edge_index)
+    return F.log_softmax(x, dim=1)  

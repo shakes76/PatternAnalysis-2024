e5e345f6f4c2041b5f5bdcd649107c8487a81b80
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 00:29:54 +1000

    Update train.py

--- recognition/49094642 Graph Neural Network/train.py
+++ recognition/49094642 Graph Neural Network/train.py
@@ -1,19 +1,10 @@
 class GNNTrainer:
   def __init__(self, model, optimizer, data, loss_fn=torch.nn.functional.nll_loss, device='cuda'):
-    self.model = model
-    self.optimizer = optimizer
-    self.data = data.to(device)
-    self.loss_fn = loss_fn
-    self.device = device
-    self.model.to(device)
-
-  def train_epoch(self):
-    self.model.train()
-    self.optimizer.zero_grad()
-    out = self.model(self.data)  
-    loss = self.loss_fn(out[self.data.train_mask], self.data.y[self.data.train_mask])  
-    loss.backward()  
-    self.optimizer.step() 
+    model.train()
+    model.optimizer()
+    out = model(data)
+    loss = F.nll_loss()
+    loss.backward()
     return loss.item()
 
   def train(self, epochs=50):
@@ -23,15 +14,17 @@ class GNNTrainer:
       
 class GNNTester:
   def __init__(self, model, data, device='cuda'):
+     model.test()
      self.model = model
      self.data = data.to(device)
      self.device = device
      self.model.to(device)
+    
   def test(self):
      self.model.eval()
      with torch.no_grad():  
-     out = self.model(self.data)  
-     pred = out.argmax(dim=1)  
-     correct = (pred[self.data.test_mask] == self.data.y[self.data.test_mask]).sum() 
-     acc = int(correct) / int(self.data.test_mask.sum())  
-     return acc
+       out = self.model(self.data)  
+       pred = out.argmax(dim=1)  
+       correct = (pred[self.data.test_mask] == self.data.y[self.data.test_mask]).sum() 
+       acc = int(correct) / int(self.data.test_mask.sum())  
+       return acc

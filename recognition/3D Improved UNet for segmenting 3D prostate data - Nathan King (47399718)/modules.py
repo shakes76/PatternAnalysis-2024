72af80940ec4162a1a694c44a86884560c34b263
nk20032003 <nk20032003@gmail.com>
2024-10-23 19:34:44 +1000

    modules.py - Reordered module functions logically

--- recognition/3D Improved UNet for segmenting 3D prostate data - Nathan King (47399718)/modules.py
+++ recognition/3D Improved UNet for segmenting 3D prostate data - Nathan King (47399718)/modules.py
@@ -34,29 +34,6 @@ def context_module(convolution, filters):
     
     return context
 
-def localisation_module(concatenation, filters):
-    """
-    The localisation module is defined as a 3 x 3 x 3 convolutional layer followed by a 1 x 1 x 1 convolutional layer.
-    Reference [https://arxiv.org/pdf/1802.10508v1]
-    input: concatenation - recombined features
-           filters - number of filters to output
-    output: localisation - produced localisation
-    """
-    
-    #3 x 3 x 3 Stride-1 Convolution
-    localisation = layers.Conv3D(filters, (3, 3, 3), padding="same")(concatenation)
-    
-    #Leaky ReLU Activation
-    localisation = layers.LeakyReLU(negative_slope=0.01)(localisation)
-    
-    #1 x 1 x 1 Stride-1 Convolution
-    localisation = layers.Conv3D(filters, (1, 1, 1), padding="same")(localisation)
-    
-    #Leaky ReLU Activation
-    localisation = layers.LeakyReLU(negative_slope=0.01)(localisation)
-    
-    return localisation
-
 def decreasing_layer(inputs, stride, filters):
     """
     The decreasing layer represents a level of the UNet that is decreasing.
@@ -80,6 +57,29 @@ def decreasing_layer(inputs, stride, filters):
     addition = layers.Add()([convolution, context])
 
     return addition
+
+def localisation_module(concatenation, filters):
+    """
+    The localisation module is defined as a 3 x 3 x 3 convolutional layer followed by a 1 x 1 x 1 convolutional layer.
+    Reference [https://arxiv.org/pdf/1802.10508v1]
+    input: concatenation - recombined features
+           filters - number of filters to output
+    output: localisation - produced localisation
+    """
+    
+    #3 x 3 x 3 Stride-1 Convolution
+    localisation = layers.Conv3D(filters, (3, 3, 3), padding="same")(concatenation)
+    
+    #Leaky ReLU Activation
+    localisation = layers.LeakyReLU(negative_slope=0.01)(localisation)
+    
+    #1 x 1 x 1 Stride-1 Convolution
+    localisation = layers.Conv3D(filters, (1, 1, 1), padding="same")(localisation)
+    
+    #Leaky ReLU Activation
+    localisation = layers.LeakyReLU(negative_slope=0.01)(localisation)
+    
+    return localisation
 
 def upsampling_layer(localisation, addition, filters):
     """

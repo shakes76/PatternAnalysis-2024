ce251d97d6bbe4ca312e0b2ed97a31f70067ab3d
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 04:47:33 +1000

    Update dataset.py

--- recognition/49094642 Graph Neural Network/dataset.py
+++ recognition/49094642 Graph Neural Network/dataset.py
@@ -1,24 +1,24 @@
 class CustomDataset(Dataset):
-    def __init__(self, edge_path, data2_path, target_path, transform=None):
+    def __init__(self, edge_path, features_path, target_path, transform=None):
         self.edge_path = edge_path
         self.data2_path = data2_path
         self.target_path = target_path
       
     def load_edge(self):
-        edge_def = pd.read_csv(self.edge_path)
+        edge_df = pd.read_csv(self.edge_path)
         return torch.tensor(edge_df.values.T, dtype=torch.long)
 
-    def data_data2(self):
-        data2_def = pd.read_csv(self.data2_path)
-        return torch.tensor(data2_df.values.T, dtype=torch.long)
-
+    def load_features(self):
+        features_df = pd.DataFrame(features_list)
+        return torch.tensor(features_df.iloc[:, 1:].values, dtype=torch.float)
+   
     def load_target(self):
-        target_def = pd.read_csv(self.target_path)
+        target_df = pd.read_csv(self.target_path)
         return torch.tensor(target_df['target'].values, dtype=torch.long)
 
     def create_data_loader(self, transform=None):
         edge = self.load_edges()
-        x = self.load_data2()
+        x = self.load_features()
         y = self.load_target()
         dataset = Data(data_path, transform=transform)
         return data_loader

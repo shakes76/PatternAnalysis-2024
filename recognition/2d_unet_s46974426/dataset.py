bfc81fcb4681469a5488f45bcad5339fd14cacfa
Robert Slomka <s4697442@uq.edu.au>
2024-10-23 11:15:39 +1000

    topic-recognition - tested image loading implementation and resize function added to handle images that are not 246*128

--- recognition/2d_unet_s46974426/dataset.py
+++ recognition/2d_unet_s46974426/dataset.py
@@ -2,7 +2,11 @@ import numpy as np
 import nibabel as nib
 from tqdm import tqdm   
 import utils as utils
+import cv2
 
+def resize_image(image, target_shape):
+    """Resize image to the target shape using OpenCV."""
+    return cv2.resize(image, (target_shape[1], target_shape[0]), interpolation=cv2.INTER_LINEAR)
 
 def to_channels ( arr : np.ndarray, dtype = np.uint8) -> np.ndarray :
     channels = np . unique ( arr )
@@ -14,54 +18,56 @@ def to_channels ( arr : np.ndarray, dtype = np.uint8) -> np.ndarray :
     return res
 
 # load medical image functions
-def load_data_2D(imageNames, normImage=False, categorical=False, dtype = np.float32, getAffines = False , early_stop = False) :
+def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float32, getAffines=False, early_stop=False):
     '''
-    Load medical image data from names , cases list provided into a list for each .
+    Load medical image data from names , cases list provided into a list for each.
 
-    This function pre - allocates 4D arrays for conv2d to avoid excessive memory &
-    usage .
+    This function pre - allocates 4D arrays for conv2d to avoid excessive memory &usage.
 
     normImage : bool ( normalise the image 0.0 -1.0)
     early_stop : Stop loading pre - maturely , leaves arrays mostly empty , for quick &
-    loading and testing scripts .
-    ''' 
+        loading and testing scripts.
+    '''
+    
     affines = []
-
-    # get fixed size
     num = len(imageNames)
-    first_case = nib.load(imageNames[0]).get_fdata(caching = 'unchanged')
+    first_case = nib.load(imageNames[0]).get_fdata(caching='unchanged')
     if len(first_case.shape) == 3:
-        first_case = first_case [: ,: ,0] # sometimes extra dims , remove
+        first_case = first_case[:, :, 0]  # remove extra dims if necessary
     if categorical:
-        first_case = to_channels (first_case, dtype = dtype)
+        first_case = to_channels(first_case, dtype=dtype)
         rows, cols, channels = first_case.shape
-        images = np.zeros((num, rows, cols, channels), dtype = dtype)
-    else :
+        images = np.zeros((num, rows, cols, channels), dtype=dtype)
+    else:
         rows, cols = first_case.shape
-        images = np.zeros((num , rows , cols), dtype = dtype)
+        images = np.zeros((num, rows, cols), dtype=dtype)
 
     for i, inName in enumerate(tqdm(imageNames)):
         niftiImage = nib.load(inName)
-        inImage = niftiImage.get_fdata(caching = 'unchanged') # read disk only
+        inImage = niftiImage.get_fdata(caching='unchanged')
         affine = niftiImage.affine
         if len(inImage.shape) == 3:
-            inImage = inImage [: ,: ,0] # sometimes extra dims in HipMRI_study data
-            inImage = inImage.astype(dtype)
+            inImage = inImage[:, :, 0]  # remove extra dims if necessary
+        inImage = inImage.astype(dtype)
+        
+        # Resize the image if necessary
+        if inImage.shape != (rows, cols):
+            inImage = resize_image(inImage, (rows, cols))
+        
         if normImage:
-            #~ inImage = inImage / np. linalg . norm ( inImage )
-            #~ inImage = 255. * inImage / inImage . max ()
             inImage = (inImage - inImage.mean()) / inImage.std()
-        if categorical :
-            inImage = utils.to_channels(inImage, dtype = dtype)
-            images [i ,: ,: ,:] = inImage
-        else :
-            images[i,:,:]=inImage
-
+        
+        if categorical:
+            inImage = utils.to_channels(inImage, dtype=dtype)
+            images[i, :, :, :] = inImage
+        else:
+            images[i, :, :] = inImage
+        
         affines.append(affine)
         if i > 20 and early_stop:
             break
 
     if getAffines:
         return images, affines
-    else :
+    else:
         return images
--- recognition/2d_unet_s46974426/train.py
+++ recognition/2d_unet_s46974426/train.py
@@ -1,2 +1,20 @@
-import torch
-print(torch.cuda.is_available())
+import os
+import numpy as np
+from dataset import load_data_2D
+
+# folder that contains slices
+image_folder = r'C:/Users/rober/Desktop/COMP3710/keras_slices_seg_train'
+image_filenames = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith('.nii.gz')]
+
+# test loading the images
+images = load_data_2D(image_filenames, normImage=True, categorical=False, dtype=np.float32, early_stop=False)
+images, affines = load_data_2D(image_filenames, normImage=True, getAffines=True)
+print(f'Loaded {len(affines)} affines')
+
+import matplotlib.pyplot as plt
+
+# display one imaage to confirm loading was correct
+plt.imshow(images[0, :, :], cmap='gray')
+plt.title('First Image Slice')
+plt.axis('off')
+plt.show()

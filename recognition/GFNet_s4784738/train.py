4cb8118bc624d80ca61d8dc52ee01b0fb4869771
beenalf <sth48624bt@outlook.com>
2024-10-23 12:19:20 +1000

    Fixed a bug with how loss and accuracy were recorded and imported the image path from utils.py

--- recognition/GFNet_s4784738/train.py
+++ recognition/GFNet_s4784738/train.py
@@ -26,7 +26,7 @@ import torch.nn as nn
 
 from dataset import get_data_loader
 from modules import GFNet
-from utils import get_parameters
+from utils import get_parameters, get_path_to_images
 
 from torchvision import transforms
 import torchvision.transforms.functional as TF
@@ -45,8 +45,6 @@ def train_one_epoch(model: torch.nn.Module, criterion,
     for inputs, labels in data_loader:
         inputs, labels = inputs.to(device), labels.long().to(device)
 
-        optimizer.zero_grad()
-
         # Forward pass
         outputs = model(inputs)
         loss = criterion(outputs, labels)
@@ -64,7 +62,7 @@ def train_one_epoch(model: torch.nn.Module, criterion,
     epoch_loss = running_loss / len(data_loader)
     accuracy = 100 * correct / total
 
-    return epoch_loss, accuracy
+    return accuracy, epoch_loss
 
 
 @torch.no_grad()
@@ -93,7 +91,7 @@ def evaluate(data_loader, model, criterion, device):
 
     epoch_loss = test_loss / len(data_loader)
     accuracy = 100 * correct / total
-    return epoch_loss, accuracy
+    return accuracy, epoch_loss
 
 
 def plot_data(train_acc, train_loss, val_acc, val_loss):
@@ -141,7 +139,6 @@ def train_GFNet(dataloaders):
     learning_rate,
     patch_size,
     embed_dim,
-    num_classes,
     depth,
     mlp_ratio,
     drop_rate,
@@ -154,25 +151,22 @@ def train_GFNet(dataloaders):
     start_time = time.time()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    #cudnn.benchmark = True
-
     # Load training and validation datasets
     data_loader_train, data_loader_val = dataloaders['train']
     data_loader_test = dataloaders['test']
 
     # Create model with hard-coded parameters
-    print(f"Creating GFNet model with img_size: 224x224")
+    print(f"Creating GFNet model with img_size: 224x224 (device = {device})")
     model = GFNet(
         img_size=(224, 224),
         patch_size=patch_size,
-        num_classes=num_classes,
         in_chans=1, # For greyscale images
         embed_dim=embed_dim,
         depth=depth,
         mlp_ratio=mlp_ratio,
         drop_rate=drop_rate,
         drop_path_rate=drop_path_rate,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6)
+        #norm_layer=partial(nn.LayerNorm, eps=1e-6)
     ).to(device)
 
     if device == 'cuda':
@@ -180,20 +174,12 @@ def train_GFNet(dataloaders):
         print(f"Memory Allocated: {torch.cuda.memory_allocated(0) / 1024 ** 3:.1f} GB")
         print(f"Memory Reserved: {torch.cuda.memory_reserved(0) / 1024 ** 3:.1f} GB")
 
-    #n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    #print(f"Number of parameters: {n_parameters}")
-
     # Optimizer and scheduler
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-    #optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
 
-    #loss_scaler = NativeScaler()
-
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=t_max, eta_min=eta_min)
-    # lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=t_max)
 
     # Loss criterion
-    #criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
     criterion = torch.nn.CrossEntropyLoss()
 
     # Track the training and validation accuracy and loss at each epoch
@@ -225,7 +211,7 @@ def train_GFNet(dataloaders):
     
     plot_data(train_loss, val_loss, train_acc, val_acc)
     print('Saved loss and accuracy plots in ./assets/')
-    
+
     print('Saving model...')
     torch.save(model.state_dict(), 'best_model.pth')
 
@@ -235,11 +221,12 @@ def train_GFNet(dataloaders):
 
 if __name__ == "__main__":
     # Paths to the training and validation datasets
+    train_path, test_path = get_path_to_images()
     dataloaders = {
-        'train': get_data_loader("/home/groups/comp3710/ADNI/AD_NC/train", 'train', batch_size = 64, shuffle = True, split=0.2),
-        'test': get_data_loader("/home/groups/comp3710/ADNI/AD_NC/test", 'test', batch_size = 64, shuffle = False, slit=0.2)
-        #'train': get_data_loader("../AD_NC/train", 'train', batch_size = 32, shuffle = True, split=0.2),
-        #'test': get_data_loader("../AD_NC/test", 'test', batch_size = 32, shuffle = False, split=0.2)
+        #'train': get_data_loader(train_path, 'train', batch_size = 32, shuffle = True, split=0.2),
+        #'test': get_data_loader(test_path, 'test', batch_size = 32, shuffle = False, split=0.2)
+        'train': get_data_loader(train_path, 'train', batch_size = 32, shuffle = True, split=0.2),
+        'test': get_data_loader(test_path, 'test', batch_size = 32, shuffle = False, split=0.2)
     }
 
     train_GFNet(dataloaders)

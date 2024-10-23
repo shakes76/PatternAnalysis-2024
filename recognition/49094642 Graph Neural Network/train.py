41a04222460b7e67d6d2d03eba499bdb8a7e8f2f
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 02:03:22 +1000

    Update train.py

--- recognition/49094642 Graph Neural Network/train.py
+++ recognition/49094642 Graph Neural Network/train.py
@@ -1,30 +1,31 @@
 class GNNTrainer:
-  def __init__(self, model, optimizer, data, loss_fn=torch.nn.functional.nll_loss, device='cuda'):
-    model.train()
-    model.optimizer()
-    out = model(data)
-    loss = F.nll_loss()
-    loss.backward()
-    return loss.item()
+    def train(model, data, optimizer):
+        model.train()
+        model.optimizer()
+        out = model(data)
+        loss = F.nll_loss()
+        loss.backward()
+        optimizer.step()
+        return loss.item()
 
-  def train(self, epochs=50):
-    for epoch in range(epochs):
-      loss = self.train_epoch()
-      print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss:.4f}')
+    def train(self, epochs=50):
+        for epoch in range(epochs):
+        loss = self.train_epoch()
+        print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss:.4f}')
       
 class GNNTester:
-  def __init__(self, model, data, device='cuda'):
-     model.test()
-     self.model = model
-     self.data = data.to(device)
-     self.device = device
-     self.model.to(device)
+    def __init__(self, model, data, device='cuda'):
+        model.test()
+        self.model = model
+        self.data = data.to(device)
+        self.device = device
+        self.model.to(device)
     
-  def test(self):
-     self.model.eval()
-     with torch.no_grad():  
-       out = self.model(self.data)  
-       pred = out.argmax(dim=1)  
-       correct = (pred[self.data.test_mask] == self.data.y[self.data.test_mask]).sum() 
-       acc = int(correct) / int(self.data.test_mask.sum())  
-       return acc
+    def test(self):
+        self.model.eval()
+        with torch.no_grad():  
+            out = self.model(self.data)  
+            pred = out.argmax(dim=1)  
+            correct = (pred[self.data.test_mask] == self.data.y[self.data.test_mask]).sum() 
+            acc = int(correct) / int(self.data.test_mask.sum())  
+            return acc

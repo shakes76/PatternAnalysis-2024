781937cc20b69b5906ba9e1fda5a8b57350b5ae0
ETraffordUQ <ewantrafford@gmail.com>
2024-10-24 03:03:28 +1000

    Added customisation of raw dataset paths

---
+++ recognition/ISIC_LesionNet_48022835/utils.py
@@ -0,0 +1,8 @@
+raw_training_images_path = 'C:/Users/ewani/Downloads/ISIC2018_Task1-2_Training_Input_x2'
+raw_training_masks_path = 'C:/Users/ewani/Downloads/ISIC2018_Task1_Training_GroundTruth_x2'
+
+raw_validation_images_path = 'C:/Users/ewani/Downloads/ISIC2018_Task1-2_Validation_Input'
+raw_validation_masks_path = 'C:/Users/ewani/Downloads/ISIC2018_Task1_Validation_GroundTruth'
+
+raw_testing_images_path = 'C:/Users/ewani/Downloads/ISIC2018_Task1-2_Test_Input'
+raw_testing_masks_path = 'C:/Users/ewani/Downloads/ISIC2018_Task1_Test_GroundTruth'

3de4e18b389b796620688e075cbb4c62dd7a12bf
Mr Yuda Hendriawan Budi Handoko <s4708525@login0.compute.eait.uq.edu.au>
2024-10-23 15:06:03 +1000

    Update: add code to visualize the sample of data and count the number of training, validation, and testing

--- recognition/VisionTransformer-s4708525/dataset.py
+++ recognition/VisionTransformer-s4708525/dataset.py
@@ -2,6 +2,8 @@ from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset, random_split
 import os
 from PIL import Image
+import matplotlib.pyplot as plt
+from utils import *
 
 # local path
 train_path_local = '/Users/yudahendriawan/Course [LOCAL]/PATTERN/projects/AD_NC/train/'
@@ -95,6 +97,12 @@ transform_test = transforms.Compose([
     transforms.Normalize(mean=[0.1155, 0.1155, 0.1155], std=[0.2224, 0.2224, 0.2224])
 ])
 
+transform_no_augmentation = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.1155, 0.1155, 0.1155], std=[0.2224, 0.2224, 0.2224])
+])
+
 train_dataset = ADNI_Dataset(train_path_server, transform=transform)
 test_dataset = ADNI_Dataset(test_path_server, transform=transform_test)
 
@@ -109,3 +117,52 @@ train_subset, val_subset = random_split(train_dataset, [train_size, val_size])
 train_loader = DataLoader(train_subset, batch_size=16, shuffle=True)
 val_loader = DataLoader(val_subset, batch_size=16, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+classes = ['NC', 'AD']
+
+# save sample of train images augmented
+show_sample_data(train_loader, classes, 'img_training_samples_augmented')
+
+# save sample of test images augmented
+# shuffle=True for the purpose of showing the samples
+test_loader_augmented = DataLoader(test_dataset, batch_size=16, shuffle=True)
+show_sample_data(test_loader_augmented, classes, 'img_testing_samples_augmented')
+
+
+# show sample of original dataset
+train_dataset_no_augmented = ADNI_Dataset(train_path_server, transform=transform_no_augmentation)
+test_dataset_no_augmented = ADNI_Dataset(test_path_server, transform=transform_no_augmentation)
+
+train_loader_no_augmented = DataLoader(train_dataset_no_augmented, batch_size=16, shuffle=True)
+# shuffle=True for the purpose of showing the samples
+test_loader_no_augmented = DataLoader(test_dataset_no_augmented, batch_size=16, shuffle=True)
+
+# save sample of training images no augmented
+show_sample_data(train_loader_no_augmented, classes, 'img_training_samples_original')
+show_sample_data(test_loader_no_augmented, classes, 'img_testing_samples_original')
+
+# get counts for the training dataset
+train_counts = train_dataset.get_category_counts()
+print(f"Total training data (before splitting): {train_counts}")
+
+# get counts for the train subset after splitting
+train_subset_counts = {'NC': 0, 'AD': 0}
+for _, label in train_subset:
+    if label == 0:
+        train_subset_counts['NC'] += 1
+    else:
+        train_subset_counts['AD'] += 1
+print(f"Training subset counts: {train_subset_counts}")
+
+# get counts for the validation subset
+val_subset_counts = {'NC': 0, 'AD': 0}
+for _, label in val_subset:
+    if label == 0:
+        val_subset_counts['NC'] += 1
+    else:
+        val_subset_counts['AD'] += 1
+print(f"Validation subset counts: {val_subset_counts}")
+
+# get counts for the test dataset
+test_counts = test_dataset.get_category_counts()
+print(f"Test dataset counts: {test_counts}")

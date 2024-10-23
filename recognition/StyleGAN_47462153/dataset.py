06600247c04c77a3d1a5ebfc8bd9d6b2f22d830f
Ojas0305 <ojmay3@gmail.com>
2024-10-24 04:16:40 +1000

    Implemented image path and label extraction from 'AD' and 'NC' directories.

--- recognition/StyleGAN_47462153/dataset.py
+++ recognition/StyleGAN_47462153/dataset.py
@@ -8,3 +8,22 @@ class BrainDataset(Dataset):
         self.image_paths = []
         self.labels = []
         self.transform = transform
+
+        ad_path = os.path.join(root_dir, 'AD')
+        nc_path = os.path.join(root_dir, 'NC')
+
+        if os.path.isdir(ad_path):
+            for file_name in os.listdir(ad_path):
+                file_path = os.path.join(ad_path, file_name)
+                if file_name.lower().endswith(('png', 'jpg', 'jpeg')):
+                    self.image_paths.append(file_path)
+                    self.labels.append(0)
+
+        if os.path.isdir(nc_path):
+            for file_name in os.listdir(nc_path):
+                file_path = os.path.join(nc_path, file_name)
+                if file_name.lower().endswith(('png', 'jpg', 'jpeg')):
+                    self.image_paths.append(file_path)
+                    self.labels.append(1)
+
+        print(f"Loaded {len(self.image_paths)} images from {root_dir}")

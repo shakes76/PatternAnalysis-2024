9223456126070b96d958b004c0aa9f6c091d7efd
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 01:51:22 +1000

    Update dataset.py

--- recognition/49094642 Graph Neural Network/dataset.py
+++ recognition/49094642 Graph Neural Network/dataset.py
@@ -1,22 +1,24 @@
 class CustomDataset(Dataset):
-  def __init__(self, data_path, transform=None):
-    self.data_path = data_path
+    def __init__(self, edge_path, data2_path, data3_pate, transform=None):
+        self.edge_path = edge_path
+        self.data2_path = data2_path
+        self.data3_path = data3_path
+      
+    def load_edge(self):
+        edge_def = pd.read_csv(self.edge_path)
+        return torch.tensor(edge_df.values.T, dtype=torch.long)
 
-  def data_data1(self):
-    data1_def = pd.read_csv(self.edge_path)
-    return torch.tensor(data1_df.values.T, dtype=torch.long)
+    def data_data2(self):
+        data2_def = pd.read_csv(self.edge_path)
+        return torch.tensor(data2_df.values.T, dtype=torch.long)
 
-  def data_data2(self):
-    data2_def = pd.read_csv(self.edge_path)
-    return torch.tensor(data2_df.values.T, dtype=torch.long)
+    def data_data3(self):
+        data3_def = pd.read_csv(self.edge_path)
+        return torch.tensor(data3_df.values.T, dtype=torch.long)
 
-  def data_data3(self):
-    data3_def = pd.read_csv(self.edge_path)
-    return torch.tensor(data3_df.values.T, dtype=torch.long)
-
-  def create_data_loader(self, transform=None):
-    data1 = self.load_edges()
-    data2 = self.load_edges()
-    data3 = self.load_edges()
-    dataset = Dataset(data_path, transform=transform)
-    return data_loader
+    def create_data_loader(self, transform=None):
+        edge = self.load_edges()
+        data2 = self.load_edges()
+        data3 = self.load_edges()
+        dataset = Data(data_path, transform=transform)
+        return data_loader

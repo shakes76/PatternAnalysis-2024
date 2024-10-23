3dc123a89a0ad20fc29bce3ecef9bdf21c5f91d1
ETraffordUQ <ewantrafford@gmail.com>
2024-10-24 03:04:43 +1000

    Added functionality to install commonly used weights when script is run

--- recognition/ISIC_LesionNet_48022835/modules.py
+++ recognition/ISIC_LesionNet_48022835/modules.py
@@ -3,3 +3,9 @@ from ultralytics import YOLO
 def YOLOv11(weights_path):
 
     return YOLO(weights_path)
+
+# Only run modules.py to pre-install weights for useful models | not required
+if __name__ == "__main__":
+    model_s = YOLOv11("yolo/yolo11n.pt")
+    model_m = YOLOv11("yolo/yolo11m.pt")
+    model_l = YOLOv11("yolo/yolo11l.pt")

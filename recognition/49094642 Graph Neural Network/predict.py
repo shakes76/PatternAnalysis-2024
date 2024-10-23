f0ea55991d30558d5531c0a8e8e01864d3c85d8b
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 00:33:04 +1000

    Update predict.py

--- recognition/49094642 Graph Neural Network/predict.py
+++ recognition/49094642 Graph Neural Network/predict.py
@@ -1,13 +1,10 @@
-def plot_2d_umap(features, labels=None, n_neighbors=15, min_dist=0.1, metric='euclidean'):
+def visualize(model, deviece, data):
+  model.eval()
+  plot_2d_umap(features, labels=None, n_neighbors=15, min_dist=0.1, metric='euclidean'):
   reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, n_components=2, metric=metric)
   embedding = reducer.fit_transform(features)
-  plt.figure(figsize=(10, 8))
-  if labels is not None:
-    plt.scatter(embedding[:, 0], embedding[:, 1], c=labels, cmap='Spectral', s=5)
-    plt.colorbar(boundaries=range(len(set(labels))+1)) 
-  else:
-    plt.scatter(embedding[:, 0], embedding[:, 1], s=5)
-    plt.title('UMAP')
-    plt.xlabel('UMAP Dimension 1')
-    plt.ylabel('UMAP Dimension 2')
-    plt.show()
+  plt.figure(figsize)=(10, 8))
+  scatter = plt.scatter(umap_embeds[:, 0], umap_embeds[:, 1], c=data.y.cpu().numpy(), cmap='Spectral', s=5)
+  plt.colorbar(scatter)
+  plt.title('UMAP')
+  plt.show()

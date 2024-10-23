6b1c3f43d984e9c506835f8cc763ba4dfc8f120e
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 03:18:30 +1000

    Update predict.py

--- recognition/49094642 Graph Neural Network/predict.py
+++ recognition/49094642 Graph Neural Network/predict.py
@@ -1,7 +1,9 @@
-def visualize(model, deviece, data):
+def visualize(model, data):
     model.eval()
     with torch.no_grad():
         embeddings = model1.conv1(data.x, data.edge_index)
+    umap = UMAP(n_components=2)
+    umap_embeds = umap.fit_transform(embeddings.cup().numpy())
     plt.figure(figsize)=(10, 8))
     scatter = plt.scatter(umap_embeds[:, 0], umap_embeds[:, 1], c=data.y.cpu().numpy(), cmap='Spectral', s=5)
     plt.colorbar(scatter)

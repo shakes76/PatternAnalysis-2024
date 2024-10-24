3749e1b30a29c6adb314684dec10aff8b66eaa2d
InverseMist <jccockle@outlook.com>
2024-10-24 22:18:45 +1000

    Fixed line 7 by changing type of device to format properly.

--- recognition/yolo-48097824/modules.py
+++ recognition/yolo-48097824/modules.py
@@ -4,10 +4,13 @@ from ultralytics import yolov8
 def assign_device():
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print("The device selected is " + device)
+    print("The device selected is " + str(device))
 
     return device
 
 def use_yolo(device):
 
-    model = yolov8()
+    model = yolov8("yolov8n.yaml")
+    model.to(device)
+
+    return model

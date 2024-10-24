37b405f489c924faa776772eba9d57f297e51723
nk20032003 <nk20032003@gmail.com>
2024-10-24 13:50:52 +1000

    predict.py - Added test data loading and batching

--- recognition/3D Improved UNet for segmenting 3D prostate data - Nathan King (47399718)/predict.py
+++ recognition/3D Improved UNet for segmenting 3D prostate data - Nathan King (47399718)/predict.py
@@ -1,7 +1,20 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import os
+import time
+
+from dataset import load_mri_data
+from train import DATA_PATH, BATCH_LENGTH, BUFFER_SIZE
+
 def predict_model():
     """
     Make predictions using the trained model.
     """
+    
+    #Load and batch test data
+    test_dataset = load_mri_data(DATA_PATH, True)[1]
+    test_batches = test_dataset.shuffle(BUFFER_SIZE).batch(BATCH_LENGTH)
 
 if __name__ == "__main__":
     

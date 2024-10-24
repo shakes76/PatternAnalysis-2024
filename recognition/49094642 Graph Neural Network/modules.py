a1700504424cb4a43c400e804951e0119564c198
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-25 02:59:43 +1000

    Optimize the flexibility of network structure

--- recognition/49094642 Graph Neural Network/modules.py
+++ recognition/49094642 Graph Neural Network/modules.py
@@ -1,29 +1,18 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, BatchNorm
+from torch_geometric.nn import GCNConv
 
+# GCN Model class
 class GCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, dropout_rate=0.5, num_layers=3):
+    def __init__(self, in_channels, hidden_channels, out_channels):
         super(GCN, self).__init__()
-        self.num_layers = num_layers
-        self.dropout_rate = dropout_rate
-        self.convs = torch.nn.ModuleList()
-        self.convs.append(GCNConv(in_channels, hidden_channels))
-        for _ in range(num_layers - 2):
-          self.convs.append(GCNConv(hidden_channels, hidden_channels))
-          self.convs.append(GCNConv(hidden_channels, out_channels)
-          self.bns = torch.nn.ModuleList([BatchNorm(hidden_channels) for _ in range(num_layers - 1)])
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, out_channels)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = self.convs[0](x, edge_index)
-        x = F.relu(x)
-        x = self.bns[0](x)
-        x = F.dropout(x, p=self.dropout_rate, training=self.training)
-        for i in range(1, self.num_layers - 1):
-            x = self.convs[i](x, edge_index)
-            x = F.relu(x)
-            x = self.bns[i](x)
-            x = F.dropout(x, p=self.dropout_rate, training=self.training)
-        x = self.convs[-1](x, edge_index)
-        return F.log_softmax(x, dim=1)
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.conv2(x, edge_index))
+        return F.log_softmax(self.conv3(x, edge_index), dim=1)

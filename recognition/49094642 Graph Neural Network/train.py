9f053c45171cc289db7c78f56a58af70ec027b5f
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 13:46:11 +1000

    Update train.py

--- recognition/49094642 Graph Neural Network/train.py
+++ recognition/49094642 Graph Neural Network/train.py
@@ -6,20 +6,8 @@ class Trainer:
         loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
         loss.backward()
         optimizer.step()
-
-    def train(self, epochs=50):
-        for epoch in range(epochs):
-        loss = self.train_epoch()
-        print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss:.4f}')
       
 class Tester:
-    def __init__(self, model, data, device='cuda'):
-        model.test()
-        self.model = model
-        self.data = data.to(device)
-        self.device = device
-        self.model.to(device)
-    
     def test(model,data):
         model.eval()
         with torch.no_grad():  

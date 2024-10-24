f23eb2ce7b1aa0903be647149632de1e42a07c79
Uncle Mitch <damon.fok@outlook.com>
2024-10-24 22:34:49 +1000

    Removed unnecessary comments and sections from dataset.py

--- recognition/47436761-alzheimer/dataset.py
+++ recognition/47436761-alzheimer/dataset.py
@@ -6,19 +6,6 @@ import torch
 
 IMAGE_DIM = 224 
 
-transform = transforms.Compose([
-    transforms.Resize((IMAGE_DIM, IMAGE_DIM)),
-    transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
-    transforms.RandomHorizontalFlip(p=0.3),
-    transforms.RandomRotation(degrees=5),
-    transforms.ColorJitter(brightness=0.1, contrast=0.1),
-    transforms.RandomAffine(degrees=5, translate=(0.02, 0.02), scale=(0.95, 1.05)),
-    transforms.RandomResizedCrop(size=IMAGE_DIM, scale=(0.8, 1.0)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485], std=[0.229]),  # Imagenet values, or adjust based on MRI dataset stats
-    transforms.RandomErasing(p=0.3, scale=(0.02, 0.05), ratio=(0.3, 3.3)),
-])
-
 
 def create_train_loader(root_dir, batch_size=32, val_split=0.2, seed=69):
     """
@@ -37,14 +24,14 @@ def create_train_loader(root_dir, batch_size=32, val_split=0.2, seed=69):
     train_transform = transforms.Compose([
         transforms.Resize((IMAGE_DIM, IMAGE_DIM)),
         transforms.Grayscale(num_output_channels=1),
-        #transforms.RandomHorizontalFlip(p=0.3),
+        transforms.RandomHorizontalFlip(p=0.3),
         transforms.RandomRotation(degrees=10),
-        #transforms.ColorJitter(brightness=0.1, contrast=0.1),
-        #transforms.RandomAffine(degrees=5, translate=(0.02, 0.02), scale=(0.95, 1.05)),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1),
+        transforms.RandomAffine(degrees=5, translate=(0.02, 0.02), scale=(0.95, 1.05)),
         transforms.RandomResizedCrop(size=IMAGE_DIM, scale=(0.9, 1.1)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485], std=[0.229]),
-        #transforms.RandomErasing(p=0.3, scale=(0.02, 0.05), ratio=(0.3, 3.3)),
+        transforms.RandomErasing(p=0.3, scale=(0.02, 0.05), ratio=(0.3, 3.3)),
     ])
     
     dataset = datasets.ImageFolder(root=root_dir, transform=train_transform)
@@ -64,7 +51,6 @@ def create_train_loader(root_dir, batch_size=32, val_split=0.2, seed=69):
     return train_loader
 
 
-# Function to create a validation DataLoader
 def create_val_loader(root_dir, batch_size=32, val_split=0.2, seed=69):
     """
     Creates a DataLoader for the validation set without data augmentation.
@@ -103,7 +89,6 @@ def create_val_loader(root_dir, batch_size=32, val_split=0.2, seed=69):
     return val_loader
 
 
-# Function to create a test DataLoader
 def create_test_loader(root_dir, batch_size=32):
     """
     Creates a DataLoader for the test set without data augmentation.

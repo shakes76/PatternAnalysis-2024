75395fc8e35b73446a0fb08c860fc6c452a7b820
Kevin-Gu-2022-UQ <kevin.gu@student.uq.edu.au>
2024-10-24 23:56:46 +1000

    Break up large main function so that it is more modular

--- recognition/GFNet-47438886/dataset.py
+++ recognition/GFNet-47438886/dataset.py
@@ -1,11 +1,10 @@
-""" Dataset Loaders
-
-Set of functions to use to load the ADNI dataset.
-
+""" 
+Dataset Loaders
+Set of functions and classes to load the ADNI dataset.
+Author: Kevin Gu
 """
 import os
 import re
-import platform
 import torch
 from torchvision import datasets, transforms
 from torchvision.transforms import v2
@@ -14,6 +13,10 @@ from sklearn.model_selection import train_test_split
 from PIL import Image
 import pandas as pd
 from collections import Counter 
+
+# Mean and std for the ADNI dataset
+ADNI_MEAN = 0.1156
+ADNI_STD = 0.2199
 
 
 def extract_patient_id(file_name):
@@ -45,9 +48,15 @@ class ADNIDataset(Dataset):
         self.transform = transform
 
     def __len__(self):
+        """
+        Return length of the image path list
+        """
         return len(self.image_paths)
 
     def __getitem__(self, idx):
+        """
+        Return the indexed image, with transform applied
+        """
         img_path = self.image_paths[idx]
         image = Image.open(img_path)
         label = self.labels[idx]
@@ -58,7 +67,59 @@ class ADNIDataset(Dataset):
         return image, label
     
 
-def load_adni_data(root_dir, valid_size=0.1, batch_size=32, testing=False):
+def ADNI_split_validation_set(image_paths: list, labels: list):
+    """
+    Splits the data on a patient level, then on a class level, ensuring equal
+    balance of classes.
+
+    Parameters:
+        image_paths: A list of paths to all the images
+        labels: A list containing the class labels associated with each image
+    Returns:
+        train_image_paths: A list of paths to images in train set
+        train_labels: A list of labels of images in train set
+        val_image_paths: A list of paths to images in validation set
+        val_labels: A list of labels of images in validation set
+    """
+     # Extract patient IDs to ensure no leakage
+    patient_ids = [extract_patient_id(os.path.basename(path)) for path in image_paths]
+    
+    # Create a DataFrame to assist in patient-wise splitting
+    data = list(zip(image_paths, labels, patient_ids))
+    
+    # Perform a patient-wise split
+    unique_patients = set(patient_ids)
+    train_patients, val_patients = train_test_split(list(unique_patients), test_size=0.1, random_state=42)
+
+    df = pd.DataFrame(data, columns=['image_path', 'label', 'patient_id'])
+
+    # Split patients by class (NC and AD) to maintain class balance
+    nc_patients = df[df['label'] == 0]['patient_id'].unique()  # Patients with NC
+    ad_patients = df[df['label'] == 1]['patient_id'].unique()  # Patients with AD
+
+    # Split patients with stratification to ensure even class distribution
+    train_nc_patients, val_nc_patients = train_test_split(nc_patients, test_size=0.1, random_state=42)
+    train_ad_patients, val_ad_patients = train_test_split(ad_patients, test_size=0.1, random_state=42)
+
+    # Combine train and validation patients
+    train_patients = list(train_nc_patients) + list(train_ad_patients)
+    val_patients = list(val_nc_patients) + list(val_ad_patients)
+
+    # Split the dataset into training and validation based on patient IDs
+    train_data = df[df['patient_id'].isin(train_patients)]
+    val_data = df[df['patient_id'].isin(val_patients)]
+
+    # Separate image paths and labels
+    train_image_paths = train_data['image_path'].tolist()
+    train_labels = train_data['label'].tolist()
+    
+    val_image_paths = val_data['image_path'].tolist()
+    val_labels = val_data['label'].tolist()
+
+    return (train_image_paths, train_labels), (val_image_paths, val_labels)
+    
+
+def load_adni_data(root_dir, batch_size=32, testing=False):
     """
     Load the ADNI dataset and ensure no leakage by splitting data subject-wise.
     
@@ -87,70 +148,34 @@ def load_adni_data(root_dir, valid_size=0.1, batch_size=32, testing=False):
                 labels.append(label)
 
     if not testing:  # If not testing, need to split into training and validation set
-        # Extract patient IDs to ensure no leakage
-        patient_ids = [extract_patient_id(os.path.basename(path)) for path in image_paths]
-        
-        # Create a DataFrame to assist in patient-wise splitting
-        data = list(zip(image_paths, labels, patient_ids))
-        
-        # Perform a patient-wise split
-        unique_patients = set(patient_ids)
-        train_patients, val_patients = train_test_split(list(unique_patients), test_size=0.1, random_state=42)
 
-        df = pd.DataFrame(data, columns=['image_path', 'label', 'patient_id'])
+        train_data, val_data = ADNI_split_validation_set(image_paths, labels)
 
-        # Split patients by class (NC and AD) to maintain class balance
-        nc_patients = df[df['label'] == 0]['patient_id'].unique()  # Patients with NC
-        ad_patients = df[df['label'] == 1]['patient_id'].unique()  # Patients with AD
-
-        # Split patients with stratification to ensure even class distribution
-        train_nc_patients, val_nc_patients = train_test_split(nc_patients, test_size=0.1, random_state=42)
-        train_ad_patients, val_ad_patients = train_test_split(ad_patients, test_size=0.1, random_state=42)
-
-        # Combine train and validation patients
-        train_patients = list(train_nc_patients) + list(train_ad_patients)
-        val_patients = list(val_nc_patients) + list(val_ad_patients)
-
-        # Split the dataset into training and validation based on patient IDs
-        train_data = df[df['patient_id'].isin(train_patients)]
-        val_data = df[df['patient_id'].isin(val_patients)]
-
-        # Separate image paths and labels
-        train_image_paths = train_data['image_path'].tolist()
-        train_labels = train_data['label'].tolist()
-        
-        val_image_paths = val_data['image_path'].tolist()
-        val_labels = val_data['label'].tolist()
-
-    mean = 0.1156
-    std = 0.2199
-    # Define image transformations
-    if not testing:
+        # Define image transformations
         transform = v2.Compose([
             v2.CenterCrop((224, 224)),  # Resize to fit model input size (256 x 240)
             v2.RandomVerticalFlip(p=0.5),
             v2.ToTensor(),  # Convert image to PyTorch tensor
             v2.GaussianNoise(),
-            v2.Normalize(mean=[mean], std=[std])
+            v2.Normalize(mean=[ADNI_MEAN], std=[ADNI_STD])
         ])
 
-    else:  # Testing
-        transform = v2.Compose([
-        v2.CenterCrop((224, 224)),  # Resize to fit model input size (256 x 240)
-        v2.ToTensor(),  # Convert image to PyTorch tensor
-        v2.Normalize(mean=[mean], std=[std]) 
-    ])
-
-    if not testing:
         # Create ADNIDataset instances for training and validation
-        train_dataset = ADNIDataset(train_image_paths, train_labels, transform=transform)
-        val_dataset = ADNIDataset(val_image_paths, val_labels, transform=transform)
+        train_dataset = ADNIDataset(*train_data, transform=transform)
+        val_dataset = ADNIDataset(*val_data, transform=transform)
         # Create DataLoaders for batching and parallel data loading
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
         val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+
         return train_loader, val_loader
 
-    else:
+    else:  # Testing
+        transform = v2.Compose([
+            v2.CenterCrop((224, 224)),  # Resize to fit model input size (256 x 240)
+            v2.ToTensor(),  # Convert image to PyTorch tensor
+            v2.Normalize(mean=[ADNI_MEAN], std=[ADNI_STD]) 
+        ])
+
         test_dataset = ADNIDataset(image_paths, labels, transform=transform)
         test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
         return test_loader

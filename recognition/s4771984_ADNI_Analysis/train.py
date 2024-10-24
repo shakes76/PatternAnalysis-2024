e642c637b4f74fdaa18f01af5f1c4fee3b323358
sid <sgaikwad2001@gmail.com>
2024-10-25 03:23:00 +1000

    Increased number of epochs from 150 to 200 for better accuracy, kept learning rate at 0.001 While training with 150 epochs previously, the model was able to obtain 75% accuracy. In the following, we have increased the number of epochs to 200 while keeping the learning rate fixed at 0.001 for better performance and accuracy of the model. Additional 50 epochs will give a chance for the model to further work on feature extraction and Improve both training and validation accuracy.

--- recognition/s4771984_ADNI_Analysis/train.py
+++ recognition/s4771984_ADNI_Analysis/train.py
@@ -86,7 +86,7 @@ model = GFNet(num_classes=2).to(device)
 # Loss function, optimizer, and number of epochs
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
-num_epochs = 150
+num_epochs = 200
 
 # Train the model
 train_loss, val_loss, train_acc, val_acc = train_model_with_val_loss(model, criterion, optimizer, num_epochs, train_loader, val_loader)

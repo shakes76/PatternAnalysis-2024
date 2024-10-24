a7d178ef3554ec2b243f8f34fc8201cfa9f0d639
Georgia <s4743401@student.uq.edu.au>
2024-10-25 00:54:24 +1000

    fixed error with RGB being used. Want greyscale for these. Also fixed image size initialisation.

--- recognition/s4743401_project/dataset.py
+++ recognition/s4743401_project/dataset.py
@@ -5,27 +5,26 @@ data_dir = '/Users/gghollyd/comp3710/report/AD_NC/'
 def dataloader(data_dir):
     data_transforms = {
         'train': transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.RandomHorizontalFlip(),
+            transforms.Grayscale(num_output_channels=1),  
+            transforms.Resize((240, 256)),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) #check normalisation and where done. See if better here or in predict idk.
+            transforms.Normalize([0.5], [0.5])  # Use single-channel normalization for grayscale
         ]),
         'test': transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Grayscale(num_output_channels=1),  
+            transforms.Resize((240, 256)),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize([0.5], [0.5])  # Single-channel normalization for grayscale
         ]),
     }
-    
+
     image_datasets = {
         'train': datasets.ImageFolder(root=data_dir + '/train', transform=data_transforms['train']),
         'test': datasets.ImageFolder(root=data_dir + '/test', transform=data_transforms['test']),
     }
 
-    dataloaders = {
-        'train': DataLoader(image_datasets['train'], batch_size=32, shuffle=True, num_workers=4),
-        'test': DataLoader(image_datasets['test'], batch_size=32, shuffle=False, num_workers=4),
-    }
-
-    return dataloaders, image_datasets['train'].classes
+    train_loader = DataLoader(image_datasets['train'], batch_size=32, shuffle=True, num_workers=4)
+    test_loader = DataLoader(image_datasets['test'], batch_size=32, shuffle=False, num_workers=4)
+    
+    return train_loader, test_loader, image_datasets['train'].classes
 

5ba581bfa530e174074a49b1e507ebe29c630e13
oystkva <oystkva@stud.ntnu.no>
2024-10-24 22:41:11 +1000

    General fixes: there was a missing label/channel and replaced interpolating for padding

--- recognition/s4904983_UNET/modules.py
+++ recognition/s4904983_UNET/modules.py
@@ -43,7 +43,7 @@ class UNet2D(nn.Module):
                                             stride=2)
         self.up_conv_4 = double_conv2d(128, 64)
         self.out = nn.Conv2d(in_channels=64,
-                            out_channels=5,
+                            out_channels=6,
                             kernel_size=1)
     
     def forward(self, image):
@@ -56,39 +56,37 @@ class UNet2D(nn.Module):
         x6 = self.max_pool_2x2(x5)
         x7 = self.down_conv_4(x6)
         # x8 = self.max_pool_2x2(x7)
-        # print(x8.size())
         # x9 = self.down_conv_5(x8)
-        # print(x9.size())
 
         ### Decoding
 
         # x = self.up_trans_1(x9)
         # print(x.size())
-        # # Interpolate to match the size of the skip connection
-        # x = F.interpolate(x, size=x7.size()[2:], mode='bilinear', align_corners=True)
         # x = self.up_conv_1(torch.cat([x, x7], 1))
-        # print('9',x.size())
+        
         x = self.up_trans_2(x7)
-        x = F.interpolate(x, size=x5.size()[2:], mode='bilinear', align_corners=True)
+
         x = self.up_conv_2(torch.cat([x, x5], 1))
 
         x = self.up_trans_3(x)
-        x = F.interpolate(x, size=x3.size()[2:], mode='bilinear', align_corners=True)
         x = self.up_conv_3(torch.cat([x, x3], 1))
 
         x = self.up_trans_4(x)
-        x = F.interpolate(x, size=x1.size()[2:], mode='bilinear', align_corners=True)
         x = self.up_conv_4(torch.cat([x, x1], 1))
 
         x = self.out(x)
-        x = F.interpolate(x, size=image.size()[2:], mode='bilinear', align_corners=True)
         return x
 
 
 def double_conv2d(in_channels, out_channels):
     return nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=3),
+        nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
         nn.ReLU(inplace=True),
-        nn.Conv2d(out_channels, out_channels, kernel_size=3),
+        nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
         nn.ReLU(inplace=True)
     )
+
+
+# def crop_image(tensor, target_tensor):
+#     border = (tensor.size()[2] - target_tensor.size()[2]) // 2
+#     return tensor[:, :, border:(tensor.size()[2] - border), border:(tensor.size()[3] - border)]

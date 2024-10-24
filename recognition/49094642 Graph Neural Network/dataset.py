98edbf86be12402130b82523354203092f6b3ec3
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-25 03:54:11 +1000

    The feature vector is truncated or padded to 128 dimensions and a loop is used to process the features of each node one by one

--- recognition/49094642 Graph Neural Network/dataset.py
+++ recognition/49094642 Graph Neural Network/dataset.py
@@ -1,41 +1,32 @@
 import torch
 import pandas as pd
 import json
-import numpy as np
-from torch_geometric.data import Data
+from torch_geometric.utils import to_undirected
 
-class DataLoader:
-    def __init__(self, edge_path, features_path, target_path):
+# Data Loader Class
+class FacebookDatasetLoader:
+    def __init__(self, edge_path, target_path, feature_path):
         self.edge_path = edge_path
-        self.features_path = features_path
         self.target_path = target_path
+        self.feature_path = feature_path
 
-    def load_edges(self):
-        return torch.tensor(pd.read_csv(self.edge_path).values.T, dtype=torch.long)
+    def load_data(self):
+        edges = pd.read_csv(self.edge_path)
+        labels = pd.read_csv(self.target_path)
+        with open(self.feature_path) as f:
+            features = json.load(f)
 
-    def load_features(self):
-        with open(self.features_path) as f:
-            features_dict = json.load(f)
-        return torch.tensor(np.array(list(features_dict.values())), dtype=torch.float)
+        # Process node features, truncate or pad to 128 dimensions
+        node_features = torch.zeros((len(features), 128))
+        for node, feats in features.items():
+            feats = feats[:128]
+            if len(feats) < 128:
+                feats += [0] * (128 - len(feats))
+            node_features[int(node)] = torch.tensor(feats, dtype=torch.float)
 
-    def load_target(self):
-        return torch.tensor(pd.read_csv(self.target_path)['target'].values, dtype=torch.long)
+        # Convert edge list to tensor and labels
+        edge_index = to_undirected(torch.tensor(edges.values.T, dtype=torch.long))
+        labels['page_type'], _ = pd.factorize(labels['page_type'])
+        y = torch.tensor(labels['page_type'].values, dtype=torch.long)
 
-    def create_data(self):
-        edges = self.load_edges()
-        x = self.load_features()
-        y = self.load_target()
-
-        num_nodes = y.size(0)
-        train_mask, test_mask = self.create_masks(num_nodes)
-
-        data = Data(x=x, edge_index=edges, y=y, train_mask=train_mask, test_mask=test_mask)
-        return data
-
-    @staticmethod
-    def create_masks(num_nodes, train_split=0.8):
-        mask = np.zeros(num_nodes, dtype=bool)
-        train_size = int(train_split * num_nodes)
-        indices = np.random.permutation(num_nodes)
-        mask[indices[:train_size]] = True
-        return torch.tensor(mask), torch.tensor(~mask)
+        return node_features, edge_index, y, labels

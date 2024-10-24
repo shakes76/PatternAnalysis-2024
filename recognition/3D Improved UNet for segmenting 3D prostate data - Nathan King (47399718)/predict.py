0cafdd24d7a658cb2ba6f4b871c8ffa9616b5f59
nk20032003 <nk20032003@gmail.com>
2024-10-24 22:21:09 +1000

    predict.py - Added function to save displayed examples

--- recognition/3D Improved UNet for segmenting 3D prostate data - Nathan King (47399718)/predict.py
+++ recognition/3D Improved UNet for segmenting 3D prostate data - Nathan King (47399718)/predict.py
@@ -97,6 +97,50 @@ def display(display_list, display_index):
     ani.save(SAVED_RESULTS_PATH + "Predicted" + str(display_index) + ".gif")
     plt.show()
 
+def display_and_save_examples(dataset, number_of_examples, model):
+    """
+    Display examples with accuracy and dice similarity coefficient from a dataset and save them.
+    input: dataset - dataset to take from
+           number_of_examples - number of examples to take
+           model - trained model
+    """
+    
+    prediction_index = 0
+    
+    #Iterate through each image-segmentation pair
+    for image, segmentation in dataset.take(number_of_examples):
+        
+        #Calculate true segmentation
+        true = segmentation
+        
+        #Calculate predicted segmentation
+        predicted = model.predict(image)
+        
+        #Print statistics
+        print("Multiclass DSC:", multiclass_dice_coefficient(true, predicted).numpy())
+        print("Background DSC:", background_dsc(true, predicted).numpy())
+        print("Body DSC:", body_dsc(true, predicted).numpy())
+        print("Bone DSC:", bone_dsc(true, predicted).numpy())
+        print("Bladder DSC:", bladder_dsc(true, predicted).numpy())
+        print("Rectum DSC:", rectum_dsc(true, predicted).numpy())
+        print("Prostate DSC:", prostate_dsc(true, predicted).numpy())
+        
+        #Process image for displaying
+        image_for_display = image[0]
+        
+        #Process true segmentation for displaying
+        true_for_display = [[[[list(x).index(1)] for x in y] for y in z] for z in segmentation[0].numpy()]
+        
+        #Process predicted segmentation for displaying
+        predicted_for_display = model.predict(image)
+        predicted_for_display = tf.argmax(predicted_for_display, axis = -1)
+        predicted_for_display = predicted_for_display[..., tf.newaxis][0]
+        
+        #Display and save example
+        display([image_for_display, true_for_display, predicted_for_display], prediction_index)
+        
+        prediction_index += 1
+
 def predict_model():
     """
     Make predictions using the trained model.
@@ -105,13 +149,16 @@ def predict_model():
     #Load and batch test data
     test_dataset = load_mri_data(DATA_PATH, True)[1]
     test_batches = test_dataset.shuffle(BUFFER_SIZE).batch(BATCH_LENGTH)
-    
+
     #Load trained model
     trained_improved_3d_unet_model = keras.models.load_model(SAVED_RESULTS_PATH + "improved_3d_unet_model.keras", custom_objects={"multiclass_dice_coefficient": multiclass_dice_coefficient, "background_dsc": background_dsc, "body_dsc": body_dsc, "bone_dsc": bone_dsc, "bladder_dsc": bladder_dsc, "rectum_dsc": rectum_dsc, "prostate_dsc": prostate_dsc})
     
     #Evaluate the trained model on the test set by calculating the dice similarity
     # coefficients for each class and the multiclass dice similarity coefficient
     trained_improved_3d_unet_model.evaluate(test_batches)
+    
+    #Display and save examples
+    display_and_save_examples(test_batches, 3, trained_improved_3d_unet_model)
 
 if __name__ == "__main__":
     

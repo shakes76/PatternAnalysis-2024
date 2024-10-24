723b597587259f050a203a4098830b6bb8bd7c1a
Georgia <s4743401@student.uq.edu.au>
2024-10-24 22:56:17 +1000

    Added training loop similar to lab2's implementation that links to dataset by taking dataloader and other info instantiated elsewhere as variables.

---
+++ recognition/s4743401_project/train.py
@@ -0,0 +1,22 @@
+import torch
+
+def train_model(model, dataloader, optimizer, criterion, num_epochs):
+    for epoch in range(num_epochs):
+        model.train()
+        correct = 0
+        total = 0
+        running_loss = 0.0
+        for images, labels in dataloader:
+            optimizer.zero_grad()
+            
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+        
+        accuracy = correct / total * 100
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss:.4f}, Accuracy: {accuracy:.2f}%')
+    
+    return model

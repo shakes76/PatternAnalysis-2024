ed989111a3a225f3a04640979b8daf2d8632308c
IgrisIX <93921275+IgrisIX@users.noreply.github.com>
2024-10-24 10:04:29 +1000

    Fix load_data_2D to resize image with different size

--- recognition/HipMRI_2D_UNet_46964472/dataset.py
+++ recognition/HipMRI_2D_UNet_46964472/dataset.py
@@ -1,5 +1,8 @@
+import os
 import numpy as np
 import nibabel as nib
+import tensorflow as tf
+import skimage.transform
 from tqdm import tqdm
 
 def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:
@@ -36,20 +39,23 @@ def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float3
     else:
         rows, cols = first_case.shape
         images = np.zeros((num, rows, cols), dtype=dtype)
-    
+
     for i, inName in enumerate(tqdm(imageNames)):
         niftiImage = nib.load(inName)
         inImage = niftiImage.get_fdata(caching='unchanged') # read disk only
         affine = niftiImage.affine
         if len(inImage.shape) == 3:
             inImage = inImage[:, :, 0] # sometimes extra dims in HimMRI_study data
+        if inImage.shape != (rows, cols):
+            inImage = skimage.transform.resize(inImage, (rows, cols), 
+                                              order=1, preserve_range=True)
         inImage = inImage.astype(dtype)
         if normImage:
             #~ inImage = inImage / np.linalg.norm(inImage)
             #~ inImage = 255. * inImage / inImage.max()
             inImage = (inImage - inImage.mean()) / inImage.std()
         if categorical:
-            inImage = utils.to_channels(inImage, dtype=dtype)
+            inImage = to_channels(inImage, dtype=dtype)
             images[i, :, :, :] = inImage
         else:
             images[i, :, :] = inImage
@@ -57,8 +63,19 @@ def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float3
         affines.append(affine)
         if i > 20 and early_stop:
             break
-    
+
     if getAffines:
         return images, affines
     else:
         return images
+
+def load_data_tf(image_dir, seg_dir, dtype=np.float32):
+    image_list = os.listdir(image_dir)
+    image_path = []
+    seg_path = []
+    for image in image_list:
+        image_path.append(os.path.join(image_dir, image))
+        seg_path.append(os.path.join(seg_dir, image.replace("case", "seg")))
+    images = load_data_2D(image_path)
+    segs = load_data_2D(seg_path, categorical=True)
+    return images, segs

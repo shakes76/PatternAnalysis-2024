e72e309ac6a1ddda2ff65e357692ffb8c5576aa9
Hemil Shah <hemilnikesh.shah@student.uq.edu.au>
2024-10-25 02:06:57 +1000

    The GCN model with theembeddings

---
+++ GCN_Recognition_HemilShah/modules.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+class GCN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.conv3 = GCNConv(hidden_dim, output_dim)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        embeddings = F.relu(self.conv2(x, edge_index))
+        out = self.conv3(embeddings, edge_index)
+        return out, embeddings

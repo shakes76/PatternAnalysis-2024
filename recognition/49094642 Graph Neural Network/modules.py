b29700b3e211ef1f8fabfb76fbab9a98f44d798c
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 21:17:54 +1000

    Add a layer of convolution

--- recognition/49094642 Graph Neural Network/modules.py
+++ recognition/49094642 Graph Neural Network/modules.py
@@ -2,16 +2,19 @@ import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
-class GNN(torch.nn.Module):
+class GCN(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
-        super(GNN, self).__init__()
+        super(GCN, self).__init__()
         self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, out_channels)
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index 
+        x, edge_index = data.x, data.edge_index
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        F = F.dropout(x, training=self.training)
+        x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)  
+        x = F.relu(x)
+        x = self.conv3(x, edge_index)
+        return F.log_softmax(x, dim=1)

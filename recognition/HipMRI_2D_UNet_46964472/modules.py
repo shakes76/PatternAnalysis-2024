e70c61c3d804b0661aa6b067c4e3d7ff893cf58f
IgrisIX <93921275+IgrisIX@users.noreply.github.com>
2024-10-24 14:24:38 +1000

    debug module

--- recognition/HipMRI_2D_UNet_46964472/modules.py
+++ recognition/HipMRI_2D_UNet_46964472/modules.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
-from keras.layers import Input, Conv2D, Conv2DTranspose, BatchNormalization, ReLU, MaxPool2D, Concatenate
+from keras.layers import Input, Conv2D, Conv2DTranspose, BatchNormalization, Activation, MaxPool2D, Concatenate
 from keras.initializers import GlorotNormal
+from keras import Model
 
 def Norm_Conv2D(input, n_filters, 
                 kernel_size=(3, 3), 
                 strides=(1, 1), 
-                activation=ReLU(), 
+                activation="relu", 
                 use_bias=True, 
                 kernel_initializer=GlorotNormal(), 
                 **kwargs):
@@ -19,12 +20,12 @@ def Norm_Conv2D(input, n_filters,
                         kernel_initializer=kernel_initializer,
                         **kwargs)(input)
     norm_layer = BatchNormalization()(conv_layer)
-    return activation(norm_layer)
+    return Activation(activation)(norm_layer)
 
 def Norm_Conv2DTranspose(input, n_filters, 
                          kernel_size=(3, 3), 
                          strides=(1, 1), 
-                         activation=ReLU(), 
+                         activation="relu", 
                          use_bias=True, 
                          kernel_initializer=GlorotNormal(), 
                          **kwargs):
@@ -37,42 +38,45 @@ def Norm_Conv2DTranspose(input, n_filters,
                                  kernel_initializer=kernel_initializer,
                                  **kwargs)(input)
     norm_layer = BatchNormalization()(conv_layer)
-    return activation(norm_layer)
+    return Activation(activation)(norm_layer)
 
-def UNet2D(input, latent_dim=64, activation=None, kernel=(3, 3), channels=1, name_prefix=''):
+def UNet2D(input_size, latent_dim=64, activation=None, kernel=(3, 3), channels=1, name_prefix=''):
+    input = Input(input_size)
+
     # Encoder
-    net = Norm_Conv2D(input, latent_dim//16, kernel, activation=ReLU())
-    down1 = Norm_Conv2D(net, latent_dim//16, kernel, activation=ReLU())
+    net = Norm_Conv2D(input, latent_dim//16, kernel)
+    down1 = Norm_Conv2D(net, latent_dim//16, kernel)
     net = MaxPool2D(2, padding='same')(down1)
-    net = Norm_Conv2D(input, latent_dim//8, kernel, activation=ReLU())
-    down2 = Norm_Conv2D(net, latent_dim//8, kernel, activation=ReLU())
-    net = MaxPool2D(2, padding='same')(down1)
-    net = Norm_Conv2D(input, latent_dim//4, kernel, activation=ReLU())
-    down3 = Norm_Conv2D(net, latent_dim//4, kernel, activation=ReLU())
-    net = MaxPool2D(2, padding='same')(down1)
-    net = Norm_Conv2D(input, latent_dim//2, kernel, activation=ReLU())
-    down4 = Norm_Conv2D(net, latent_dim//2, kernel, activation=ReLU())
-    net = MaxPool2D(2, padding='same')(down1)
-    net = Norm_Conv2D(net, latent_dim, kernel, activation=ReLU())
-    latent = Norm_Conv2D(net, latent_dim, kernel, activation=ReLU())
+    net = Norm_Conv2D(net, latent_dim//8, kernel)
+    down2 = Norm_Conv2D(net, latent_dim//8, kernel)
+    net = MaxPool2D(2, padding='same')(down2)
+    net = Norm_Conv2D(net, latent_dim//4, kernel)
+    down3 = Norm_Conv2D(net, latent_dim//4, kernel)
+    net = MaxPool2D(2, padding='same')(down3)
+    net = Norm_Conv2D(net, latent_dim//2, kernel)
+    down4 = Norm_Conv2D(net, latent_dim//2, kernel)
+    net = MaxPool2D(2, padding='same')(down4)
+    net = Norm_Conv2D(net, latent_dim, kernel)
+    latent = Norm_Conv2D(net, latent_dim, kernel)
 
     # Decoder
-    up4 = Norm_Conv2DTranspose(latent, latent_dim//2, kernel, activation=ReLU())
+    up4 = Norm_Conv2DTranspose(latent, latent_dim//2, kernel, 2)
     net = Concatenate(axis=3)([up4, down4])
-    net = Norm_Conv2D(net, latent_dim//2, kernel, activation=ReLU())
-    net = Norm_Conv2D(net, latent_dim//2, kernel, activation=ReLU())
-    up3 = Norm_Conv2DTranspose(net, latent_dim//4, kernel, 2, activation=ReLU())
+    net = Norm_Conv2D(net, latent_dim//2, kernel)
+    net = Norm_Conv2D(net, latent_dim//2, kernel)
+    up3 = Norm_Conv2DTranspose(net, latent_dim//4, kernel, 2)
     net = Concatenate(axis=3)([up3, down3])
-    net = Norm_Conv2D(net, latent_dim//4, kernel, activation=ReLU())
-    net = Norm_Conv2D(net, latent_dim//4, kernel, activation=ReLU())
-    up2 = Norm_Conv2DTranspose(net, latent_dim//8, kernel, 2, activation=ReLU())
+    net = Norm_Conv2D(net, latent_dim//4, kernel)
+    net = Norm_Conv2D(net, latent_dim//4, kernel)
+    up2 = Norm_Conv2DTranspose(net, latent_dim//8, kernel, 2)
     net = Concatenate(axis=3)([up2, down2])
-    net = Norm_Conv2D(net, latent_dim//8, kernel, activation=ReLU())
-    net = Norm_Conv2D(net, latent_dim//8, kernel, activation=ReLU())
-    up1 = Norm_Conv2DTranspose(net, latent_dim//16, kernel, 2, activation=ReLU())
+    net = Norm_Conv2D(net, latent_dim//8, kernel)
+    net = Norm_Conv2D(net, latent_dim//8, kernel)
+    up1 = Norm_Conv2DTranspose(net, latent_dim//16, kernel, 2)
     net = Concatenate(axis=3)([up1, down1])
-    net = Norm_Conv2D(net, latent_dim//16, kernel, activation=ReLU())
-    net = Norm_Conv2D(net, latent_dim//16, kernel, activation=ReLU())
+    net = Norm_Conv2D(net, latent_dim//16, kernel)
+    net = Norm_Conv2D(net, latent_dim//16, kernel)
 
     # Segmentation
-    return Norm_Conv2D(net, channels, (1, 1), activation=activation)
+    output = Norm_Conv2D(net, channels, (1, 1), activation=activation)
+    return Model(input, output)

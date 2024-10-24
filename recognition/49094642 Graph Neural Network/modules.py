627a45c53099af20fbaed5a63551fc8e7072a637
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-25 01:54:27 +1000

    Introducing the num_layers parameter

--- recognition/49094642 Graph Neural Network/modules.py
+++ recognition/49094642 Graph Neural Network/modules.py
@@ -1,20 +1,29 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, BatchNorm
 
 class GCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
+    def __init__(self, in_channels, hidden_channels, out_channels, dropout_rate=0.5, num_layers=3):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, out_channels)
+        self.num_layers = num_layers
+        self.dropout_rate = dropout_rate
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(GCNConv(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+          self.convs.append(GCNConv(hidden_channels, hidden_channels))
+          self.convs.append(GCNConv(hidden_channels, out_channels)
+          self.bns = torch.nn.ModuleList([BatchNorm(hidden_channels) for _ in range(num_layers - 1)])
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = self.conv1(x, edge_index)
+        x = self.convs[0](x, edge_index)
         x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
-        x = self.conv3(x, edge_index)
+        x = self.bns[0](x)
+        x = F.dropout(x, p=self.dropout_rate, training=self.training)
+        for i in range(1, self.num_layers - 1):
+            x = self.convs[i](x, edge_index)
+            x = F.relu(x)
+            x = self.bns[i](x)
+            x = F.dropout(x, p=self.dropout_rate, training=self.training)
+        x = self.convs[-1](x, edge_index)
         return F.log_softmax(x, dim=1)

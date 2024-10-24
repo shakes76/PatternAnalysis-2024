378057b16ca687178098715c6b939de0acac1c41
sid <sgaikwad2001@gmail.com>
2024-10-25 02:33:41 +1000

    Added global filter layer and increased model complexity and epochs for better performance
    Previous training with 50 epochs and a learning rate of 0.00001 resulted in much lower training accuracy than expected. The strategy to deal with that was to add a global filter layer of 1x1 for the refinement of features and increase the number of epochs for deeper training. This extra global filter will allow it to capture global dependencies from the feature maps. It is expected that this will let the model learn features of higher complexity that would result in better overall accuracy.

--- recognition/s4771984_ADNI_Analysis/modules.py
+++ recognition/s4771984_ADNI_Analysis/modules.py
@@ -1,41 +1,42 @@
 import torch
 import torch.nn as nn
-import warnings as w
+import warning as w
 w.filterwarnings('ignore')
 
 class GFNet(nn.Module):
     def __init__(self, num_classes=2):
         super(GFNet, self).__init__()
-        
-        # Initial convolutional layers with basic pooling
-        
+        # Convolutional layers with 96 and 192 filters
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=48, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(48, 96, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(96, 192, kernel_size=3, padding=1)
-        
-        self.pool = nn.MaxPool2d(2,2) # Reduce the dimensionality
-        self.elu = nn.ELU() #Activation Function
-        self.dropout = nn.Dropout(0.5)
-        self.fc1 = None
-        self.fc2 = nn.Linear(256, num_classes)
-        
-    def forward(self, x):
-        # Apply the convolutional layers with ELU and Maxpool
-       x = self.pool(self.elu(self.conv1(x)))
-       x = self.pool(self.elu(self.conv2(x)))
-       x = self.pool(self.elu(self.conv3(x)))
-       
-       # Flattening the output for the full connection of the layers
-       x = x.view(x.size(0), -1)
-       
-       if self.fc1 is None:
-           self.fc1 = nn.Linear(x.size(1), 256).to(x.device)
-       
-    # Now applying the fully connected layer with the dropout layer
-       x = self.elu(self.fc1(x))
-       x = self.dropout(x)
-       x = self.fc2(x)
+        self.conv4 = nn.Conv2d(192, 192, kernel_size=3, padding=1)
 
-       return x
+        # Global Filter Layer
+        self.global_filter = nn.Conv2d(192, 192, kernel_size=1)
+
+        # Fully connected layers
+        self.fc1 = None  # Placeholder for fc1, initialized in forward based on input size
+        self.fc2 = nn.Linear(256, num_classes)  # Output layer
+
+        # Pooling, dropout, and activation layers
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.5)
+        self.elu = nn.ELU()  # ELU activation function
+
+    def forward(self, x):
+        x = self.pool(self.elu(self.conv1(x)))
+        x = self.pool(self.elu(self.conv2(x)))
+        x = self.pool(self.elu(self.conv3(x)))
+        x = self.pool(self.elu(self.conv4(x)))
+        x = self.global_filter(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor for fully connected layers
+
+        if self.fc1 is None:
+            self.fc1 = nn.Linear(x.size(1), 256).to(x.device)
         
-        
+        x = self.elu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x

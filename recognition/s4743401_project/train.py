7c729352a009ec9f2ec974d8bdbf38f10447919d
Georgia <s4743401@student.uq.edu.au>
2024-10-24 23:01:18 +1000

    added code in training loop to keep track of accuracy

--- recognition/s4743401_project/train.py
+++ recognition/s4743401_project/train.py
@@ -15,8 +15,14 @@ def train_model(model, dataloader, optimizer, criterion, num_epochs):
             optimizer.step()
             
             running_loss += loss.item()
+            
+            # Calculate accuracy
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
         
         accuracy = correct / total * 100
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss:.4f}, Accuracy: {accuracy:.2f}%')
     
     return model
+

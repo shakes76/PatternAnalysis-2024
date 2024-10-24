0a47542ef4d70e3ae230f0ddb40669554ded9c80
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-24 14:25:05 +1000

    Update train.py

--- recognition/49094642 Graph Neural Network/train.py
+++ recognition/49094642 Graph Neural Network/train.py
@@ -22,3 +22,14 @@ class Tester:
             correct = (pred == data.y).sum().item()
             acc = correct / data.num_nodes 
             return acc
+
+def main():
+    edge_path = r"C:\Users\wuzhe\Desktop\musae_facebook_edges.csv"
+    features_path = r"C:\Users\wuzhe\Desktop\musae_facebook_features.json"
+    target_path = r"C:\Users\wuzhe\Desktop\musae_facebook_target.csv"
+
+    data_loader = DataLoader(edge_path, features_path, target_path)
+    data = data_loader.create_data()
+
+if __name__ == "__main__":
+    main()

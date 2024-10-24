32bf673bea580f83fe966faf69b2cd70ca07e5bb
Malaika <malaikavaz0909@gmail.com>
2024-10-24 22:37:50 +1000

    Created params.py to define global parameters for the 2D Unet model

---
+++ recognition/2D UNet (46992701)/params.py
@@ -0,0 +1,14 @@
+"""
+This file contains definitions for various parameters for the model such as file paths  
+and hyperparameter values 
+"""
+
+# Dataset paths
+TRAIN_IMG_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_train'
+TRAIN_MASK_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_train'
+
+TEST_IMG_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_test'
+TEST_MASK_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_test'
+
+VAL_IMG_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_validate'
+VAL_MASK_DIR = '/home/groups/comp3710/HipMRI_Study_open/keras_slices_data/keras_slices_seg_validate'

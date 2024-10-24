f2f50c2cd92630ad464e7b1e31b36ce25f29796d
Nicholas Lany <nicklany01@gmail.com>
2024-10-24 20:00:52 +1000

    Updated dataset.py to load resized data from kaggle (much smaller file sizes)

--- recognition/Siamese_Network_Nicholas_Lany/dataset.py
+++ recognition/Siamese_Network_Nicholas_Lany/dataset.py
@@ -1,46 +1,47 @@
 import os
-import torch
-import pydicom
-from torchvision import transforms
-from torch.utils.data import Dataset, DataLoader
+import kagglehub
+from torchvision import datasets, transforms
 from PIL import Image
 
-class ISICDICOMDataset(Dataset):
-    def __init__(self, directory, transform=None):
-        self.directory = directory
+class ISICDataset:
+    def __init__(self, dataset_path, transform=None):
+        self.dataset_path = dataset_path
         self.transform = transform
-        self.dicom_files = [f for f in os.listdir(directory) if f.endswith('.dcm')]
-    
+        self.data = self.load_data()
+
+    def load_data(self):
+        image_files = []
+        for root, _, files in os.walk(self.dataset_path):
+            for file in files:
+                if file.endswith('.jpg') or file.endswith('.jpeg'):
+                    image_files.append(os.path.join(root, file))
+        return image_files
+
     def __len__(self):
-        return len(self.dicom_files)
-    
-    def __getitem__(self, idx):
-        dicom_path = os.path.join(self.directory, self.dicom_files[idx])
-        dicom_data = pydicom.dcmread(dicom_path)
-        
-        image = Image.fromarray(dicom_data.pixel_array)
-        
+        return len(self.data)
+
+    def __getitem__(self, index):
+        img_path = self.data[index]
+        image = Image.open(img_path).convert("RGB")
+
         if self.transform:
             image = self.transform(image)
-        
-        label = 0
-        return image, label
 
-def create_dicom_dataloader(directory, batch_size=32, img_size=(224, 224)):
+        return image
+
+def download_and_load_dataset():
+    path = kagglehub.dataset_download("nischaydnk/isic-2020-jpg-256x256-resized")
+    print("Path to dataset files:", path)
+
     transform = transforms.Compose([
-        transforms.Resize(img_size),
+        transforms.Resize((256, 256)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5], std=[0.5])  # Assuming grayscale images; adjust if RGB
     ])
-    
-    dataset = ISICDICOMDataset(directory, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    
-    return dataloader
 
-if __name__ == '__main__':
-    data_dir = 'path_to_your_dicom_files'
-    dataloader = create_dicom_dataloader(data_dir, batch_size=32)
-    
-    for images, labels in dataloader:
-        print(images.shape, labels.shape)
+    dataset = ISICDataset(path, transform=transform)
+
+    return dataset
+
+if __name__ == "__main__":
+    dataset = download_and_load_dataset()
+    print(f"Loaded {len(dataset)} images.")

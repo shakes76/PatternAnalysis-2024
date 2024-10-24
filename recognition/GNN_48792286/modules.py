64413c56df44b115a289de116ad7a0f474a5c951
Sylvie1216 <xinyue.yuan2@student.uq.edu.au>
2024-10-25 05:39:35 +1000

    write initial model for the project

--- recognition/GNN_48792286/modules.py
+++ recognition/GNN_48792286/modules.py
@@ -1,1 +1,18 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+
+# Define Graph Neural Network model as a class
+class GNN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(GNN, self).__init__()
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, output_dim)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
 

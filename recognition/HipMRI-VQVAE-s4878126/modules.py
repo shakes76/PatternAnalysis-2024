efaa805a646ebda6efb7586cd84d3b8a2ebbee9d
aditya48781262 <a.sant@student.uq.edu.au>
2024-10-24 16:27:10 +1000

    Added one more reference to another repository

--- recognition/HipMRI-VQVAE-s4878126/modules.py
+++ recognition/HipMRI-VQVAE-s4878126/modules.py
@@ -4,9 +4,9 @@ import torch.nn.functional as F
 
 """
 REFERENCES:
-Kang, J. (2024, February 15). Pytorch-VAE-tutorial. Retrieved 31st August 2024 from 
+Kang, J. (2024, Feburary 15). Pytorch-VAE-tutorial. Retrieved 31st August 2024 from 
     https://github.com/Jackson-Kang/Pytorch-VAE-tutorial
-Malysheva, S. (2018). _Pytorch-VAE_. https://github.com/SashaMalysheva/Pytorch-VAE
+Rosinality. (2021, January 23). VQ-VAE-2-PYTORCH. Retrieved 2nd September 2024 from https://github.com/rosinality/vq-vae-2-pytorch
 Yadav, S. (2019, September 1). Understanding Vector Quantized Variational Autoencoders (VQ-VAE) [Blog]. Medium.
     https://shashank7-iitd.medium.com/understanding-vector-quantized-variational-autoencoders-vq-vae-323d710a888a 
 """
@@ -62,55 +62,56 @@ class Quantise(nn.Module):
         init_bound = 1 / nEmbeddings
         embedding = torch.Tensor(nEmbeddings, embed_dim)
         embedding.uniform_(-init_bound, init_bound)
-        # Buffers are parameters that should not be touched by the optimiser.
+        # Buffers are parameters that should not be touched by the optimiser, and these will be
+        # passed back to the start of the quantisation layer, not the start of the model (i.e. the encoder).
         self.register_buffer("embedding", embedding)
         self.register_buffer("ema_count", torch.zeros(nEmbeddings))
         self.register_buffer("ema_weight", self.embedding.clone())
 
     def encode(self, inp):
-        M, D = self.embedding.size()
-        inpFlat = inp.detach().reshape(-1, D)
+        nEmbed, Dim = self.embedding.size()
+        inpFlat = inp.detach().reshape(-1, Dim)
 
         # Calculate the distance between the input and all embeddings
-        distances = (-torch.cdist(inpFlat, self.embedding, p=2)) ** 2
+        embedDistances = (-torch.cdist(inpFlat, self.embedding, p=2)) ** 2
 
         # Fetch the closest embedding
-        indices = torch.argmin(distances.float(), dim=-1)
+        i = torch.argmin(embedDistances.float(), dim=-1)
 
         # Replace the feature map vectors with the closest embeddings.
-        quantised = F.embedding(indices, self.embedding)
-        quantised = quantised.view_as(inp)
-        return quantised, indices.view(inp.size(0), inp.size(1))
+        q = F.embedding(i, self.embedding)
+        q = q.view_as(inp)
+        return q, i.view(inp.size(0), inp.size(1))
 
     def retrieve(self, random_i):
-        quantised = F.embedding(random_i, self.embedding)
-        quantised = quantised.transpose(1, 3)
-        return quantised
+        q = F.embedding(random_i, self.embedding)
+        q = q.transpose(1, 3)
+        return q
 
     def forward(self, inp):
-        M, D = self.embedding.size()
-        inpFlat = inp.detach().reshape(-1, D)
+        nEmbed, Dim = self.embedding.size()
+        inpFlat = inp.detach().reshape(-1, Dim)
 
         # Calculate the distance between the input and all embeddings
-        distances = (-torch.cdist(inpFlat, self.embedding, p=2)) ** 2
+        embedDistances = (-torch.cdist(inpFlat, self.embedding, p=2)) ** 2
 
         # Fetch the closest embedding
-        indices = torch.argmin(distances.float(), dim=-1)
+        i = torch.argmin(embedDistances.float(), dim=-1)
 
         # One hot encoding is necessary as the model is being trained over a categorial distribution
-        encodings = F.one_hot(indices, M).float()
+        encodings = F.one_hot(i, nEmbed).float()
 
         # Replace the feature map vectors with the closest embeddings.
-        quantised = F.embedding(indices, self.embedding)
-        quantised = quantised.view_as(inp)
+        q = F.embedding(i, self.embedding)
+        q = q.view_as(inp)
 
         # For passing the gradients back to the start
         if self.training:
-            # Calculate the exponential moving average and apply a constant to avoid reaching a saddle point
+            # Calculate the exponential moving average and apply a constant to avoid reaching a saddle point (Kang, 2024)
             self.ema_count = self.decay * self.ema_count + (1 - self.decay) * torch.sum(encodings, dim=0)
             n = torch.sum(self.ema_count)
             # Normalise the EMA count
-            self.ema_count = (self.ema_count + self.eps) / (n + M * self.eps) * n
+            self.ema_count = (self.ema_count + self.eps) / (n + nEmbed * self.eps) * n
 
             # Update model weights
             dw = torch.matmul(encodings.t(), inpFlat)
@@ -118,22 +119,23 @@ class Quantise(nn.Module):
             self.embedding = self.ema_weight / self.ema_count.unsqueeze(-1)
 
         # Calculating the difference between the feature map and the feature map after embeddings
-        reconstrLoss = F.mse_loss(inp.detach(), quantised)
+        reconstrLoss = F.mse_loss(inp.detach(), q)
 
         #  Codebook loss refers to the amount of change in the interpolation distance between an embedded vector and the closest mean of cluster of embeddings, undertaken by the VQVAE during quantisation.
-        codebookLoss = F.mse_loss(inp, quantised.detach())
+        codebookLoss = F.mse_loss(inp, q.detach())
 
         # Similar to codebook loss, however instead of moving the embedded vectors closer to the nearest cluster, 
         # the clustered embeddings are adjusted according to the embedded vector space provided by the encoder. 
         # Otherwise, the dictionary may grow in size and fail to allocate any embeddings to the feature map. 
         commitmentLoss = self.commitmentCost * reconstrLoss
 
-        quantised = inp + (quantised - inp).detach()
+        q = inp + (q - inp).detach()
 
         avg = torch.mean(encodings, dim=0)
-        # perplexity is defined as the amount of times embeddings were accessed, not necessarily the level of entropy when referring to VQVAEs
+        # perplexity is defined as the amount of times embeddings were accessed, 
+        # not necessarily the level of entropy when referring to VQVAEs (Kang, 2024; Rosalinity, 2021)
         perplexity = torch.exp(-torch.sum(avg * torch.log(avg + 1e-10)))
-        return quantised, commitmentLoss, codebookLoss, perplexity
+        return q, commitmentLoss, codebookLoss, perplexity
 
 
 """Decodes the latent space into higher dimensionality,

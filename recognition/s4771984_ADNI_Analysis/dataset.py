330b72e127788bf5f82788ca2ca0dac83c21a5c5
sid <sgaikwad2001@gmail.com>
2024-10-25 01:47:53 +1000

    Implemented data loading with class balancing and train/val split Added the functionality of loading the ADNI dataset, adding custom transforms for data augmentation: random flipping, rotation, resizing, and normalization. Balanced the dataset through shuffling and splitting the indices of each class (NC and AD) into training and validation sets separately. Created DataLoaders for train, validation, And test datasets, making sure of efficient batch loading and shuffling during training.

--- recognition/s4771984_ADNI_Analysis/dataset.py
+++ recognition/s4771984_ADNI_Analysis/dataset.py
@@ -1,17 +1,10 @@
-import numpy as np
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, SubsetRandomSampler
-from google.colab import drive
+import numpy as np
 
-# Mounting the Google Drive
-drive.mount('/content/drive',force_remount=True)
-
-train_dir = '/content/drive/MyDrive/ADNI/AD_NC/train'
-test_dir = '/content/drive/MyDrive/ADNI/AD_NC/test'
-
-def get_data_loaders(train_dir, test_dir, batch_size = 64, val_split=0.2):
-    # Now let's apply Data Augmentation techniques for the training and testing datasets
+def get_data_loaders(train_dir, test_dir, batch_size=64):
+    # Define transforms
     transform_train = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(10),
@@ -20,35 +13,50 @@ def get_data_loaders(train_dir, test_dir, batch_size = 64, val_split=0.2):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
-    
+
     transform_test = transforms.Compose([
-        transforms.Resize((128,128)),
+        transforms.Resize((128, 128)),
         transforms.Grayscale(num_output_channels=1),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5], std=[0.5])
-        ])
-    
-    # Load the dataset from the google drive
+    ])
+
+    # Load train dataset
     train_dataset = datasets.ImageFolder(root=train_dir, transform=transform_train)
-    
-    #Finding out the data indices and shuffling them
-    dataset_size = len(train_dataset)
-    indices = list(range(dataset_size))
-    np.random.shuffle(indices)
-    
-    split = int(np.floor(val_split * dataset_size))
-    train_indices, val_indices = indices[split:], indices[:split]
+    train_dataset.class_to_idx = {'NC': 0, 'AD': 1}
+
+    # Get the indices for each class
+    targets = np.array([sample[1] for sample in train_dataset.samples])
+    nc_indices = np.where(targets == train_dataset.class_to_idx['NC'])[0]
+    ad_indices = np.where(targets == train_dataset.class_to_idx['AD'])[0]
+
+    # Shuffle and split indices for AD and NC class (70% train, 30% validation)
+    np.random.shuffle(nc_indices)
+    np.random.shuffle(ad_indices)
+
+    train_size_nc = int(0.7 * len(nc_indices))
+    train_size_ad = int(0.7 * len(ad_indices))
+
+    nc_train_indices = nc_indices[:train_size_nc]
+    nc_val_indices = nc_indices[train_size_nc:]
+
+    ad_train_indices = ad_indices[:train_size_ad]
+    ad_val_indices = ad_indices[train_size_ad:]
+
+    # Combine indices
+    train_indices = np.concatenate([nc_train_indices, ad_train_indices])
+    val_indices = np.concatenate([nc_val_indices, ad_val_indices])
 
     train_sampler = SubsetRandomSampler(train_indices)
     val_sampler = SubsetRandomSampler(val_indices)
 
-    # Create DataLoaders for training and validation datasets
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=4)
-    val_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=4)
+    # Create DataLoader for train and validation sets
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=12)
+    val_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=12)
 
+    # Load test data
     test_dataset = datasets.ImageFolder(root=test_dir, transform=transform_test)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    test_dataset.class_to_idx = {'NC': 0, 'AD': 1}
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=12)
 
     return train_loader, val_loader, test_loader
-    test_dataset = datasets.ImageFolder(root=test_dir, transform=transform_test)
-

71aaae1e446c780014f7895662ef4f5177444dae
Kevin-Gu-2022-UQ <kevin.gu@student.uq.edu.au>
2024-10-24 20:22:59 +1000

    Fix error where classes were not evenly distributed between train and validation sets

--- recognition/GFNet-47438886/dataset.py
+++ recognition/GFNet-47438886/dataset.py
@@ -12,12 +12,14 @@ from torchvision.transforms import v2
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
 from PIL import Image
+import pandas as pd
+from collections import Counter 
 
-# Helper function to extract patient ID from the filename
+
 def extract_patient_id(file_name):
     """
-    Function to extract the first part of the file name out, i.e., patientID
-    E.g. 23542_290.jpeg will return 23542
+    Helper function to extract the first part of the file name out, 
+    i.e., patientID. For example, 23542_290.jpeg will return 23542.
 
     Parameters:
         file_name: Name of file
@@ -26,6 +28,7 @@ def extract_patient_id(file_name):
         The patientID from the file name
     """
     return re.match(r"([^_]+)_", file_name).group(1)
+
 
 class ADNIDataset(Dataset):
     def __init__(self, image_paths, labels, transform=None):
@@ -59,7 +62,7 @@ def load_adni_data(root_dir, valid_size=0.1, batch_size=32, testing=False):
     """
     Load the ADNI dataset and ensure no leakage by splitting data subject-wise.
     
-    Args:
+    Parameters:
         root_dir (str): Root directory containing the 'train' folder with NC and AD subfolders.
         valid_size (float): Fraction of the training set to be used for validation.
         batch_size (int): Batch size for DataLoader.
@@ -84,7 +87,6 @@ def load_adni_data(root_dir, valid_size=0.1, batch_size=32, testing=False):
                 labels.append(label)
 
     if not testing:  # If not testing, need to split into training and validation set
-
         # Extract patient IDs to ensure no leakage
         patient_ids = [extract_patient_id(os.path.basename(path)) for path in image_paths]
         
@@ -94,19 +96,35 @@ def load_adni_data(root_dir, valid_size=0.1, batch_size=32, testing=False):
         # Perform a patient-wise split
         unique_patients = set(patient_ids)
         train_patients, val_patients = train_test_split(list(unique_patients), test_size=0.1, random_state=42)
-        
+
+        df = pd.DataFrame(data, columns=['image_path', 'label', 'patient_id'])
+
+        # Split patients by class (NC and AD) to maintain class balance
+        nc_patients = df[df['label'] == 0]['patient_id'].unique()  # Patients with NC
+        ad_patients = df[df['label'] == 1]['patient_id'].unique()  # Patients with AD
+
+        # Split patients with stratification to ensure even class distribution
+        train_nc_patients, val_nc_patients = train_test_split(nc_patients, test_size=0.1, random_state=42)
+        train_ad_patients, val_ad_patients = train_test_split(ad_patients, test_size=0.1, random_state=42)
+
+        # Combine train and validation patients
+        train_patients = list(train_nc_patients) + list(train_ad_patients)
+        val_patients = list(val_nc_patients) + list(val_ad_patients)
+
         # Split the dataset into training and validation based on patient IDs
-        train_data = [(img, lbl) for img, lbl, pid in data if pid in train_patients]
-        val_data = [(img, lbl) for img, lbl, pid in data if pid in val_patients]
+        train_data = df[df['patient_id'].isin(train_patients)]
+        val_data = df[df['patient_id'].isin(val_patients)]
 
         # Separate image paths and labels
-        train_image_paths, train_labels = zip(*train_data)
-        val_image_paths, val_labels = zip(*val_data)
+        train_image_paths = train_data['image_path'].tolist()
+        train_labels = train_data['label'].tolist()
+        
+        val_image_paths = val_data['image_path'].tolist()
+        val_labels = val_data['label'].tolist()
 
     mean = 0.1156
     std = 0.2199
     # Define image transformations
-    # ! Check the correct transforms are used
     if not testing:
         transform = v2.Compose([
             v2.CenterCrop((224, 224)),  # Resize to fit model input size (256 x 240)
@@ -136,10 +154,3 @@ def load_adni_data(root_dir, valid_size=0.1, batch_size=32, testing=False):
         test_dataset = ADNIDataset(image_paths, labels, transform=transform)
         test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
         return test_loader
-
-#if platform.system() == "Windows":
-#    root_dir = 'ADNI_AD_NC_2D/AD_NC'
-#else:
-#    root_dir = '~/ADNI/AD_NC'
-
-#train_loader, val_loader = load_adni_data(root_dir)

5770a1bed54f8c8328d4803b877ac5cb40fb6ea9
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-25 03:55:55 +1000

    Perform batch normalization on hidden layer features

--- recognition/49094642 Graph Neural Network/modules.py
+++ recognition/49094642 Graph Neural Network/modules.py
@@ -2,17 +2,19 @@ import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
-# GCN Model class
 class GCN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
+    def __init__(self, num_features, hidden_dim, num_classes):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, hidden_channels)
-        self.conv3 = GCNConv(hidden_channels, out_channels)
+        self.conv1 = GCNConv(num_features, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, hidden_dim)
+        self.conv3 = GCNConv(hidden_dim, num_classes)
+        self.bn1 = torch.nn.BatchNorm1d(hidden_dim)
+        self.bn2 = torch.nn.BatchNorm1d(hidden_dim)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.dropout(x, training=self.training)
-        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.bn1(self.conv1(x, edge_index)))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = F.relu(self.bn2(self.conv2(x, edge_index)))
+        x = F.dropout(x, p=0.5, training=self.training)
         return F.log_softmax(self.conv3(x, edge_index), dim=1)

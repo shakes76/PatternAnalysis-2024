bfad2b82cb3924c6061bd044e11409079f031627
Nicholas Lany <nicklany01@gmail.com>
2024-10-25 05:22:10 +1000

    Fixed bug where images are not loaded properly

--- recognition/Siamese_Network_Nicholas_Lany/predict.py
+++ recognition/Siamese_Network_Nicholas_Lany/predict.py
@@ -4,35 +4,35 @@ import pandas as pd
 import kagglehub
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from modules import SiameseNetwork, ContrastiveLoss
-from dataset import ISICDataset, SiameseDataset
+from modules import CNN
+from dataset import ISICDataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 prediction_threshold = 0.5
 
-def evaluate_model(dataset, model_path, transform=None):
-    model = SiameseNetwork().to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
-    model.eval()
+def load_model(model_path):
+    model = CNN(shape=(256, 256), num_classes=2).to(device)  # Load CNN model
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model.eval()  # Set model to evaluation mode
+    return model
 
-    dataloader = DataLoader(SiameseDataset(dataset, transform=transform, num_pairs=1000), batch_size=16, shuffle=False)
+def classify_images(model, dataset, transform=None):
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=False)
     
     correct_predictions = 0
-    total_pairs = 0
+    total_images = 0
 
     with torch.no_grad():
-        for img1, img2, labels in dataloader:
-            img1, img2, labels = img1.to(device), img2.to(device), labels.to(device)
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
 
-            similarity_scores, _ = model(img1, img2)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
 
-            predicted_labels = (similarity_scores > prediction_threshold).float() 
-            predicted_classes = predicted_labels.argmax(dim=1)
+            correct_predictions += (predicted == labels).sum().item()
+            total_images += labels.size(0)
 
-            correct_predictions += (predicted_classes == labels).sum().item()
-            total_pairs += labels.size(0)
-
-    accuracy = correct_predictions / total_pairs
+    accuracy = correct_predictions / total_images
     print(f'Accuracy of the model on the test dataset: {accuracy * 100:.2f}%')
 
 if __name__ == "__main__":
@@ -46,6 +46,7 @@ if __name__ == "__main__":
         transforms.ToTensor(),
     ])
 
-    isic_dataset = ISICDataset(dataset_path=dataset_path, metadata_path=meta_data_path)
+    isic_dataset = ISICDataset(dataset_path=dataset_image_path, metadata_path=meta_data_path, transform=transform)
 
-    evaluate_model(dataset=isic_dataset, model_path=model_save_path, transform=transform)
+    model = load_model(model_save_path)
+    classify_images(model, isic_dataset, transform=transform)

5f7fcfa3a784ba0442ac4b6b3f8d7e095eac9ffc
mraula <movilaandi@gmail.com>
2024-10-24 20:50:21 +1000

    testing read_image

--- recognition/YOLOs4703527/dataset.py
+++ recognition/YOLOs4703527/dataset.py
@@ -2,11 +2,17 @@ import cv2
 import os
 
 # Paths for input data and output labels
-HOME_PATH = r'C:\Users\raulm\OneDrive\Desktop\Yolo\ISIC-2017_Test_v2_Part1_GroundTruth'
-OUTPUT_PATH = r'C:\Users\raulm\OneDrive\Desktop\Yolo\test\labels'
+HOME_PATH = r'C:\Users\raulm\OneDrive\Desktop\Yolo\ISIC-2017_Training_Part1_GroundTruth'
+OUTPUT_PATH = r'C:\Users\raulm\OneDrive\Desktop\Yolo\train\labels'
 
 def ensure_directory(path):
     os.makedirs(path, exist_ok=True)
+
+def read_image(img_path):
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        raise FileNotFoundError(f"Image not found: {img_path}")
+    return img
 
 def process_masks(input_dir, output_dir):
     ensure_directory(output_dir)
@@ -15,7 +21,11 @@ def process_masks(input_dir, output_dir):
         if filename.endswith('_segmentation.png'):
             img_path = os.path.join(input_dir, filename)
             output_file = os.path.join(output_dir, filename.replace("_segmentation.png", ".txt"))
-            print("Found")
+            
+            try:
+                img = read_image(img_path)
+            except Exception as e:
+                print(f"Error processing {filename}: {str(e)}")
            
 if __name__ == "__main__":
     process_masks(HOME_PATH, OUTPUT_PATH)

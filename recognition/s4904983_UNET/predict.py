a078acd820d15902d6318231f6591813830c8882
oystkva <oystkva@stud.ntnu.no>
2024-10-25 14:13:56 +1000

    Integrated the new dataset.py classes into the prediction functions

--- recognition/s4904983_UNET/predict.py
+++ recognition/s4904983_UNET/predict.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.optim as optim
 from modules import UNet2D
 from train import dice_coefficient
-from dataset import load_img_seg_pair
+from dataset import MRIDataLoader, MRIDataset
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -66,24 +66,22 @@ def plot_prediction(input_img, prediction, target, dice_coefficient):
     plt.imshow(input_img, cmap='gray')
     plt.title('Input Image')
     plt.axis('off')
-    for i in range(5):
+    for i in range(6):
         plt.subplot(1, 8, i+2)
         plt.imshow(prediction[i], cmap='gray')
-        plt.title('Prediction. Dice: {:.4f}'.format(dice_coefficient))
+        if i == 3:
+            plt.title(f'Mask {i}')
         plt.axis('off')
-    plt.subplot(1, 8, 7)
-    plt.imshow(torch.add(torch.add(prediction[0], prediction[1]), torch.add(prediction[2], torch.add(prediction[3], prediction[4]))), cmap='gray')
-    plt.title('Prediction. Dice: {:.4f}'.format(dice_coefficient))
     plt.axis('off')
     plt.subplot(1, 8, 8)
     plt.imshow(target, cmap='gray')
     plt.title('Target')
     plt.axis('off')
     ### Show the plot
-    plt.show()
+    # plt.show()
     ### Save the plot
     # plt.savefig('C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/figures/prediction.png') # Local path
-    # plt.savefig('/home/Student/s4904983/COMP3710/project/figures/prediction.png') # Rangpur path
+    plt.savefig('/home/Student/s4904983/COMP3710/project/figures/prediction.png') # Rangpur path
 
 
 def test_model():
@@ -99,43 +97,41 @@ def test_model():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
     # Directory where model is saved
-    model_path = 'C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/models/unet_model_ep3_1.pth' # Local path
-    # model_dir = '/home/Student/s4904983/COMP3710/project/models/unet_model_ep3.pth' # Rangpur path
+    model_path = 'C:/Users/oykva/OneDrive - NTNU/Semester 7/PatRec/Project/models/unet_model_ep10.pth' # Local path
+    # model_dir = '/home/Student/s4904983/COMP3710/project/models/unet_model_ep10.pth' # Rangpur path
     print("Loading model")
     model = load_model(model_path, device)
 
     # Load test data
     print("Loading data")
-    test_images, test_segs = load_img_seg_pair('test')
+    TestDataLoader = MRIDataLoader("test", batch_size=1)
     print("Model and test data loaded")
-
-    print("Dice test:", dice_coefficient(test_images[0], test_segs[0]))
-    print(test_images[0].shape)
 
     # Predict and calculate dice coefficient
     dice_coefficients = []
     predictions = []
 
-    for i in tqdm(range(len(test_images))):
-        image, seg = test_images[i], test_segs[i]
-        image = image[None, None, :, :]
+    for i, (image, label) in enumerate(tqdm(TestDataLoader)):
+        # image = image[None, None, :, :]
         pred = predict_image(model, image, device)
         predictions.append(pred)
-        dice_coefficients.append(dice_coefficient(pred, seg))
+        dice_coefficients.append(dice_coefficient(pred, label))
 
 
     # Print average dice coefficient
     avg_dice = sum(dice_coefficients) / len(dice_coefficients)
     print("Average Dice Coefficient: {:.4f}".format(avg_dice))
 
+    min_dice = min(dice_coefficients)
+    print("Minimum Dice Coefficient: {:.4f}".format(min_dice))
+
     # Plot five random predictions
-    for i in range(5):
-        idx = torch.randint(0, len(test_images), (1,)).item()
-        input_image = test_images[idx]
+    for _ in range(5):
+        idx = torch.randint(0, len(TestDataLoader.dataset), (1,)).item()
+        input_image, target = TestDataLoader.dataset[idx]
         prediction = predictions[idx]
-        target = test_segs[idx]
         dice = dice_coefficients[idx]
-        plot_prediction(input_image, prediction[0], target, dice)    
+        plot_prediction(input_image.squeeze(0), prediction[0], target.squeeze(0), dice)
 
     return
 

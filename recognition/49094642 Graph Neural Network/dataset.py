bd238cd7127a73f77ac6b10ee8afea70d35dde15
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-25 19:44:39 +1000

    Calculation of class_weights

--- recognition/49094642 Graph Neural Network/dataset.py
+++ recognition/49094642 Graph Neural Network/dataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from torch_geometric.utils import to_undirected
 from torch_geometric.data import Data
+from sklearn.utils.class_weight import compute_class_weight
 
 def load_data():
     edges = pd.read_csv(r"C:\Users\wuzhe\Desktop\musae_facebook_edges.csv")
@@ -21,8 +22,13 @@ def load_data():
     labels['page_type'], _ = pd.factorize(labels['page_type'])
     y = torch.tensor(labels['page_type'].values, dtype=torch.long)
 
+    class_weights = compute_class_weight('balanced', classes=np.unique(labels['page_type']), y=labels['page_type'])
+    class_weights = torch.tensor(class_weights, dtype=torch.float).to('cuda' if torch.cuda.is_available() else 'cpu')
+
     data = Data(x=node_features, edge_index=edge_index, y=y)
     data.train_mask = torch.rand(data.num_nodes) < 0.8
     data.test_mask = ~data.train_mask
-    return data, len(labels['page_type'].unique())
+
+    return data, class_weights, len(labels['page_type'].unique())
+
 

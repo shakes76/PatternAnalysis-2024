0d5abf50350e3c80968be5151959b2696b718122
Arjun Srikanth <arjunsrikanth15@gmail.com>
2024-10-25 13:52:35 +1000

    Moved all constants and hyperparameters to this file

---
+++ recognition/StyleGAN_arjun/config.py
@@ -0,0 +1,22 @@
+"""
+Contain constants and hyperparameters for codebase.
+"""
+
+RANGPUR_PATH = "/home/groups/comp3710/" # Dataset path on Rangpur Cluster
+PATH = '/Volumes/Acasis WD_Black/Documents/Deep Learning/Datasets/' # Dataset path on personal machine
+
+# -- Dataset Attributes --
+ADNI_TRAIN_PATH = "ADNI/AD_NC/train"
+ADNI_TEST_PATH = "ADNI/AD_NC/test"
+CIFAR_PATH = "cifar10/"
+
+ADNI_IMG_SIZE = 64
+CIFAR_IMG_SIZE = 32
+
+# -- Hyper-parameters --
+num_epochs = 50
+lr = 1e-4 # Use 1e-3 for Adam
+batch_size = 64
+channels = 3
+z_dim = 512
+w_dim = 512

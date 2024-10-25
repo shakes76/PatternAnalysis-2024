789ee4bc5e01ff8fe3ea2058387dd0823512b1a0
Nicholas-Roughley <nroughley03@gmail.com>
2024-10-25 11:39:15 +1000

    Reshaped the plot to handle 3D

--- recognition/s4745026_3DUnet/predict.py
+++ recognition/s4745026_3DUnet/predict.py
@@ -14,25 +14,26 @@ def test_and_visualize(model, test_loader):
                 device), test_masks.to(device)
             test_outputs = model(test_images)
             predicted_masks = torch.argmax(test_outputs, dim=1)
-
+            middle_slice_idx = test_images.shape[2] // 2
             # Visualize the first example in the batch
             plt.figure(figsize=(12, 6))
 
             # Input image
             plt.subplot(1, 3, 1)
-            plt.imshow(test_images[0].cpu().squeeze(), cmap='gray')
+            plt.imshow(test_images[0, 0, middle_slice_idx].cpu(), cmap='gray')
             plt.title("Input Image")
 
             # Ground truth mask
             plt.subplot(1, 3, 2)
-            plt.imshow(test_masks[0].cpu().squeeze(), cmap='gray')
+            plt.imshow(test_masks[0, 0, middle_slice_idx].cpu(), cmap='gray')
             plt.title("Ground Truth Mask")
 
             # Predicted mask
             plt.subplot(1, 3, 3)
-            plt.imshow(predicted_masks[0].cpu().squeeze(), cmap='gray')
+            plt.imshow(predicted_masks[0, middle_slice_idx].cpu(), cmap='gray')
             plt.title("Predicted Mask")
 
             plt.tight_layout()
+            plt.savefig("output.png")
             plt.show()
             break

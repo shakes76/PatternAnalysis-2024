47841005a193ad94ca7ca8c989d1b4090988becc
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-25 17:04:34 +1000

    Optimize code structure

--- recognition/49094642 Graph Neural Network/dataset.py
+++ recognition/49094642 Graph Neural Network/dataset.py
@@ -2,31 +2,29 @@ import torch
 import pandas as pd
 import json
 from torch_geometric.utils import to_undirected
+from torch_geometric.data import Data
 
-# Data Loader Class
-class FacebookDatasetLoader:
-    def __init__(self, edge_path, target_path, feature_path):
-        self.edge_path = edge_path
-        self.target_path = target_path
-        self.feature_path = feature_path
+def load_data():
+    edges = pd.read_csv(r"C:\Users\wuzhe\Desktop\musae_facebook_edges.csv")
+    labels = pd.read_csv(r"C:\Users\wuzhe\Desktop\musae_facebook_target.csv")
+    
+    with open(r"C:\Users\wuzhe\Desktop\musae_facebook_features.json") as f:
+        features = json.load(f)
+    
+    node_features = torch.zeros((len(features), 128))
+    for node, feats in features.items():
+        feats = feats[:128]
+        if len(feats) < 128:
+            feats += [0] * (128 - len(feats))
+        node_features[int(node)] = torch.tensor(feats, dtype=torch.float)
 
-    def load_data(self):
-        edges = pd.read_csv(self.edge_path)
-        labels = pd.read_csv(self.target_path)
-        with open(self.feature_path) as f:
-            features = json.load(f)
+    edge_index = to_undirected(torch.tensor(edges.values.T, dtype=torch.long))
+    labels['page_type'], _ = pd.factorize(labels['page_type'])
+    y = torch.tensor(labels['page_type'].values, dtype=torch.long)
 
-        # Process node features, truncate or pad to 128 dimensions
-        node_features = torch.zeros((len(features), 128))
-        for node, feats in features.items():
-            feats = feats[:128]
-            if len(feats) < 128:
-                feats += [0] * (128 - len(feats))
-            node_features[int(node)] = torch.tensor(feats, dtype=torch.float)
+    data = Data(x=node_features, edge_index=edge_index, y=y)
+    data.train_mask = torch.rand(data.num_nodes) < 0.8
+    data.test_mask = ~data.train_mask
 
-        # Convert edge list to tensor and labels
-        edge_index = to_undirected(torch.tensor(edges.values.T, dtype=torch.long))
-        labels['page_type'], _ = pd.factorize(labels['page_type'])
-        y = torch.tensor(labels['page_type'].values, dtype=torch.long)
+    return data
 
-        return node_features, edge_index, y, labels

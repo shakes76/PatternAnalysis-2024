2d7fe0cbd4064c88d85879f06ae53a1def1fd414
Shannon <smsearle41@gmail.com>
2024-10-25 13:10:49 +1000

    changed batch size and epochs ready for testing on slurm

--- unet3d_Shannon_Searle/test_driver.py
+++ unet3d_Shannon_Searle/test_driver.py
@@ -47,7 +47,7 @@ def main():
     train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
     
     # DataLoader for batching
-    batch_size = 32
+    batch_size = 128
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)

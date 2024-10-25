bdfa53f0470c6664c72b98e530e16fadb5ad4544
carl-c2 <carl20031114@gmail.com>
2024-10-26 08:49:16 +1000

    add combined VQVAE model to modules.py

--- recognition/vqvae_Carl_Chang/modules.py
+++ recognition/vqvae_Carl_Chang/modules.py
@@ -151,3 +151,29 @@ class Decoder(nn.Module):
         return self._conv_trans_2(x)
     
 
+class VQVAE(nn.Module):
+    def __init__(self, num_hiddens, num_residual_layers, num_residual_hiddens, 
+                 num_embeddings, embedding_dim, commitment_cost):
+        super(VQVAE, self).__init__()
+        
+        self._encoder = Encoder(1, num_hiddens,
+                                num_residual_layers, 
+                                num_residual_hiddens)
+        self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens, 
+                                      out_channels=embedding_dim,
+                                      kernel_size=1, 
+                                      stride=1)
+        self._vq_vae = VectorQuantizer(num_embeddings, embedding_dim,
+                                           commitment_cost)
+        self._decoder = Decoder(embedding_dim,
+                                num_hiddens, 
+                                num_residual_layers, 
+                                num_residual_hiddens)
+
+    def forward(self, x):
+        z = self._encoder(x)
+        z = self._pre_vq_conv(z)
+        loss, quantized, perplexity, _ = self._vq_vae(z)
+        x_recon = self._decoder(quantized)
+
+        return loss, x_recon, perplexity

edb2bb7b853ae9d68302067fe7a2796467fbe596
wuzhe1234567 <s4909464@student.uq.edu.au>
2024-10-26 15:14:33 +1000

    Minor formatting adjustments

--- recognition/49094642 Graph Neural Network/utils.py
+++ recognition/49094642 Graph Neural Network/utils.py
@@ -1,12 +1,12 @@
-import torch
-import umap.umap_ as umap
 import matplotlib.pyplot as plt
+import umap.umap_ as umap
+import torch
+import torch.nn.functional as F
 
-# UMAP plotting function for visualizing embeddings
+# Function to plot UMAP
 def plot_umap(data, model):
     model.eval()
     with torch.no_grad():
-        # Obtain embeddings from the model's final layer
         x, edge_index = data.x, data.edge_index
         x = F.relu(model.bn1(model.conv1(x, edge_index)))
         x = F.dropout(x, p=0.5, training=model.training)
@@ -16,21 +16,18 @@ def plot_umap(data, model):
         x = F.dropout(x, p=0.5, training=model.training)
         embeddings = model.conv4(x, edge_index).cpu().numpy()
 
-    # Reduce the dimensionality using UMAP
     reducer = umap.UMAP(n_neighbors=15, min_dist=0.1)
     embedding_2d = reducer.fit_transform(embeddings)
 
-    # Plot the embeddings with colors representing the labels
     plt.scatter(embedding_2d[:, 0], embedding_2d[:, 1], c=data.y.cpu().numpy(), cmap="Spectral", s=5)
     plt.colorbar()
     plt.title("UMAP Projection")
     plt.show()
 
-# Function to plot the loss and accuracy curves
+# Function to plot loss and accuracy curves
 def plot_loss_accuracy(train_loss_values, test_loss_values, train_acc_values, test_acc_values):
     plt.figure(figsize=(12, 6))
 
-    # Plot loss curves
     plt.subplot(1, 2, 1)
     plt.plot(train_loss_values, label='Train Loss', color='red')
     plt.plot(test_loss_values, label='Test Loss', color='blue')
@@ -39,7 +36,6 @@ def plot_loss_accuracy(train_loss_values, test_loss_values, train_acc_values, te
     plt.title('Loss Curve')
     plt.legend()
 
-    # Plot accuracy curves
     plt.subplot(1, 2, 2)
     plt.plot(train_acc_values, label='Train Accuracy', color='red')
     plt.plot(test_acc_values, label='Test Accuracy', color='blue')

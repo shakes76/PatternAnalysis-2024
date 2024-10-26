df88d7019fe9ee727bcfcbf776a10a7465587200
Sylvie1216 <xinyue.yuan2@student.uq.edu.au>
2024-10-27 02:59:53 +1000

    define the plot of loss and accuracy each epoch

--- recognition/GNN_48792286/predict.py
+++ recognition/GNN_48792286/predict.py
@@ -1,1 +1,15 @@
+def plot_loss_accuracy(losses, accuracies):
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Loss', color='tab:blue')
+    ax1.plot(losses, color='tab:blue', label='Loss')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Accuracy', color='tab:red')
+    ax2.plot(accuracies, color='tab:red', label='Accuracy')
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+
+    plt.title('Loss and Accuracy over Epochs')
+    plt.show()
 

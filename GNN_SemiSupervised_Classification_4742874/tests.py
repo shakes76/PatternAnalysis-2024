eb91268182309db6af95334648ae030d6f8c4d66
Liam Mulhern <liam.mulhern@outlook.com>
2024-10-26 11:44:38 +1000

    Adds test and util file descriptions, updates gitignore for model files

---
+++ GNN_SemiSupervised_Classification_4742874/tests.py
@@ -0,0 +1,8 @@
+"""
+File: tests.py
+Description: Unit tests for the models and data set implementation.
+Course: COMP3710 Pattern Recognition
+Author: Liam Mulhern (S4742847)
+Date: 26/10/2024
+"""
+
---
+++ GNN_SemiSupervised_Classification_4742874/utils.py
@@ -0,0 +1,8 @@
+"""
+File: utils.py
+Description: Utility functions used for model implementation.
+Course: COMP3710 Pattern Recognition
+Author: Liam Mulhern (S4742847)
+Date: 26/10/2024
+"""
+

3b34f274c6cab9c39008c6caa853af88e66fc681
Liam Mulhern <liam.mulhern@outlook.com>
2024-10-26 14:59:04 +1000

    Adds FLPP dataset loader and test cases to validate features, classes, and nodes

--- GNN_SemiSupervised_Classification_4742874/dataset.py
+++ GNN_SemiSupervised_Classification_4742874/dataset.py
@@ -1,8 +1,50 @@
 """
 File: dataset.py 
 Description: Contains the data loader for loading and preprocessing 
-    the Facebook Large Page-Page Network dataset.
+    the Facebook Large Page-Page (FLPP) Network dataset.
 Course: COMP3710 Pattern Recognition
 Author: Liam Mulhern (S4742847)
 Date: 26/10/2024
 """
+
+import torch
+import numpy as np
+
+from typing import Any
+from torch.utils.data import Dataset
+
+FLPP_CATEGORIES = ['politicians', 'governmental organizations', 'television shows', 'companies']
+
+class FLPPDataset(Dataset):
+    def __init__(self, image_dir: str) -> None:
+        """
+            Initialise the FLPP dataset from file 
+            and apply normalisation and transformation.
+
+            Parameters:
+                image_dir: The directory to load the FLPP .npz dataset
+        """
+        # Open dataset
+        with np.load(image_dir) as data:
+            self.edges = torch.tensor(data['edges'])
+            self.features = torch.tensor(data['features'])
+            self.labels = torch.tensor(data['target'])
+
+    def __len__(self) -> int:
+        """
+            Get thh lenght of the load FLPP dataset
+        """
+        return len(self.labels)
+
+    def __getitem__(self, index: int) -> tuple[Any, int]:
+        """
+            Get an element at the given index from the FLPP 
+            dataset.
+
+            Parameters:
+                index: The index of the dataset to get the element
+
+            Returns:
+                A feature and label at the given index.
+        """
+        return self.features[index], self.labels[index]
--- GNN_SemiSupervised_Classification_4742874/tests.py
+++ GNN_SemiSupervised_Classification_4742874/tests.py
@@ -6,3 +6,30 @@ Author: Liam Mulhern (S4742847)
 Date: 26/10/2024
 """
 
+import unittest
+
+from torch_geometric.datasets import FacebookPagePage
+
+DATASET_DIR = "./dataset/"
+FLPP_DATASET = "raw/facebook.npz"
+
+class TestDataSet(unittest.TestCase):
+    def test_load_dataset(self):
+        """
+            Tests loading .npz file from FLPPDataset
+        """
+        dataset = FacebookPagePage(DATASET_DIR)
+
+        print('Dataset properties')
+        print('==============================================================')
+        print(f'Dataset: {dataset}') #This prints the name of the dataset
+        print(f'Number of graphs in the dataset: {len(dataset)}')
+        print(f'Number of features: {dataset.num_features}') #Number of features each node in the dataset has
+        print(f'Number of classes: {dataset.num_classes}') #Number of classes that a node can be classified into
+        print(f'Number of nodes: {dataset.x.shape[0]}')
+
+        assert len(dataset) == 1
+        assert dataset.num_features == 128
+        assert dataset.num_classes == 4
+        assert dataset.x.shape[0] == 22470
+

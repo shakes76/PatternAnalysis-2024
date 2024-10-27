559c989ffcdd363dd24831de6570b46f07ea505b
Liam Mulhern <liam.mulhern@outlook.com>
2024-10-27 18:22:45 +1000

    Adds GNN and sparse layer modules

--- GNN_SemiSupervised_Classification_4742874/modules.py
+++ GNN_SemiSupervised_Classification_4742874/modules.py
@@ -7,10 +7,9 @@ Date: 26/10/2024
 """
 
 import torch
+import torch.nn as nn
 
 from torch.functional import Tensor
-from torch.nn import Linear
-import torch.nn.functional as F
 
 from torch_geometric.utils import to_dense_adj
 
@@ -24,7 +23,6 @@ def create_adjacency_matrix(edges) -> Tensor:
         Returns:
             Adjacency matrix with self referential nodes.
     """
-    # Create adjancency matrix using torch geometric function
     adjacency_matrix = to_dense_adj(edges)
 
     # Add identity matrix to adjacency matrix to make 
@@ -33,3 +31,73 @@ def create_adjacency_matrix(edges) -> Tensor:
 
     return adjacency_matrix
 
+class SparseLayer(nn.Module):
+    """
+        Sparse Layer Module (Used to construct GNN layers)
+    """
+    def __init__(self, dim_in: int, dim_out: int) -> None:
+        """
+            Ini
+            Parameters:
+                dim_in: the size of the vector input into the GNN.
+                dim_out: the output catergorisation vector size.
+
+        """
+        super(SparseLayer, self).__init__()
+
+        self.linear = nn.Linear(dim_in, dim_out, bias=False)
+
+    def forward(self, x: Tensor, adjacency_matrix: Tensor) -> Tensor:
+        """
+            Forwared training pass of the GNN Sparse Layer
+
+            Parameters:
+                x: The training tensor.
+                adjacency_matrix: The graph adjacency matrix.
+
+            Returns:
+                The tensor after the sparse layer has been applied to the input.
+        """
+        x = self.linear(x)
+
+        # Sparse matrix multiply the linearised 
+        # input with the adjacency matrix
+        x = torch.sparse.mm(adjacency_matrix, x)
+
+        return x
+
+class GNN(nn.Module):
+    """
+        Graphical Neural Network Model
+    """
+    def __init__(self, dim_in: int, dim_hidden: int, dim_out: int) -> None:
+        """
+            Initialise the GNN and create sparse layers for training.
+
+            Parameters:
+                dim_in: The size of the vector input into the gnn.
+                dim_hidden: The size of the hidden layer vector to transform the gnn to.
+                dim_out: The output catergorisation vector size.
+        """
+        super(GNN, self).__init__()
+
+        self.gnn_1 = SparseLayer(dim_in, dim_hidden)
+        self.gnn_2 = SparseLayer(dim_hidden, dim_out)
+
+    def forward(self, x: Tensor, adjacency_matrix: Tensor) -> Tensor:
+        """
+            Forward training pass of the GNN model.
+
+            Parameters:
+                x: The training tensor.
+                adjacency_matrix: The graph adjacency matrix.
+
+            Returns:
+                The tensor after the sparse layers have been applied to the input.
+        """
+
+        h = self.gnn_1(x, adjacency_matrix)
+        h = torch.relu(h)
+        h = self.gnn_2(h, adjacency_matrix)
+
+        return nn.functional.log_softmax(h, dim=1)

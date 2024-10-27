f08bc903e38a004850781865b1fb19f0f684e808
Alexander <s4742351@student.uq.edu.au>
2024-10-27 20:23:10 +1000

    added basic functional UNET module based on the one shakes showed in class and prac 2

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -3,6 +3,7 @@
 import numpy as np
 import nibabel as nib
 from tqdm import tqdm
+
 def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:
     channels = np.unique(arr)
     res = np.zeros(arr.shape + (len(channels),), dtype=dtype)
--- recognition/modules.py
+++ recognition/modules.py
@@ -1,1 +1,48 @@
-# This is for the modules
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def unet(tensor_in, depth=64):
+    
+    # For each layer of the UNET filter convolute twice and then pool.
+    encode1 = layers.Conv2D(depth, 3, activation='relu', padding='same')(tensor_in)
+    encode1 = layers.Conv2D(depth, 3, activation='relu', padding='same')(encode1)
+    pool1 = layers.MaxPooling2D(pool_size=(2,2), padding='same')(encode1)
+
+    encode2 = layers.Conv2D(depth*2, 3, activation='relu', padding='same')(pool1)
+    encode2 = layers.Conv2D(depth*2, 3, activation='relu', padding='same')(encode2)
+    pool2 = layers.MaxPooling2D(pool_size=(2,2), padding='same')(encode2)
+
+    encode3 = layers.Conv2D(depth*4, 3, activation='relu', padding='same')(pool2)
+    encode3 = layers.Conv2D(depth*4, 3, activation='relu', padding='same')(encode3)
+    pool3 = layers.MaxPooling2D(pool_size=(2,2), padding='same')(encode3)
+
+    encode4 = layers.Conv2D(depth*8, 3, activation='relu', padding='same')(pool3)
+    encode4 = layers.Conv2D(depth*8, 3, activation='relu', padding='same')(encode4)
+    pool4 = layers.MaxPooling2D(pool_size=(2,2), padding='same')(encode4)
+
+    encode5 = layers.Conv2D(depth*16, 3, activation='relu', padding='same')(pool4)
+    encode5 = layers.Conv2D(depth*16, 3, activation='relu', padding='same')(encode5)
+
+    up1 = layers.Conv2DTranspose(depth*8, 2, strides=2, padding='same')(encode5)
+    merge1 = layers.concatenate([encode4, up1])
+    decode1 = layers.Conv2D(depth*8, 3, activation='relu', padding='same')(merge1)
+    decode1 = layers.Conv2D(depth*8, 3, activation='relu', padding='same')(decode1)
+    
+    up2 = layers.Conv2DTranspose(depth*4, 2, strides=2, padding='same')(decode1)
+    merge2 = layers.concatenate([encode3, up2])
+    decode2 = layers.Conv2D(depth*4, 3, activation='relu', padding='same')(merge2)
+    decode2 = layers.Conv2D(depth*4, 3, activation='relu', padding='same')(decode2)
+
+    up3 = layers.Conv2DTranspose(depth*2, 2, strides=2, padding='same')(decode2)
+    merge3 = layers.concatenate([encode2, up3])
+    decode3 = layers.Conv2D(depth*2, 3, activation='relu', padding='same')(merge3)
+    decode3 = layers.Conv2D(depth*2, 3, activation='relu', padding='same')(decode3)
+
+    up4 = layers.Conv2DTranspose(depth, 2, strides=2, padding='same')(decode3)
+    merge4 = layers.concatenate([encode1, up4])
+    decode4 = layers.Conv2D(depth, 3, activation='relu', padding='same')(merge4)
+    decode4 = layers.Conv2D(depth, 3, activation='relu', padding='same')(decode4)
+    tensor_out = layers.Conv2D(1, 1, activation='sigmoid', padding='same')(decode4)
+    model = models.Model(inputs=tensor_in, outputs=tensor_out)
+    

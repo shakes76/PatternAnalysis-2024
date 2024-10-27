5d5585efb906cb7ff851886861a61c099be6e4a8
InverseMist <jccockle@outlook.com>
2024-10-27 13:36:25 +1000

    Changed file path

--- recognition/yolo-48097824/dataset.py
+++ recognition/yolo-48097824/dataset.py
@@ -36,8 +36,8 @@ def process_masks(data_path, output_location):
 
 def main():
     
-    data_path = "/mnt/c/Users/jccoc/Downloads/ISIC_DATASET/validation/masks"
-    output_location = "/mnt/c/Users/jccoc/Downloads/ISIC_DATASET/validation/labels"
+    data_path = "/mnt/c/Users/jccoc/Downloads/ISIC_DATASET/train/masks"
+    output_location = "/mnt/c/Users/jccoc/Downloads/ISIC_DATASET/train/labels"
     process_masks(data_path, output_location)
 
 if __name__ == "__main__":

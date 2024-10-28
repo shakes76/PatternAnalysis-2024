aa4cd02ac89cc34f3863d6e9fd516357e77d189a
Solomon Tjipto <solomontjipto@gmail.com>
2024-10-29 00:31:49 +1000

    Added Functions

--- recognition/StyleGAN_s4653241/StyleGAN/utils.py
+++ recognition/StyleGAN_s4653241/StyleGAN/utils.py
@@ -2,10 +2,13 @@
 '''Utils for the project'''
 
 import torch
+import warnings
 import config
-from config import log_resolution
+from config import *
 
-def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss,path = config.path + 'checkpoint.pth'):
+
+
+def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss):
     checkpoint = {
         'epoch': epoch,
         'G_state_dict': G.state_dict(),
@@ -18,6 +21,7 @@ def save_model(epoch,G,D,optimizer_G,optimizer_D, gen_loss, disc_loss,path = con
         'disc_loss': disc_loss,
 
     }
+    path = config.save_path + 'checkpoint.pth'
     torch.save(checkpoint, path)
     print(f"Checkpoint saved at epoch {epoch}.", flush =True)
 
@@ -44,12 +48,62 @@ def devicer():
     else:
         device = torch.device("cpu")
 
-    print(f'Using device: {device}', flush=True)
+    # print(f'Using device: {device}', flush=True)
     return device
 
-def get_w(batch_size, mapping_network, device):
+def get_w(batch_size, mapping_network,device=devicer()):
 
-    z = torch.randn(batch_size, config.w_dim).to(device)
+    z = torch.randn(batch_size, W_DIM).to(device)
     w = mapping_network(z)
     # Expand w from the generator blocks
-    return w[None,:,:].expand(log_resolution, -1, -1)
+    return w[None,:,:].expand(config.log_resolution, -1, -1)
+
+def get_noise(batch_size,device=devicer()):
+
+    noise = []
+    #noise res starts from 4x4
+    resolution = 4
+
+    # For each gen block
+    for i in range(config.log_resolution):
+        # First block uses 3x3 conv
+        if i == 0:
+            n1 = None
+        # For rest of conv layer
+        else:
+            n1 = torch.randn(batch_size, 1, resolution, resolution, device= device)
+        n2 = torch.randn(batch_size, 1, resolution, resolution,device= device)
+
+        # add the noise tensors to the lsit
+        noise.append((n1, n2))
+        # subsequent block has 2x2 res
+        resolution *= 2
+
+    return noise
+
+def gradient_penalty(critic, real, fake,device=devicer()):
+    BATCH_SIZE, C, H, W = real.shape
+    beta = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * beta + fake.detach() * (1 - beta)
+    interpolated_images.requires_grad_(True)
+
+    # Calculate critic scores
+    mixed_scores = critic(interpolated_images)
+
+    # Calculates the gradient of scores with respect to the images
+    # and we need to create and retain graph since we have to compute gradients
+    # with respect to weight on this loss.
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    # Reshape gradients to calculate the norm
+    gradient = gradient.view(gradient.shape[0], -1)
+    # Calculate the norm and then the loss
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+
+    return gradient_penalty

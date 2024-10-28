96aa5ceb06d26a501f32d33a71cdab919b437a56
Alexander <s4742351@student.uq.edu.au>
2024-10-28 15:22:16 +1000

    optimized dice calculation

--- recognition/train.py
+++ recognition/train.py
@@ -10,15 +10,12 @@ from dataset import load_data_2D, to_channels
 
 # https://medium.com/@vipul.sarode007/u-net-unleashed-a-step-by-step-guide-on-implementing-and-training-your-own-segmentation-model-in-a90ed89399c6
 # Function to calculate the mathmatical dice coefficient
-def dice_coeff(y_true, y_predicted):
+def dice(y_true, y_predicted):
     """
     Function to calculate the max label dice coefficient
 
     """
-    intersection = K.sum(y_true*y_predicted, axis = -1)
-    union = K.sum(y_true, axis = -1) + K.sum(y_predicted, axis = -1)
-    dice_coeff = (2*intersection) / (union)
-    return dice_coeff
+    return (2 * K.sum(y_true*y_predicted, axis = -1)) /  (K.sum(y_true, axis = -1) + K.sum(y_predicted, axis = -1))
 
 
 #Function to train the Unet model, using tensorflow's inbuilt keras.model.fit() function
@@ -31,7 +28,7 @@ def train_unet(data_train, data_train_seg, data_validate, data_validate_seg, epo
     early_stopping = EarlyStopping(monitor = 'val_loss', patience = 3, restore_best_weights = True)
     # Compile and train the UNET model on the training set and validation set
     # Using the loss function for ones hot encoding
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', dice_coeff])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', dice])
     model.fit(data_train, data_train_seg, epochs=epochs, batch_size=batch_size, validation_data=(data_validate, data_validate_seg), callbacks=[early_stopping], verbose=1)
     # save model reference: https://www.tensorflow.org/tutorials/keras/save_and_load
     model.save('mri_unet.keras')

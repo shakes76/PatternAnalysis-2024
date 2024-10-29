30833706fcc00743d84f1799895f8362c6918418
LonelyNo <sbondmurphy@gmail.com>
2024-10-29 21:47:41 +1000

    Fixed train.py not using cuda correctly and limited it to 60% of VRAM due to OOM errors. Also updated dataset.py to handle validation ground truths.

--- recognition/yolo-47450253/dataset.py
+++ recognition/yolo-47450253/dataset.py
@@ -2,7 +2,10 @@ import os
 import cv2 as cv
 
 
-ISC2018_TRUTH_PATH = ".\data-ISC2018\ISIC2018_Task1_Training_GroundTruth_x2"
+ISC2018_TRUTH_TRAIN = "./data-ISC2018/ISIC2018_Task1_Training_GroundTruth_x2"
+ISC2018_TRUTH_VALIDATE = "./data-ISC2018/ISIC2018_Task1_Validation_GroundTruth"
+OUTPUT_TRAIN = "./data/labels/train"
+OUTPUT_VALIDATE = "./data/labels/validate"
 CLASS_NO = "0" #Only testing for 1 class: skin legions.
 
 """
@@ -27,22 +30,34 @@ def create_label(mask):
     label = f"{CLASS_NO} {x_n:.6f} {y_n:.6f} {w_n:.6f} {h_n:.6f}"
     return label
 
+"""
+mask2label(input_path, output_path)
+    Converts all ground truth masks within a folder to YOLO format labels
+    and then returns this rectangle in YOLO format
+    input_path: input location
+    output_path: output location
+"""
+def mask2label(input_path, output_path):
+    for file in os.listdir(input_path):
+        if file.endswith(".png"):
+            mask_path = os.path.join(input_path, file)
+            label_path = os.path.join(output_path, file.replace("_segmentation.png", ".txt"))
+
+            mask = cv.imread(mask_path, cv.IMREAD_GRAYSCALE)
+            if mask is None:
+                raise FileNotFoundError("Not Found: " + mask_path)
+            label = create_label(mask)
+            if label:
+                with open(label_path, "w") as f:
+                    f.write(label)
+                #hidden because printing was actually slowing the process down
+                #print("Created label for: " + file)
+            else:
+                print(file + " is missing contours.")
+
+
 #Make directories needed for processing if they do not already exist
-os.makedirs("./data/labels/train", exist_ok=True)
-
-for file in os.listdir(ISC2018_TRUTH_PATH):
-    if file.endswith(".png"):
-        mask_path = os.path.join(ISC2018_TRUTH_PATH, file)
-        label_path = os.path.join("./data/labels/train", file.replace("_segmentation.png", ".txt"))
-
-        mask = cv.imread(mask_path, cv.IMREAD_GRAYSCALE)
-        if mask is None:
-            raise FileNotFoundError("Not Found: " + mask_path)
-        label = create_label(mask)
-        if label:
-            with open(label_path, "w") as f:
-                f.write(label)
-            #hidden because printing was actually slowing the process down
-            #print("Created label for: " + file)
-        else:
-            print(file + " is missing contours.")
+os.makedirs(OUTPUT_TRAIN, exist_ok=True)
+os.makedirs(OUTPUT_VALIDATE, exist_ok=True)
+mask2label(ISC2018_TRUTH_TRAIN, OUTPUT_TRAIN)
+mask2label(ISC2018_TRUTH_VALIDATE, OUTPUT_VALIDATE)
--- recognition/yolo-47450253/train.py
+++ recognition/yolo-47450253/train.py
@@ -1,19 +1,23 @@
-from ultralytics import YOLO
 import torch
+from ultralytics import YOLO
+
 
 DATA_YML = "./data.yml"
 OUTPUT = "./models"
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' #default to gpu
-model = YOLO("yolo11n.pt")
-model = model.to(device)
+model = YOLO("yolo11n.pt").to('cuda')
 
 settings = {
         'data': DATA_YML,
         'epochs': 75,
         'imgsz': 640,
         'project': OUTPUT,
-        'save_period': 10
+        'save_period': 10,
+        'workers': 5,
+        'batch': -1
     }
 
-model.train(**settings)
+
+if __name__ == '__main__':
+    results = model.train(**settings)

785b868c75f210cd84345875d65d35190afa6e64
Mr Timothy Tjipto <s4653241@login0.compute.eait.uq.edu.au>
2024-10-30 00:29:05 +1000

    Added more configuration

--- recognition/StyleGAN_s4653241/StyleGAN/config.py
+++ recognition/StyleGAN_s4653241/StyleGAN/config.py
@@ -1,22 +1,23 @@
 ''' Wil be used to store all the configuration variables for the project '''
 
 
-
-
+TRAIN = True
+LOAD = False
+TEST = False
 
 
 # Data path
-# AD_train = "/home/groups/comp3710/ADNI/AD_NC/train/AD"
-# AD_test = "/home/groups/comp3710/ADNI/AD_NC/test/AD"
-# NC_train = "/home/groups/comp3710/ADNI/AD_NC/train/NC"
-# NC_test = "/home/groups/comp3710/ADNI/AD_NC/test/NC"
+AD_train = "/home/groups/comp3710/ADNI/AD_NC/train/AD"
+AD_test = "/home/groups/comp3710/ADNI/AD_NC/test/AD"
+NC_train = "/home/groups/comp3710/ADNI/AD_NC/train/NC"
+NC_test = "/home/groups/comp3710/ADNI/AD_NC/test/NC"
 
-AD_train = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/train/AD"
-AD_test = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/test/AD"
-NC_train = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/train/NC"
-NC_test = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/test/NC"
+# AD_train = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/train/AD"
+# AD_test = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/test/AD"
+# NC_train = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/train/NC"
+# NC_test = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/test/NC"
 
-BRAIN = 'NC'
+BRAIN = 'AD'
 
 # ----Image Parameters----
 #
@@ -25,23 +26,23 @@ IMAGE_SIZE = (256, 240) # this resizes the image
 image_height = 2**log_resolution    # The height of the generated image
 image_width = 2**log_resolution     # The width of the generated image
 #------------------------
-BATCH_SIZE = 64
-LEARNING_RATE = 0.0002
+BATCH_SIZE = 16
+LEARNING_RATE = 0.001
 CHANNELS = 1  # Single channel image !!!!
 interpolation = 'bilinear'
 channels = 1
 
 N_FEATURES = 256
 MAX_FEATURES = 512
-EPOCHS = 10
+EPOCHS = 30
 
 #------ New code ------
-lambda_gp = 10
+lambda_gp = 10 # gradient penalty
 W_DIM = 256
 Z_DIM = 256 # Should I change this to 256?
 w_dim = 256
 
 # ----------------------
 
-save_path = 'recognition/checkpoints/'
-load_path = ''
+save_path = '/home/Student/s4653241/checkpoints/'
+LOAD_CHECK= "/home/Student/s4653241/checkpoints/checkpoint_StyleGAN2_Epoch16.pth" # LOAD checkpoint

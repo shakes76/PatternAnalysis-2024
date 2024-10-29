52874d88bd3277dc956b095b7d27030d0737cd21
Miss Prianka Indla <s4749392@login0.compute.eait.uq.edu.au>
2024-10-30 08:47:59 +1000

    Save all changes 30/08

--- recognition/UNet_PriankaIndla/test_driver.py
+++ recognition/UNet_PriankaIndla/test_driver.py
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1 
 #SBATCH --gres=gpu:a100 
 #SBATCH --job-name=train_test 
-#SBATCH -o predict_test.out 
+#SBATCH -o train_test.out 
 
 conda activate torch_env
-python predict.py     
+python train.py     

5f1af79b2790c7f9deb105e45bfd2d574df1a13e
Mr Timothy Tjipto <s4653241@login0.compute.eait.uq.edu.au>
2024-10-30 05:06:45 +1000

    Finalize and  commented config

--- recognition/StyleGAN_s4653241/StyleGAN/config.py
+++ recognition/StyleGAN_s4653241/StyleGAN/config.py
@@ -1,9 +1,10 @@
 ''' Wil be used to store all the configuration variables for the project '''
 
 
-TRAIN = True
-LOAD = False
-TEST = False
+TRAIN = True # When Training
+LOAD = False # Load Checkpoint from LOAD_CHECK
+SAVE_MODEL = False # Saves model to save_path
+TEST = False # TEST loaded model usually when using checkpoint model
 
 
 # Data path
@@ -12,37 +13,31 @@ AD_test = "/home/groups/comp3710/ADNI/AD_NC/test/AD"
 NC_train = "/home/groups/comp3710/ADNI/AD_NC/train/NC"
 NC_test = "/home/groups/comp3710/ADNI/AD_NC/test/NC"
 
-# AD_train = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/train/AD"
-# AD_test = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/test/AD"
-# NC_train = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/train/NC"
-# NC_test = "/Users/solomontjipto/Documents/COMP3710/PatternAnalysis-2024/recognition/Data/AD_NC/test/NC"
-
-BRAIN = 'AD'
+BRAIN = 'AD' # Change for different type of Brain
 
 # ----Image Parameters----
 #
 log_resolution = 7 # 256x256 image size as such 2^8 = 256 # use 2^7 for single gpu (faster)
-IMAGE_SIZE = (256, 240) # this resizes the image
+IMAGE_SIZE = (256, 240) 
 image_height = 2**log_resolution    # The height of the generated image
 image_width = 2**log_resolution     # The width of the generated image
 #------------------------
 BATCH_SIZE = 16
 LEARNING_RATE = 0.001
-CHANNELS = 1  # Single channel image !!!!
+CHANNELS = 1  
 interpolation = 'bilinear'
-channels = 1
 
 N_FEATURES = 256
 MAX_FEATURES = 512
-EPOCHS = 30
+EPOCHS = 41 # ! Training Epoch
 
 #------ New code ------
 lambda_gp = 10 # gradient penalty
 W_DIM = 256
-Z_DIM = 256 # Should I change this to 256?
-w_dim = 256
+Z_DIM = 256 #
+
 
 # ----------------------
 
 save_path = '/home/Student/s4653241/checkpoints/'
-LOAD_CHECK= "/home/Student/s4653241/checkpoints/checkpoint_StyleGAN2_Epoch16.pth" # LOAD checkpoint
+LOAD_CHECK= "/home/Student/s4653241/checkpoints/checkpoint_StyleGAN2_Epoch26.pth" # LOAD checkpoint

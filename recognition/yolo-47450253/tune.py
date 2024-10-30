3755d536b61affb30e1e6bd9b46f96a8d76f3a92
LonelyNo <sbondmurphy@gmail.com>
2024-10-30 16:06:47 +1000

    Added tune.py to tune hyperperameters using a genetic algorithm provided by ultralytics YOLO implementation

---
+++ recognition/yolo-47450253/tune.py
@@ -0,0 +1,19 @@
+import torch
+from ultralytics import YOLO
+
+
+DATA_YML = "./data.yml"
+OUTPUT = "./models"
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu' #default to gpu
+model = YOLO("yolo11n.pt").to(device)
+
+settings = {
+        'data': DATA_YML,
+        'epochs': 30,
+        'imgsz': 640
+    }
+
+
+if __name__ == '__main__':
+    results = model.tune(**settings)

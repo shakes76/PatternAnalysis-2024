642a40e76ca1a5d23c54d0a9e7c955069924334a
LonelyNo <sbondmurphy@gmail.com>
2024-10-30 16:34:49 +1000

    Cleaned up train.py to fit more inline with tune, evaluate and predict.

--- recognition/yolo-47450253/train.py
+++ recognition/yolo-47450253/train.py
@@ -5,19 +5,22 @@ from ultralytics import YOLO
 DATA_YML = "./data.yml"
 OUTPUT = "./models"
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu' #default to gpu
-model = YOLO("yolo11n.pt").to(device)
 
-settings = {
-        'data': DATA_YML,
-        'epochs': 75,
-        'imgsz': 640,
-        'project': OUTPUT,
-        'save_period': 10,
-        'workers': 5,
-        'batch': -1
-    }
+def train():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu' #default to gpu
+    model = YOLO("yolo11n.pt").to(device)
+
+    settings = {
+            'data': DATA_YML,
+            'epochs': 75,
+            'imgsz': 640,
+            'project': OUTPUT,
+            'save_period': 10,
+            'workers': 5,
+            'batch': -1
+        }
+    results = model.train(**settings)
 
 
 if __name__ == '__main__':
-    results = model.train(**settings)
+    train()

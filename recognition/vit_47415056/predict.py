8c951dbee21c92ffd68b5b5f5f4889debcdcc011
yttrium400 <swastiklohchab123@gmail.com>
2024-11-01 04:54:38 +1000

    Added model loading and set to evaluation mode.

--- recognition/vit_47415056/predict.py
+++ recognition/vit_47415056/predict.py
@@ -5,7 +5,10 @@ from dataset import get_test_loader
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def predict_and_visualize(model_path, test_data_dir):
-    pass
+    # Load the model
+    model = initialize_model()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
 
 if __name__ == "__main__":
     model_path = "model_weights.pth"

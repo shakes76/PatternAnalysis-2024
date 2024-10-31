c35dbc5a6d38d809338b148c3116cf22c97dd99d
Ananya Dubey <s4491308@source.eait.uq.edu.au>
2024-11-01 09:32:16 +1000

    Addded function to perform the splits for test, training and validation sets. Also added a test to check the splitting

--- recognition/GCN_s4491308/dataset.py
+++ recognition/GCN_s4491308/dataset.py
@@ -35,4 +35,48 @@ def index_to_mask(index, size):
     mask[index] = True 
     return mask 
 
+def perform_split(data, train_ratio, validation_ratio, test_ratio):
+    """
+    Function to perform the train, test and validation splits. 
+    Reference: Inspired and adapted from code at https://github.com/pyg-team/pytorch_geometric/blob/master/torch_geometric/utils/mask.py#L41
+    """
+    #total nodes in the dataset
+    total_nodes = data.num_nodes
+    #randomly shuffle the nodes 
+    indices = torch.randperm(total_nodes)
+    # sizes for the train, test and validation sets 
+    test_size = int(total_nodes * test_ratio)
+    train_size = int(total_nodes * train_ratio)
+    validation_size = int(total_nodes * validation_ratio)
+    # indices for the test, train and validation data
+    train_indices = indices[ :train_size]
+    validation_indices = indices[train_size:train_size + validation_size]
+    test_indices = indices[train_size + validation_size: ]
+    # getting the masks for each set 
+    data.train_mask = index_to_mask(train_indices,total_nodes)
+    data.validation_mask = index_to_mask(validation_indices, total_nodes)
+    data.test_mask = index_to_mask(test_indices, total_nodes)
+    return data.train_mask, data.validation_mask, data.test_mask
+
+# Test for the dataloader 
+file_path = '/content/facebook.npz'  # Replace with the path to your .npz file
+data = load_data(file_path)
+train_mask, validation_mask, test_mask = perform_split(data, 0.80, 0.10, 0.10)
+# checking splits 
+total_nodes = data.num_nodes
+print(f"Total nodes: {total_nodes}")
+print(f"Train nodes (expected {int(0.80 * total_nodes)}): {train_mask.sum().item()}")
+print(f"Validation nodes (expected {int(0.10 * total_nodes)}): {validation_mask.sum().item()}")
+print(f"Test nodes (expected {int(0.10 * total_nodes)}): {test_mask.sum().item()}")
+
+# check for overlap
+train_and_val_overlap = (train_mask & validation_mask).sum().item()
+train_and_test_overlap = (train_mask & test_mask).sum().item()
+val_and_test_overlap = (validation_mask & test_mask).sum().item()
+
+print(f"Train and validation overlap: {train_and_val_overlap}")
+print(f"Train and test overlap: {train_and_test_overlap}")
+print(f"Validation and test overlap: {val_and_test_overlap}")
+
+
 

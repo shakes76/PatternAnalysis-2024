da45389e1c1ecf136dfb986967bc3a6e0a74f823
yttrium400 <swastiklohchab123@gmail.com>
2024-11-01 04:55:14 +1000

    Added loading of test data using DataLoader.

--- recognition/vit_47415056/predict.py
+++ recognition/vit_47415056/predict.py
@@ -10,6 +10,9 @@ def predict_and_visualize(model_path, test_data_dir):
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
+    # Load test data
+    test_loader = get_test_loader(test_data_dir)
+
 if __name__ == "__main__":
     model_path = "model_weights.pth"
     test_data_dir = "/home/groups/comp3710/ADNI/AD_NC/test"

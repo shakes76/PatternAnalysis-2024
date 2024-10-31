eef098e5402f76aded34ac552099c561ba0ae226
yttrium400 <swastiklohchab123@gmail.com>
2024-11-01 04:44:02 +1000

    Implemented separate data loaders for training and validation with data splitting.

--- recognition/vit_47415056/dataset.py
+++ recognition/vit_47415056/dataset.py
@@ -1,6 +1,6 @@
 import os
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 # Constants for image processing
 IMAGE_SIZE = 224
@@ -23,11 +23,16 @@ def get_transforms(is_train=True):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
-# Updated data loader logic to use `get_transforms`
-def get_dataloaders(data_dir):
+# Function to create training and validation loaders
+def get_train_val_loaders(data_dir):
     transform = get_transforms(is_train=True)
     dataset = datasets.ImageFolder(root=data_dir, transform=transform)
     
-    # Create DataLoader for the dataset
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-    return dataloader
+    # Split dataset: 90% for training, 10% for validation
+    train_size = int(0.9 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=6)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=6)
+    return train_loader, val_loader

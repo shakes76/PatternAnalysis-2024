d8b5278832eff449111c286f10f9562c3143ddae
yttrium400 <swastiklohchab123@gmail.com>
2024-10-31 17:56:25 +1000

    Loaded data using DataLoader with dataset sizes.

--- recognition/vit_47415056/train.py
+++ recognition/vit_47415056/train.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 from modules import create_model
 from dataset import get_dataloaders
 
-def train_model():
+def train_model(data_dir='/home/groups/comp3710/ADNI/AD_NC', batch_size=128):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
+
+    dataloaders, dataset_sizes = get_dataloaders(data_dir, batch_size)

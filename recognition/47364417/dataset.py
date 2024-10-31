d5aa0b5a8034e0a2ef89d24bcaf4bdf33227939a
Praneet Dhoolia <praneet.dhoolia@gmail.com>
2024-11-01 00:07:50 +1000

    Created DataHandler class for local dataset paths (to be shifted to rangpur paths).

--- recognition/47364417/dataset.py
+++ recognition/47364417/dataset.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 from PIL import Image
+import os
 
 class BrainDataset(Dataset):
     def __init__(self, image_paths, transform=None):
@@ -16,3 +17,24 @@ class BrainDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, label
+
+class DataHandler:
+    @staticmethod
+    def load_data_paths():
+        train_ad = 'recognition/47364417/AD_NC/train/AD'
+        train_nc = 'recognition/47364417/AD_NC/train/NC'
+        test_ad = 'recognition/47364417/AD_NC/test/AD'
+        test_nc = 'recognition/47364417/AD_NC/test/NC'
+        
+        train_paths = [os.path.join(train_ad, img) for img in os.listdir(train_ad)] + \
+                      [os.path.join(train_nc, img) for img in os.listdir(train_nc)]
+        test_paths = [os.path.join(test_ad, img) for img in os.listdir(test_ad)] + \
+                     [os.path.join(test_nc, img) for img in os.listdir(test_nc)]
+        
+        return train_paths, test_paths
+    
+    @staticmethod
+    def _get_image_paths(directory):
+        if not os.path.exists(directory):
+            raise FileNotFoundError(f"Directory not found: {directory}")
+        return [os.path.join(directory, img) for img in os.listdir(directory)]

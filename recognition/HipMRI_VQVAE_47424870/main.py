c70be3763702fa907fb90915bf42c24189652637
ConyllUQ <conyll.forbes@student.uq.edu.au>
2024-10-31 22:43:23 +1000

    Updated main.py: Finished testing modules

--- recognition/HipMRI_VQVAE_47424870/main.py
+++ recognition/HipMRI_VQVAE_47424870/main.py
@@ -69,17 +69,18 @@ def test_vqvae():
     print(f"Quantisation Loss: {quantisation_loss.item():.4f}")    # Output should have shape corresponding to latent space
 
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # Define data directory
     current_dir = os.path.dirname(__file__)
     data_dir = os.path.join(current_dir, "keras_slices", "keras_slices_train")
     print(f"Data Directory: {data_dir}")
 
     # Create dataloader using custom function
-    dataloader = get_dataloader(root_dir=data_dir, batch_size=16, image_size=64, shuffle=True)
+    dataloader = get_dataloader(root_dir=data_dir, batch_size=16, image_size=256, shuffle=True)
 
     # Iterate through the dataloader and visualize the first batch
     for batch_idx, (images, _) in enumerate(dataloader):
-        print(f"Batch {batch_idx + 1} | Image Batch Shape: {images.shape}")
+        #print(f"Batch {batch_idx + 1} | Image Batch Shape: {images.shape}")
         if batch_idx == 0:  # Visualize only the first batch
             visualize_batch(images)
             break

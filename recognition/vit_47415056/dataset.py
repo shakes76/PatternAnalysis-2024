7fd19759355f125794c56cd836669da6dd2963b7
yttrium400 <swastiklohchab123@gmail.com>
2024-11-01 04:42:14 +1000

    Introduced constants and a helper function for image transformations.

--- recognition/vit_47415056/dataset.py
+++ recognition/vit_47415056/dataset.py
@@ -2,49 +2,30 @@ import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def get_dataloaders(data_dir, batch_size=128):
-    """
-    Creates and returns DataLoader objects for training and testing datasets.
+# Constants for image processing
+IMAGE_SIZE = 224  # Size to which images will be resized or cropped
+BATCH_SIZE = 32  # Number of samples per batch for data loading
 
-    Parameters:
-    - data_dir (str): The directory containing 'train' and 'test' subdirectories.
-    - batch_size (int): Number of samples per batch. Default is 128.
-
-    Returns:
-    - dataloaders (dict): DataLoader objects for training and testing datasets.
-    - dataset_sizes (dict): Number of images in training and testing datasets.
+# Function to apply image transformations
+def get_transforms(is_train=True):
     """
-    
-    # Define data transformations for training and testing datasets
-    data_transforms = {
-        'train': transforms.Compose([
-            transforms.Resize((224, 224)),
+    Returns a set of transformations to apply to the images.
+
+    Args: is_train (bool): If True, applies data augmentation for training. If False, applies standard transformations for testing/validation.
+
+    Returns: transforms.Compose: Composed transformations to apply to images.
+    """
+    if is_train:
+        return transforms.Compose([
+            transforms.RandomResizedCrop(IMAGE_SIZE),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Normalize image channels
-                                 std=[0.229, 0.224, 0.225]),
-        ]),
-        'test': transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+    else:
+        return transforms.Compose([
+            transforms.Resize(IMAGE_SIZE),
+            transforms.CenterCrop(IMAGE_SIZE),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ]),
-    }
-
-    # Load images from 'train' and 'test' directories with specified transformations
-    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-                      for x in ['train', 'test']}
-    
-    # Create DataLoader objects for training and testing datasets
-    dataloaders = {
-        'train': DataLoader(image_datasets['train'], batch_size=batch_size, shuffle=True, 
-                            num_workers=4, pin_memory=True),
-        'test': DataLoader(image_datasets['test'], batch_size=batch_size, shuffle=False, 
-                           num_workers=4, pin_memory=True)
-    }
-    
-    # Calculate the number of images in training and testing datasets
-    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test']}
-    
-    return dataloaders, dataset_sizes
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])

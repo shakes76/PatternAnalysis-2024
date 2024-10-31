e5f8f74a0d091aff877c079215e837f0c9739c0b
yttrium400 <swastiklohchab123@gmail.com>
2024-10-31 17:57:29 +1000

    Initialized model and move it to the appropriate device.

--- recognition/vit_47415056/train.py
+++ recognition/vit_47415056/train.py
@@ -4,8 +4,10 @@ import torch.nn as nn
 from modules import create_model
 from dataset import get_dataloaders
 
-def train_model(data_dir='/home/groups/comp3710/ADNI/AD_NC', batch_size=128):
+def train_model(data_dir='/home/groups/comp3710/ADNI/AD_NC', batch_size=128, num_classes=2):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
     dataloaders, dataset_sizes = get_dataloaders(data_dir, batch_size)
+    model = create_model(num_classes)
+    model = model.to(device)

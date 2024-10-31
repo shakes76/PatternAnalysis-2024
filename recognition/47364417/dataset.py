2f6a42fd2e186a98c1aff838d7a9ac46e5992cc5
Praneet Dhoolia <praneet.dhoolia@gmail.com>
2024-11-01 00:05:13 +1000

    Added image paths/transform arguments for storing and manipulating images in the dataset.

---
+++ recognition/47364417/dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+class BrainDataset(Dataset):
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        pass

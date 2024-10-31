1a41c8c341eedea38b7e821be619378a62e4aff6
yttrium400 <swastiklohchab123@gmail.com>
2024-10-31 17:58:27 +1000

    Set up CrossEntropyLoss and AdamW optimizer.

--- recognition/vit_47415056/train.py
+++ recognition/vit_47415056/train.py
@@ -4,10 +4,13 @@ import torch.nn as nn
 from modules import create_model
 from dataset import get_dataloaders
 
-def train_model(data_dir='/home/groups/comp3710/ADNI/AD_NC', batch_size=128, num_classes=2):
+def train_model(data_dir='/home/groups/comp3710/ADNI/AD_NC', batch_size=128, learning_rate=1e-4, num_classes=2):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
     dataloaders, dataset_sizes = get_dataloaders(data_dir, batch_size)
     model = create_model(num_classes)
     model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-4)

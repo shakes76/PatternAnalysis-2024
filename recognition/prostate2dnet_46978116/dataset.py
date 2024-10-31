6417225f677e37c79de1bcfdcfa4819b007de8ff
obbamna <llemonstrawberry@gmail.com>
2024-10-31 10:40:47 +1000

    updated dataset slightly

--- recognition/prostate2dnet_46978116/dataset.py
+++ recognition/prostate2dnet_46978116/dataset.py
@@ -19,29 +19,58 @@ def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:
 
     return res
 
-def load_data_2D(imageNames, normImage=False, categorical=False, dtype=np.float32, getAffines=False, early_stop=False):
-    images = []
+
+# load medical image functions
+def load_data_2D(imageNames, normImage = False, categorical = False, dtype = np.float32, getAffines = False, early_stop = False):
+    '''
+    Load medical image data from names, cases list provided into a list for each.
+    This function pre - allocates 4D arrays for conv2d to avoid excessive memory &
+    usage.
+    normImage: bool (normalise the image 0.0 -1.0)
+    early_stop: Stop loading pre - maturely, leaves arrays mostly empty, for quick &
+    loading and testing scripts.
+    '''
     affines = []
 
-    for inName in imageNames:
+    # get fixed size
+    num = len(imageNames)
+    first_case = nib.load(imageNames[0]).get_fdata(caching = 'unchanged')
+    if len(first_case.shape) == 3:
+        first_case = first_case [:,:,0] # sometimes extra dims, remove
+    if categorical:
+        first_case = to_channels(first_case, dtype = dtype)
+        rows, cols, channels = first_case.shape
+        images = np.zeros((num, rows, cols, channels), dtype = dtype)
+    else:
+        rows, cols = first_case.shape
+        images = np.zeros((num, rows, cols), dtype = dtype)
+
+    for i, inName in enumerate(imageNames):
         niftiImage = nib.load(inName)
-        inImage = niftiImage.get_fdata(caching='unchanged')
+        inImage = niftiImage.get_fdata(caching = 'unchanged') # read disk only
         affine = niftiImage.affine
         if len(inImage.shape) == 3:
-            inImage = inImage[:, :, 0]
-        inImage = inImage.astype(dtype)
+            inImage = inImage[:,:,0] # sometimes extra dims in HipMRI_study data
+            inImage = inImage.astype(dtype)
         if normImage:
+            #~ inImage = inImage / np.linalg.norm(inImage)
+            #~ inImage = 255. * inImage / inImage.max()
             inImage = (inImage - inImage.mean()) / inImage.std()
         if categorical:
-            inImage = to_channels(inImage, dtype=dtype)
-        images.append(inImage)
-        affines.append(affine)
+            inImage = to_channels(inImage, dtype = dtype)
+            images[i,:,:,:] = inImage
+        else:
+            images[i,:,:] = inImage
 
-    images = np.stack(images)
+        affines.append(affine)
+        if i > 20 and early_stop:
+            break
+
     if getAffines:
-        return images, affines
+        return torch.tensor(images, dtype = torch.float32), affines
     else:
-        return images
+        return torch.tensor(images, dtype = torch.float32)
+
 
 class ProstateDataset(Dataset):
     def __init__(self, image_path, mask_path, norm_image=False, transform=None, target_size=(128, 64)):
@@ -62,8 +91,12 @@ class ProstateDataset(Dataset):
         image = load_data_2D([img_pth], normImage=True)
         mask = load_data_2D([mask_pth])
 
-        if self.transform is not None:
-            augm = self.transform(image=image,mask=mask)
+                # Apply transformations
+        image = transforms.Resize((256, 256))(image)
+        mask = transforms.Resize((256, 256))(mask)
+
+        mask = mask.long()
+        
         
 
         return image, mask

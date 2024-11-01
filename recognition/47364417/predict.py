4e566668607349e8c5f9da730b2ef49496aff7f5
Praneet Dhoolia <praneet.dhoolia@gmail.com>
2024-11-01 12:21:50 +1000

    Added dataloaders and directories for I/O

--- recognition/47364417/predict.py
+++ recognition/47364417/predict.py
@@ -1,5 +1,7 @@
 import os
 import torch
+from modules import create_model
+from dataset import get_dataloaders
 
 def predict():
     """
@@ -8,3 +10,17 @@ def predict():
     # Set device to GPU if available.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
+
+    # Define directories and model path.
+    data_dir = '/home/groups/comp3710/ADNI/AD_NC'
+    checkpoints_dir = 'checkpoints'
+    final_model_path = os.path.join(checkpoints_dir, 'final_model.pth')
+
+    # Get dataloaders and class names.
+    dataloaders, class_names = get_dataloaders(data_dir)
+    num_classes = len(class_names)
+    print(f'Classes: {class_names}')
+
+    # Initialize the model.
+    model = create_model(num_classes)
+    model = model.to(device)

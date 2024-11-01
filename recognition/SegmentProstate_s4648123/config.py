b184f4f86192e1650a05eae422c431fd86f963fa
ellinovak <39504423+ellinovak@users.noreply.github.com>
2024-11-01 11:58:21 +1000

    moved config specs to utils

--- recognition/SegmentProstate_s4648123/config.py
+++
@@ -1,6 +0,0 @@
-
-IMAGE_DIR="/home/groups/comp3710/HipMRI_Study_open/semantic_MRs"
-MASK_DIR="/home/groups/comp3710/HipMRI_Study_open/semantic_labels_only"
-MODEL_PATH = "/home/Student/s4648123/MRI/best_unet.pth"
-
-EARLY_STOP = True
--- recognition/SegmentProstate_s4648123/utils.py
+++ recognition/SegmentProstate_s4648123/utils.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 
 from typing import Sequence, Mapping
 from torch.utils.data._utils.collate import default_collate
-from config import (IMAGE_DIR, MASK_DIR)
+
+# user defined parameters
+IMAGE_DIR="/home/groups/comp3710/HipMRI_Study_open/semantic_MRs"
+MASK_DIR="/home/groups/comp3710/HipMRI_Study_open/semantic_labels_only"
+MODEL_PATH = "/home/Student/s4648123/MRI/best_unet.pth"
 
 
 def to_channels(arr: np.ndarray, dtype=np.uint8) -> np.ndarray:

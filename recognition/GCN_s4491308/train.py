712ff4f4e1511e9bdd0a4dbff9d0c84ce29897c0
Ananya Dubey <s4491308@source.eait.uq.edu.au>
2024-11-01 19:54:52 +1000

    Set random seed as 42 for reproducability before splitting the dataset and added code to plot training and validation losses over epochs also changed number of epochs to 200 after initial inspection

--- recognition/GCN_s4491308/train.py
+++ recognition/GCN_s4491308/train.py
@@ -2,19 +2,34 @@
 Author: Ananya Dubey
 Student No: 44913083
 This script trains a GCN model on the Facebook dataset using PyTorch Geometric.
-Reference: Based on https://github.com/tkipf/pygcn/blob/master/pygcn/train.py
+Reference: Based on https://github.com/pyg-team/pytorch_geometric/blob/master/examples/gcn.py#L81
 """
 
 import torch
 from torch.optim import Adam
 from dataset import load_data, perform_split
 from modules import GCN
+import numpy as np 
+import random 
+import matplotlib.pyplot as plt 
+
+
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 # Set device to CUDA if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #print(f'Using device: {device}')
 
 # Load and split data
-file_path = '/content/facebook.npz'
+file_path = '/content/drive/MyDrive/comp3710_project/facebook.npz'
 data = load_data(file_path)
 train_mask, val_mask, test_mask = perform_split(data, 0.80, 0.10, 0.10)
 
@@ -29,27 +44,49 @@ model = GCN(in_channels, hidden_channels, out_channels, dropout)
 model = model.to(device)
 data = data.to(device)
 optimizer = Adam(model.parameters(), lr=0.01)
-criterion = torch.nn.CrossEntropyLoss()
+loss_criterion = torch.nn.CrossEntropyLoss()
+
+# list for train, test and validation losses 
+train_losses = []
+validation_losses = []
+
 
 # Training loop
-epochs = 100
+epochs = 400
 for epoch in range(1, epochs + 1):
     model.train()
     optimizer.zero_grad()
     out = model(data.x, data.edge_index)
-    loss = criterion(out[train_mask], data.y[train_mask])
+    loss = loss_criterion(out[train_mask], data.y[train_mask])
     loss.backward()
     optimizer.step()
+    train_losses.append(loss.item())
 
     # Validation accuracy
     model.eval()
     with torch.no_grad():
         val_out = model(data.x, data.edge_index)
         val_pred = val_out[val_mask].argmax(dim=1)
+        val_loss = loss_criterion(val_out[val_mask], data.y[val_mask])
         val_acc = (val_pred == data.y[val_mask]).sum().item() / val_mask.sum().item()
+        validation_losses.append(val_loss)
 
-    print(f'Epoch {epoch:03d}, Loss: {loss:.4f}, Val Acc: {val_acc:.4f}')
+    print(f'Epoch {epoch:03d}, Training Loss: {loss:.4f}, Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}')
+    
 
 # Save the trained model
 torch.save(model.state_dict(), 'gcn_model.pth')
 print("Model saved as gcn_model.pth")
+
+#plot the losses 
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, epochs + 1), train_losses, label="Training Loss")
+plt.plot(range(1, epochs + 1), validation_losses, label="Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.title("Training and Validation Loss over Epochs")
+plt.legend()
+plt.grid(True)
+plt.savefig('/content/drive/MyDrive/comp3710_project/loss_plot.png')
+plt.show()
+

b7e635a21161240c8633b9d3938b830d9980200e
Mariam <112161752+mermalade0325@users.noreply.github.com>
2024-11-01 16:47:36 +1000

    Add preprocessing function dataset.py
    Takes in label and returns if benign or cancerous

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -65,6 +65,31 @@ class ISICDataset(Dataset):
         )
         return cv2.imread(mask_name, cv2.IMREAD_GRAYSCALE)
 
+        # preparing data
+    # generate CSV metadata file to read img paths and labels from it
+    def generate_csv(folder, label2int):
+        folder_name = os.path.basename(folder)
+        labels = list(label2int)
+        # generate CSV file
+        df = pd.DataFrame(columns=["filepath", "label"])
+        i = 0
+        for label in labels:
+            print("Reading", os.path.join(folder, label, "*"))
+            for filepath in glob.glob(os.path.join(folder, label, "*")):
+                df.loc[i] = [filepath, label2int[label]]
+                i += 1
+        output_file = f"{folder_name}.csv"
+        print("Saving", output_file)
+        df.to_csv(output_file)
+    
+    # generate CSV files for all data portions, labeling nevus and seborrheic keratosis
+    # as 0 (benign), and melanoma as 1 (malignant)
+    # you should replace "data" path to your extracted dataset path
+    # don't replace if you used download_and_extract_dataset() function
+    generate_csv("data/train", {"nevus": 0, "seborrheic_keratosis": 0, "melanoma": 1})
+    generate_csv("data/valid", {"nevus": 0, "seborrheic_keratosis": 0, "melanoma": 1})
+    generate_csv("data/test", {"nevus": 0, "seborrheic_keratosis": 0, "melanoma": 1})
+
     def _extract_bounding_box(self, mask):
         """Extracts the bounding box from the mask image."""
         _, thresh = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)

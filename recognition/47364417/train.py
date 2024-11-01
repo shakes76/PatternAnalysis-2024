da6fccf1310235bb9457533c87acbc3f8bfe1615
Praneet Dhoolia <praneet.dhoolia@gmail.com>
2024-11-01 12:08:02 +1000

    Added directories for datasets, images and model checkpoints.

--- recognition/47364417/train.py
+++ recognition/47364417/train.py
@@ -11,3 +11,9 @@ def train_model():
     # Set device to GPU if available.
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f'Using device: {device}')
+
+    data_dir = '/home/groups/comp3710/ADNI/AD_NC'
+    output_dir = 'output'
+    checkpoints_dir = 'checkpoints'
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(checkpoints_dir, exist_ok=True)

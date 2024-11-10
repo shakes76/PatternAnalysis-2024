067a94214f564240cf58ca129a54d4a5ceeb5925
Donghyug David Jeong <donghyug.jeong@student.uq.edu.au>
2024-11-11 09:43:23 +1000

    Addressing comment "commented-off code expected to be deleted"

--- recognition/ADNI-GFNet-47430004/dataset.py
+++ recognition/ADNI-GFNet-47430004/dataset.py
@@ -96,7 +96,7 @@ def get_dataloaders(data_dir, batch_size=32, crop_size=224, image_size=224):
     
     return train_loader, test_loader
 
-# Running the function below, get_mean_std(), prints:
+# The mean and std have been pre-calculated:
 # tensor(0.1415) tensor(0.2385), so mean = 0.1415, std = 0.2385.
 # This is where the "magic values" in the above section come from.
 # Inspired by Yuvraj Fowdar's edstem post: https://edstem.org/au/courses/18266/discussion/2253272.
@@ -105,38 +105,3 @@ def get_dataloaders(data_dir, batch_size=32, crop_size=224, image_size=224):
 # No centercrop and no resize: tensor(0.1155) tensor(0.2224) 
 # No centercrop but resize: tensor(0.1156) tensor(0.2198) 
 # Centercrop and no resize: tensor(0.1415) tensor(0.2385) 
-
-# def get_mean_std():
-
-#     # data_dir = "/home/groups/comp3710/ADNI/AD_NC"
-#     data_dir = "/home/daqoblade/Uni/2024 Sem 2/COMP3710/PatternAnalysis-2024/resources/AD_NC"
-#     batch_size = 32
-
-#     transform = tf.Compose([
-#         tf.Grayscale(num_output_channels=1),
-#         tf.CenterCrop(224),
-#         tf.Resize((224, 224)),
-#         tf.ToTensor(),
-#     ])
-    
-#     train_dataset = ADNIDataset(root_dir=data_dir, transform=transform, train=True)
-    
-#     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-
-#     num_images = 0
-#     mean = 0.0
-#     std = 0.0
-#     for images, _ in train_loader:
-#         batch_size = images.size(0)
-#         images = images.view(batch_size, -1)
-#         mean += images.mean(1).sum(0)
-#         std += images.std(1).sum(0)
-#         num_images += batch_size
-    
-#     mean /= num_images
-#     std /= num_images
-
-#     print(mean, std)
-
-# if __name__ == '__main__':
-#     get_mean_std()
